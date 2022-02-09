b734862ca4864a51354b3cbbbc22c73c20f818b9
Yury <olennikovyv@mail.ru>
2022-02-10 01:35:44 +0500

    first commit

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,34 @@
-function duplicateCount(text) {
-    return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+function rot13(message) {
+    let decode = '';
+
+    for (let i = 0; i < message.length; i++) {
+        let char = message.charCodeAt(i);
+
+        if (char >= 65 && char <= 90) {
+            if (char + 13 > 90) {
+                char -= 77;
+                decode += String.fromCodePoint(char + 64);
+            } else
+                decode += String.fromCodePoint(char + 13);
+            continue;
+        }
+
+        if (char >= 97 && char <= 122) {
+            if (char + 13 > 122) {
+                char -= 109;
+                decode += String.fromCodePoint(char + 96);
+            } else
+                decode += String.fromCodePoint(char + 13);
+            continue;
+        }
+
+        decode += String.fromCodePoint(char);
+    }
+
+    return decode;
+
 }
 
-console.log(duplicateCount(""));
-console.log(duplicateCount("aA11"));
-console.log(duplicateCount("Indivisibility"));
-console.log(duplicateCount("aabbcdee"));
+console.log(rot13('grfg'));
+console.log(rot13('Grfg'));
+console.log(rot13('Zax'));

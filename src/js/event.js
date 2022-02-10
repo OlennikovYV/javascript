5078a53c1aa265b4ce194647714ea03d20d36a63
Yury <olennikovyv@mail.ru>
2022-02-10 23:03:09 +0500

    DNA to RNA Conversion

--- src/js/event.js
+++ src/js/event.js
@@ -1,32 +1,9 @@
-function openOrSenior(data) {
-    /*     let club = [];
-
-        for (let member of data)
-            (member[0] >= 55 && member[1] >= 7) ? club.push('Senior') : club.push('Open');
-
-        return club; */
-
-    return data.reduce((club, member) => {
-        club.push((member[0] >= 55 && member[1] > 7) ? 'Senior' : 'Open');
-        return club;
-    }, []);
+function DNAtoRNA(dna) {
+    return dna.split('').map(dnaElement => {
+        return (dnaElement == 'T' ? 'U' : dnaElement);
+    }).join('');
 }
 
-console.log(openOrSenior([
-    [45, 12],
-    [55, 21],
-    [19, -2],
-    [104, 20]
-]));
-console.log(openOrSenior([
-    [3, 12],
-    [55, 1],
-    [91, -2],
-    [53, 23]
-]));
-console.log(openOrSenior([
-    [59, 12],
-    [55, -1],
-    [12, -2],
-    [12, 12]
-]));
+console.log(DNAtoRNA('TTTT'));
+console.log(DNAtoRNA('GCAT'));
+console.log(DNAtoRNA('GACCGCCGCC'));

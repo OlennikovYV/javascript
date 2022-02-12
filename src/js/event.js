2b2dcb8c6b0d2eb7d39387327d1ac73956906bbe
Yury <olennikovyv@mail.ru>
2022-02-12 20:27:59 +0500

    Count by X

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,16 @@
-function accum(s) {
-    return s.toLowerCase().split('').map((el, idx) => {
-        return el.toUpperCase() + el.toLowerCase().repeat(idx)
-    }).join('-')
+function countBy(x, n) {
+    let z = [];
+    let i = 1;
+
+    while (z.length < n) {
+        if (i % x == 0) {
+            z.push(i);
+        }
+        i++;
+    }
+
+    return z;
 }
 
-console.log(accum('abcd'));
-console.log(accum('AbRaC'));
+console.log(countBy(1, 10));
+console.log(countBy(2, 5));

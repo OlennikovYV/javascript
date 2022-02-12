5e6ca82673ebc7969919b84de3fa6005498640a4
Yury <olennikovyv@mail.ru>
2022-02-13 02:23:53 +0500

    Equal Sides Of An Array

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,24 @@
-function countBy(x, n) {
-    let z = [];
-    let i = 1;
+function findEvenIndex(arr) {
+    let left = 0;
+    let right = arr.reduce((acc, el) => {
+        return acc + el;
+    }, 0);
 
-    while (z.length < n) {
-        if (i % x == 0) {
-            z.push(i);
-        }
-        i++;
+    for (let i = 0; i < arr.length; i++) {
+        if (i > 0) left += arr[i - 1];
+        right -= arr[i];
+
+        if (left == right) return i;
     }
 
-    return z;
+    return -1;
 }
 
-console.log(countBy(1, 10));
-console.log(countBy(2, 5));
+console.log(findEvenIndex([]));
+console.log(findEvenIndex([1]));
+console.log(findEvenIndex([10, -80, 10, 10, 15, 35, 20]));
+console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]));
+console.log(findEvenIndex([1, 100, 50, -51, 1, 1]));
+console.log(findEvenIndex([1, 2, 3, 4, 5, 6]));
+console.log(findEvenIndex([20, 10, 30, 10, 10, 15, 35]));
+console.log(findEvenIndex([10, -80, 10, 10, 15, 35, 10]));

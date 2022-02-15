8e8e0f201b775dbff9ba132ff58b67766abd940a
Yury <olennikovyv@mail.ru>
2022-02-15 21:41:30 +0500

    Disemvowel Trolls

--- src/js/event.js
+++ src/js/event.js
@@ -1,24 +1,6 @@
-function longestConsec(strarr, k) {
-    const n = strarr.length;
-    let arrLengths = [],
-        index,
-        result = "";
-
-    if (n == 0 || k > n || k <= 0) return "";
-
-    for (let i = 0; i < n; i++) {
-        if (i + k > n) break;
-        arrLengths[i] = strarr.slice(i, i + k).reduce((acc, el) => {
-            return acc + el.length;
-        }, 0);
-    }
-
-    index = arrLengths.indexOf(Math.max(...arrLengths));
-
-    return strarr.slice(index, index + k).join("");
+function disemvowel(str) {
+    return [...str.matchAll(/[^aeiou]/ig)].join('');
 }
 
-console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 2));
-console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2));
-console.log(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1));
-console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 3));
+console.log(disemvowel('O zax dead for'));
+console.log(disemvowel('This website is for losers LOL!'));

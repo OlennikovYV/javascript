fa8640ce86ceea14fdb1f7724eab87916cd38390
Yury <olennikovyv@mail.ru>
2022-02-16 21:04:07 +0500

    Your order, please refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,21 +1,7 @@
 function order(words) {
-    let arrSortWords;
-    const findIndex = word => {
-        const i = word.search(/[1-9]/);
-        return word[i];
-    };
-
-    if (words == '') return '';
-
-    words = words.split(' ');
-    arrSortWords = new Array(words.length);
-
-    for (let i = 0; i < words.length; i++) {
-        let index = findIndex(words[i]);
-        arrSortWords[index - 1] = words[i];
-    }
-
-    return arrSortWords.join(' ');
+    return words.split(' ').sort((a, b) => {
+        return a.match(/\d/) - b.match(/\d/);
+    }).join(' ');
 }
 
 console.log(order('is2 Thi1s T4est 3a'));

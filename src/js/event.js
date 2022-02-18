1ef6812803ce19b82cf878c98352ac29b4d48968
Yury <olennikovyv@mail.ru>
2022-02-18 23:50:18 +0500

    Grasshopper - Personalized Message refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,5 @@
 function greet(name, owner) {
-    return (name === owner) ? 'Hello boss' : 'Hello guest';
+    return `Hello ${name === owner ? 'boss' : 'guest'}`;
 }
 
 console.log(greet('Daniel', 'Daniel')); //'Hello boss'

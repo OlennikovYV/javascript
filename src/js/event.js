a4ac7869541b77ec3838f4d7fc8d7b35f60711ee
Yury <olennikovyv@mail.ru>
2022-02-20 20:24:36 +0500

    Bouncing Balls

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,34 @@
-function fakeBin(x) {
-    return [...x].map(el => {
-        return el < 5 ? '0' : '1';
-    }).join('');
+function bouncingBall(h, bounce, window) {
+    let countSee = 0;
+
+    function checkconditions() {
+        if (h > 0 && h > window && bounce > 0 && bounce < 1) return 1;
+        return 0;
+    }
+
+    if (!checkconditions()) return -1;
+
+    countSee++;
+    h *= bounce;
+
+    while (h > window) {
+        countSee += 2;
+        h *= bounce;
+    }
+
+    return countSee;
 }
 
-console.log(fakeBin('45385593107843568')); //'01011110001100111'
-console.log(fakeBin('509321967506747')); //'101000111101101'
-console.log(fakeBin('366058562030849490134388085')); //'011011110000101010000011011'
+console.log(bouncingBall(3.0, 0.66, 1.5)); //3
+console.log(bouncingBall(30.0, 0.66, 1.5)); //15
+console.log(bouncingBall(2.0, 0.5, 1)); //1
+console.log(bouncingBall(3.0, 0.66, 1.5)); //3
+console.log(bouncingBall(30.0, 0.66, 1.5)); //15
+console.log(bouncingBall(30, 0.75, 1.5)); //21
+console.log(bouncingBall(30, 0.4, 10)); //3
+console.log(bouncingBall(40, 0.4, 10)) //3
+console.log(bouncingBall(10, 0.6, 10)); //-1
+console.log(bouncingBall(40, 1, 10)); //-1
+console.log(bouncingBall(-5, 0.66, 1.5)); //-1
+console.log(bouncingBall(5, -1, 1.5)); //-1
+console.log(bouncingBall(4.0, 0.25, 1)); //1

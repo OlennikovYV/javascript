03edca937fee69e198c751a8c825d118b061058e
Yury <olennikovyv@mail.ru>
2022-02-20 20:37:29 +0500

    Friend or Foe?

--- src/js/event.js
+++ src/js/event.js
@@ -1,34 +1,8 @@
-function bouncingBall(h, bounce, window) {
-    let countSee = 0;
-
-    function checkconditions() {
-        if (h > 0 && h > window && bounce > 0 && bounce < 1) return 1;
-        return 0;
-    }
-
-    if (!checkconditions()) return -1;
-
-    countSee++;
-    h *= bounce;
-
-    while (h > window) {
-        countSee += 2;
-        h *= bounce;
-    }
-
-    return countSee;
+function friend(friends) {
+    return friends.reduce((acc, el) => {
+        if (el.length == 4) acc.push(el);
+        return acc;
+    }, []);
 }
 
-console.log(bouncingBall(3.0, 0.66, 1.5)); //3
-console.log(bouncingBall(30.0, 0.66, 1.5)); //15
-console.log(bouncingBall(2.0, 0.5, 1)); //1
-console.log(bouncingBall(3.0, 0.66, 1.5)); //3
-console.log(bouncingBall(30.0, 0.66, 1.5)); //15
-console.log(bouncingBall(30, 0.75, 1.5)); //21
-console.log(bouncingBall(30, 0.4, 10)); //3
-console.log(bouncingBall(40, 0.4, 10)) //3
-console.log(bouncingBall(10, 0.6, 10)); //-1
-console.log(bouncingBall(40, 1, 10)); //-1
-console.log(bouncingBall(-5, 0.66, 1.5)); //-1
-console.log(bouncingBall(5, -1, 1.5)); //-1
-console.log(bouncingBall(4.0, 0.25, 1)); //1
+console.log(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"])); //["Ryan"]

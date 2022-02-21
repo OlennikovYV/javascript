c83f1f18bd6e14558d74a50479a23c8b695b082c
Yury <olennikovyv@mail.ru>
2022-02-21 21:05:18 +0500

    Transportation on vacation

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,11 @@
-function friend(friends) {
-    return friends.filter(el => el.length == 4);
+function rentalCarCost(d) {
+    let dayCost = 40;
+    if (d >= 7) return d * dayCost - 50;
+    if (d >= 3) return d * dayCost - 20;
+    return d * dayCost;
 }
 
-console.log(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"])); //["Ryan"]
+console.log(rentalCarCost(3)); //100
+console.log(rentalCarCost(4)); //140
+console.log(rentalCarCost(5)); //180
+console.log(rentalCarCost(6)); //220

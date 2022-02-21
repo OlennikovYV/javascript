03c1ed0dba7b3a8f627a13a99d5558eaedac3870
Yury <olennikovyv@mail.ru>
2022-02-21 21:20:14 +0500

    Simple multiplication

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,13 @@
-function rentalCarCost(d) {
-    let dayCost = 40;
-    if (d >= 7) return d * dayCost - 50;
-    if (d >= 3) return d * dayCost - 20;
-    return d * dayCost;
+function checkEven(number) {
+    return (number % 2 == 0);
 }
 
-console.log(rentalCarCost(3)); //100
-console.log(rentalCarCost(4)); //140
-console.log(rentalCarCost(5)); //180
-console.log(rentalCarCost(6)); //220
+function simpleMultiplication(number) {
+    return number * (checkEven(number) ? 8 : 9);
+}
+
+console.log(simpleMultiplication(2)); //16
+console.log(simpleMultiplication(1)); //9
+console.log(simpleMultiplication(8)); //64
+console.log(simpleMultiplication(4)); //32
+console.log(simpleMultiplication(5)); //45

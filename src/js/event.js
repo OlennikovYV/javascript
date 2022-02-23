222035cafefbdc2529606de1dcb64699f21dadc1
Yury <olennikovyv@mail.ru>
2022-02-23 19:10:25 +0500

    Shortest Word

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,13 @@
-function setAlarm(employed, vacation) {
-    return (employed && employed !== vacation);
+function findShort(s) {
+    return s
+        .split(' ')
+        .reduce((acc, el) => {
+            acc = (acc.length < el.length ? acc : el);
+            return acc;
+        })
+        .length;
 }
 
-console.log(setAlarm(true, true)); //false
-console.log(setAlarm(false, true)); //false
-console.log(setAlarm(false, false)); //false
-console.log(setAlarm(true, false)); //true
+console.log(findShort("bitcoin take over the world maybe who knows perhaps")); //3
+console.log(findShort("turns out random test cases are easier than writing out basic ones")); //3 
+console.log(findShort("Let's travel abroad shall we")); //2

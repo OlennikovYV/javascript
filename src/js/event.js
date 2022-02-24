b9b5d57d6c270f52d9886ea0b1aa1ed6c01e29bf
Yury <olennikovyv@mail.ru>
2022-02-24 20:20:35 +0500

    Are You Playing Banjo?

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,10 @@
-function findShort(s) {
-    return s
-        .split(' ')
-        .reduce((acc, el) => {
-            acc = (acc.length < el.length ? acc : el);
-            return acc;
-        })
-        .length;
+function areYouPlayingBanjo(name) {
+    return `${name} ` +
+        `${name[0].toUpperCase() === 'R' ? 'plays banjo' : 'does not play banjo'}`;
 }
 
-console.log(findShort("bitcoin take over the world maybe who knows perhaps")); //3
-console.log(findShort("turns out random test cases are easier than writing out basic ones")); //3 
-console.log(findShort("Let's travel abroad shall we")); //2
+console.log(areYouPlayingBanjo("Adam")); // "Adam does not play banjo"
+console.log(areYouPlayingBanjo("Paul")); // "Paul does not play banjo"
+console.log(areYouPlayingBanjo("Ringo")); // "Ringo plays banjo"
+console.log(areYouPlayingBanjo("bravo")); // "bravo does not play banjo"
+console.log(areYouPlayingBanjo("rolf")); // "rolf plays banjo"

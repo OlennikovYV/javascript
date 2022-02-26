82fbb0da0c6d8eca602958a44751df2f8c48e028
Yury <olennikovyv@mail.ru>
2022-02-26 23:32:48 +0500

    const arrChar = word.toLowerCase().split('');

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,27 @@
 // return masked string
-function maskify(cc) {
-    return cc.slice(-4).padStart(cc.length, '#')
+function duplicateEncode(word) {
+    const arrChar = word
+        .toLowerCase()
+        .split('');
+    const countChar = calcChar(arrChar);
+
+    function calcChar(str) {
+        return str
+            .reduce((acc, el) => {
+                acc[el] = (acc[el] || 0) + 1;
+                return acc;
+            }, {});
+    }
+
+    return arrChar
+        .reduce((acc, el) => {
+            acc.push((countChar[el] > 1 ? ')' : '('));
+            return acc;
+        }, [])
+        .join('');
 }
 
-console.log(maskify('4556364607935616')); // '############5616'
-console.log(maskify('1')); // '1'
-console.log(maskify('11111')); // '#1111'
-console.log(maskify('4567')); // '4567'
-console.log(maskify('1234567890')); // '######7890'
+console.log(duplicateEncode("din")); //"((("
+console.log(duplicateEncode("recede")); //"()()()"
+console.log(duplicateEncode("Success")); //")())())","should ignore case"
+console.log(duplicateEncode("(( @")); //"))(("

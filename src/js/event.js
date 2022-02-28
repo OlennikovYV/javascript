0387f189ed40a3ac49d79c9c0c525967ebf08efe
Yury <olennikovyv@mail.ru>
2022-02-28 21:00:43 +0500

    Remove the minimum refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,6 @@
 function removeSmallest(numbers) {
-    let minNumber, indexMinNumber;
-    if (numbers.length === 0) return ([]);
-    minNumber = Math.min(...numbers);
-    indexMinNumber = numbers.findIndex(el => el === minNumber);
-    return numbers.filter((el, i) => {
-        return i !== indexMinNumber;
-    });
+    const indexMinNumber = numbers.indexOf(Math.min(...numbers));
+    return [...numbers.slice(0, indexMinNumber), ...numbers.slice(indexMinNumber + 1)];
 }
 
 console.log(removeSmallest([1, 2, 3, 4, 5])); // [2, 3, 4, 5]

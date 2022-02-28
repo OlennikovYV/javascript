8da42b4f9e708c462dff20ff35b9a7880afbfd21
Yury <olennikovyv@mail.ru>
2022-02-28 20:57:55 +0500

    Remove the minimum

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,14 @@
-function XO(str) {
-    function calcCountChar(string, char) {
-        const regExp = new RegExp(char, `gi`);
-        return (string.match(regExp) || []).length;
-    }
-    return calcCountChar(str, 'x') === calcCountChar(str, 'o');
+function removeSmallest(numbers) {
+    let minNumber, indexMinNumber;
+    if (numbers.length === 0) return ([]);
+    minNumber = Math.min(...numbers);
+    indexMinNumber = numbers.findIndex(el => el === minNumber);
+    return numbers.filter((el, i) => {
+        return i !== indexMinNumber;
+    });
 }
 
-console.log(XO('xo')); //true
-console.log(XO("xxOo")); //true
-console.log(XO("xxxm")); //false
-console.log(XO("Oo")); //false
-console.log(XO("ooom")); //false
+console.log(removeSmallest([1, 2, 3, 4, 5])); // [2, 3, 4, 5]
+console.log(removeSmallest([5, 3, 2, 1, 4])); // [5, 3, 2, 4]
+console.log(removeSmallest([2, 2, 1, 2, 1])); // [2, 2, 2, 1]
+console.log(removeSmallest([])); // []

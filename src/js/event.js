0085161b7a6dfbbe5a3ad9f5adccf74444ba7d84
Yury <olennikovyv@mail.ru>
2022-03-02 17:18:01 +0500

    Valid Braces

--- src/js/event.js
+++ src/js/event.js
@@ -1,18 +1,41 @@
-function humanReadable(seconds) {
-	const hours = Math.floor(seconds / 60 / 60).toString().padStart(2, '0');
-	const minutes = Math.floor((seconds / 60) - hours * 60).toString().padStart(2, '0');
-	seconds = Math.floor(seconds % 60).toString().padStart(2, '0');
-	return hours + ':' + minutes + ':' + seconds;
+function validBraces(braces) {
+	const br = [['(', '{', '['], [')', '}', ']'],];
+	const stack = [];
+	let i = 0;
+
+	if (br[1].includes(braces[i])) return false;
+	stack.push(braces[i++]);
+
+	while (i < braces.length) {
+		let index = -1;
+		if (br[0].includes(braces[i])) {
+			stack.push(braces[i++]);
+			continue;
+		}
+		index = br[1].indexOf(braces[i]);
+		if (index === -1) return false;
+		if (br[0][index] === stack.pop()) {
+			i++;
+			continue;
+		}
+		return false;
+	}
+	return (stack.length) ? false : true;
 }
 
-//max time = 359999  
-console.log(humanReadable(0));// '00:00:00'
-console.log(humanReadable(59));// '00:00:59'
-console.log(humanReadable(60));// '00:01:00'
-console.log(humanReadable(90));// '00:01:30'
-console.log(humanReadable(3599));// '00:59:59'
-console.log(humanReadable(3600));// '01:00:00'
-console.log(humanReadable(45296));// '12:34:56'
-console.log(humanReadable(86399));// '23:59:59'
-console.log(humanReadable(86400));// '24:00:00'
-console.log(humanReadable(359999));// '99:59:59'
+console.log(validBraces("()))"));// false
+console.log(validBraces("()"));// true
+console.log(validBraces("[]"));// true
+console.log(validBraces("{}"));// true
+console.log(validBraces("(){}[]"));// true
+console.log(validBraces("([{}])"));// true
+console.log(validBraces("(}"));// false
+console.log(validBraces("[(])"));// false
+console.log(validBraces("({})[({})]"));// true
+console.log(validBraces("(})"));// false
+console.log(validBraces("(({{[[]]}}))"));// true
+console.log(validBraces("{}({})[]"));// true
+console.log(validBraces(")(}{]["));// false
+console.log(validBraces("())({}}{()][]["));// false
+console.log(validBraces("(((({{"));// false
+console.log(validBraces("}}]]))}])"));// false

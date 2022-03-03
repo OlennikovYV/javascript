037db0260bca968a475c9da46491b30077b4fdf1
Yury <olennikovyv@mail.ru>
2022-03-03 22:45:27 +0500

    Find The Parity Outlier

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,14 @@
-var maxSequence = function (arr) {
-	let sum = 0;
-
-	return arr.reduce((acc, el) => {
-		sum = Math.max(sum + el, 0);
-		return Math.max(acc, sum)
-	}, 0);
+function findOutlier(integers) {
+    const evenOdd = num => (num % 2) ? 1 : 0;
+    const arr = integers.map(el => evenOdd(el));
+    const sort = (arr.slice(0)).sort();
+    const findStr = (sort[0] === sort[1] ? sort[sort.length - 1] : sort[0]);
+    return integers[arr.indexOf(findStr)];
 }
 
-console.log(maxSequence([]));// 0
-console.log(maxSequence([-1, -2, -3]));// 0
-console.log(maxSequence([-1, 2, -1, 4, -3]));// 5
-console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));// 6
-console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 20]));// 21
+console.log(findOutlier([0, 1, 2])); // 1
+console.log(findOutlier([1, 2, 3])); // 2
+console.log(findOutlier([2, 6, 8, 10, 3])); // 3
+console.log(findOutlier([0, 0, 3, 0, 0])); // 3
+console.log(findOutlier([1, 1, 0, 1, 1])); // 0
+console.log(findOutlier([-25967637, 107861855, -70681809, 43800735, -44100612, -84251667, -164195005, -135297709])); //-44100612

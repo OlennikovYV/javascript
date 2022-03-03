04fd9460775ba635fcd464f706b2979df060ab7e
Yury <olennikovyv@mail.ru>
2022-03-03 17:22:05 +0500

    Maximum subarray sum refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,21 +1,10 @@
 var maxSequence = function (arr) {
-	let maxSum = 0;
+	let sum = 0;
 
-	function chechNegativeNumber(arr) {
-		return arr.filter(el => el > 0).length === 0;
-	}
-
-	if (arr.length === 0) return 0;
-	if (chechNegativeNumber(arr)) return 0;
-
-	for (let i = 0; i < arr.length - 1; i++) {
-		for (let j = i; j < arr.length + 2; j++) {
-			const sum = arr.slice(i, j + 2).reduce((acc, el) => acc + el, 0);
-			if (sum > maxSum) maxSum = sum;
-		}
-	}
-
-	return maxSum;
+	return arr.reduce((acc, el) => {
+		sum = Math.max(sum + el, 0);
+		return Math.max(acc, sum)
+	}, 0);
 }
 
 console.log(maxSequence([]));// 0
@@ -23,4 +12,3 @@ console.log(maxSequence([-1, -2, -3]));// 0
 console.log(maxSequence([-1, 2, -1, 4, -3]));// 5
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));// 6
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 20]));// 21
-console.log([-1, 2, -1, 4, -3].slice(0, 0));

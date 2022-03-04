8018e742ad46321ed4f7ff63741db856c7c522b8
Yury <olennikovyv@mail.ru>
2022-03-04 13:39:52 +0500

    Calculating with Functions

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,22 @@
-//return the total number of smiling faces in the array
-function countSmileys(arr) {
-	return arr.reduce((acc, el) => acc + /[:;][~-]?[)D]/.test(el), 0);
-}
+function calc(num, operator) { return (!operator) ? num : operator(num) }
 
-console.log(countSmileys([]));// 0
-console.log(countSmileys([':D', ':~)', ';~D', ':)']));// 4
-console.log(countSmileys([':)', ':(', ':D', ':O', ':;']));// 2
-console.log(countSmileys([';]', ':[', ';*', ':$', ';-D']));//1
-console.log(countSmileys([';~>', ';~>', ';-(', ';->', ':>', ';~(', ';o>']));//0
+function zero(operator) { return calc(0, operator) }
+function one(operator) { return calc(1, operator) }
+function two(operator) { return calc(2, operator) }
+function three(operator) { return calc(3, operator) }
+function four(operator) { return calc(4, operator) }
+function five(operator) { return calc(5, operator) }
+function six(operator) { return calc(6, operator) }
+function seven(operator) { return calc(7, operator) }
+function eight(operator) { return calc(8, operator) }
+function nine(operator) { return calc(9, operator) }
+
+function plus(b) { return a => a + b }
+function minus(b) { return a => a - b }
+function times(b) { return a => a * b }
+function dividedBy(b) { return a => a / b }
+
+console.log(seven(times(five())));// 35
+console.log(four(plus(nine())));// 13
+console.log(six(minus(four())));// 2
+console.log(eight(dividedBy(three())));// 2

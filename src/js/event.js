a23494dc8d0f7f63d32b9b4c0b85681c1b8299bd
Yury <olennikovyv@mail.ru>
2022-03-04 12:52:08 +0500

    Count the smiley faces! refactor 2

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,6 @@
 //return the total number of smiling faces in the array
 function countSmileys(arr) {
-	return arr.filter(el => /[:;][~-]?[)D]/.test(el)).length;
+	return arr.reduce((acc, el) => acc + /[:;][~-]?[)D]/.test(el), 0);
 }
 
 console.log(countSmileys([]));// 0

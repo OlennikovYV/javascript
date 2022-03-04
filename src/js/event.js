d2b8a195f27ee8f43336f9f58f3a7a2aff41b489
Yury <olennikovyv@mail.ru>
2022-03-04 13:51:35 +0500

    Calculating with Functions refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,20 +1,18 @@
-function calc(num, operator) { return (!operator) ? num : operator(num) }
-
-function zero(operator) { return calc(0, operator) }
-function one(operator) { return calc(1, operator) }
-function two(operator) { return calc(2, operator) }
-function three(operator) { return calc(3, operator) }
-function four(operator) { return calc(4, operator) }
-function five(operator) { return calc(5, operator) }
-function six(operator) { return calc(6, operator) }
-function seven(operator) { return calc(7, operator) }
-function eight(operator) { return calc(8, operator) }
-function nine(operator) { return calc(9, operator) }
+function zero(f) { return f ? f(0) : 0 }
+function one(f) { return f ? f(1) : 1 }
+function two(f) { return f ? f(2) : 2 }
+function three(f) { return f ? f(3) : 3 }
+function four(f) { return f ? f(4) : 4 }
+function five(f) { return f ? f(5) : 5 }
+function six(f) { return f ? f(6) : 6 }
+function seven(f) { return f ? f(7) : 7 }
+function eight(f) { return f ? f(8) : 8 }
+function nine(f) { return f ? f(9) : 9 }
 
 function plus(b) { return a => a + b }
 function minus(b) { return a => a - b }
 function times(b) { return a => a * b }
-function dividedBy(b) { return a => a / b }
+function dividedBy(b) { return a => Math.floor(a / b) }
 
 console.log(seven(times(five())));// 35
 console.log(four(plus(nine())));// 13

dc1be83e1ee9042d5a7ab1fe9e59945b5ba2d122
Yury <olennikovyv@mail.ru>
2022-03-04 14:35:18 +0500

    Total amount of points

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,15 @@
-const makeUpperCase = Function.prototype.call.bind(String.prototype.toUpperCase);
+function points(games) {
+	function calcPoint([x, , y]) {
+		if (x > y) return 3;
+		if (x < y) return 0;
+		return 1;
+	}
 
-console.log(makeUpperCase('hello'));//HELLO
-console.log(makeUpperCase('Sam'));//SAM
-console.log(makeUpperCase('San Francisco'));//SAN FRANCISCO
+	return games.reduce((acc, el) => acc + calcPoint(el), 0);
+}
+
+console.log(points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"]));// 30
+console.log(points(["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"]));// 10
+console.log(points(["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"]));// 0
+console.log(points(["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"]));// 15
+console.log(points(["1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"]));// 12

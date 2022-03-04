9b8d2ca1d0f3e8f0638c99ec20253c5f0bef602e
Yury <olennikovyv@mail.ru>
2022-03-04 12:42:22 +0500

    Count the smiley faces!

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,11 @@
-var maxSequence = function (arr) {
-	let sum = 0;
-
-	return arr.reduce((acc, el) => {
-		sum = Math.max(sum + el, 0);
-		return Math.max(acc, sum)
-	}, 0);
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+	const countSmile = arr.length && arr.join('').match(/[:;][~-]?[)D]/g);
+	return countSmile ? countSmile.length : 0;
 }
 
-console.log(maxSequence([]));// 0
-console.log(maxSequence([-1, -2, -3]));// 0
-console.log(maxSequence([-1, 2, -1, 4, -3]));// 5
-console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));// 6
-console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 20]));// 21
+console.log(countSmileys([]));// 0
+console.log(countSmileys([':D', ':~)', ';~D', ':)']));// 4
+console.log(countSmileys([':)', ':(', ':D', ':O', ':;']));// 2
+console.log(countSmileys([';]', ':[', ';*', ':$', ';-D']));//1
+console.log(countSmileys([';~>', ';~>', ';-(', ';->', ':>', ';~(', ';o>']));//0

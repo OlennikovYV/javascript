5f12e1852a05e3100300ae2eba48fa75bdb8e1df
Yury <olennikovyv@mail.ru>
2022-03-04 13:56:48 +0500

    Convert a Boolean to a String

--- src/js/event.js
+++ src/js/event.js
@@ -1,20 +1,6 @@
-function zero(f) { return f ? f(0) : 0 }
-function one(f) { return f ? f(1) : 1 }
-function two(f) { return f ? f(2) : 2 }
-function three(f) { return f ? f(3) : 3 }
-function four(f) { return f ? f(4) : 4 }
-function five(f) { return f ? f(5) : 5 }
-function six(f) { return f ? f(6) : 6 }
-function seven(f) { return f ? f(7) : 7 }
-function eight(f) { return f ? f(8) : 8 }
-function nine(f) { return f ? f(9) : 9 }
+function booleanToString(b) {
+	return b.toString();
+}
 
-function plus(b) { return a => a + b }
-function minus(b) { return a => a - b }
-function times(b) { return a => a * b }
-function dividedBy(b) { return a => Math.floor(a / b) }
-
-console.log(seven(times(five())));// 35
-console.log(four(plus(nine())));// 13
-console.log(six(minus(four())));// 2
-console.log(eight(dividedBy(three())));// 2
+console.log(booleanToString(true));// "true"
+console.log(booleanToString(false));// "false"

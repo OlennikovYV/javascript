a44f6200d4c391d27000b5f48c5df564cf223d62
Yury <olennikovyv@mail.ru>
2022-03-07 00:16:09 +0500

    Moving Zeros To The End refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,8 @@
 function moveZeros(arr) {
-    let arrNoZeros = [],
-        arrZeros = [];
-
-    arr.map(el => (el !== 0) ? arrNoZeros.push(el) : arrZeros.push(el));
-
-    return arrNoZeros.concat(arrZeros);
+    return [
+        ...arr.filter(el => el !== 0),
+        ...arr.filter(el => el === 0)
+    ];
 }
 
 console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])); // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]

c59146ffbfdbe38f5a4f29ed5e8ad0afef245e0d
Yury <olennikovyv@mail.ru>
2022-03-07 23:13:05 +0500

    Build Tower

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,20 @@
-function greet(name) {
-    return `Hello, ${name === "Johnny" ? "my love" : name}!`;
+function towerBuilder(nFloors) {
+    let tower = [],
+        towerWidth = 2 * nFloors - 1;
+
+    for (let i = 1, j = towerWidth; i <= nFloors; i++) {
+        const towerSpace = Math.floor((j - 1) / 2),
+            floor = 'x'.repeat(towerSpace) +
+            '*'.repeat(2 * i - 1) +
+            'x'.repeat(towerSpace);
+        tower.push(floor);
+        j -= 2;
+    }
+
+    return tower;
 }
 
-console.log(greet("Jim")); // "Hello, Jim!"
-console.log(greet("Jane")); // "Hello, Jane!"
-console.log(greet("Simon")); // "Hello, Simon!"
-console.log(greet("Johnny")); // "Hello, my love!"
+console.log(towerBuilder(1)); // ["*"]);
+console.log(towerBuilder(2)); // [" * ","***"]);
+console.log(towerBuilder(3)); // ["  *  "," *** ","*****"]
+console.log(towerBuilder(4)); // ["   *   ","  ***  "," ***** ","*******"]

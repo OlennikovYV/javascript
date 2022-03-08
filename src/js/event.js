5e38ec9f7e5e84850e3bb2fcce544da66ea386a1
Yury <olennikovyv@mail.ru>
2022-03-08 19:46:37 +0500

    Are they the same? refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,22 +1,13 @@
 function comp(array1, array2) {
-    let isEqual = true,
-        sortArray = (a, b) => a - b;
+    let sortArray = (a, b) => a - b;
 
     if (array1 === null || array2 === null) return false;
-    // if (!array1.length || !array2.length) return false;
-    // if (array1.length !== array2.length) return false;
 
     array1.sort(sortArray);
     array2.sort(sortArray);
 
-    for (let i = 0; i < array1.length; i++) {
-        if ((array1[i] ** 2) !== array2[i]) {
-            isEqual = false;
-            break;
-        }
-    }
-
-    return isEqual;
+    return array1.map(el => el ** 2)
+        .every((el, index) => el === array2[index]);
 }
 
 let a1 = [

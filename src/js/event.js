96782a65554644f4077a61940c8fb55bb59bcccf
Yury <olennikovyv@mail.ru>
2022-03-09 10:50:59 +0500

    Playing with digits

--- src/js/event.js
+++ src/js/event.js
@@ -1,24 +1,13 @@
-function comp(array1, array2) {
-    let sortArray = (a, b) => a - b;
+function digPow(n, p) {
+	let result = [...n.toString()].reduce((acc, el) => {
+		acc += el ** p;
+		p++;
+		return acc;
+	}, 0);
 
-    return !!array1 && !!array2 &&
-        array1.map(el => el * el).sort(sortArray).toString() ===
-        array2.sort(sortArray).toString();
+	return (result % n) ? -1 : result / n;
 }
 
-let a1 = [
-        121, 144, 19, 161, 19, 144, 19, 11
-    ],
-    a2 = [
-        11 * 11, 121 * 121, 144 * 144, 19 * 19,
-        161 * 161, 19 * 19, 144 * 144, 19 * 19
-    ];
-console.log(comp(a1, a2)); // true
-
-a1 = [
-    10, 3, 7, 5, 3, 6, 10, 10, 3, 5, 1
-];
-a2 = [
-    25, 25, 9, 100, 9, 9, 100, 1, 36, 100, 49
-];
-console.log(comp(a1, a2)); // true
+console.log(digPow(89, 1));// 1
+console.log(digPow(92, 1));// -1
+console.log(digPow(46288, 3));// 51

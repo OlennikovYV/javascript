d6adce950bf64838d13fe7cb49d71a53a7f88574
Yury <olennikovyv@mail.ru>
2022-03-12 22:02:59 +0500

    Is n divisible by x and y?

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,8 @@
-var lastDigit = function (str1, str2) {
-	str1 = str1.slice(-1);
-	str2 = str2.slice(-2);
-
-	return (str2 === '0' ? 1 : 0) || Math.pow(str1, str2 % 4 || 4) % 10;
+function isDivisible(n, x, y) {
+    return (n % x === 0) && (n % y === 0)
 }
 
-console.log(lastDigit("4", "0"));// 1
-console.log(lastDigit("4", "1"));// 4
-console.log(lastDigit("4", "2"));// 6
-console.log(lastDigit("9", "7"));// 9
-console.log(lastDigit("10", "10000000000"));// 0
-console.log(lastDigit("1606938044258990275541962092341162602522202993782792835301376", "2037035976334486086268445688409378161051468393665936250636140449354381299763336706183397376"));// 6
-console.log(lastDigit("3715290469715693021198967285016729344580685479654510946723", "68819615221552997273737174557165657483427362207517952651"));// 7
+console.log(isDivisible(3, 3, 4)); //false
+console.log(isDivisible(12, 3, 4)); //true
+console.log(isDivisible(8, 3, 4)); //false
+console.log(isDivisible(48, 3, 4)); //true

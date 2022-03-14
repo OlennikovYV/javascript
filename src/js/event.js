102a4b851d73d6ab68a8cf895d7c161d58f000b7
Yury <olennikovyv@mail.ru>
2022-03-14 15:27:58 +0500

    Count the Monkeys!

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,11 @@
-function rgb(r, g, b) {
-    function decToHex(num) {
-        if (num > 255) num = 255;
-        if (num < 0) num = 0;
-        return num.toString(16).padStart(2, '0').toUpperCase();
-    }
-    return decToHex(r) + decToHex(g) + decToHex(b);
+function monkeyCount(n) {
+	return (new Array(n)).fill(0).map((el, index) => {
+		return el = index + 1;
+	});
 }
 
-console.log(rgb(0, 0, 0)); // '000000'
-console.log(rgb(0, 0, -20)); // '000000'
-console.log(rgb(300, 255, 255)); // 'FFFFFF'
-console.log(rgb(173, 255, 47)); // 'ADFF2F'
+console.log((monkeyCount(5)));// [1, 2, 3, 4, 5]
+console.log((monkeyCount(3)));// [1, 2, 3]
+console.log((monkeyCount(9)));// [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log((monkeyCount(10)));// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log((monkeyCount(20)));// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]

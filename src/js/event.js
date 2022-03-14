4039c3b4e5454022e6f9da00e13122cdca593561
Yury <olennikovyv@mail.ru>
2022-03-14 15:34:34 +0500

    Invert values

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,8 @@
-function monkeyCount(n) {
-	return Array.from({ length: n }, (v, k) => k + 1);
+function invert(array) {
+	return array.map(el => -el);
 }
 
-console.log((monkeyCount(5)));// [1, 2, 3, 4, 5]
-console.log((monkeyCount(3)));// [1, 2, 3]
-console.log((monkeyCount(9)));// [1, 2, 3, 4, 5, 6, 7, 8, 9]
-console.log((monkeyCount(10)));// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log((monkeyCount(20)));// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+console.log(invert([1, 2, 3, 4, 5]));// [-1,-2,-3,-4,-5]
+console.log(invert([1, -2, 3, -4, 5]));// [-1,2,-3,4,-5]
+console.log(invert([]));// []
+console.log(invert([0]));// [-0]

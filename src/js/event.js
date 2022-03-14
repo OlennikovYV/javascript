9ca866d37f238e97998655363153a3f4751a3804
Yury <olennikovyv@mail.ru>
2022-03-14 17:35:43 +0500

    Maximum Length Difference

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,41 @@
-function pigIt(str) {
-	return str.split(' ')
-		.map(el => (/[A-Za-z]/.test(el)) ? el.slice(1) + el.slice(0, 1) + 'ay' : el)
-		.join(' ');
+let s1 = ["hoqq",
+	"bbllkw",
+	"oox",
+	"ejjuyyy",
+	"plmiis",
+	"xxxzgpsssa",
+	"xxwwkktt",
+	"znnnnfqknaz",
+	"qqquuhii",
+	"dvvvwz"],
+	s2 = ["cccooommaaqqoxii",
+		"gggqaffhhh",
+		"tttoowwwmmww"];
+
+function mxdiflg(a1, a2) {
+	let maxLength1,
+		minLength1,
+		maxLength2,
+		minLength2,
+		diff1,
+		diff2,
+		arrLength1,
+		arrLength2;
+
+	if (!a1.length || !a2.length) return -1;
+
+	arrLength1 = a1.map(el => el.length);
+	arrLength2 = a2.map(el => el.length);
+
+	maxLength1 = Math.max(...arrLength1);
+	maxLength2 = Math.max(...arrLength2);
+	minLength1 = Math.min(...arrLength1);
+	minLength2 = Math.min(...arrLength2);
+
+	diff1 = Math.abs(maxLength1 - minLength2);
+	diff2 = Math.abs(minLength1 - maxLength2);
+
+	return (diff1 > diff2) ? diff1 : diff2;
 }
 
-console.log(pigIt('Pig latin is cool'));//'igPay atinlay siay oolcay'
-console.log(pigIt('This is my string'));//'hisTay siay ymay tringsay'
+console.log(mxdiflg(s1, s2));

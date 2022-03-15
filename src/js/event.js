565134e42d8114ff0c562eaa8af3e3eb7726fdbb
Yury <olennikovyv@mail.ru>
2022-03-15 16:43:18 +0500

    Find the first non-consecutive number

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,22 @@
-function symmetricPoint(p, q) {
-	return [2 * q[0] - p[0], 2 * q[1] - p[1]];
+function firstNonConsecutive(arr) {
+	let array = [],
+		diff = 0,
+		result;
+
+	if (arr.length < 2) return null;
+
+	for (let i = 1; i < arr.length; i++) {
+		array.push(arr[i] - arr[i - 1]);
+	}
+
+	diff = Math.max(...array);
+	result = array.indexOf(diff);
+	array.sort();
+	if (array[0] === array[array.length - 1]) return null;
+
+	return (result === -1) ? null : arr[result + 1];
 }
 
-console.log(symmetricPoint([0, 0], [1, 1]));// [2, 2]
-console.log(symmetricPoint([2, 6], [-2, -6]));// [-6, -18]
-console.log(symmetricPoint([10, -10], [-10, 10]));// [-30, 30]
-console.log(symmetricPoint([1, -35], [-12, 1]));// [-25, 37]
-console.log(symmetricPoint([1000, 15], [-7, -214]));// [-1014, -443]
-console.log(symmetricPoint([0, 0], [0, 0]));// [0, 0]
+console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])); //6
+console.log(firstNonConsecutive([1, 2, 3, 4, 5, 6, 7])); //null
+console.log(firstNonConsecutive([6, 8, 9, 10, 11, 12, 14])); //8

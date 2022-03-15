a8ed276dca8a1cd76f43ad454ae054d2780763a9
Yury <olennikovyv@mail.ru>
2022-03-15 12:30:47 +0500

    Count of positives / sum of negatives refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,12 @@
 function countPositivesSumNegatives(input) {
-	if (input === null) return [];
-	return (!input.length && []) || input.reduce((acc, el) => {
+	if (input === null || input.length === 0) return [];
+
+	return input.reduce((acc, el) => {
 		if (!el) return acc;
 		if (el > 0) { acc[0] += 1; return acc; }
 		if (el < 0) { acc[1] += el; return acc; }
-	}, [0, 0]);
+	}, [0, 0]
+	);
 }
 
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));// [10, -65]

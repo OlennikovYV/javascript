28b8e0ed77a4086912c605f7327e7261eaaed43e
Yury <olennikovyv@mail.ru>
2022-03-15 10:42:35 +0500

    Can you keep a secret?

--- src/js/event.js
+++ src/js/event.js
@@ -1,30 +1,12 @@
-let s1 = ["hoqq",
-	"bbllkw",
-	"oox",
-	"ejjuyyy",
-	"plmiis",
-	"xxxzgpsssa",
-	"xxwwkktt",
-	"znnnnfqknaz",
-	"qqquuhii",
-	"dvvvwz"],
-	s2 = ["cccooommaaqqoxii",
-		"gggqaffhhh",
-		"tttoowwwmmww"];
-
-function mxdiflg(a1, a2) {
-	let arrLength1,
-		arrLength2;
-
-	if (!a1.length || !a2.length) return -1;
-
-	arrLength1 = a1.map(el => el.length);
-	arrLength2 = a2.map(el => el.length);
-
-	return Math.max(
-		Math.max(...arrLength1) - Math.min(...arrLength2),
-		Math.max(...arrLength2) - Math.min(...arrLength1)
-	);
+function createSecretHolder(secret) {
+	return {
+		_secret: secret,
+		getSecret() { return this._secret },
+		setSecret(num) { return this._secret = num }
+	};
 }
 
-console.log(mxdiflg(s1, s2));
+let obj = createSecretHolder(5);
+console.log(obj.getSecret()); // returns 5
+obj.setSecret(2);
+console.log(obj.getSecret()); // returns 2

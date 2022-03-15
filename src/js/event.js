e5f3ab775b3a10d49fb7e53eff17d447ee38428c
Yury <olennikovyv@mail.ru>
2022-03-15 12:01:53 +0500

    Is this a triangle?

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,10 @@
-function createSecretHolder(secret) {
-	return {
-		_secret: secret,
-		getSecret() { return this._secret },
-		setSecret(num) { return this._secret = num }
-	};
+function isTriangle(a, b, c) {
+	return (a + b > c && b + c > a && a + c > b);
 }
 
-let obj = createSecretHolder(5);
-console.log(obj.getSecret()); // returns 5
-obj.setSecret(2);
-console.log(obj.getSecret()); // returns 2
+console.log(isTriangle(1, 2, 2));// true
+console.log(isTriangle(7, 2, 2));// false
+console.log(isTriangle(3, 7, 10));// false
+console.log(isTriangle(9, -2, -1));// false
+console.log(isTriangle(9, 7, 5));// true
+console.log(isTriangle(3, 6, 5));// true

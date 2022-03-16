a5d27fddb086753765c4d19ce3eb359956f4e76e
Yury <olennikovyv@mail.ru>
2022-03-16 14:53:11 +0500

    Nested List Depth

--- src/js/event.js
+++ src/js/event.js
@@ -1,3 +1,17 @@
-let a = "code";
-let b = "wa.rs";
-let name = a + b;
+function arrayDepth(array) {
+	function flat(arr) {
+		return arr.reduce((acc, el) => acc.concat(el), []);
+	}
+
+	if (array.filter(el => Array.isArray(el)).length != 0) {
+		return arrayDepth(flat(array)) + 1;
+	}
+
+	return 1;
+}
+
+console.log(arrayDepth([true])); //  // returns 1
+console.log(arrayDepth([])); //  // returns 1
+console.log(arrayDepth([2, "yes", [true, false]])); //  // returns 2
+console.log(arrayDepth([1, [2, [3, [4, [5, [6], 5], 4], 3], 2], 1])); //  // returns 6
+console.log(arrayDepth([2.0, [2, 0], 3.7, [3, 7], 6.7, [6, 7]])); //  // returns 2

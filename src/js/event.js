faa8eb9213651358269fd3abc8d0962c397d5bd2
Yury <olennikovyv@mail.ru>
2022-03-16 15:14:44 +0500

    Array Deep Count

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,9 @@
-function arrayDepth(array) {
-	function flat(arr) {
-		return arr.reduce((acc, el) => acc.concat(el), []);
-	}
-
-	if (array.filter(el => Array.isArray(el)).length != 0) {
-		return arrayDepth(flat(array)) + 1;
-	}
-
-	return 1;
+function deepCount(a) {
+	return a.reduce((acc, el) => (Array.isArray(el)) ? acc + deepCount(el) + 1 : acc + 1, 0)
 }
 
-console.log(arrayDepth([true])); //  // returns 1
-console.log(arrayDepth([])); //  // returns 1
-console.log(arrayDepth([2, "yes", [true, false]])); //  // returns 2
-console.log(arrayDepth([1, [2, [3, [4, [5, [6], 5], 4], 3], 2], 1])); //  // returns 6
-console.log(arrayDepth([2.0, [2, 0], 3.7, [3, 7], 6.7, [6, 7]])); //  // returns 2
+console.log(deepCount([])); // , 0, "Expected 0"
+console.log(deepCount([1, 2, 3])); // , 3, "Expected 3"
+console.log(deepCount(["x", "y", ["z"]])); // , 4, "Expected 4"
+console.log(deepCount([1, 2, [3, 4, [5]]])); // , 7, "Expected 7"
+console.log(deepCount([[[[[[[[[]]]]]]]]])); // , 8, "Expected 8"

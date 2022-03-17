09ccdd9e4297a59759f092434374af8f97024aa8
Yury <olennikovyv@mail.ru>
2022-03-17 10:27:08 +0500

    Multiples of 3 or 5 refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,22 +1,18 @@
 function solution(number) {
 	let sum = 0;
 
-	if (number < 3) return 0;
-
 	for (let i = 3; i < number; i++) {
-		if (i % 3 === 0) {
+		if (i % 3 === 0 || i % 5 === 0)
 			sum += i;
-			continue;
-		}
-		if (i % 5 === 0) {
-			sum += i;
-			continue;
-		}
 	}
 
 	return sum;
 }
 
+console.log(solution(-1)); // 0
+console.log(solution(0)); // 0
+console.log(solution(1)); // 0
+console.log(solution(2)); // 0
 console.log(solution(10)); // 23
 console.log(solution(50)); // 543
 console.log(solution(100)); // 2318

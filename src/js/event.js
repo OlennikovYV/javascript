f5dc30c43fd05c7db2159caa491b134cddcd0830
Yury <olennikovyv@mail.ru>
2022-03-17 12:21:37 +0500

    Roman Numerals Decoder

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,21 @@
-function noSpace(x) {
-	return x.replace(/ +/g, '');
+function solution(roman) {
+	let convert = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 },
+		romanDigits = Object.keys(convert),
+		result = 0;
+
+	for (let i = 0; i < roman.length; ++i) {
+		if (romanDigits.indexOf(roman[i]) < romanDigits.indexOf(roman[i + 1])) {
+			result -= convert[roman[i]];
+		} else {
+			result += convert[roman[i]];
+		}
+	}
+
+	return result;
 }
 
-console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')); //  '8j8mBliB8gimjB8B8jlB'
-console.log(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd')); //  '88Bifk8hB8BB8BBBB888chl8BhBfd'
-console.log(noSpace('8aaaaa dddd r     ')); //  '8aaaaaddddr'
+console.log(solution('XXI')); //  21
+console.log(solution('I')); //  1
+console.log(solution('IV')); //  4
+console.log(solution('MMVIII')); //  2008
+console.log(solution('MDCLXVI')); //  1666

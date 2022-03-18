7367d7e2e44572032fa11c034e1d3b05d87a1b56
Yury <olennikovyv@mail.ru>
2022-03-18 14:40:54 +0500

    First non-repeating character refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,8 @@
 function firstNonRepeatingLetter(s) {
-	const countChars = {};
-
-	s.split('').map(el => {
-		const char = el.toLowerCase();
-		countChars[char] = (countChars[char] || 0) + 1;
-	});
-	for (let key in countChars)
-		if (countChars[key] === 1)
-			return s[s.toLowerCase().indexOf(key)];
+	for (const key in s) {
+		if (s.match(new RegExp(s[key], 'gi')).length === 1)
+			return s[key];
+	}
 
 	return '';
 }

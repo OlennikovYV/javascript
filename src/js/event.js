cdfefd4e2924d754500de225a6396dbd25e2f82c
Yury <olennikovyv@mail.ru>
2022-03-20 20:18:58 +0500

    Counting sheep...

--- src/js/event.js
+++ src/js/event.js
@@ -1,25 +1,13 @@
-function formatDuration(seconds) {
-    let time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 },
-        result = [];
+var array1 = [true, true, true, false,
+    true, true, true, true,
+    true, false, true, false,
+    true, false, false, true,
+    true, true, true, true,
+    false, false, true, true
+];
 
-    if (seconds === 0) return 'now';
-
-    for (let key in time)
-        if (seconds >= time[key]) {
-            let el = Math.floor(seconds / time[key]);
-
-            result.push(el += el > 1 ? ' ' + key + 's' : ' ' + key);
-            seconds %= time[key];
-        }
-
-    return result.length > 1 ? result.join(', ').replace(/,([^,]*)$/, ' and' + '$1') : result[0];
+function countSheeps(arrayOfSheep) {
+    return arrayOfSheep.reduce((acc, el) => el ? acc + el : acc, 0);
 }
 
-console.log(formatDuration(1)); //  "1 second"
-console.log(formatDuration(62)); //  "1 minute and 2 seconds"
-console.log(formatDuration(120)); //  "2 minutes"
-console.log(formatDuration(3600)); //  "1 hour"
-console.log(formatDuration(3662)); //  "1 hour, 1 minute and 2 seconds"
-console.log(formatDuration(3600 * 24)); //  "1 day"
-console.log(formatDuration(3600 * 24 * 365)); //  "1 year"
-console.log(formatDuration(3600 * 24 * 365 + 3662)); //  "1 year, 1 hour, 1 minute and 2 seconds"
+console.log(countSheeps(array1)); // 17

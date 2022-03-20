3816fd0d4321854bb1473d3d7697d1a12a45a2ad
Yury <olennikovyv@mail.ru>
2022-03-20 19:45:07 +0500

    Human readable duration format

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,39 @@
-function getPlanetName(id) {
-    return {
-        1: 'Mercury',
-        2: 'Venus',
-        3: 'Earth',
-        4: 'Mars',
-        5: 'Jupiter',
-        6: 'Saturn',
-        7: 'Uranus',
-        8: 'Neptune',
-    }[id];
+function formatDuration(seconds) {
+    let y = Math.floor(seconds / 31536000),
+        d = Math.floor(seconds / 86400) % 365,
+        h = Math.floor(seconds / 3600) % 24,
+        m = Math.floor(seconds / 60) % 60,
+        s = seconds % 60,
+        result = '';
+
+    function setText(time) {
+        time[0] = (time[0] ? time[0] + ' year' : '') + (time[0] > 1 ? 's' : '');
+        time[1] = (time[1] ? time[1] + ' day' : '') + (time[1] > 1 ? 's' : '');
+        time[2] = (time[2] ? time[2] + ' hour' : '') + (time[2] > 1 ? 's' : '');
+        time[3] = (time[3] ? time[3] + ' minute' : '') + (time[3] > 1 ? 's' : '');
+        time[4] = (time[4] ? time[4] + ' second' : '') + (time[4] > 1 ? 's' : '');
+
+        return time;
+    }
+
+    if (!seconds) return 'now';
+
+    result = setText([y, d, h, m, s]).filter(el => el);
+
+    if (result.length > 1)
+        result[result.length - 1] = ' and ' + result[result.length - 1];
+    if (result.length > 2)
+        for (let i = 0; i < result.length - 2; i++)
+            result[i] = result[i] + ', ';
+
+    return result.join('');
 }
 
-console.log(getPlanetName(2)); //  'Venus'
-console.log(getPlanetName(5)); //  'Jupiter'
-console.log(getPlanetName(3)); //  'Earth'
-console.log(getPlanetName(8)); //  'Neptune'
+console.log(formatDuration(1)); //  "1 second"
+console.log(formatDuration(62)); //  "1 minute and 2 seconds"
+console.log(formatDuration(120)); //  "2 minutes"
+console.log(formatDuration(3600)); //  "1 hour"
+console.log(formatDuration(3662)); //  "1 hour, 1 minute and 2 seconds"
+console.log(formatDuration(3600 * 24)); //  "1 day"
+console.log(formatDuration(3600 * 24 * 365)); //  "1 year"
+console.log(formatDuration(3600 * 24 * 365 + 3662)); //  "1 year, 1 hour, 1 minute and 2 seconds"

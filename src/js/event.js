6fb93be0b2fe24f7d3ca50dec8408afcce6e5acc
Yury <olennikovyv@mail.ru>
2022-03-21 14:10:38 +0500

    The Feast of Many Beasts

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,8 @@
-function deleteNth(arr, n) {
-	let countLst = {};
-
-	return arr.filter(el => {
-		countLst[el] = (countLst[el] || 0) + 1;
-		return countLst[el] <= n;
-	});
+function feast(beast, dish) {
+	return beast[0] === dish[0] &&
+		beast[beast.length - 1] === dish[dish.length - 1];
 }
 
-console.log(deleteNth([20, 37, 20, 21], 1)); //  [20,37,21]
-console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3)); //  [1, 1, 3, 3, 7, 2, 2, 2]
+console.log(feast("great blue heron", "garlic naan")); //  true
+console.log(feast("chickadee", "chocolate cake")); //  true
+console.log(feast("brown bear", "bear claw")); //  false

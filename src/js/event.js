ecc700c361b317370ffd2cfd372d89eb566cd046
Yury <olennikovyv@mail.ru>
2022-03-21 14:01:26 +0500

    Delete occurrences of an element if it occurs more than n times

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,12 @@
-var array1 = [true, true, true, false,
-    true, true, true, true,
-    true, false, true, false,
-    true, false, false, true,
-    true, true, true, true,
-    false, false, true, true
-];
+function deleteNth(arr, n) {
+	let countLst = {};
 
-function countSheeps(arrayOfSheep) {
-    return arrayOfSheep.filter(Boolean).length;
+	return arr.reduce((acc, el) => {
+		countLst[el] = (countLst[el] || 0) + 1;
+		if (countLst[el] <= n) acc.push(el);
+		return acc;
+	}, []);
 }
 
-console.log(countSheeps(array1)); // 17
+console.log(deleteNth([20, 37, 20, 21], 1)); //  [20,37,21]
+console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3)); //  [1, 1, 3, 3, 7, 2, 2, 2]

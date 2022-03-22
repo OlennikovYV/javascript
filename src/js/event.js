71f6f9e22c08ac74f91a97dc32a07ff74c9b2ac4
Yury <olennikovyv@mail.ru>
2022-03-22 22:00:52 +0500

    Pillars

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,9 @@
-function solve(s) {
-    return (s.replace(/[A-Z]/g, '').length < s.length / 2) ?
-        s.toUpperCase() :
-        s.toLowerCase();
+function pillars(num_pill, dist, width) {
+    if (num_pill === 1) return 0;
+    return (num_pill - 2) * width + (num_pill - 1) * dist * 100;
 }
 
-console.log(solve("code")); // "code"
-console.log(solve("CODe")); // "CODE"
-console.log(solve("COde")); // "code"
-console.log(solve("Code")); // "code"
+console.log(pillars(1, 10, 10)); //  0
+console.log(pillars(2, 20, 25)); //  2000
+console.log(pillars(3, 20, 25)); //  4025
+console.log(pillars(11, 15, 30)); //  15270

88c3602d7fd42040f2c10679cc3b5ddb18c6f404
Yury <olennikovyv@mail.ru>
2022-03-23 22:40:25 +0500

    Will you make it?

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,6 @@
-function dontGiveMeFive(start, end) {
-    let count = 0;
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return distanceToPump <= mpg * fuelLeft;
+};
 
-    for (let i = start; i <= end; i++)
-        if (String(i).indexOf('5') === -1)
-            count++;
-
-    return count;
-}
-
-console.log(dontGiveMeFive(-9, 9)); //  17
-console.log(dontGiveMeFive(-1, 9)); //  10
-console.log(dontGiveMeFive(1, 9)); //  8
-console.log(dontGiveMeFive(4, 17)); //  12
-console.log(dontGiveMeFive(22, 47)); //  23
-console.log(dontGiveMeFive(50, 100)); //  37
+console.log(zeroFuel(50, 25, 2)); //  true
+console.log(zeroFuel(100, 50, 1)); //  false

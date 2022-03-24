5f27dfc0ad4c33b078243a511511e1e4e62d59b7
Yury <olennikovyv@mail.ru>
2022-03-24 09:44:40 +0500

    Product of consecutive Fib numbers

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,18 @@
-function isDivideBy(number, a, b) {
-    return !(number % a + number % b);
+function productFib(prod) {
+	let f0 = 0, f1 = 1;
+
+	while (f0 * f1 < prod) {
+		[f0, f1] = [f1, f0 + f1];
+	}
+
+	return [f0, f1, (f0 * f1 === prod ? true : false)];
 }
 
-console.log(isDivideBy(-12, 2, -6)); //  true
-console.log(isDivideBy(-12, 2, -5)); //  false
-console.log(isDivideBy(45, 1, 6)); //  false
-console.log(isDivideBy(45, 5, 15)); //  true
-console.log(isDivideBy(4, 1, 4)); //  true
-console.log(isDivideBy(15, -5, 3)); //  true
+console.log(productFib(15)); //  [2, 3, ]
+console.log(productFib(4895)); //  [55, 89, true]
+console.log(productFib(5895)); //  [89, 144, false]
+console.log(productFib(74049690)); //  [6765, 10946, true]
+console.log(productFib(84049690)); //  [10946, 17711, false]
+console.log(productFib(193864606)); //  [10946, 17711, true]
+console.log(productFib(447577)); //  [610, 987, false]
+console.log(productFib(602070)); //  [610, 987, true]

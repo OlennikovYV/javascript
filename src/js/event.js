7a4893cf87e94e5292bc740d2c13fbb66598aac4
Yury <olennikovyv@mail.ru>
2022-03-24 17:37:11 +0500

    Find numbers which are divisible by given number

--- src/js/event.js
+++ src/js/event.js
@@ -1,18 +1,9 @@
-function productFib(prod) {
-	let f0 = 0, f1 = 1;
-
-	while (f0 * f1 < prod) {
-		[f0, f1] = [f1, f0 + f1];
-	}
-
-	return [f0, f1, (f0 * f1 === prod ? true : false)];
+function divisibleBy(numbers, divisor) {
+	return numbers.filter(el => !(el % divisor));
 }
 
-console.log(productFib(15)); //  [2, 3, ]
-console.log(productFib(4895)); //  [55, 89, true]
-console.log(productFib(5895)); //  [89, 144, false]
-console.log(productFib(74049690)); //  [6765, 10946, true]
-console.log(productFib(84049690)); //  [10946, 17711, false]
-console.log(productFib(193864606)); //  [10946, 17711, true]
-console.log(productFib(447577)); //  [610, 987, false]
-console.log(productFib(602070)); //  [610, 987, true]
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2)); //  [2,4,6]
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 3)); //  [3,6]
+console.log(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4)); //  [0,4]
+console.log(divisibleBy([0], 4)); //  [0]
+console.log(divisibleBy([1, 3, 5], 2)); //  []

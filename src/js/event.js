e3f3aa6f1fb0d1c94b25d4d424e2c2027c6818a4
Yury <olennikovyv@mail.ru>
2022-03-29 22:14:43 +0500

    The Vowel Code

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,33 @@
-function twiceAsOld(dadYearsOld, sonYearsOld) {
-	return (dadYearsOld >= sonYearsOld * 2)
-		? (dadYearsOld - sonYearsOld * 2)
-		: (sonYearsOld * 2 - dadYearsOld);
+/*
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+ */
+const code = ['a', 'e', 'i', 'o', 'u'];
+
+function encode(string) {
+    return string
+        .split('')
+        .map(el => {
+            if (code.includes(el)) return code.indexOf(el) + 1;
+            return el;
+        })
+        .join('');
 }
 
-console.log(twiceAsOld(36, 7)); //  22
-console.log(twiceAsOld(55, 30)); //  5
-console.log(twiceAsOld(42, 21)); //  0
-console.log(twiceAsOld(22, 1)); //  20
-console.log(twiceAsOld(29, 0)); //  29
+function decode(string) {
+    return string
+        .split('')
+        .map(el => {
+            if (el >= 1 && el <= 5) return code[el - 1];
+            return el;
+        })
+        .join('');
+}
+
+console.log(encode('hello')); //  'h2ll4'
+console.log(encode('How are you today?')); //  'H4w 1r2 y45 t4d1y?'
+console.log(encode('This is an encoding test.')); //  'Th3s 3s 1n 2nc4d3ng t2st.'
+console.log(decode('h2ll4')); //  'hello'

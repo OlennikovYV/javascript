fab1539689c8c5ae9e027916503528625f539cb0
Yury <olennikovyv@mail.ru>
2022-03-29 22:32:03 +0500

    Convert number to reversed array of digits

--- src/js/event.js
+++ src/js/event.js
@@ -1,22 +1,6 @@
-/*
-a -> 1
-e -> 2
-i -> 3
-o -> 4
-u -> 5
- */
-const code = ['a', 'e', 'i', 'o', 'u'];
-
-function encode(string) {
-    return string.replace(/[aeiou]/g, el => '_aeiou'.indexOf(el));
+function digitize(n) {
+    return [...String(n)].reverse().map(Number);
 }
 
-function decode(string) {
-    // return string.replace(/[1-5]/g, el => [...'_aeiou'][el])
-    return string.replace(/[1-5]/g, el => '_aeiou'.charAt(el))
-}
-
-console.log(encode('hello')); //  'h2ll4'
-console.log(encode('How are you today?')); //  'H4w 1r2 y45 t4d1y?'
-console.log(encode('This is an encoding test.')); //  'Th3s 3s 1n 2nc4d3ng t2st.'
-console.log(decode('h2ll4')); //  'hello'
+console.log(digitize(35231)); // [1,3,2,5,3]
+console.log(digitize(0)); // [0]

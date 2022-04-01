fbc8883cdff35074793e3df38d79b9e18804241b
Yury <olennikovyv@mail.ru>
2022-04-01 08:47:12 +0500

    Sum of Digits / Digital Root

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,15 @@
-function stringToArray(string) {
-	return string.split(' ');
+function digital_root(n) {
+	while (n.toString().length > 1) {
+		n = Array
+			.from(String(n), Number)
+			.reduce((acc, el) => acc + el, 0);
+	}
+
+	return n;
 }
 
-console.log(stringToArray("Robin Singh")); //  ["Robin", "Singh"]
-console.log(stringToArray("I love arrays they are my favorite")); //  ["I", "love", "arrays", "they", "are", "my", "favorite"]
+console.log(digital_root(16)); //  7
+console.log(digital_root(456)); //  6
+console.log(digital_root(942)); //  6
+console.log(digital_root(132189)); //  6
+console.log(digital_root(493193)); //  2

aee259be1a3824f4b507f9dc342c45e85fc1685b
Yury <olennikovyv@mail.ru>
2022-04-02 23:26:43 +0500

    Split Strings

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,15 @@
-function nbDig(n, d) {
-    return Array
-        .from(Array(n + 1), (el, index) => index ** 2)
-        .join('')
-        .match(new RegExp(d, 'g'))
-        .length;
+function solution(str) {
+    const result = [];
+
+    for (let i = 0; i < str.length; i += 2) {
+        result.push(
+            str[i] + (i + 1 < str.length ? str[i + 1] : '_')
+        );
+    }
+
+    return result;
 }
 
-console.log(nbDig(10, 1)); //   4
-console.log(nbDig(5750, 0)); //   4700
-console.log(nbDig(11011, 2)); //   9481
-console.log(nbDig(12224, 8)); //   7733
-console.log(nbDig(11549, 1)); //  11905
+console.log(solution("abcdef")); //  ["ab", "cd", "ef"]
+console.log(solution("abcdefg")); //  ["ab", "cd", "ef", "g_"]
+console.log(solution("")); //  []

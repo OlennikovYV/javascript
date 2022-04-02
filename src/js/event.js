ee89247a832cfc2ed624b2283a15abe43c0c1f56
Yury <olennikovyv@mail.ru>
2022-04-02 23:01:39 +0500

    Count the Digit

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,21 @@
-function multiply(x, y) {
-	return x * y;
+function nbDig(n, d) {
+    let count = 0;
+
+    for (let k = 0; k <= n; k++) {
+        const square = k ** 2;
+        count += Array
+            .from(String(square), Number)
+            .reduce((acc, el) => {
+                if (el === d) acc += 1;
+                return acc;
+            }, 0);
+    }
+
+    return count;
 }
 
-console.log(multiply(2, 3)); // 6
+console.log(nbDig(10, 1)); //   4
+console.log(nbDig(5750, 0)); //   4700
+console.log(nbDig(11011, 2)); //   9481
+console.log(nbDig(12224, 8)); //   7733
+console.log(nbDig(11549, 1)); //  11905

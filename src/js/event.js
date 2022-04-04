e70f2d64f7742c6222cd8a2fd4fd1321517a86f1
Yury <olennikovyv@mail.ru>
2022-04-04 08:47:13 +0500

    Do I get a bonus?

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,11 @@
-function solution(input, markers) {
-    return input
-        .replace(new RegExp(`\s*[${markers.join('')}].+`, 'g'), '')
-        .split('\n')
-        .map(el => el.trim())
-        .join('\n');
-};
+function bonusTime(salary, bonus) {
+	return `\u00A3${(bonus ? salary * 10 : salary)}`;
+}
 
-console.log(solution("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"])); // "apples, plums\npears\noranges"
-console.log(solution("Q @b\nu\ne -e f g", ["@", "-"])); // "Q\nu\ne"
+console.log(bonusTime(10000, true)); //  '£100000'
+console.log(bonusTime(25000, true)); //  '£250000'
+console.log(bonusTime(10000, false)); //  '£10000'
+console.log(bonusTime(60000, false)); //  '£60000'
+console.log(bonusTime(2, true)); //  '£20'
+console.log(bonusTime(78, false)); //  '£78'
+console.log(bonusTime(67890, true)); //  '£678900'

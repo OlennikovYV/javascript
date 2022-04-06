ce83a039227f3c69c0cbcd1dedd93502a73f6c47
Yury <olennikovyv@mail.ru>
2022-04-06 14:36:49 +0500

    Scramblies

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,30 @@
-function incrementString(strng) {
-	return strng.replace(/[0-8]?9*$/, el => String(++el));
+function scramble(str1, str2) {
+	str1 = Array
+		.from(str1)
+		.reduce((acc, el) => {
+			acc[el] = (acc[el] || 0) + 1;
+			return acc;
+		}, {});
+
+	str2 = Array.from(str2);
+
+	for (let i = 0; i < str2.length; i++) {
+		if (str1[str2[i]] === 0 || str1[str2[i]] === undefined)
+			return false;
+		str1[str2[i]]--;
+	}
+
+	return true;
 }
 
-console.log(incrementString("foobar000")); //  "foobar001"
-console.log(incrementString("foo")); //  "foo1"
-console.log(incrementString("foobar001")); //  "foobar002"
-console.log(incrementString("foobar99")); //  "foobar100"
-console.log(incrementString("foobar099")); //  "foobar100"
-console.log(incrementString("")); //  "1"
+console.log(scramble('rkqodlw', 'world')); // true
+console.log(scramble('cedewaraaossoqqyt', 'codewars')); // true
+console.log(scramble('katas', 'steak')); // false
+console.log(scramble('scriptjavx', 'javascript')); // false
+console.log(scramble('scriptingjava', 'javascript')); // true
+console.log(scramble('scriptsjava', 'javascripts')); // true
+console.log(scramble('javscripts', 'javascript')); // false
+console.log(scramble('jscripts', 'javascript')); // false
+console.log(scramble('aabbcamaomsccdd', 'commas')); // true
+console.log(scramble('commas', 'commas')); // true
+console.log(scramble('sammoc', 'commas')); // true

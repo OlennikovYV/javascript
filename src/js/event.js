76cdc60c57278d78c99da1ea5af2fa165ccc6f1b
Yury <olennikovyv@mail.ru>
2022-04-07 12:55:05 +0500

    Roman Numerals Encoder refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,60 +1,31 @@
 function solution(number) {
-	var romane = {
-		1: 'I',
-		2: 'II',
-		3: 'III',
-		4: 'IV',
-		5: 'V',
-		6: 'VI',
-		7: 'VII',
-		8: 'VIII',
-		9: 'IX',
-		10: 'X',
-		20: 'XX',
-		30: 'XXX',
-		40: 'XL',
-		50: 'L',
-		60: 'LX',
-		70: 'LXX',
-		80: 'LXXX',
-		90: 'XC',
-		100: 'C',
-		200: 'CC',
-		300: 'CCC',
-		400: 'CD',
-		500: 'D',
-		600: 'DC',
-		700: 'DCC',
-		800: 'DCCC',
-		900: 'CM',
-		1000: 'M',
-		2000: 'MM',
-		3000: 'MMM',
-		4000: 'MMMM',
-		5000: 'MMMMM',
-		6000: 'MMMMMM',
-		7000: 'MMMMMMM',
-		8000: 'MMMMMMMM',
-		9000: 'MMMMMMMMM'
-	};
-	let digitArray = [],
-		romaneArray = [],
-		r = 1;
+	let roman = {
+		M: 1000,
+		CM: 900,
+		D: 500,
+		CD: 400,
+		C: 100,
+		XC: 90,
+		L: 50,
+		XL: 40,
+		X: 10,
+		IX: 9,
+		V: 5,
+		IV: 4,
+		I: 1
+	},
+		result = '';
 
-	digitArray = Array
-		.from(String(number))
-		.reverse();
+	while (number > 0)
+		for (let key in roman) {
+			if (roman[key] <= number) {
+				result += key;
+				number -= roman[key];
+				break;
+			}
+		}
 
-	for (let i = 0; i < digitArray.length; i++) {
-		digitArray.splice(i, 1, digitArray[i] * r);
-		r *= 10;
-	}
-
-	for (let i = 0; i < digitArray.length; i++) {
-		romaneArray.push(romane[digitArray[i]]);
-	}
-
-	return romaneArray.reverse().join('');
+	return result;
 }
 
 console.log(solution(1)); //  'I', '1 should, "I"'

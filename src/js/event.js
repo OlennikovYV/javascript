50cb9faf3fe8882e82c68656c281c282a282e962
Yury <olennikovyv@mail.ru>
2022-04-07 12:45:40 +0500

    Roman Numerals Encoder

--- src/js/event.js
+++ src/js/event.js
@@ -1,24 +1,70 @@
-function scramble(str1, str2) {
-	str1 = Array
-		.from(str1)
-		.reduce((acc, el) => {
-			acc[el] = (acc[el] || 0) + 1;
-			return acc;
-		}, {});
+function solution(number) {
+	var romane = {
+		1: 'I',
+		2: 'II',
+		3: 'III',
+		4: 'IV',
+		5: 'V',
+		6: 'VI',
+		7: 'VII',
+		8: 'VIII',
+		9: 'IX',
+		10: 'X',
+		20: 'XX',
+		30: 'XXX',
+		40: 'XL',
+		50: 'L',
+		60: 'LX',
+		70: 'LXX',
+		80: 'LXXX',
+		90: 'XC',
+		100: 'C',
+		200: 'CC',
+		300: 'CCC',
+		400: 'CD',
+		500: 'D',
+		600: 'DC',
+		700: 'DCC',
+		800: 'DCCC',
+		900: 'CM',
+		1000: 'M',
+		2000: 'MM',
+		3000: 'MMM',
+		4000: 'MMMM',
+		5000: 'MMMMM',
+		6000: 'MMMMMM',
+		7000: 'MMMMMMM',
+		8000: 'MMMMMMMM',
+		9000: 'MMMMMMMMM'
+	};
+	let digitArray = [],
+		romaneArray = [],
+		r = 1;
 
-	str2 = Array.from(str2);
+	digitArray = Array
+		.from(String(number))
+		.reverse();
 
-	return str2.every(el => --str1[el] >= 0);
+	for (let i = 0; i < digitArray.length; i++) {
+		digitArray.splice(i, 1, digitArray[i] * r);
+		r *= 10;
+	}
+
+	for (let i = 0; i < digitArray.length; i++) {
+		romaneArray.push(romane[digitArray[i]]);
+	}
+
+	return romaneArray.reverse().join('');
 }
 
-console.log(scramble('rkqodlw', 'world')); // true
-console.log(scramble('cedewaraaossoqqyt', 'codewars')); // true
-console.log(scramble('katas', 'steak')); // false
-console.log(scramble('scriptjavx', 'javascript')); // false
-console.log(scramble('scriptingjava', 'javascript')); // true
-console.log(scramble('scriptsjava', 'javascripts')); // true
-console.log(scramble('javscripts', 'javascript')); // false
-console.log(scramble('jscripts', 'javascript')); // false
-console.log(scramble('aabbcamaomsccdd', 'commas')); // true
-console.log(scramble('commas', 'commas')); // true
-console.log(scramble('sammoc', 'commas')); // true
+console.log(solution(1)); //  'I', '1 should, "I"'
+console.log(solution(2)); //  'II', '2 should, "II"'
+console.log(solution(3)); //  'III', '3 should, "III"'
+console.log(solution(4)); //  'IV', '4 should, "IV"'
+console.log(solution(5)); //  'V', '5 should, "V"'
+console.log(solution(9)); //  'IX', '9 should, "IX"'
+console.log(solution(10)); //  'X', '10 should, "X"'
+console.log(solution(11)); //  'XI'
+console.log(solution(19)); //  'XIX'
+console.log(solution(22)); //  'XXII'
+console.log(solution(15)); //  'XV'

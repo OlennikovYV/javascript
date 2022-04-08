c6b839933acaf23377245fafc5b4f3a6774c9632
Yury <olennikovyv@mail.ru>
2022-04-08 16:20:43 +0500

    Multiplying numbers as strings

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,17 @@
-function find_average(array) {
-	const sum = array.reduce((acc, el) => acc + el);
-	const length = array.length;
+function multiply(a, b) {
+	let mult = BigInt(a) * BigInt(b);
 
-	return length === 0 ? 0 : sum / length;
+	return mult.toString();
 }
 
-console.log(find_average([1, 1, 1])); //  1
-console.log(find_average([1, 2, 3])); //  2
-console.log(find_average([1, 2, 3, 4])); //  2.5
+console.log(multiply("2", "3")); //  "6"
+console.log(multiply("30", "69")); //  "2070"
+console.log(multiply("11", "85")); //  "935"
+console.log(multiply("2", "0")); //  "0"
+console.log(multiply("0", "30")); //  "0"
+console.log(multiply("0000001", "3")); //  "3"
+console.log(multiply("1009", "03")); //  "3027"
+console.log(multiply("98765", "56894")); //  "5619135910"
+console.log(multiply("1020303004875647366210", "2774537626200857473632627613")); //  "2830869077153280552556547081187254342445169156730"
+console.log(multiply("58608473622772837728372827", "7586374672263726736374")); //  "444625839871840560024489175424316205566214109298"
+console.log(multiply("9007199254740991", "9007199254740991")); //  "81129638414606663681390495662081"

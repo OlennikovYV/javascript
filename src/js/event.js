d90be32e60303fbee23013c8eb6e5eb8320ff4d6
Yury <olennikovyv@mail.ru>
2022-04-09 23:07:33 +0500

    Break camelCase

--- src/js/event.js
+++ src/js/event.js
@@ -1,41 +1,7 @@
-function nextBigger(n) {
-    const combination = [];
-    let nextBigger = Number.MAX_VALUE;
-
-    function getCombination(array, memo) {
-        let currentElement;
-
-        memo = memo || [];
-
-        for (let i = 0; i < array.length; i++) {
-            currentElement = array.splice(i, 1);
-
-            if (array.length === 0) {
-                let digit = +memo.concat(currentElement).join('');
-                if (digit > n && digit < nextBigger) {
-                    nextBigger = digit;
-                    if (combination.length > 0)
-                        combination.pop();
-                    combination.push(digit);
-                }
-            }
-
-            getCombination(array.slice(), memo.concat(currentElement));
-            array.splice(i, 0, currentElement[0]);
-        }
-
-        return combination;
-    }
-
-    getCombination(Array.from(String(n), Number));
-
-    return combination.length > 0 ? combination[0] : -1;
+function solution(string) {
+    return string.match(/(^[a-z]+)|([A-Z]{1}[a-z]+)/g).join(' ');
 }
 
-console.log(nextBigger(12)); // 21
-console.log(nextBigger(513)); // 531
-console.log(nextBigger(2017)); // 2071
-console.log(nextBigger(414)); // 441
-console.log(nextBigger(144)); // 414
-console.log(nextBigger(321)); // -1
-console.log(nextBigger(1234567890)); //1234567908
+console.log(solution('camelCasing')); //  'camel Casing'
+console.log(solution('camelCasingTest')); //  'camel Casing Test'
+console.log(solution('ZamelCasingTest')); //  'Zamel Casing Test'

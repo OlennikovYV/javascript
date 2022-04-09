87c5eaa07aff9806e84c808152655f60faf91690
Yury <olennikovyv@mail.ru>
2022-04-10 00:14:09 +0500

    Difference of Volumes of Cuboids

--- src/js/event.js
+++ src/js/event.js
@@ -1,62 +1,10 @@
-function encrypt(text, n) {
-    let odd = [],
-        even = [],
-        textEncrypt = '';
-
-    if (!text || n < 1) return text;
-
-    text
-        .split('')
-        .map((el, index) => {
-            if (index % 2 === 0) {
-                even.push(el);
-            } else {
-                odd.push(el);
-            }
-        });
-
-    textEncrypt = odd.concat(even).join('');
-
-    if (n > 1) textEncrypt = encrypt(textEncrypt, n - 1);
-
-    return textEncrypt;
+function findDifference(a, b) {
+    return Math.abs((a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2]));
 }
 
-function decrypt(encryptedText, n) {
-    let odd,
-        even,
-        centerString,
-        textDecrypt = [];
 
-    if (!encryptedText || n < 1) return encryptedText;
-
-    centerString = Math.floor(encryptedText.length / 2);
-    even = encryptedText.slice(0, centerString);
-    odd = encryptedText.slice(centerString);
-
-    for (let i = 0; i < odd.length; i++) {
-        textDecrypt.push(odd[i]);
-        if (even[i]) textDecrypt.push(even[i]);
-    }
-    textDecrypt = textDecrypt.join('');
-
-    if (n > 1) textDecrypt = decrypt(textDecrypt, n - 1);
-
-    return textDecrypt;
-}
-
-console.log(encrypt("This is a test!", 0)); //  "This is a test!"
-console.log(encrypt("This is a test!", 1)); //  "hsi  etTi sats!"
-console.log(encrypt("This is a test!", 2)); //  "s eT ashi tist!"
-console.log(encrypt("This is a test!", 3)); //  " Tah itse sits!"
-console.log(encrypt("This is a test!", 4)); //  "This is a test!"
-console.log(encrypt("This is a test!", -1)); //  "This is a test!"
-console.log(encrypt("This kata is very interesting!", 1)); //  "hskt svr neetn!Ti aai eyitrsig"
-
-console.log(decrypt("This is a test!", 0)); //  "This is a test!"
-console.log(decrypt("hsi  etTi sats!", 1)); //  "This is a test!"
-console.log(decrypt("s eT ashi tist!", 2)); //  "This is a test!"
-console.log(decrypt(" Tah itse sits!", 3)); //  "This is a test!"
-console.log(decrypt("This is a test!", 4)); //  "This is a test!"
-console.log(decrypt("This is a test!", -1)); //  "This is a test!"
-console.log(decrypt("hskt svr neetn!Ti aai eyitrsig", 1)); //  "This kata is very interesting!"
+console.log(findDifference([3, 2, 5], [1, 4, 4])); //  14
+console.log(findDifference([9, 7, 2], [5, 2, 2])); //  106
+console.log(findDifference([11, 2, 5], [1, 10, 8])); //  30
+console.log(findDifference([4, 4, 7], [3, 9, 3])); //  31
+console.log(findDifference([15, 20, 25], [10, 30, 25])); //  0

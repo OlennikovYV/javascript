437fdea4538b722044517f06ae335ff9f31fd63a
Yury <olennikovyv@mail.ru>
2022-04-10 21:51:52 +0500

    Change it up

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,29 @@
-const haystack_1 = [
-    '3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false
-];
-const haystack_2 = [
-    '283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago'
-];
-const haystack_3 = [
-    1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4, 2, 34, 234, 23, 4, 234, 324, 324, 'needle', 1, 2, 3, 4, 5, 5, 6, 5, 4, 32, 3, 45, 54
-];
+function changer(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    return str
+        .toLowerCase()
+        .split('')
+        .map(el => {
+            if (/[a-z]/.test(el)) {
+                let codeChar;
+                if (el === 'z') return 'A';
 
-function findNeedle(haystack) {
-    return `found the needle at position ${haystack.indexOf('needle')}`;
+                codeChar = String.fromCharCode(el.codePointAt(0) + 1);
+
+                if (vowels.indexOf(codeChar) !== -1) {
+                    return codeChar.toUpperCase();
+                }
+
+                return codeChar;
+            }
+            return el;
+        })
+        .join('');
 }
 
-console.log(findNeedle(haystack_1)); //  'found the needle at position 3'
-console.log(findNeedle(haystack_2)); //  'found the needle at position 5'
-console.log(findNeedle(haystack_3)); //  'found the needle at position 30'
+console.log(changer('Cat30')); //  'dbU30'
+console.log(changer('Alice')); //  'bmjdf'
+console.log(changer('sponge1')); //  'tqpOhf1'
+console.log(changer('Hello World')); //  'Ifmmp xpsmE'
+console.log(changer('dogs')); //  'Epht'
+console.log(changer('z')); //  'A'

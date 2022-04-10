b9cd490d55ad00880c2ea133272e7b9bd993c802
Yury <olennikovyv@mail.ru>
2022-04-10 21:30:12 +0500

    A Needle in the Haystack

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,17 @@
-const getChar = String.fromCharCode;
+const haystack_1 = [
+    '3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false
+];
+const haystack_2 = [
+    '283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago'
+];
+const haystack_3 = [
+    1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4, 2, 34, 234, 23, 4, 234, 324, 324, 'needle', 1, 2, 3, 4, 5, 5, 6, 5, 4, 32, 3, 45, 54
+];
 
-console.log(getChar(55)); // '7'
-console.log(getChar(56)); // '8'
-console.log(getChar(57)); // '9'
-console.log(getChar(58)); // ':'
-console.log(getChar(59)); // ';'
-console.log(getChar(60)); // '<'
-console.log(getChar(61)); // '='
-console.log(getChar(62)); // '>'
-console.log(getChar(63)); // '?'
-console.log(getChar(64)); // '@'
-console.log(getChar(65)); // 'A'
+function findNeedle(haystack) {
+    return `found the needle at position ${haystack.indexOf('needle')}`;
+}
+
+console.log(findNeedle(haystack_1)); //  'found the needle at position 3'
+console.log(findNeedle(haystack_2)); //  'found the needle at position 5'
+console.log(findNeedle(haystack_3)); //  'found the needle at position 30'

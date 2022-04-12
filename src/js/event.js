9d527bc7c1aa7a7bc848d4a75fa0288a8d28d90a
Yury <olennikovyv@mail.ru>
2022-04-12 16:27:03 +0500

    Switch it Up!

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,18 @@
-function powersOfTwo(n) {
-    return Array.from({ length: n + 1 }, (e, i) => 2 ** i);
+function switchItUp(number) {
+    return {
+        0: 'Zero',
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine',
+    }[number];
 }
 
-console.log(powersOfTwo(0)); //  [1]
-console.log(powersOfTwo(1)); //  [1, 2]
-console.log(powersOfTwo(4)); //  [1, 2, 4, 8, 16]
+console.log(switchItUp(1)); // "One"
+console.log(switchItUp(3)); // "Three"
+console.log(switchItUp(5)); // "Five"

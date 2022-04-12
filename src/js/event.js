e50afde094a6be49c457b902f6d41de34193af6b
Yury <olennikovyv@mail.ru>
2022-04-12 16:08:46 +0500

    Double Char

--- src/js/event.js
+++ src/js/event.js
@@ -1,25 +1,10 @@
-function isValidIP(str) {
-	let ip = str.split('.');
-
-	return (ip.length === 4) && ip.every(el => {
-		return el === String(+el) && (+el <= 255) && (+el >= 0)
-	});
+function doubleChar(str) {
+    return str.replace(/./g, e => e.repeat(2));
 }
 
-console.log(isValidIP("0.0.0.0")); //  true
-console.log(isValidIP("12.255.56.1")); //  true
-console.log(isValidIP("137.255.156.100")); //  true
-console.log(isValidIP('')); //  false
-console.log(isValidIP('abc.def.ghi.jkl')); //  false
-console.log(isValidIP('123.456.789.0')); //  false
-console.log(isValidIP('12.34.56')); //  false
-console.log(isValidIP('01.02.03.04')); //  false
-console.log(isValidIP('256.1.2.3')); //  false
-console.log(isValidIP('1.2.3.4.5')); //  false
-console.log(isValidIP('123,45,67,89')); //  false
-console.log(isValidIP('1e0.1e1.1e2.2e2')); //  false
-console.log(isValidIP(' 1.2.3.4')); //  false
-console.log(isValidIP('1.2.3.4 ')); //  false
-console.log(isValidIP('12.34.56.-7')); //  false
-console.log(isValidIP('1.2.3.4\n')); //  false
-console.log(isValidIP('\n1.2.3.4')); //  false
+console.log(doubleChar("abcd")); //  "aabbccdd"
+console.log(doubleChar("Adidas")); //  "AAddiiddaass"
+console.log(doubleChar("1337")); //  "11333377"
+console.log(doubleChar("illuminati")); //  "iilllluummiinnaattii"
+console.log(doubleChar("123456")); //  "112233445566"
+console.log(doubleChar("%^&*(")); //  "%%^^&&**(("

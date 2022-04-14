7c54c1a517c2f034b0865f835a25302742094998
Yury <olennikovyv@mail.ru>
2022-04-14 10:01:04 +0500

    Beginner - Reduce but Grow

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,7 @@
-function queueTime(customers, n) {
-    let selfCheckout = Array(n).fill(0);
-
-    for (let timeCustomer of customers) {
-        const indexInQueue = selfCheckout
-            .indexOf(Math.min(...selfCheckout));
-        selfCheckout[indexInQueue] += timeCustomer;
-    }
-
-    return Math.max(...selfCheckout);
+function grow(x) {
+    return x.reduce((acc, el) => acc * el)
 }
 
-console.log(queueTime([], 1)); //  0
-console.log(queueTime([1, 2, 3, 4], 1)); //  10
-console.log(queueTime([2, 2, 3, 3, 4, 4], 2)); //  9
-console.log(queueTime([1, 2, 3, 4, 5], 100)); //  5
-console.log(queueTime([32, 50, 12, 14, 28, 4, 38, 8, 19, 24, 22, 4, 11, 35, 4, 7, 22], 3)); //  117
+console.log(grow([1, 2, 3])); //  6
+console.log(grow([4, 1, 1, 1, 4])); //  16
+console.log(grow([2, 2, 2, 2, 2, 2])); //  64

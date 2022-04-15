b3ad45ef1cc39cec9856db3e11d255a29b01e1a6
Yury <olennikovyv@mail.ru>
2022-04-15 14:12:34 +0500

    Sort the odd refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,12 @@
 function sortArray(array) {
     let odd = [];
 
-    const checkOdd = num => num % 2 !== 0;
-    array.forEach(el => checkOdd(el) ? odd.push(el) : 0);
-    odd.sort((a, b) => a - b);
+    odd = array.filter(el => el % 2 !== 0).sort((a, b) => a - b);
 
-    return array.map(el => checkOdd(el) ? odd.shift() : el);
+    return array.map(el => el % 2 !== 0 ? odd.shift() : el);
 }
 
 console.log(sortArray([5, 3, 2, 8, 1, 4])); //  [1, 3, 2, 8, 5, 4]
 console.log(sortArray([5, 3, 1, 8, 0])); //  [1, 3, 5, 8, 0]
-console.log(sortArray([5, 3, 2, 8, 1, 4])); // [ 1, 3, 5, 2, 8, 4 ]
+console.log(sortArray([5, 3, 2, 8, 1, 4])); // [ 1, 3, 2, 8, 5, 4 ]
 console.log(sortArray([])); // []

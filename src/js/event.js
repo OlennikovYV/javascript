895ab0c9fff2dcb3dcd515e8b9698fffb314f4fa
Yury <olennikovyv@mail.ru>
2022-04-17 19:38:54 +0500

    Sort Numbers refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,5 @@
 function solution(nums) {
-    return nums ? nums.sort((a, b) => a - b) : [];
+    return (nums || []).sort((a, b) => a - b);
 }
 
 console.log(solution([1, 2, 3, 10, 5])); //  [1,2,3,5,10]

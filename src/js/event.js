71311a7fac65a698ee898e64a2269f3308d5fcaf
Yury <olennikovyv@mail.ru>
2022-04-18 11:20:15 +0500

    Anagram Detection

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,20 @@
-function solution(nums) {
-    return (nums || []).sort((a, b) => a - b);
-}
+// write the function isAnagram
+var isAnagram = function(test, original) {
+    const sortString = str => str
+        .toLowerCase()
+        .split('')
+        .sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0))
+        .join('');
 
-console.log(solution([1, 2, 3, 10, 5])); //  [1,2,3,5,10]
-console.log(solution(null)); //  []
-console.log(solution([])); //  []
-console.log(solution([20, 2, 10])); //  [2,10,20]
-console.log(solution([2, 20, 10])); //  [2,10,20]
+    test = sortString(test);
+    original = sortString(original);
+
+    return test === original;
+};
+
+console.log(isAnagram("foefet", "toffee")); //  true, 'The word foefet is an anagram of toffee'
+console.log(isAnagram("Buckethead", "DeathCubeK")); //  true, 'The word Buckethead is an anagram of DeathCubeK'
+console.log(isAnagram("Twoo", "WooT")); //  true, 'The word Twoo is an anagram of WooT'
+console.log(isAnagram("dumble", "bumble")); //  false, 'Characters do not match for test case dumble, bumble'
+console.log(isAnagram("ound", "round")); //  false, 'Missing characters for test case ound, round'
+console.log(isAnagram("apple", "pale")); //  false, 'Same letters, but different count'

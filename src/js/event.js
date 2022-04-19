f0037f824f4ef5d123ba3e667429ebdae683e6ae
Yury <olennikovyv@mail.ru>
2022-04-19 08:39:25 +0500

    Complementary DNA

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,17 @@
-// write the function isAnagram
-var isAnagram = function(test, original) {
-    const sortString = str => str.toLowerCase().split('').sort().join('');
+function DNAStrand(dna) {
+    const dnaDecode = {
+        A: 'T',
+        C: 'G',
+        T: 'A',
+        G: 'C',
+    };
 
-    return sortString(test) === sortString(original);
-};
+    return dna
+        .split('')
+        .map(el => dnaDecode[el])
+        .join('');
+}
 
-console.log(isAnagram("foefet", "toffee")); //  true, 'The word foefet is an anagram of toffee'
-console.log(isAnagram("Buckethead", "DeathCubeK")); //  true, 'The word Buckethead is an anagram of DeathCubeK'
-console.log(isAnagram("Twoo", "WooT")); //  true, 'The word Twoo is an anagram of WooT'
-console.log(isAnagram("dumble", "bumble")); //  false, 'Characters do not match for test case dumble, bumble'
-console.log(isAnagram("ound", "round")); //  false, 'Missing characters for test case ound, round'
-console.log(isAnagram("apple", "pale")); //  false, 'Same letters, but different count'
+console.log(DNAStrand("AAAA")); // "TTTT","String AAAA is"
+console.log(DNAStrand("ATTGC")); // "TAACG","String ATTGC is"
+console.log(DNAStrand("GTAT")); // "CATA","String GTAT is"

52f7ba1602bc6e0f176decbdc6a92194ee984978
Yury <olennikovyv@mail.ru>
2022-04-20 08:40:26 +0500

    Is it a palindrome?

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,12 @@
-function move(position, roll) {
-    return position + roll * 2;
+function isPalindrome(x) {
+    return x.toLowerCase() === x.toLowerCase().split('').reverse().join('');
 }
 
-console.log(move(0, 4)); //  8
-console.log(move(3, 6)); //  15
-console.log(move(2, 5)); //  12
+console.log(isPalindrome("a")); //  true
+console.log(isPalindrome("aba")); //  true
+console.log(isPalindrome("Abba")); //  true
+console.log(isPalindrome("hello")); //  false
+console.log(isPalindrome("Bob")); //  true
+console.log(isPalindrome("Madam")); //  true
+console.log(isPalindrome("AbBa")); //  true
+console.log(isPalindrome("")); //  true

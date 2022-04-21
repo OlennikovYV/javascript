2826edcf60ef834398e861adc006e6abaa9a50ae
Yury <olennikovyv@mail.ru>
2022-04-21 08:55:03 +0500

    Exclusive 'or' (xor) Logical Operator refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,9 @@
 function xor(a, b) {
-    return Boolean(a ^ b);
+    return a != b;
 }
 
 console.log(xor(false, false)); //  false
 console.log(xor(true, false)); //  true
 console.log(xor(false, true)); //  true
 console.log(xor(true, true)); //  false
-console.log(xor(true, true)); //  true
+console.log(xor(true, true)); //  false

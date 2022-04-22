c0da9ee793d37e2d5611a7fd9180333964219239
Yury <olennikovyv@mail.ru>
2022-04-22 09:42:05 +0500

    Two fighters, one winner

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,41 @@
-function take(arr, n) {
-    return arr.slice(0, n);
+function Fighter(name, health, damagePerAttack) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttack = damagePerAttack;
+    this.toString = function() { return this.name; }
 }
 
-console.log(take([0, 1, 2, 3, 5, 8, 13], 3)); // [0, 1, 2]
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    let currentFighter;
+
+    if (fighter1.name === firstAttacker) {
+        currentFighter = fighter1;
+    } else {
+        currentFighter = fighter2;
+    }
+
+    do {
+        if (currentFighter === fighter1) {
+            fighter2.health -= fighter1.damagePerAttack;
+        } else {
+            fighter1.health -= fighter2.damagePerAttack;
+        }
+
+        if (fighter1.health <= 0 || fighter2.health <= 0) break;
+
+        currentFighter = (
+            currentFighter === fighter1 ?
+            fighter2 :
+            fighter1
+        );
+    } while (1);
+
+    return currentFighter.name;
+}
+
+console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew")); // "Lew"
+console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry")); // "Harry"
+console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry")); // "Harald"
+console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald")); // "Harald"
+console.log(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry")); // "Harald"
+console.log(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald")); // "Harald"

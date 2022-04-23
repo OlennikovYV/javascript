76b5a08423c7cb28388f16daf04dc8dc785392db
Yury <olennikovyv@mail.ru>
2022-04-23 19:02:29 +0500

    Sum of angles

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,6 @@
-function findMultiples(integer, limit) {
-    const result = [];
-    for (let i = integer; i <= limit; i += integer)
-        if (i % integer === 0)
-            result.push(i);
-    return result;
+function angle(n) {
+    return 180 * (n - 2);
 }
 
-console.log(findMultiples(5, 25)); //  [5, 10, 15, 20, 25]
-console.log(findMultiples(1, 2)); //  [1, 2]
-console.log(findMultiples(5, 7)); //  [5]
-console.log(findMultiples(4, 27)); //  [4, 8, 12, 16, 20, 24]
-console.log(findMultiples(11, 54)); //  [11, 22, 33, 44]
+console.log(angle(3)); //  180
+console.log(angle(4)); //  360

1db73840d9c490fd12cee4b8dfed25369571ce97
Yury <olennikovyv@mail.ru>
2022-04-23 18:42:01 +0500

    Holiday VIII - Duty Free

--- src/js/event.js
+++ src/js/event.js
@@ -1,22 +1,7 @@
-function Fighter(name, health, damagePerAttack) {
-    this.name = name;
-    this.health = health;
-    this.damagePerAttack = damagePerAttack;
-    this.toString = function() { return this.name; }
+function dutyFree(normPrice, discount, hol) {
+    return Math.floor((hol * 100) / (normPrice * discount))
 }
 
-function declareWinner(fighter1, fighter2, firstAttacker) {
-    const countRound1 = Math.ceil(fighter1.health / fighter2.damagePerAttack);
-    const countRound2 = Math.ceil(fighter2.health / fighter1.damagePerAttack);
-
-    return countRound1 > countRound2 ?
-        fighter1.name :
-        countRound2 > countRound1 ? fighter2.name : firstAttacker;
-}
-
-console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew")); // "Lew"
-console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry")); // "Harry"
-console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry")); // "Harald"
-console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald")); // "Harald"
-console.log(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry")); // "Harald"
-console.log(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald")); // "Harald"
+console.log(dutyFree(12, 50, 1000)); //  166
+console.log(dutyFree(17, 10, 500)); //  294
+console.log(dutyFree(24, 35, 3000)); //  357

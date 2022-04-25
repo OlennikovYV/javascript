b43c41cd8438991ee1e2d6ab69eb4cf0c60d4da5
Yury <olennikovyv@mail.ru>
2022-04-25 08:36:52 +0500

    Multiplication table for number

--- src/js/event.js
+++ src/js/event.js
@@ -1,26 +1,12 @@
-"use strict";
+function multiTable(number) {
+    let result = '';
 
-function flattenAndSort(array) {
-    // Good luck, brave code warrior!
-    return [].concat(...array).sort((a, b) => a - b);
+    for (let i = 1; i <= 10; i++) {
+        result += i + ' * ' + number + ' = ' + i * number + '\n';
+    }
+
+    return result.slice(0, result.length - 1);
 }
 
-console.log(flattenAndSort([])); //  []
-console.log(flattenAndSort([
-    [],
-    []
-])); //  []
-console.log(flattenAndSort([
-    [],
-    [1]
-])); //  [1]
-console.log(flattenAndSort([
-    [3, 2, 1],
-    [7, 9, 8],
-    [6, 4, 5]
-])); //  [1, 2, 3, 4, 5, 6, 7, 8, 9]
-console.log(flattenAndSort([
-    [1, 3, 5],
-    [100],
-    [2, 4, 6]
-])); //  [1, 2, 3, 4, 5, 6, 100]
+console.log(multiTable(5)); //  '1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50'
+console.log(multiTable(1)); //  '1 * 1 = 1\n2 * 1 = 2\n3 * 1 = 3\n4 * 1 = 4\n5 * 1 = 5\n6 * 1 = 6\n7 * 1 = 7\n8 * 1 = 8\n9 * 1 = 9\n10 * 1 = 10'

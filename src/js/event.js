658b1eda171e45fa6125026957787b61e57aaad1
Yury <olennikovyv@mail.ru>
2022-04-26 11:03:34 +0500

    Reverse or rotate? refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,36 +1,25 @@
 function revrot(str, sz) {
-    let result = [];
-    const calculateSumCubes = arr => arr.reduce((acc, el) => acc + el ** 3, 0);
+    const calculateSumCubes = array => {
+        return array
+            .split('')
+            .reduce((acc, el) => acc + (+el) ** 3, 0);
+    }
     const checkEven = num => num % 2 === 0;
+    const reverseStr = str => str.split('').reverse().join('');
+    const rotateStr = str => str.slice(1) + str.slice(0, 1);
 
-    if (str === '' || sz === 0 || str.length < sz) return '';
+    if (sz < 1 || str.length < sz) return '';
 
-    str = Array.from(str, Number);
-
-    for (let i = 0; i < str.length; i += sz) {
-        result.push(str.slice(i, i + sz));
-    }
-
-    if (result[result.length - 1].length < sz) {
-        result.pop();
-    }
-
-    for (let i = 0; i < result.length; i++) {
-        if (checkEven(calculateSumCubes(result[i]))) {
-            result[i].reverse();
-        } else {
-            result[i].push(result[i].shift());
-        }
-        result[i] = result[i].join('');
-    }
-
-    return result.join('');
+    return str
+        .match(new RegExp('\\d{' + sz + '}', 'g'))
+        .map(el => checkEven(calculateSumCubes(el)) ? reverseStr(el) : rotateStr(el))
+        .join('');
 }
 
 console.log(revrot("1234", 0)); //  "")
 console.log(revrot("", 0)); //  "")
 console.log(revrot("1234", 5)); // "")
-console.table(revrot("733049910872815764", 5)); //  "330479108928157")
+console.log(revrot("733049910872815764", 5)); //  "330479108928157")
 console.log(revrot("123456987654", 6)); //  "234561876549"
 console.log(revrot("123456987653", 6)); //  "234561356789"
 console.log(revrot("66443875", 4)); //  "44668753"

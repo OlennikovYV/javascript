33102875e44840edd300fcced9f87e67318ee3e2
Yury <olennikovyv@mail.ru>
2022-04-29 10:03:13 +0500

    Training JS #4: Basic data types--Array

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,21 @@
-function alphanumeric(string) {
-    return /^[a-z\d]+$/i.test(string);
+const getLength = arr => arr.length;
+
+const getFirst = arr => arr[0];
+
+const getLast = arr => arr[arr.length - 1];
+
+const pushElement = arr => {
+    arr.push(1);
+    return arr;
 }
 
-console.log(alphanumeric("Mazinkaiser")); //  true
-console.log(alphanumeric("hello world_")); //  false
-console.log(alphanumeric("PassW0rd")); //  true
-console.log(alphanumeric("     ")); //  false
+const popElement = arr => {
+    arr.pop();
+    return arr;
+}
+
+console.log(getLength([1, 2, 3])); // 3
+console.log(getFirst([1, 2, 3])); // 1
+console.log(getLast([1, 2, 3])); // 3
+console.log(pushElement([1, 2, 3]).length); // 4
+console.log(popElement([1, 2, 3]).length); // 2

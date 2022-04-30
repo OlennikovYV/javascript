84e7b8fffb2cbe7bedcbc4283e1f18f3150ce044
Yury <olennikovyv@mail.ru>
2022-04-30 21:53:20 +0500

    Encrypt this!

--- src/js/event.js
+++ src/js/event.js
@@ -1,27 +1,31 @@
-function greet(language) {
-    const welcome = {
-        english: 'Welcome',
-        czech: 'Vitejte',
-        danish: 'Velkomst',
-        dutch: 'Welkom',
-        estonian: 'Tere tulemast',
-        finnish: 'Tervetuloa',
-        flemish: 'Welgekomen',
-        french: 'Bienvenue',
-        german: 'Willkommen',
-        irish: 'Failte',
-        italian: 'Benvenuto',
-        latvian: 'Gaidits',
-        lithuanian: 'Laukiamas',
-        polish: 'Witamy',
-        spanish: 'Bienvenido',
-        swedish: 'Valkommen',
-        welsh: 'Croeso',
-    };
+var encryptThis = function(text) {
+    const encodeWord = word => {
+        const charOne = word.charCodeAt(0);
+        const length = word.length;
+        if (length < 3) {
+            return charOne + word.slice(1);
+        } else {
+            const charTwo = word.slice(-1);
+            const charLast = word[1];
+            return charOne + charTwo + word.slice(2, -1) + charLast;
+        }
+    }
 
-    return welcome[language] || welcome['english'];
+    return text
+        .split(' ')
+        .map(word => encodeWord(word))
+        .join(' ');
 }
 
-console.log(greet('english')); //  'Welcome'
-console.log(greet('dutch')); //  'Welkom'
-console.log(greet('IP_ADDRESS_INVALID')); //  'Welcome'
+console.log(encryptThis("A"));
+// "65"
+console.log(encryptThis("A wise old owl lived in an oak"));
+// "65 119esi 111dl 111lw 108dvei 105n 97n 111ka"
+console.log(encryptThis("The more he saw the less he spoke"));
+// "84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp"
+console.log(encryptThis("The less he spoke the more he heard"));
+// "84eh 108sse 104e 115eokp 116eh 109ero 104e 104dare"
+console.log(encryptThis("Why can we not all be like that wise old bird"));
+// "87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri"
+console.log(encryptThis("Thank you Piotr for all your help"));
+// "84kanh 121uo 80roti 102ro 97ll 121ruo 104ple"

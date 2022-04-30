292cb7e9db2d7f6cadacbc371efc7d9daa041956
Yury <olennikovyv@mail.ru>
2022-04-30 22:09:06 +0500

    Quarter of the year

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,26 @@
-function toBinary(n) {
-    return +n.toString(2);
+const quarterOf = (month) => {
+    let quarter;
+
+    switch (month) {
+        case 1:
+        case 2:
+        case 3:
+            return 1;
+        case 4:
+        case 5:
+        case 6:
+            return 2;
+        case 7:
+        case 8:
+        case 9:
+            return 3;
+        case 10:
+        case 11:
+        case 12:
+            return 4;
+    }
 }
 
-console.log(toBinary(1)); //  1
-console.log(toBinary(2)); //  10
-console.log(toBinary(3)); //  11
-console.log(toBinary(5)); //  101
+console.log(quarterOf(3)); //  1
+console.log(quarterOf(8)); //  3
+console.log(quarterOf(11)); //  4

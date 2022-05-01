5f4f09621780a28669857e78dfc9484afb9e2085
Yury <olennikovyv@mail.ru>
2022-05-01 20:30:41 +0500

    Training JS #2: Basic data types--Number

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,42 @@
-function testEven(n) {
-    return n % 2 === 0
+let v1 = 50;
+let v2 = 100;
+let v3 = 150;
+let v4 = 200;
+let v5 = 2;
+let v6 = 250;
+
+function equal1() {
+    let a = v1
+    let b = v1
+    return a + b;
 }
 
-console.log(testEven(0)); //  true, "testEven for 0"
-console.log(testEven(0.5)); //  false, "testEven for 0.5"
-console.log(testEven(1)); //  false, "testEven for 1"
-console.log(testEven(2)); //  true, "testEven for 2"
-console.log(testEven(-4)); //  true, "testEven for 2"
+function equal2() {
+    let a = v4;
+    let b = v2;
+    return a - b;
+}
+
+function equal3() {
+    let a = v1;
+    let b = v5;
+    return a * b;
+}
+
+function equal4() {
+    let a = v4;
+    let b = v5;
+    return a / b;
+}
+
+function equal5() {
+    let a = v6;
+    let b = v3;
+    return a % b;
+}
+
+console.log(equal1()); // 100,"value of a+b is not equal to 100"
+console.log(equal2()); // 100,"value of a-b is not equal to 100"
+console.log(equal3()); // 100,"value of a*b is not equal to 100"
+console.log(equal4()); // 100,"value of a/b is not equal to 100"
+console.log(equal5()); // 100,"value of a%b is not equal to 100"

494569a92f850712a2f00493733d93e7e9b5e913
Yury <olennikovyv@mail.ru>
2022-05-01 20:36:58 +0500

    Sum of a sequence

--- src/js/event.js
+++ src/js/event.js
@@ -1,42 +1,14 @@
-let v1 = 50;
-let v2 = 100;
-let v3 = 150;
-let v4 = 200;
-let v5 = 2;
-let v6 = 250;
+const sequenceSum = (begin, end, step) => {
+    let sum = 0;
 
-function equal1() {
-    let a = v1
-    let b = v1
-    return a + b;
-}
+    if (begin > end) return 0;
 
-function equal2() {
-    let a = v4;
-    let b = v2;
-    return a - b;
-}
+    for (let i = begin; i <= end; i += step)
+        sum += i;
 
-function equal3() {
-    let a = v1;
-    let b = v5;
-    return a * b;
-}
+    return sum;
+};
 
-function equal4() {
-    let a = v4;
-    let b = v5;
-    return a / b;
-}
-
-function equal5() {
-    let a = v6;
-    let b = v3;
-    return a % b;
-}
-
-console.log(equal1()); // 100,"value of a+b is not equal to 100"
-console.log(equal2()); // 100,"value of a-b is not equal to 100"
-console.log(equal3()); // 100,"value of a*b is not equal to 100"
-console.log(equal4()); // 100,"value of a/b is not equal to 100"
-console.log(equal5()); // 100,"value of a%b is not equal to 100"
+console.log(sequenceSum(2, 6, 2)); //  12
+console.log(sequenceSum(1, 5, 1)); //  15
+console.log(sequenceSum(1, 5, 3)); //  5

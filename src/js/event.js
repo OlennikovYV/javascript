293bcb13b91513eed6ac131563c254fa2d1b7f37
Yury <olennikovyv@mail.ru>
2022-05-02 16:22:41 +0500

    Grasshopper - Basic Function Fixer

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,8 @@
-function factorial(n) {
-    if (n < 0 || n > 12)
-        throw RangeError('RangeError');
-
-    return n ? n * factorial(n - 1) : 1;
+function addFive(num) {
+    const total = num + 5;
+    return total;
 }
 
-console.log(factorial(0)); //  1, "factorial for 0 is 1"
-console.log(factorial(1)); //  1, "factorial for 1 is 1"
-console.log(factorial(2)); //  2, "factorial for 2 is 2"
-console.log(factorial(3)); //  6, "factorial for 3 is 6"
+console.log(addFive(5)); //  10
+console.log(addFive(0)); //  5
+console.log(addFive(-5)); //  0

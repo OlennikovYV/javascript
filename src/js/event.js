abd1bd5480a7f9203d9a752488a6930f72ae96d0
Yury <olennikovyv@mail.ru>
2022-05-02 16:01:06 +0500

    Filter out the geese

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,9 @@
-function hexToDec(hexString) {
-    return parseInt(hexString, 16);
-}
+function gooseFilter(birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
 
-console.log(hexToDec("1")); //  1
-console.log(hexToDec("a")); //  10
-console.log(hexToDec("10")); //  16
-console.log(hexToDec("FF")); //  255
-console.log(hexToDec("-C")); //  -12
+    return birds.filter(el => !geese.includes(el));
+};
+
+console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"])); // ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+console.log(gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"])); // ["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]
+console.log(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"])); // []

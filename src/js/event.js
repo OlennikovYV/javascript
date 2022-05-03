ad9374fd9e012634599f5a6c210de16bc4976cf4
Yury <olennikovyv@mail.ru>
2022-05-03 21:26:09 +0500

    Sum of pairs

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,33 @@
-function warnTheSheep(queue) {
-    const wolf = queue.reverse().indexOf('wolf');
+function sumPairs(ints, s) {
+    const nums = {};
+    let minIndex = ints.length;
+    let pairs = [];
 
-    return wolf === 0 ?
-        'Pls go away and stop eating my sheep' :
-        `Oi! Sheep number ${queue.indexOf('wolf')}! You are about to be eaten by a wolf!`;
+    for (let i = 0; i < ints.length; i++) {
+        nums[ints[i]] = i;
+    }
+
+    for (let i = 0; i < ints.length; i++) {
+        const find = s - ints[i];
+
+        if (i !== nums[find])
+            if (nums[find]) {
+                let maxIndex = i > nums[find] ? i : nums[find]
+                if (maxIndex < minIndex) {
+                    pairs = [ints[i], find];
+                    minIndex = maxIndex;
+                }
+            }
+    }
+
+    return !pairs.length ? undefined : pairs;
 }
 
-console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"])); // "Oi! Sheep number 2! You are about to be eaten by a wolf!"
-console.log(warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"])); // "Oi! Sheep number 5! You are about to be eaten by a wolf!"
-console.log(warnTheSheep(["wolf", "sheep", "sheep", "sheep", "sheep", "sheep", "sheep"])); // "Oi! Sheep number 6! You are about to be eaten by a wolf!"
-console.log(warnTheSheep(["sheep", "wolf", "sheep"])); // "Oi! Sheep number 1! You are about to be eaten by a wolf!"
-console.log(warnTheSheep(["wolf"])); // "Pls go away and stop eating my sheep"
-console.log(warnTheSheep(["sheep", "sheep", "wolf"])); // "Pls go away and stop eating my sheep"
+console.log(sumPairs([1, 4, 8, 7, 3, 15], 8)); //  [1, 7], "Basic: [1, 4, 8, 7, 3, 15] should return [1, 7] for sum = 8"
+console.log(sumPairs([1, -2, 3, 0, -6, 1], -6)); //  [0, -6], "Negatives: [1, -2, 3, 0, -6, 1] should return [0, -6] for sum = -6"
+console.log(sumPairs([20, -13, 40], -7)); //  undefined, "No Match: [20, -13, 40] should return undefined for sum = -7"
+console.log(sumPairs([1, 2, 3, 4, 1, 0], 2)); //  [1, 1], "First Match From Left: [1, 2, 3, 4, 1, 0] should return [1, 1] for sum = 2"
+console.log(sumPairs([10, 5, 2, 3, 7, 5], 10)); //  [3, 7], "First Match From Left REDUX!: [10, 5, 2, 3, 7, 5] should return [3, 7] for sum = 10"
+console.log(sumPairs([4, -2, 3, 3, 4], 8)); //  [4, 4], "Duplicates: [4, -2, 3, 3, 4] should return [4, 4] for sum = 8"
+console.log(sumPairs([0, 2, 0], 0)); //  [0, 0], "Zeroes: [0, 2, 0] should return [0, 0] for sum = 0"
+console.log(sumPairs([5, 9, 13, -3], 10)); //  [13, -3], "Subtraction: [5, 9, 13, -3] should return [13, -3] for sum = 10"

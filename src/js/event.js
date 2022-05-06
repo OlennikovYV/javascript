d4d34931d09a8d3c3bfc7af7cec102c9483dd2f0
Yury <olennikovyv@mail.ru>
2022-05-06 09:40:37 +0500

    Data Reverse

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,18 @@
-function squareOrSquareRoot(array) {
-    return array.map(el => Math.sqrt(el) % 1 > 0 ? el ** 2 : Math.sqrt(el));
+function dataReverse(data) {
+    const resultArray = [];
+
+    for (let i = 0; i < data.length; i += 8) {
+        resultArray.push(data.slice(i, i + 8));
+    }
+
+    return resultArray.reverse().reduce((acc, el) => acc.concat(el), []);
 }
 
-console.log(squareOrSquareRoot([4, 3, 9, 7, 2, 1])); // [ 2, 9, 3, 49, 4, 1 ]
-console.log(squareOrSquareRoot([100, 101, 5, 5, 1, 1])); // [ 10, 10201, 25, 25, 1, 1 ]
-console.log(squareOrSquareRoot([1, 2, 3, 4, 5, 6])); // [ 1, 4, 9, 2, 25, 36 ]
+console.log(dataReverse([1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0]));
+// 
+console.log(dataReverse([1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]));
+// 
+console.log(dataReverse([0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1]));
+// 
+console.log(dataReverse([0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0]));
+//

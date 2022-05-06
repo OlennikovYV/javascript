bae231fa4c3dc4b0a8dbaeff6324964b059efd8b
Yury <olennikovyv@mail.ru>
2022-05-06 09:45:54 +0500

    Data Reverse refactor

--- src/js/event.js
+++ src/js/event.js
@@ -2,17 +2,17 @@ function dataReverse(data) {
     const resultArray = [];
 
     for (let i = 0; i < data.length; i += 8) {
-        resultArray.push(data.slice(i, i + 8));
+        resultArray.unshift(...data.slice(i, i + 8));
     }
 
-    return resultArray.reverse().reduce((acc, el) => acc.concat(el), []);
+    return resultArray;
 }
 
 console.log(dataReverse([1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0]));
-// 
+// [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
 console.log(dataReverse([1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]));
-// 
+// [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0]
 console.log(dataReverse([0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1]));
-// 
+// [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0]
 console.log(dataReverse([0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0]));
-//
+// [0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1]

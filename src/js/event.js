b717b09b8382805cb6b2aabfb7cea62f72b9b880
Yury <olennikovyv@mail.ru>
2022-05-10 17:00:48 +0500

    Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,22 @@
-function hello(name) {
-    return name ?
-        `Hello, ${name[0].toUpperCase()+name.slice(1).toLowerCase()}!` :
-        `Hello, World!`;
+function sumDigPow(a, b) {
+    const result = [];
+    const checkSumDigPow = num => {
+        const arrayDigit = String(num).split('').map(dig => +dig);
+        return num === arrayDigit.reduce((sum, dig, pow) => sum + dig ** ++pow, 0);
+    }
+
+    for (let i = a; i <= b; i++) {
+        if (checkSumDigPow(i))
+            result.push(i);
+    }
+
+    return result;
 }
 
-console.log(hello('alice')); //  'Hello, Alice!', "returns 'Hello, Alice!' when given 'alice'"
-console.log(hello()); //  'Hello, World!', "returns 'Hello, World!' when name is not given"
-console.log(hello('')); //  'Hello, World!', "returns 'Hello, World!' when name is an empty string"
+console.log(sumDigPow(1, 10)); //  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(sumDigPow(1, 100)); //  [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+console.log(sumDigPow(10, 100)); //   [89]
+console.log(sumDigPow(90, 100)); //  []
+console.log(sumDigPow(90, 150)); //  [135]
+console.log(sumDigPow(50, 150)); //  [89, 135]
+console.log(sumDigPow(10, 150)); //  [89, 135]

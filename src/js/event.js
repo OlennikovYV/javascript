a8baa9a140237cd01c2860bba8421d8bf9f0d810
Yury <olennikovyv@mail.ru>
2022-05-11 09:07:56 +0500

    Bin to Decimal

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,7 @@
-function rainAmount(mm) {
-    if (mm < 40) {
-        return `You need to give your plant ${40 - mm}mm of water`
-    } else {
-        return `Your plant has had more than enough water for today!`
-    };
+function binToDec(bin) {
+    return parseInt(bin, 2);
 }
 
-console.log(rainAmount(100)); //  "Your plant has had more than enough water for today!"
-console.log(rainAmount(39)); //  "You need to give your plant 1mm of water"
+console.log(binToDec("0000001")); // 1
+console.log(binToDec("0000111")); // 7
+console.log(binToDec("1001001")); // 73

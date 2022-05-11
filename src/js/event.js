e17a58c5f5a491cbe33ac0b3d080590bf9a6f74d
Yury <olennikovyv@mail.ru>
2022-05-11 09:02:09 +0500

    Fix your code before the garden dies!

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,10 @@
-function printArray(array) {
-    return array.join(',');
+function rainAmount(mm) {
+    if (mm < 40) {
+        return `You need to give your plant ${40 - mm}mm of water`
+    } else {
+        return `Your plant has had more than enough water for today!`
+    };
 }
 
-console.log(printArray(["h", "o", "l", "a"])); // "h,o,l,a"
+console.log(rainAmount(100)); //  "Your plant has had more than enough water for today!"
+console.log(rainAmount(39)); //  "You need to give your plant 1mm of water"

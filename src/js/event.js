6084c78aa51f10ca0cf1f0f11d5fa17c62b2b6a1
Yury <olennikovyv@mail.ru>
2022-05-12 09:44:56 +0500

    Determine offspring sex based on genes XX and XY chromosomes

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,6 @@
-function problem(x) {
-    return typeof x === "number" ? x * 50 + 6 : 'Error';
+function chromosomeCheck(sperm) {
+    return `Congratulations! You're going to have a ${sperm === 'XY' ? 'son' : 'daughter'}.`;
 }
 
-console.log(problem("hello")); //  "Error"
-console.log(problem(1)); //  56
-console.log(problem(5)); //  256
-console.log(problem(0)); //  6
-console.log(problem(1.2)); //  66
-console.log(problem(3)); //  156
-console.log(problem("RyanIsCool")); //  "Error"
-console.log(problem(-3)); //  -144
-console.log(problem("")); //  "Error"
-console.log(problem(0.03)); //  7.5
+console.log(chromosomeCheck('XY')); //  "Congratulations! You're going to have a son."
+console.log(chromosomeCheck('XX')); //  "Congratulations! You're going to have a daughter."

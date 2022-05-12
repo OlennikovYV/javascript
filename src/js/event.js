7db9de7e1966849ac3a7b96d7de9c3e8c9193c9f
Yury <olennikovyv@mail.ru>
2022-05-12 08:46:14 +0500

    Super Duper Easy refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,5 @@
 function problem(x) {
-    return typeof x === "string" ? 'Error' : x * 50 + 6;
+    return typeof x === "number" ? x * 50 + 6 : 'Error';
 }
 
 console.log(problem("hello")); //  "Error"

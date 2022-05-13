97a6cbb61ae09e9551f5d9bcd36134fe3e9c6bbe
Yury <olennikovyv@mail.ru>
2022-05-13 08:34:58 +0500

    Pre-FizzBuzz Workout #1

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,9 @@
-function mergeArrays(arr1, arr2) {
-    return [...new Set(arr1.concat(arr2).sort((a, b) => a - b))];
+function preFizz(n) {
+    return Array.from(Array(n), (el, i) => ++i);
 }
 
-console.log(mergeArrays([1, 2, 3, 4], [5, 6, 7, 8])); //  [1,2,3,4,5,6,7,8]
-console.log(mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2])); //  [1,2,3,4,5,6,7,8,9,10]
-console.log(mergeArrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12])); //  [1,2,3,4,5,7,9,10,11,12]
+console.log(preFizz(1)); //  [1]
+console.log(preFizz(2)); //  [1,2]
+console.log(preFizz(3)); //  [1,2,3]
+console.log(preFizz(4)); //  [1,2,3,4]
+console.log(preFizz(5)); //  [1,2,3,4,5]

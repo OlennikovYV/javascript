c6fc7c2d178d12c05d1b9b26f5d0580d3f5dd9b5
Yury <olennikovyv@mail.ru>
2022-05-14 16:36:14 +0500

    Predict your age!

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,11 @@
-function unusualFive() {
-    return 'i'.charCodeAt() % 'd'.charCodeAt();
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+    let result = Array.from(arguments);
+    result = result.map(age => age * age);
+    result = result.reduce((sum, age) => sum + age);
+    result = Math.sqrt(result);
+    result = result / 2;
+
+    return Math.floor(result);
 }
 
-console.log(unusualFive()); // 5
+console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45)); // 86

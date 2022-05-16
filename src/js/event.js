51ce7eb7754d109a28c60d6770829a4c10d7652a
Yury <olennikovyv@mail.ru>
2022-05-16 08:50:52 +0500

    Find the unique number

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,16 @@
-function replace(s) {
-    return s.replace(/[aeiou]/gi, '!');
+function findUniq(arr) {
+    let result = arr;
+    result = result.sort((a, b) => a - b);
+
+    return result[0] !== result[1] ?
+        result[0] :
+        result[result.length - 1];
 }
 
-console.log(replace("Hi!")); //  "H!!"
-console.log(replace("!Hi! Hi!")); //  "!H!! H!!"
-console.log(replace("aeiou")); //  "!!!!!"
-console.log(replace("ABCDE")); //  "!BCD!"
+console.log(findUniq([0.55, 0, 0])); //  0.55
+console.log(findUniq([1, 0, 0])); //  1
+console.log(findUniq([0, 1, 0])); //  1
+console.log(findUniq([0, 0, 1])); //  1
+console.log(findUniq([1, 1, 1, 2, 1, 1])); //  2
+console.log(findUniq([1, 1, 2, 1, 1])); //  2
+console.log(findUniq([3, 10, 3, 3, 3])); //  10

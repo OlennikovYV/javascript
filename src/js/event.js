9cd72efa420597d62e59a465610ed335dd9180fd
Yury <olennikovyv@mail.ru>
2022-05-16 08:42:36 +0500

    Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,8 @@
-function remove(string) {
-    return string.replace(/\!/g, '') + '!';
+function replace(s) {
+    return s.replace(/[aeiouAEIOU]/g, '!');
 }
 
-console.log(remove("Hi!")); //  "Hi!"
-console.log(remove("Hi!!!")); //  "Hi!"
-console.log(remove("!Hi")); //  "Hi!"
-console.log(remove("!Hi!")); //  "Hi!"
-console.log(remove("Hi! Hi!")); //  "Hi Hi!"
-console.log(remove("Hi")); //  "Hi!"
+console.log(replace("Hi!")); //  "H!!"
+console.log(replace("!Hi! Hi!")); //  "!H!! H!!"
+console.log(replace("aeiou")); //  "!!!!!"
+console.log(replace("ABCDE")); //  "!BCD!"

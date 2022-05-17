d56b261a4d17dc88ff08e104029d9d3d36841585
Yury <olennikovyv@mail.ru>
2022-05-17 09:07:46 +0500

    Multiple of index

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,10 @@
-function derive(coefficient, exponent) {
-    return `${coefficient * exponent}x^${exponent - 1}`;
+function multipleOfIndex(array) {
+    return array.filter((el, i) => el % i === 0);
 }
 
-console.log(derive(7, 8)); //  "56x^7"
-console.log(derive(5, 9)); //  "45x^8"
+console.log(multipleOfIndex([22, -6, 32, 82, 9, 25])); //  [-6, 32, 25]
+console.log(multipleOfIndex([68, -1, 1, -7, 10, 10])); //  [-1, 10]
+console.log(multipleOfIndex([11, -11])); //  [-11]
+console.log(multipleOfIndex([-56, -85, 72, -26, -14, 76, -27, 72, 35, -21, -67, 87, 0, 21, 59, 27, -92, 68])); //  [-85, 72, 0, 68]
+console.log(multipleOfIndex([28, 38, -44, -99, -13, -54, 77, -51])); //  [38, -44, -99]
+console.log(multipleOfIndex([-1, -49, -1, 67, 8, -60, 39, 35])); //  [-49, 8, -60, 35]

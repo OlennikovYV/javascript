ee8a03ba2da1756e97e8e9c533158637d3331afa
Yury <olennikovyv@mail.ru>
2022-05-17 16:35:46 +0500

    Cat years, Dog years

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,20 @@
-function multipleOfIndex(array) {
-    return array.filter((el, i) => el % i === 0);
+var humanYearsCatYearsDogYears = function(humanYears) {
+    const calcYearsAnimal = (yearsHuman, animal) => {
+        if (yearsHuman < 1) return 0;
+        if (yearsHuman === 1) return 15;
+        if (yearsHuman === 2) return 24;
+
+        return 24 + (yearsHuman - 2) * (animal === 'cat' ? 4 : 5);
+    }
+
+
+    return [
+        humanYears,
+        calcYearsAnimal(humanYears, 'cat'),
+        calcYearsAnimal(humanYears, 'dog'),
+    ];
 }
 
-console.log(multipleOfIndex([22, -6, 32, 82, 9, 25])); //  [-6, 32, 25]
-console.log(multipleOfIndex([68, -1, 1, -7, 10, 10])); //  [-1, 10]
-console.log(multipleOfIndex([11, -11])); //  [-11]
-console.log(multipleOfIndex([-56, -85, 72, -26, -14, 76, -27, 72, 35, -21, -67, 87, 0, 21, 59, 27, -92, 68])); //  [-85, 72, 0, 68]
-console.log(multipleOfIndex([28, 38, -44, -99, -13, -54, 77, -51])); //  [38, -44, -99]
-console.log(multipleOfIndex([-1, -49, -1, 67, 8, -60, 39, 35])); //  [-49, 8, -60, 35]
+console.log(humanYearsCatYearsDogYears(1)); //  [1,15,15]
+console.log(humanYearsCatYearsDogYears(2)); //  [2,24,24]
+console.log(humanYearsCatYearsDogYears(10)); //  [10,56,64]

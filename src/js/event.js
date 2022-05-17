cf392e6809ed7f8e805c2eda624645b2ab2ce05c
Yury <olennikovyv@mail.ru>
2022-05-17 09:04:04 +0500

    Take the Derivative

--- src/js/event.js
+++ src/js/event.js
@@ -1,27 +1,6 @@
-function vertMirror(strng) {
-    const result = strng.split('\n');
-
-    return result
-        .map(line => line
-            .split('')
-            .reverse()
-            .join(''))
-        .join('\n');
+function derive(coefficient, exponent) {
+    return `${coefficient * exponent}x^${exponent - 1}`;
 }
 
-function horMirror(strng) {
-    const result = strng.split('\n');
-
-    return result
-        .reverse()
-        .join('\n');
-}
-
-function oper(fct, s) {
-    return fct(s);
-}
-
-console.log(oper(vertMirror, "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu")); //  "QHdgSh\noaMDnH\nXxNNlC\nHxxvRi\nAvVTqb\nuRySvw"
-console.log(oper(vertMirror, "IzOTWE\nkkbeCM\nWuzZxM\nvDddJw\njiJyHF\nPVHfSx")); //  "EWTOzI\nMCebkk\nMxZzuW\nwJddDv\nFHyJij\nxSfHVP"
-console.log(oper(horMirror, "lVHt\nJVhv\nCSbg\nyeCt")); //  "yeCt\nCSbg\nJVhv\nlVHt"
-console.log(oper(horMirror, "njMK\ndbrZ\nLPKo\ncEYz")); //  "cEYz\nLPKo\ndbrZ\nnjMK"
+console.log(derive(7, 8)); //  "56x^7"
+console.log(derive(5, 9)); //  "45x^8"

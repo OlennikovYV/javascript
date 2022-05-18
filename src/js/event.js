743b8df37ebc58b2a71765a407b5ebb12a2ff38d
Yury <olennikovyv@mail.ru>
2022-05-18 08:42:26 +0500

    Small enough? - Beginner

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,8 @@
-function sumDigits(number) {
-    return String(Math.abs(number))
-        .split('')
-        .reduce((sum, str) => sum + Number(str), 0);
+function smallEnough(a, limit) {
+    return !a.filter(el => el > limit).length;
 }
 
-console.log(sumDigits(10)); //  1
-console.log(sumDigits(99)); //  18
-console.log(sumDigits(-32)); //  5
+console.log(smallEnough([66, 101], 200)); //  true
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)); //  false
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); //  true
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120)); //  true

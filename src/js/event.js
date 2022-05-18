abe360cc55b1ba62ac05ae389d07f2cca7f2213c
Yury <olennikovyv@mail.ru>
2022-05-18 08:47:58 +0500

    Thinkful - String Drills: Repeater

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,7 @@
-function smallEnough(a, limit) {
-    return Math.max(...a) <= limit;
+function repeater(string, n) {
+    return string.repeat(n);
 }
 
-console.log(smallEnough([66, 101], 200)); //  true
-console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)); //  false
-console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); //  true
-console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120)); //  true
+console.log(repeater('a', 5)); //  'aaaaa
+console.log(repeater('Na', 16)); //  'NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa
+console.log(repeater('Wub ', 6)); //  'Wub Wub Wub Wub Wub Wub

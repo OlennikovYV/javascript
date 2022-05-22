72ba786cfefcbf37a64870399d323cf8404e8e55
Yury <olennikovyv@mail.ru>
2022-05-22 21:47:08 +0500

    Simple Fun #176: Reverse Letter

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,12 @@
-function pipeFix(numbers) {
-    const result = [];
-
-    for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++)
-        result.push(i);
-
-    return result;
+function reverseLetter(str) {
+    return str.replace(/[^a-z]/g, '')
+        .split('')
+        .reverse()
+        .join('');
 }
 
-console.log(pipeFix([1, 2, 3, 5, 6, 8, 9])); // [1,2,3,4,5,6,7,8,9]
-console.log(pipeFix([1, 2, 3, 12])); // [1,2,3,4,5,6,7,8,9,10,11,12]
-console.log(pipeFix([6, 9])); // [6,7,8,9]
-console.log(pipeFix([-1, 4])); // [-1,0,1,2,3,4]
-console.log(pipeFix([1, 2, 3])); // [1,2,3]
+console.log(reverseLetter("krishan")); // "nahsirk"
+console.log(reverseLetter("ultr53o?n")); // "nortlu"
+console.log(reverseLetter("ab23c")); // "cba"
+console.log(reverseLetter("krish21an")); // "nahsirk"
+console.log(reverseLetter("psj#dvbupoifdcvew)dnxqn,fkbrh!nofc5_fai/x")); // "xiafcfonhrbkfnqxndwevcdfiopubvdjsp"

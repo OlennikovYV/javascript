ba7e74b45d6055dc171690297ee3d095c5289d5b
Yury <olennikovyv@mail.ru>
2022-05-23 08:34:56 +0500

    Row Weights

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,25 @@
-function reverseLetter(str) {
-    return str
-        .match(/[a-z]/g)
-        .reverse()
-        .join('');
+function rowWeights(array) {
+    const result = [0, 0];
+    const checkEven = num => num % 2 === 0;
+
+    array.map((el, i) => {
+        if (checkEven(i + 1)) {
+            result[1] += el;
+        } else {
+            result[0] += el;
+        }
+    })
+
+    return result;
 }
 
-console.log(reverseLetter("krishan")); // "nahsirk"
-console.log(reverseLetter("ultr53o?n")); // "nortlu"
-console.log(reverseLetter("ab23c")); // "cba"
-console.log(reverseLetter("k_rish21an")); // "nahsirk"
+console.log(rowWeights([80])); //  [80,0]
+console.log(rowWeights([100, 50])); //  [100,50]
+console.log(rowWeights([50, 60, 70, 80])); //  [120,140]
+console.log(rowWeights([13, 27, 49])); //  [62,27]
+console.log(rowWeights([70, 58, 75, 34, 91])); //  [236,92]
+console.log(rowWeights([29, 83, 67, 53, 19, 28, 96])); //  [211,164]
+console.log(rowWeights([0])); //  [0,0]
+console.log(rowWeights([100, 51, 50, 100])); //  [150,151]
+console.log(rowWeights([39, 84, 74, 18, 59, 72, 35, 61])); //  [207,235]
+console.log(rowWeights([0, 1, 0])); //  [0,1]

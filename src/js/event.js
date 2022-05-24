66b23bdd515615a7154ece5df760241d0f40bba7
Yury <olennikovyv@mail.ru>
2022-05-24 12:40:38 +0500

    Tribonacci Sequence refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,11 @@
 function tribonacci(signature, n) {
     const [...result] = signature;
-    const countSum = 3;
-    let index = 3;
 
-    if (n === 0) return [];
-    if (n < 3) return signature.slice(0, n);
-
-    while (index < n) {
-        const signatureSubArray = result.slice(index - countSum, index + 1);
-        const nextElement = signatureSubArray.reduce((acc, el) => acc + el, 0);
-        result.push(nextElement);
-        index++;
+    for (let i = 0; i < n - 3; i++) {
+        result.push(result[i] + result[i + 1] + result[i + 2]);
     }
 
-    return result;
+    return result.slice(0, n);
 }
 
 console.log(tribonacci([1, 1, 1], 10)); // [1,1,1,3,5,9,17,31,57,105]

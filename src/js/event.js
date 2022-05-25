194331678301f7ef062f4659dc49f6f1697f173b
Yury <olennikovyv@mail.ru>
2022-05-25 10:32:27 +0500

    Expressions Matter

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,19 @@
-function validateUsr(username) {
-    res = /^[0-9a-z_]{4,16}$/.test(username)
-    return res
+function expressionMatter(a, b, c) {
+    let sums = [];
+
+    sums.push(a + b + c);
+    sums.push(a * b * c);
+    sums.push((a + b) * c);
+    sums.push(a + b * c);
+    sums.push(a * (b + c));
+    sums.push(a * b + c);
+
+    return Math.max(...sums);
 }
 
-console.log(validateUsr('asddsa')); //  true
-console.log(validateUsr('a')); //  false
-console.log(validateUsr('Hass')); //  false
-console.log(validateUsr('Hasd_12assssssasasasasasaasasasasas')); //  false
-console.log(validateUsr('')); //  false
-console.log(validateUsr('____')); //  true
-console.log(validateUsr('012')); //  false
-console.log(validateUsr('p1pp1')); //  true
-console.log(validateUsr('asd43 34')); //  false
-console.log(validateUsr('asd43_34')); //  true
+console.log(expressionMatter(2, 1, 2)); //  6
+console.log(expressionMatter(2, 1, 1)); //  4
+console.log(expressionMatter(1, 1, 1)); //  3
+console.log(expressionMatter(1, 2, 3)); //  9
+console.log(expressionMatter(1, 3, 1)); //  5
+console.log(expressionMatter(2, 2, 2)); //  8

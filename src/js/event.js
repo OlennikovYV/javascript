0c44f1d3c4513f7a2222f16a206088181aef989c
Yury <olennikovyv@mail.ru>
2022-05-26 21:04:38 +0500

    Find the position!

--- src/js/event.js
+++ src/js/event.js
@@ -1,23 +1,8 @@
-function getDrinkByProfession(param) {
-  const table = {
-    jabroni: 'Patron Tequila',
-    'school counselor': 'Anything with Alcohol',
-    programmer: 'Hipster Craft Beer',
-    'bike gang member': 'Moonshine',
-    politician: 'Your tax dollars',
-    rapper: 'Cristal',
-  };
-
-  const key = param.toLowerCase();
-
-  return table[key] ? table[key] : 'Beer';
+function position(letter) {
+  const BEGIN_ALPHABET = 96;
+  return `Position of alphabet: ${letter.charCodeAt(0) - BEGIN_ALPHABET}`;
 }
 
-console.log(getDrinkByProfession('jabrOni')); // 'Patron Tequila'
-console.log(getDrinkByProfession('scHOOl counselor')); // 'Anything with Alcohol'
-console.log(getDrinkByProfession('prOgramMer')); // 'Hipster Craft Beer'
-console.log(getDrinkByProfession('bike ganG member')); // 'Moonshine'
-console.log(getDrinkByProfession('poLiTiCian')); // 'Your tax dollars'
-console.log(getDrinkByProfession('rapper')); // 'Cristal'
-console.log(getDrinkByProfession('pundit')); // 'Beer'
-console.log(getDrinkByProfession('Pug')); // 'Beer'
+console.log(position('a')); // 'Position of alphabet: 1'
+console.log(position('z')); // 'Position of alphabet: 26'
+console.log(position('e')); // 'Position of alphabet: 5'

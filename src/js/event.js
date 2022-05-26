7bee058ef47588736d8404394dd7c8ccef5ffd2c
Yury <olennikovyv@mail.ru>
2022-05-26 17:42:49 +0500

    L1: Bartender, drinks!

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,23 @@
-function getSize(width, height, depth) {
-  const area = (width * height + width * depth + height * depth) * 2;
-  const volume = width * height * depth;
-  return [area, volume];
+function getDrinkByProfession(param) {
+  const table = {
+    jabroni: "Patron Tequila",
+    "school counselor": "Anything with Alcohol",
+    programmer: "Hipster Craft Beer",
+    "bike gang member": "Moonshine",
+    politician: "Your tax dollars",
+    rapper: "Cristal",
+  };
+
+  const key = param.toLowerCase();
+
+  return table[key] ? table[key] : "Beer";
 }
 
-console.log(getSize(4, 2, 6)); //  [88, 48]
-console.log(getSize(10, 10, 10)); //  [600, 1000]
+console.log(getDrinkByProfession("jabrOni")); // 'Patron Tequila'
+console.log(getDrinkByProfession("scHOOl counselor")); // 'Anything with Alcohol'
+console.log(getDrinkByProfession("prOgramMer")); // 'Hipster Craft Beer'
+console.log(getDrinkByProfession("bike ganG member")); // 'Moonshine'
+console.log(getDrinkByProfession("poLiTiCian")); // 'Your tax dollars'
+console.log(getDrinkByProfession("rapper")); // 'Cristal'
+console.log(getDrinkByProfession("pundit")); // 'Beer'
+console.log(getDrinkByProfession("Pug")); // 'Beer'

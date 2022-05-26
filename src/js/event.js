024e7d968e8f0ca6daa5bcdc88aaa760754a2a0d
Yury <olennikovyv@mail.ru>
2022-05-26 17:51:23 +0500

    Setting ESLint and Prettier

--- src/js/event.js
+++ src/js/event.js
@@ -1,23 +1,23 @@
 function getDrinkByProfession(param) {
   const table = {
-    jabroni: "Patron Tequila",
-    "school counselor": "Anything with Alcohol",
-    programmer: "Hipster Craft Beer",
-    "bike gang member": "Moonshine",
-    politician: "Your tax dollars",
-    rapper: "Cristal",
+    jabroni: 'Patron Tequila',
+    'school counselor': 'Anything with Alcohol',
+    programmer: 'Hipster Craft Beer',
+    'bike gang member': 'Moonshine',
+    politician: 'Your tax dollars',
+    rapper: 'Cristal',
   };
 
   const key = param.toLowerCase();
 
-  return table[key] ? table[key] : "Beer";
+  return table[key] ? table[key] : 'Beer';
 }
 
-console.log(getDrinkByProfession("jabrOni")); // 'Patron Tequila'
-console.log(getDrinkByProfession("scHOOl counselor")); // 'Anything with Alcohol'
-console.log(getDrinkByProfession("prOgramMer")); // 'Hipster Craft Beer'
-console.log(getDrinkByProfession("bike ganG member")); // 'Moonshine'
-console.log(getDrinkByProfession("poLiTiCian")); // 'Your tax dollars'
-console.log(getDrinkByProfession("rapper")); // 'Cristal'
-console.log(getDrinkByProfession("pundit")); // 'Beer'
-console.log(getDrinkByProfession("Pug")); // 'Beer'
+console.log(getDrinkByProfession('jabrOni')); // 'Patron Tequila'
+console.log(getDrinkByProfession('scHOOl counselor')); // 'Anything with Alcohol'
+console.log(getDrinkByProfession('prOgramMer')); // 'Hipster Craft Beer'
+console.log(getDrinkByProfession('bike ganG member')); // 'Moonshine'
+console.log(getDrinkByProfession('poLiTiCian')); // 'Your tax dollars'
+console.log(getDrinkByProfession('rapper')); // 'Cristal'
+console.log(getDrinkByProfession('pundit')); // 'Beer'
+console.log(getDrinkByProfession('Pug')); // 'Beer'

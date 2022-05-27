c1a84eaff6468ae6cc5ac6189e54f11278e0dcb2
Yury <olennikovyv@mail.ru>
2022-05-27 09:43:11 +0500

    Enumerable Magic #4 - True for None?

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,15 @@
-function validParentheses(parens) {
-  let countPairs = 0;
-  const [...arrayParens] = parens;
-
-  for (let i = 0; i < arrayParens.length; i += 1) {
-    if (arrayParens[i] === '(') countPairs += 1;
-    if (arrayParens[i] === ')') countPairs -= 1;
-    if (countPairs < 0) return false;
-  }
-
-  return countPairs === 0;
+function none(arr, fun) {
+  return arr.filter(fun).length === 0;
 }
 
-console.log(validParentheses('(')); //  false
-console.log(validParentheses(')')); //  false
-console.log(validParentheses('')); //  true
-console.log(validParentheses('()')); //  true
-console.log(validParentheses('())')); //  false
-console.log(validParentheses('())(')); //  false
+console.log(
+  none([1, 2, 3, 4, 5], function (item) {
+    return item > 5;
+  })
+); // true;
+
+console.log(
+  none([1, 2, 3, 4, 5], function (item) {
+    return item > 4;
+  })
+); // false;

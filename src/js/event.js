d9693b13b66e7a91fdaaaa450378a310b6405d04
Yury <olennikovyv@mail.ru>
2022-05-27 09:36:37 +0500

    Valid Parentheses

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,19 @@
-function solution(pairs) {
-  return Object.keys(pairs)
-    .map(key => `${key} = ${pairs[key]}`)
-    .join(',');
+function validParentheses(parens) {
+  let countPairs = 0;
+  const [...arrayParens] = parens;
+
+  for (let i = 0; i < arrayParens.length; i += 1) {
+    if (arrayParens[i] === '(') countPairs += 1;
+    if (arrayParens[i] === ')') countPairs -= 1;
+    if (countPairs < 0) return false;
+  }
+
+  return countPairs === 0;
 }
 
-console.log(solution({ a: 1, b: '2' })); // should return "a = 1,b = 2"
-console.log(solution({ a: 1 })); // should return "a = 1"
-console.log(solution({ a: 2, b: 4, c: '8' })); // should return "a = 2,b = 4,c = 8"
+console.log(validParentheses('(')); //  false
+console.log(validParentheses(')')); //  false
+console.log(validParentheses('')); //  true
+console.log(validParentheses('()')); //  true
+console.log(validParentheses('())')); //  false
+console.log(validParentheses('())(')); //  false

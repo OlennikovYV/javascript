42476b31e651d1ecb0954ccbd3657ae17677e7f6
Yury <olennikovyv@mail.ru>
2022-05-27 09:56:08 +0500

    Largest pair sum in array refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,9 @@
 function largestPairSum(numbers) {
-  let result = numbers[0] + numbers[1];
-  for (let i = 0; i < numbers.length; i += 1)
-    for (let j = i; j < numbers.length; j += 1) {
-      if (i !== j) {
-        const sumIJ = numbers[i] + numbers[j];
-        if (sumIJ > result) result = sumIJ;
-      }
-    }
+  const [...arrayNumbers] = numbers;
 
-  return result;
+  arrayNumbers.sort((a, b) => b - a);
+
+  return arrayNumbers[0] + arrayNumbers[1];
 }
 
 console.log(largestPairSum([10, 14, 2, 23, 19])); //  42

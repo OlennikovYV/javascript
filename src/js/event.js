f68e4d4c38f2d91211912a2050bfbb655f16bfe1
Yury <olennikovyv@mail.ru>
2022-05-28 17:56:18 +0500

    Training JS #3: Basic data types--String

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,39 @@
-function peopleWithAgeDrink(old) {
-  if (old < 14) return 'drink toddy';
-  if (old < 18) return 'drink coke';
-  if (old < 21) return 'drink beer';
-  return 'drink whisky';
+var a1 = 'A',
+  a2 = 'a',
+  b1 = 'B',
+  b2 = 'b',
+  c1 = 'C',
+  c2 = 'c',
+  d1 = 'D',
+  d2 = 'd',
+  e1 = 'E',
+  e2 = 'e',
+  n1 = 'N',
+  n2 = 'n';
+
+function Dad() {
+  //select some variable to combine "Dad"
+  return d1 + a2 + d2;
+}
+function Bee() {
+  //select some variable to combine "Bee"
+  return b1 + e2.repeat(2);
+}
+function banana() {
+  //select some variable to combine "banana"
+  return b2 + a2 + n2 + a2 + n2 + a2;
 }
 
-console.log(peopleWithAgeDrink(22)); // 'drink whisky'
+//answer some questions if you finished works above
+function answer1() {
+  //the answer should be "yes" or "no"
+  return 'no';
+}
+function answer2() {
+  //the answer should be "yes" or "no"
+  return 'no';
+}
+function answer3() {
+  //the answer should be "yes" or "no"
+  return 'yes';
+}

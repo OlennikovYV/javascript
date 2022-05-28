6e02c96c358153a72cedac2dd3381f43dac367bb
Yury <olennikovyv@mail.ru>
2022-05-28 17:44:34 +0500

    Name Shuffler

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,7 @@
-function largestPairSum(numbers) {
-  const [...arrayNumbers] = numbers;
+function nameShuffler(str) {
+  const reverseString = str;
 
-  arrayNumbers.sort((a, b) => b - a);
-
-  return arrayNumbers[0] + arrayNumbers[1];
+  return reverseString.split(' ').reverse().join(' ');
 }
 
-console.log(largestPairSum([10, 14, 2, 23, 19])); //  42
-console.log(largestPairSum([-100, -29, -24, -19, 19])); //  0
-console.log(largestPairSum([1, 2, 3, 4, 6, -1, 2])); //  10
-console.log(largestPairSum([-10, -8, -16, -18, -19])); //  -18
+console.log(nameShuffler('john McClane')); // 'McClane john'

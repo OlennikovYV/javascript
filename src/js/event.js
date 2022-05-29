b15b82b4726dda14273f4cd7af510bfaedb5535f
Yury <olennikovyv@mail.ru>
2022-05-29 16:56:24 +0500

    Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,26 @@
-function remove(string) {
-  return string.replace(/\!$/, '');
+function remove(s, n) {
+  return s
+    .split('')
+    .map(el => {
+      if (el === '!')
+        if (n > 0) {
+          n -= 1;
+          return '';
+        }
+
+      return el;
+    })
+    .join('');
 }
 
-console.log(remove('Hi!')); //  'Hi'
-console.log(remove('Hi!!!')); //  'Hi!!'
-console.log(remove('!Hi')); //  '!Hi'
-console.log(remove('!Hi!')); //  '!Hi'
-console.log(remove('Hi! Hi!')); //  'Hi! Hi'
-console.log(remove('Hi')); //  'Hi'
+console.log(remove('Hi!', 1)); //  'Hi'
+console.log(remove('Hi!', 100)); //  'Hi'
+console.log(remove('Hi!!!', 1)); //  'Hi!!'
+console.log(remove('Hi!!!', 100)); //  'Hi'
+console.log(remove('!Hi', 1)); //  'Hi'
+console.log(remove('!Hi!', 1)); //  'Hi!'
+console.log(remove('!Hi!', 100)); //  'Hi'
+console.log(remove('!!!Hi !!hi!!! !hi', 1)); //  '!!Hi !!hi!!! !hi'
+console.log(remove('!!!Hi !!hi!!! !hi', 3)); //  'Hi !!hi!!! !hi'
+console.log(remove('!!!Hi !!hi!!! !hi', 5)); //  'Hi hi!!! !hi'
+console.log(remove('!!!Hi !!hi!!! !hi', 100)); //  'Hi hi hi'

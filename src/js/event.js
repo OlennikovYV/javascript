08db94bd002f14a99bdb87bf9a2c89dceaeb34e5
Yury <olennikovyv@mail.ru>
2022-06-01 08:27:02 +0500

    Training JS #7: if..else and ternary operator

--- src/js/event.js
+++ src/js/event.js
@@ -1,36 +1,12 @@
-function meeting(s) {
-  let str = s.toUpperCase();
-
-  str = str
-    .replace(/(\w+):(\w+)/g, '$2, $1')
-    .split(';')
-    .sort()
-    .join(')(');
-
-  return `(${str})`;
+function saleHotdogs(n) {
+  if (n < 5) return n * 100;
+  if (n < 10) return n * 95;
+  return n * 90;
 }
 
-console.log(
-  meeting(
-    'Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn'
-  )
-);
-console.log(
-  '(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)'
-);
-console.log(
-  meeting(
-    'John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell'
-  )
-);
-console.log(
-  '(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)'
-);
-console.log(
-  meeting(
-    'Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern'
-  )
-);
-console.log(
-  '(ARNO, ALEX)(ARNO, HALEY)(BELL, SARAH)(CORNWELL, ALISSA)(DORNY, PAUL)(DORRIES, ANDREW)(KERN, ANN)(KERN, MADISON)'
-);
+console.log(saleHotdogs(1)); // 100
+console.log(saleHotdogs(4)); // 400
+console.log(saleHotdogs(5)); // 475
+console.log(saleHotdogs(9)); // 855
+console.log(saleHotdogs(10)); // 900
+console.log(saleHotdogs(100)); // 9000

4eb72142f95d436a8473b81d1c1a882efcf056d3
Yury <olennikovyv@mail.ru>
2022-06-01 09:09:04 +0500

    Consonant value

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,20 @@
-function sumStr(a, b) {
-  return String(Number(a) + Number(b));
+function solve(s) {
+  const CODE_CHAR_A = 96;
+  const numAlpha = alpha => alpha.charCodeAt(0) - CODE_CHAR_A;
+
+  let arraySubstConsonants = s.match(/[^aeiou]+/g);
+
+  arraySubstConsonants = arraySubstConsonants.map(el => {
+    return el.split('').reduce((acc, el) => acc + numAlpha(el), 0);
+  }, 0);
+
+  return Math.max(...arraySubstConsonants);
 }
 
-console.log(sumStr('4', '5')); //  '9'
-console.log(sumStr('34', '5')); //  '39'
+console.log(solve('zodiac')); //  26
+console.log(solve('chruschtschov')); //  80
+console.log(solve('khrushchev')); //  38
+console.log(solve('strength')); //  57
+console.log(solve('catchphrase')); //  73
+console.log(solve('twelfthstreet')); //  103
+console.log(solve('mischtschenkoana')); //  80

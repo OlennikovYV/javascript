52962f51837c4c7ebc9697e47a517231edc5e932
Yury <olennikovyv@mail.ru>
2022-06-01 08:31:21 +0500

    Do you speak 'English'?

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,6 @@
-function saleHotdogs(n) {
-  if (n < 5) return n * 100;
-  if (n < 10) return n * 95;
-  return n * 90;
+function spEng(sentence) {
+  return /english/i.test(sentence);
 }
 
-console.log(saleHotdogs(1)); // 100
-console.log(saleHotdogs(4)); // 400
-console.log(saleHotdogs(5)); // 475
-console.log(saleHotdogs(9)); // 855
-console.log(saleHotdogs(10)); // 900
-console.log(saleHotdogs(100)); // 9000
+console.log(spEng('english')); //  true
+console.log(spEng('egnlish')); //  false

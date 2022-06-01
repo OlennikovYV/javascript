6feb2363424b5dbfb63f9abe51671244f9ae95e5
Yury <olennikovyv@mail.ru>
2022-06-01 08:34:00 +0500

    Sum The Strings

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,6 @@
-function spEng(sentence) {
-  return /english/i.test(sentence);
+function sumStr(a, b) {
+  return String(+a + +b);
 }
 
-console.log(spEng('english')); //  true
-console.log(spEng('egnlish')); //  false
+console.log(sumStr('4', '5')); //  '9'
+console.log(sumStr('34', '5')); //  '39'

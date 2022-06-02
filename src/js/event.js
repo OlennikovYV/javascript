8fe629b22aaa217b3f26876d4b495031df147096
Yury <olennikovyv@mail.ru>
2022-06-02 08:56:47 +0500

    Will there be enough space?

--- src/js/event.js
+++ src/js/event.js
@@ -1,20 +1,8 @@
-function isSortedAndHow(array) {
-  const ascendingSort = array[1] - array[0] > 0;
-  let sorting = true;
-  let result = '';
-
-  for (let i = 0; i < array.length - 1; i += 1) {
-    const diffirent = array[i + 1] - array[i];
-    sorting = ascendingSort ? diffirent >= 0 : diffirent <= 0;
-    if (sorting === false) break;
-  }
-
-  result += sorting ? 'yes' : 'no';
-  if (sorting) result += ascendingSort ? ', ascending' : ', descending';
-
-  return result;
+function enough(cap, on, wait) {
+  const waitBus = cap - on - wait;
+  return waitBus < 0 ? Math.abs(waitBus) : 0;
 }
 
-console.log(isSortedAndHow([1, 2])); // 'yes, ascending'
-console.log(isSortedAndHow([15, 7, 3, -8])); // 'yes, descending'
-console.log(isSortedAndHow([4, 2, 30])); // 'no'
+console.log(enough(10, 5, 5)); // 0
+console.log(enough(100, 60, 50)); // 10
+console.log(enough(20, 5, 5)); // 0

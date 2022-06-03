93a4cae6dd640b441a1bdeb0e58cd3311fa4a548
Yury Olennikov <OlennikovYV@mail.ru>
2022-06-03 08:43:56 +0500

    No oddities here refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,5 @@
 function noOdds(values) {
-  const result = [];
-  for (let i = 0; i < values.length; i += 1)
-    if (values[i] % 2 === 0) result.push(values[i]);
-
-  return result;
+  return values.filter(el => el % 2 === 0);
 }
 
 console.log(noOdds([0, 1])); // [0]

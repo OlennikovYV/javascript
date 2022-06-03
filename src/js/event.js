7f13281c650f7a85d5fc16ef6233073b6919dec2
Yury Olennikov <OlennikovYV@mail.ru>
2022-06-03 08:42:41 +0500

    No oddities here

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,10 @@
-function sumOfDifferences(arr) {
-  const result = 0;
-  let [...calcArray] = arr;
-
-  if (calcArray.length < 2) return 0;
-
-  calcArray.sort((a, b) => b - a);
-
-  for (let i = 0; i < calcArray.length - 1; i += 1) {
-    result += calcArray[i] - calcArray[i + 1];
-  }
+function noOdds(values) {
+  const result = [];
+  for (let i = 0; i < values.length; i += 1)
+    if (values[i] % 2 === 0) result.push(values[i]);
 
   return result;
 }
 
-console.log(sumOfDifferences([])); // 0
-console.log(sumOfDifferences([-1])); // -1
-console.log(sumOfDifferences([1, 2, 10])); // 9
-console.log(sumOfDifferences([-3, -2, -1])); // 2
+console.log(noOdds([0, 1])); // [0]
+console.log(noOdds([0, 1, 2, 3])); // [0, 2]

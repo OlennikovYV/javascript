aae29691c78283aa3ac0f1ca6514fcacca4f9e15
Yury <olennikovyv@mail.ru>
2022-06-04 16:27:54 +0500

    The highest profit wins!

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,10 @@
-function noOdds(values) {
-  return values.filter(el => el % 2 === 0);
+function minMax(arr) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+
+  return [min, max];
 }
 
-console.log(noOdds([0, 1])); // [0]
-console.log(noOdds([0, 1, 2, 3])); // [0, 2]
+console.log(minMax([1, 2, 3, 4, 5])); // [1, 5]
+console.log(minMax([2334454, 5])); // [5, 2334454]
+console.log(minMax([5])); // [5, 5]

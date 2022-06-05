238835ebab9ae70aeefbf55917ccf1fac816a755
Yury <olennikovyv@mail.ru>
2022-06-05 14:05:10 +0500

    Tip Calculator

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,20 @@
-function strCount(str, letter) {
-  return str.split(letter).length - 1;
+function calculateTip(amount, rating) {
+  const tips = {
+    TERRIBLE: 0,
+    POOR: 5,
+    GOOD: 10,
+    GREAT: 15,
+    EXCELLENT: 20,
+  };
+
+  const tip = tips[rating.toUpperCase()];
+
+  if (tip === undefined) return 'Rating not recognised';
+
+  return Math.ceil(amount * tip * 0.01);
 }
 
-console.log(strCount('Hello', 'o')); // 1
-console.log(strCount('Hello', 'l')); // 2
-console.log(strCount('', 'z')); // 0
+console.log(calculateTip(20, 'Excellent')); // 4
+console.log(calculateTip(26.95, 'good')); // 3
+console.log(calculateTip(23, 'TERRIBLE')); // 0
+console.log(calculateTip(26, 'None')); // 'Rating not recognised'

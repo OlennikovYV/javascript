6055bce3affd0dc9d8265d84a9b90b57801c690b
Yury Olennikov <OlennikovYV@mail.ru>
2022-06-07 08:44:07 +0500

    Two Oldest Ages

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,18 @@
-var a = 'dev';
-var b = 'Lab';
+function twoOldestAges(ages) {
+  let primaryOldestAge;
+  let secondOldestAge;
+  let [...oldestAges] = ages;
 
-var name = a + b;
+  oldestAges.sort((a, b) => a - b);
+  primaryOldestAge = oldestAges.pop();
+  secondOldestAge = oldestAges.pop();
 
-console.log(a); // 'dev'
-console.log(b); // 'Lab'
-console.log(name); // 'devLab'
+  return [secondOldestAge, primaryOldestAge];
+}
+
+let results1 = [1, 5, 87, 45, 8, 8];
+console.log(twoOldestAges(results1)); // 45, 87
+results1 = [6, 5, 83, 5, 3, 18];
+console.log(twoOldestAges(results1)); // 18, 83
+results1 = [11, 53, 8, 29, 61, 61, 23, 35];
+console.log(twoOldestAges(results1)); // 53, 61

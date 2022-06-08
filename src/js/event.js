d003ced96f04110a3da2369deb5f1a660e5094cf
Yury Olennikov <OlennikovYV@mail.ru>
2022-06-08 08:56:11 +0500

    Power of two

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,18 @@
-function breakChocolate(n, m) {
-  return n * m === 0 ? 0 : n * m - 1;
+const countSetBit = num => {
+  let count = 0;
+  while (num) {
+    count += num & 1;
+    num >>>= 1;
+  }
+  return count;
+};
+
+function isPowerOfTwo(n) {
+  let num = n;
+
+  return countSetBit(num) === 1;
 }
 
-console.log(breakChocolate(5, 5)); // 24
-console.log(breakChocolate(1, 1)); // 0
+console.log(isPowerOfTwo(2)); // true
+console.log(isPowerOfTwo(4096)); // true
+console.log(isPowerOfTwo(5)); // false

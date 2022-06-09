e09e1373060a2592cdae8ddf8523c3a0ac5f920b
Yury Olennikov <OlennikovYV@mail.ru>
2022-06-09 09:09:26 +0500

    Third Angle of a Triangle

--- src/js/event.js
+++ src/js/event.js
@@ -1,28 +1,8 @@
-const isOperator = op => '+-*/'.includes(op);
-const isNumber = num => Number.isNaN(Number(num)) === false;
-
-const calculate = (a, b, op) => {
-  switch (op) {
-    case '+':
-      return a + b;
-    case '-':
-      return a - b;
-    case '*':
-      return a * b;
-    case '/':
-      return a / b;
-  }
-};
-
-function calculator(a, b, sign) {
-  if (!isOperator(sign)) return 'unknown value';
-  if (!isNumber(a) || !isNumber(b)) return 'unknown value';
-  return calculate(a, b, sign);
+function otherAngle(a, b) {
+  return 180 - (a + b);
 }
 
-console.log(calculator(1, 2, '+')); // 3
-console.log(calculator(1, 2, '-')); // -1
-console.log(calculator(3, 5, '*')); // 15
-console.log(calculator(6, 2, '/')); // 3
-console.log(calculator(6, 2, '$')); // 'unknown value'
-console.log(calculator(6, 'h', '*')); // 'unknown value'
+console.log(otherAngle(30, 60)); // 90
+console.log(otherAngle(60, 60)); // 60
+console.log(otherAngle(43, 78)); // 59
+console.log(otherAngle(10, 20)); // 150

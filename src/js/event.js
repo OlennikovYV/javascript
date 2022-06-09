31987c4d40a5c1fce5f4d326af61f2e8da72e93c
Yury Olennikov <OlennikovYV@mail.ru>
2022-06-09 08:35:35 +0500

    Add Length

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,9 @@
-function f(n) {
-  return Number.isInteger(n) && n > 0 ? (n * (n + 1)) / 2 : false;
+function addLength(str) {
+  return str.split(' ').map(el => {
+    const lengthWord = `${el} ${el.length}`;
+    return lengthWord;
+  });
 }
 
-console.log(f(100)); // 5050
-console.log(f(300)); // 45150
-console.log(f(50000)); // 1250025000
-console.log(f('n')); // false, 'n must be a number'
-console.log(f()); // false, 'n must be a value'
-console.log(f(3.14)); // false, 'n must be an integer'
-console.log(f(0)); // false, 'a value of n greater than 0 is required'
-console.log(f(-10)); // false, 'a positive value of n is required'
+console.log(addLength('apple ban')); // ['apple 5', 'ban 3']
+console.log(addLength('you will win')); // ['you 3', 'will 4', 'win 3']

84b17b2d166c5ce87fe00c5a1d94f76ff5e9c522
Yury <olennikovyv@mail.ru>
2022-06-11 14:16:44 +0500

    Function 2 - squaring an argument

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,5 @@
-function evenNumbers(array, number) {
-  return array.filter(el => el % 2 === 0).slice(-number);
-}
+const square = num => num * num;
 
-console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)); // [4, 6, 8]
-console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2)); // [-8, 26]
-console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1)); // [6]
+console.log(square(1)); // 1
+console.log(square(2)); // 4
+console.log(square(3)); // 9

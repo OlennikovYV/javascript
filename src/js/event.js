97d0c25933b557ad6919f1d7dc4f33f9b6753267
Yury <olennikovyv@mail.ru>
2022-06-11 14:12:58 +0500

    Alphabet symmetry

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,27 @@
-function checkTheBucket(bucket) {
-  return bucket.includes('gold');
+function solve(arr) {
+  const isIndexAlphabeticCharacter = (char, index) => {
+    const INDEX_BEGUN_ALPHABET = 96;
+    const indexInAlphabet = char.toLowerCase().charCodeAt(0);
+
+    return indexInAlphabet - INDEX_BEGUN_ALPHABET === index + 1;
+  };
+
+  const countCharSymmetry = string => {
+    return string.split('').reduce((acc, el, index) => {
+      if (isIndexAlphabeticCharacter(el, index)) acc += 1;
+      return acc;
+    }, 0);
+  };
+
+  // debugger;
+  return arr.map(el => {
+    const count = countCharSymmetry(el);
+    if (count > 0) return count;
+    return count;
+  });
 }
 
-console.log(checkTheBucket(['stone', 'stone', 'stone', 'stone', 'stone'])); // false
-console.log(checkTheBucket(['stone', 'stone', 'stone', 'stone', 'gold'])); // true
-console.log(checkTheBucket(['gold', 'stone', 'stone', 'stone', 'stone'])); // true
-console.log(checkTheBucket([])); // false
-console.log(checkTheBucket(['stone', 'stone', 'stone', 'gold', 'gold'])); // true
+console.log(solve(['abode', 'ABc', 'xyzD'])); // [4,3,1]
+console.log(solve(['abide', 'ABc', 'xyz'])); // [4,3,0]
+console.log(solve(['IAMDEFANDJKL', 'thedefgh', 'xyzDEFghijabc'])); // [6, 5, 7]
+console.log(solve(['encode', 'abc', 'xyzD', 'ABmD'])); // [1, 3, 1, 3]

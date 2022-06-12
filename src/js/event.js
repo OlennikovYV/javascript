a76b3a9dc0477a794e241afc9e83c226136981c1
Yury <olennikovyv@mail.ru>
2022-06-12 14:22:40 +0500

    Find out whether the shape is a cube

--- src/js/event.js
+++ src/js/event.js
@@ -1,22 +1,14 @@
-var SequenceSum = (function () {
-  function SequenceSum() {}
+var cubeChecker = function (volume, side) {
+  if (volume <= 0 || side <= 0) return false;
+  return volume === side ** 3 ? true : false;
+};
 
-  SequenceSum.showSequence = function (count) {
-    let text = [];
-    let sum = 0;
-
-    if (count === 0) return `0=0`;
-    if (count < 0) return `${count}<0`;
-
-    for (let i = 0; i <= count; i += 1) {
-      text.push(i);
-      sum += i;
-    }
-
-    return `${text.join('+')} = ${sum}`;
-  };
-
-  return SequenceSum;
-})();
-
-console.log(SequenceSum.showSequence(6)); // "0+1+2+3+4+5+6 = 21"
+console.log(cubeChecker(56.3, 1)); // false
+console.log(cubeChecker(-1, 2)); // false
+console.log(cubeChecker(8, 3)); // false
+console.log(cubeChecker(8, 2)); // true
+console.log(cubeChecker(-8, -2)); // false
+console.log(cubeChecker(0, 0)); // false
+console.log(cubeChecker(1, 5)); // false
+console.log(cubeChecker(125, 5)); // true
+console.log(cubeChecker(125, -5)); // false

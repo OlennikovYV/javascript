bfc5be59436df1f71965436ef32a54ad86d19217
Yury <olennikovyv@mail.ru>
2022-06-12 14:15:55 +0500

    Sum of numbers from 0 to N

--- src/js/event.js
+++ src/js/event.js
@@ -1,3 +1,22 @@
-const getAge = parseInt;
+var SequenceSum = (function () {
+  function SequenceSum() {}
 
-console.log(getAge('4 years old')); // 4
+  SequenceSum.showSequence = function (count) {
+    let text = [];
+    let sum = 0;
+
+    if (count === 0) return `0=0`;
+    if (count < 0) return `${count}<0`;
+
+    for (let i = 0; i <= count; i += 1) {
+      text.push(i);
+      sum += i;
+    }
+
+    return `${text.join('+')} = ${sum}`;
+  };
+
+  return SequenceSum;
+})();
+
+console.log(SequenceSum.showSequence(6)); // "0+1+2+3+4+5+6 = 21"

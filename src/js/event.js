c8a4bd38a25f28279f7887f2f858e598fe9b443f
Yury <olennikovyv@mail.ru>
2022-06-13 17:30:39 +0500

    Parts of a list

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,14 @@
-function sayHello(name, city, state) {
-  return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+function partlist(arr) {
+  const result = [];
+  for (let i = 1; i < arr.length; i += 1) {
+    const firstPart = arr.slice(0, i).join(' ');
+    const secondPart = arr.slice(i).join(' ');
+
+    result.push([firstPart, secondPart]);
+  }
+  return result;
 }
 
-console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')); // 'Hello, John Smith! Welcome to Phoenix, Arizona!'
-console.log(
-  sayHello(['Franklin', 'Delano', 'Roosevelt'], 'Chicago', 'Illinois')
-); // 'Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!'
-console.log(sayHello(['Wallace', 'Russel', 'Osbourne'], 'Albany', 'New York')); // 'Hello, Wallace Russel Osbourne! Welcome to Albany, New York!'
+console.log(partlist(['I', 'wish', 'I', "hadn't", 'come'])); // [["I", "wish I hadn't come"], ["I wish", "I hadn't come"], ["I wish I", "hadn't come"], ["I wish I hadn't", "come"]]
+console.log(partlist(['cdIw', 'tzIy', 'xDu', 'rThG'])); //  [["cdIw", "tzIy xDu rThG"], ["cdIw tzIy", "xDu rThG"], ["cdIw tzIy xDu", "rThG"]]
+console.log(partlist(['vJQ', 'anj', 'mQDq', 'sOZ'])); //  [["vJQ", "anj mQDq sOZ"], ["vJQ anj", "mQDq sOZ"], ["vJQ anj mQDq", "sOZ"]]

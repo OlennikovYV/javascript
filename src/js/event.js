1e2754469bdf40ce629d6cf40ef79fd1d889afb4
Yury <olennikovyv@mail.ru>
2022-06-14 16:05:53 +0500

    Alan Partridge II - Apple Turnover refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,7 @@
 function apple(x) {
   const str1 = 'Help yourself to a honeycomb Yorkie for the glovebox.';
   const str2 = "It's hotter than the sun!!";
-  return parseInt(x) ** 2 < 1000 ? str1 : str2;
+  return Math.pow(x, 2) < 1000 ? str1 : str2;
 }
 
 console.log(apple('50')); // "It's hotter than the sun!!"

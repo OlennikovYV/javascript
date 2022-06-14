e6b791ebdbac2fbd5fd3847b4cf15259ede129ee
Yury <olennikovyv@mail.ru>
2022-06-14 15:59:58 +0500

    Remove exclamation marks

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,5 @@
-function partlist(arr) {
-  const result = [];
-  for (let i = 1; i < arr.length; i += 1) {
-    const firstPart = arr.slice(0, i).join(' ');
-    const secondPart = arr.slice(i).join(' ');
-
-    result.push([firstPart, secondPart]);
-  }
-  return result;
+function removeExclamationMarks(s) {
+  return s.replace(/!/g, '');
 }
 
-console.log(partlist(['I', 'wish', 'I', "hadn't", 'come'])); // [["I", "wish I hadn't come"], ["I wish", "I hadn't come"], ["I wish I", "hadn't come"], ["I wish I hadn't", "come"]]
-console.log(partlist(['cdIw', 'tzIy', 'xDu', 'rThG'])); //  [["cdIw", "tzIy xDu rThG"], ["cdIw tzIy", "xDu rThG"], ["cdIw tzIy xDu", "rThG"]]
-console.log(partlist(['vJQ', 'anj', 'mQDq', 'sOZ'])); //  [["vJQ", "anj mQDq sOZ"], ["vJQ anj", "mQDq sOZ"], ["vJQ anj mQDq", "sOZ"]]
+console.log(removeExclamationMarks('Hello World!')); // "Hello World"

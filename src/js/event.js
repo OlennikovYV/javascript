c38ad044b7dace0fb015925de6fc3901e17fb0dd
Yury <olennikovyv@mail.ru>
2022-06-15 21:49:41 +0500

    Kata Example Twist refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,4 +1,3 @@
-var websites = [];
-for (let i = 0; i < 1000; i += 1) websites.push('codewars');
+var websites = Array.from({ length: 1000 }, () => 'codewars');
 
 // 'Every element in the array must contain the value "codewars"'

465866004a130d183662528e445e00ea9c9ee407
Yury <olennikovyv@mail.ru>
2022-06-15 21:44:33 +0500

    USD => CNY

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,9 @@
-function dashatize(num) {
-  return String(num)
-    .replace(/([13579])/g, '-$1-')
-    .replace(/--/g, '-')
-    .replace(/^-|-$/g, '');
+function usdcny(usd) {
+  const rate = 6.75;
+  return (usd * rate).toFixed(2) + ' Chinese Yuan';
 }
 
-console.log(dashatize(274)); // "2-7-4", "Should return 2-7-4"
-console.log(dashatize(5311)); // "5-3-1-1", "Should return 5-3-1-1"
-console.log(dashatize(86320)); // "86-3-20", "Should return 86-3-20"
-console.log(dashatize(974302)); // "9-7-4-3-02", "Should return 9-7-4-3-02"
-console.log(dashatize(NaN)); // "NaN", "Should return NaN"
-console.log(dashatize(0)); // "0", "Should return 0"
-console.log(dashatize(-1)); // "1", "Should return 1"
-console.log(dashatize(-28369)); // "28-3-6-9", "Should return 28-3-6-9"
+console.log(usdcny(15)); // '101.25 Chinese Yuan'
+console.log(usdcny(20)); // '135.00 Chinese Yuan'
+console.log(usdcny(30)); // '202.50 Chinese Yuan'
+console.log(usdcny(465)); // '3138.75 Chinese Yuan'

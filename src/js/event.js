609f75cbeb6257ede245b8f1a9f9d86e9d8a8426
Yury <olennikovyv@mail.ru>
2022-06-16 17:12:30 +0500

    Are arrow functions odd?

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,9 @@
-function plural(n) {
-  return n !== 1;
+function odds(values) {
+  // arrow it
+  return values.filter(el => el % 2 === 1);
 }
 
-console.log(plural(0)); // true, 'Plural for 0'
-console.log(plural(0.5)); // true, 'Plural for 0.5'
-console.log(plural(1)); // false, 'Plural for 1'
-console.log(plural(100)); // true, 'Plural for 100'
-console.log(plural(Infinity)); // true, 'Plural for Infinity'
+console.log(odds([])); // [], "Should handle empty array"
+console.log(odds([2, 4, 6])); // [], "Should handle array with even numbers only"
+console.log(odds([1, 3, 5])); // [1, 3, 5], "Should handle array with odd numbers only"
+console.log(odds([1, 2, 3, 4, 5, 6])); // [1, 3, 5], "Should handle mixed array"

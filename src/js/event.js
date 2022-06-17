e8fd89f16323510b1d019f43d5bf3a0d4a132a1a
Yury <olennikovyv@mail.ru>
2022-06-17 16:33:15 +0500

    Common Denominators

--- src/js/event.js
+++ src/js/event.js
@@ -1,22 +1,31 @@
-function _if(bool, func1, func2) {
-  return (bool ? func1 : func2)();
+const nok = (...arr) => {
+  const nod = (x, y) => (!y ? x : nod(y, x % y));
+  const _nok = (x, y) => (x * y) / nod(x, y);
+  return [...arr].reduce((a, b) => _nok(a, b));
+};
+
+function convertFrac(lst) {
+  if (lst.length === 0) return '';
+
+  const zn = lst.map(el => el[1]);
+  const nokAll = nok(...zn);
+
+  return lst.reduce((str, el) => {
+    str += `(${(el[0] * nokAll) / el[1]},${nokAll})`;
+    return str;
+  }, '');
 }
 
-_if(
-  true,
-  function () {
-    console.log('True');
-  },
-  function () {
-    console.log('false');
-  }
-); // True
-_if(
-  false,
-  function () {
-    console.log('True');
-  },
-  function () {
-    console.log('false');
-  }
-); // false
+let lst = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+];
+
+console.log(convertFrac(lst)); // '(6,12)(4,12)(3,12)'
+lst = [
+  [69, 130],
+  [87, 1310],
+  [3, 4],
+];
+console.log(convertFrac(lst)); // '(18078,34060)(2262,34060)(25545,34060)'

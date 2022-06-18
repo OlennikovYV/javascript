b6effc0ce98faabb3bd26c8829f10962818f11f9
Yury <olennikovyv@mail.ru>
2022-06-18 15:20:45 +0500

    Memoized Fibonacci

--- src/js/event.js
+++ src/js/event.js
@@ -1,31 +1,11 @@
-const nok = (...arr) => {
-  const nod = (x, y) => (!y ? x : nod(y, x % y));
-  const _nok = (x, y) => (x * y) / nod(x, y);
-  return [...arr].reduce((a, b) => _nok(a, b));
+var fibonacci = function (n, memo = [0, 1, 1]) {
+  if (memo[n]) return memo[n];
+
+  memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+
+  return memo[n];
 };
 
-function convertFrac(lst) {
-  if (lst.length === 0) return '';
-
-  const zn = lst.map(el => el[1]);
-  const nokAll = nok(...zn);
-
-  return lst.reduce((str, el) => {
-    str += `(${(el[0] * nokAll) / el[1]},${nokAll})`;
-    return str;
-  }, '');
-}
-
-let lst = [
-  [1, 2],
-  [1, 3],
-  [1, 4],
-];
-
-console.log(convertFrac(lst)); // '(6,12)(4,12)(3,12)'
-lst = [
-  [69, 130],
-  [87, 1310],
-  [3, 4],
-];
-console.log(convertFrac(lst)); // '(18078,34060)(2262,34060)(25545,34060)'
+console.log(fibonacci(70)); // 190392490709135
+console.log(fibonacci(60)); // 1548008755920
+console.log(fibonacci(50)); // 12586269025

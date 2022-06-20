7fb6136bcfa50b344a91926592ab5c86174acc48
Yury <olennikovyv@mail.ru>
2022-06-20 16:21:59 +0500

    Form The Minimum

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,18 @@
-function getAverage(marks) {
-  const sum = marks.reduce((sum, mark) => sum + mark);
+function minValue(values) {
+  let [...result] = values;
+  result = new Set(result);
+  result = [...result];
+  result.sort();
 
-  return Math.floor(sum / marks.length);
+  return +result.join('');
 }
 
-console.log(getAverage([2, 2, 2, 2])); // 2
-console.log(getAverage([1, 2, 3, 4, 5])); // 3
-console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2])); // 1
+console.log(minValue([1, 3, 1])); // 13
+console.log(minValue([4, 7, 5, 7])); // 457
+console.log(minValue([4, 8, 1, 4])); // 148
+console.log(minValue([5, 7, 9, 5, 7])); // 579
+console.log(minValue([6, 7, 8, 7, 6, 6])); // 678
+console.log(minValue([5, 6, 9, 9, 7, 6, 4])); // 45679
+console.log(minValue([1, 9, 1, 3, 7, 4, 6, 6, 7])); // 134679
+console.log(minValue([3, 6, 5, 5, 9, 8, 7, 6, 3, 5, 9])); // 356789
+console.log(minValue([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); // 1

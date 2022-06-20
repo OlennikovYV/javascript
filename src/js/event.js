e7bb277499faf242db888031b4e9fce7e0927d36
Yury <olennikovyv@mail.ru>
2022-06-20 16:07:10 +0500

    Dollars and Cents

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,5 @@
-const ultimateReverse = s => {
-  let reverseString = [...s.join('')].reverse();
+function formatMoney(amount) {
+  return `$${amount.toFixed(2)}`;
+}
 
-  return s.map(word => reverseString.splice(0, word.length).join(''));
-};
-
-console.log(
-  ultimateReverse(['I', 'like', 'big', 'butts', 'and', 'I', 'cannot', 'lie!'])
-); // ['!', 'eilt', 'onn', 'acIdn', 'ast', 't', 'ubgibe', 'kilI'];
+console.log(formatMoney(39.99)); // '$39.99'

42ed4533d25ee2cb0f2ffdac44956ffb2a7ed532
Yury <olennikovyv@mail.ru>
2022-06-22 16:28:37 +0500

    who ate the cookie?

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,14 @@
-function Sleigh() {
-  this.SantaName = 'Santa Claus';
-  this.SantaPassword = 'Ho Ho Ho!';
+function cookie(x) {
+  const zach = 'Who ate the last cookie? It was Zach!';
+  const monica = 'Who ate the last cookie? It was Monica!';
+  const dog = 'Who ate the last cookie? It was the dog!';
+
+  if (typeof x === 'string') return zach;
+  if (typeof x === 'number') return monica;
+  return dog;
 }
 
-Sleigh.prototype.authenticate = function (name, password) {
-  return this.SantaName === name && this.SantaPassword === password;
-};
-
-const sleigh = new Sleigh();
-
-console.log(sleigh.authenticate('Santa Claus', 'Ho Ho Ho!')); // true
-console.log(sleigh.authenticate('Santa', 'Ho Ho Ho!')); // false
-console.log(sleigh.authenticate('Santa Claus', 'Ho Ho!')); // false
-console.log(sleigh.authenticate('jhoffner', 'CodeWars')); // false
+console.log(cookie('Ryan')); // Who ate the last cookie? It was Zach!
+console.log(cookie(26)); // Who ate the last cookie? It was Monica!
+console.log(cookie(2.3)); // Who ate the last cookie? It was Monica!
+console.log(cookie(true)); // Who ate the last cookie? It was the dog!

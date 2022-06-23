5858bd9c7d6c5de10a94c572fd345ff09a0e4488
Yury <olennikovyv@mail.ru>
2022-06-23 20:01:29 +0500

    Compare within margin

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,14 @@
-function cookie(x) {
-  const zach = 'Who ate the last cookie? It was Zach!';
-  const monica = 'Who ate the last cookie? It was Monica!';
-  const dog = 'Who ate the last cookie? It was the dog!';
+function closeCompare(a, b, margin) {
+  const diff = a - b;
+  if (margin === undefined) margin = 0;
 
-  if (typeof x === 'string') return zach;
-  if (typeof x === 'number') return monica;
-  return dog;
+  return Math.abs(diff) <= margin ? 0 : a < b ? -1 : 1;
 }
 
-console.log(cookie('Ryan')); // Who ate the last cookie? It was Zach!
-console.log(cookie(26)); // Who ate the last cookie? It was Monica!
-console.log(cookie(2.3)); // Who ate the last cookie? It was Monica!
-console.log(cookie(true)); // Who ate the last cookie? It was the dog!
+console.log(closeCompare(4, 5)); // -1
+console.log(closeCompare(5, 5)); // 0
+console.log(closeCompare(6, 5)); // 1
+console.log(closeCompare(-6, -5)); // -1
+console.log(closeCompare(2, 5, 3)); // 0
+console.log(closeCompare(8.1, 5, 3)); // 1
+console.log(closeCompare(1.99, 5, 3)); // -1

8999ee730c61720c6c8d6353f58f0df2e3482d4f
Yury <olennikovyv@mail.ru>
2022-06-25 17:18:59 +0500

    Grasshopper - Messi goals function

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,6 @@
-function validate(password) {
-  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/.test(password);
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
-console.log(validate('djI38D55')); // 'djI38D55 - Expected true'
-console.log(!validate('a2.d412')); // 'a2.d412 - Expected false'
-console.log(!validate('JHD5FJ53')); // 'JHD5FJ53 - Expected false'
-console.log(!validate('!fdjn345')); // '!fdjn345 - Expected false'
-console.log(!validate('jfkdfj3j')); // 'jfkdfj3j - Expected false'
-console.log(!validate('123')); // '123 - Expected false'
-console.log(!validate('abc')); // 'abc - Expected false'
-console.log(validate('Password123')); // 'Password123 - Expected true'
+console.log(goals(0, 0, 0)); // 0
+console.log(goals(43, 10, 5)); // 58

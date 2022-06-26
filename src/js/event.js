55ff9ae719a025ce611fab0996815e05b1f7ecae
Yury <olennikovyv@mail.ru>
2022-06-26 18:17:56 +0500

    repeatIt

--- src/js/event.js
+++ src/js/event.js
@@ -1,23 +1,7 @@
-var orderedCount = function (text) {
-  let result = [];
-  let countAlpha = {};
-  let textArray = text.split('');
-  let textSet = new Set(textArray);
-
-  textArray.reduce((acc, el) => {
-    if (countAlpha[el]) {
-      countAlpha[el] += 1;
-    } else countAlpha[el] = 1;
-    return countAlpha;
-  }, countAlpha);
-
-  for (let key of textSet) {
-    result.push([key, countAlpha[key]]);
-  }
-
-  return result;
+var repeatIt = function (str, n) {
+  return typeof str === 'string' ? str.repeat(n) : 'Not a string';
 };
 
-console.log(orderedCount('abracadabra')); // [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]]
-console.log(orderedCount('Code Wars')); //  [['C', 1], ['o', 1], ['d', 1], ['e', 1], [' ', 1], ['W', 1], ['a', 1], ['r', 1], ['s', 1]]]
-console.log(orderedCount('233312')); // [['2', 2], ['3', 3], ['1', 1 ]]]
+console.log(repeatIt([], 3)); // 'Not a string'
+console.log(repeatIt('*', 3)); // '***'
+console.log(repeatIt('Hello', 11)); // 'HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello'

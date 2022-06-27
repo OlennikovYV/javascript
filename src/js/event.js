e10d1a05abd7567bfac7c4aa67c8151105cb6a4d
Yury Olennikov <OlennikovYV@mail.ru>
2022-06-27 10:20:33 +0500

    Rotate for a Max

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,24 @@
-String.prototype.digit = function () {
-  return /^[\d]$/.test(this);
-};
+function maxRot(n) {
+  const tempDigit = [];
+  let result = [];
+  let digit = String(n).split('');
+  const length = digit.length;
+  result.push([...digit]);
+  for (let i = 0; i < length; i += 1) {
+    const num = digit.shift();
+    digit.push(num);
+    result.push([...tempDigit, ...digit]);
+    tempDigit.push(digit.shift());
+    result.push([...tempDigit, ...digit]);
+  }
 
-console.log(''.digit()); // false
-console.log('7'.digit()); // true
-console.log(' '.digit()); // false
-console.log('a'.digit()); // false
-console.log('a5'.digit()); // false
-console.log('14'.digit()); // false
+  result = result.map(el => +el.join(''));
+
+  return Math.max(...result);
+}
+
+console.log(maxRot(38458215)); // 85821534
+console.log(maxRot(195881031)); // 988103115
+console.log(maxRot(896219342)); // 962193428
+console.log(maxRot(69418307)); // 94183076
+console.log(maxRot(507992495)); // 99249557

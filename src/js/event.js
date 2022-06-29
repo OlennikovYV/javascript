1ca189f23b5afada0242b54dfb49fbe226c61208
Yury Olennikov <OlennikovYV@mail.ru>
2022-06-29 09:07:27 +0500

    Minimize Sum Of Array (Array Series #1)

--- src/js/event.js
+++ src/js/event.js
@@ -1,26 +1,17 @@
-class God {
-  static create() {
-    return [new Man('Adam'), new Woman('Eve')];
+function minSum(arr) {
+  const countPairs = arr.length / 2;
+  const [...pairs] = arr;
+  result = 0;
+
+  pairs.sort((a, b) => a - b);
+
+  for (let i = 0; i < countPairs; i += 1) {
+    result += pairs[i] * pairs[arr.length - i - 1];
   }
+
+  return result;
 }
 
-class Human {
-  constructor(name) {
-    this.name = name;
-  }
-}
-class Man extends Human {
-  constructor(name) {
-    super(name);
-  }
-}
-class Woman extends Human {
-  constructor(name) {
-    super(name);
-  }
-}
-
-let humans = God.create();
-
-console.log(humans[0] instanceof Man); // True
-console.log(humans[1] instanceof Woman); // True
+console.log(minSum([5, 4, 2, 3])); // 22
+// console.log(minSum([12, 6, 10, 26, 3, 24])); // 342
+// console.log(minSum([9, 2, 8, 7, 5, 4, 0, 6])); // 74

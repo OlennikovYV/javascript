07163241020e44e3b61b1a15b62fa13d4b6b9e4f
Yury Olennikov <OlennikovYV@mail.ru>
2022-06-30 08:32:42 +0500

    Get number from string

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,10 @@
-function pickIt(arr) {
-  var odd = [],
-    even = [];
-  for (let el of arr) {
-    (el % 2 === 0 ? even : odd).push(el);
-  }
-
-  return [odd, even];
+function getNumberFromString(s) {
+  return +s
+    .split('')
+    .filter(el => /[0-9]/g.test(el))
+    .join('');
 }
 
-console.dir(pickIt([1, 2])); // [[1], [2]]
-console.dir(pickIt([1, 2, 3])); // [[1, 3], [2]]
-console.dir(pickIt([3, 2, 1])); // [[3, 1], [2]]
-console.dir(pickIt([10, 20, 30])); // [[], [10, 20, 30]]
-console.dir(pickIt([11, 21, 31])); // [[11, 21, 31], []]
-console.dir(pickIt([8, 1, 5, 4, 6, 1, 1])); // [[1, 5, 1, 1], [8, 4, 6]]
-console.dir(pickIt([75, 91, 5, 45])); // [[1, 5, 1, 1], [8, 4, 6]]
+console.log(getNumberFromString('1')); // 1
+console.log(getNumberFromString('123')); // 123
+console.log(getNumberFromString('this is number: 7')); // 7

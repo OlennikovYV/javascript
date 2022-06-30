8bc55dc5c77142f314d2529ec74cde8c0ead6e31
Yury Olennikov <OlennikovYV@mail.ru>
2022-06-30 08:27:32 +0500

    Training JS #10: loop statement --for

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,22 @@
-function minSum(arr) {
-  const countPairs = arr.length / 2;
-  const [...pairs] = arr;
-  result = 0;
-
-  pairs.sort((a, b) => a - b);
-
-  for (let i = 0; i < countPairs; i += 1) {
-    result += pairs[i] * pairs[arr.length - i - 1];
+function pickIt(arr) {
+  var odd = [],
+    even = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    const digit = arr[i];
+    if (digit % 2 === 0) {
+      even.push(digit);
+    } else {
+      odd.push(digit);
+    }
   }
 
-  return result;
+  return [odd, even];
 }
 
-console.log(minSum([5, 4, 2, 3])); // 22
-// console.log(minSum([12, 6, 10, 26, 3, 24])); // 342
-// console.log(minSum([9, 2, 8, 7, 5, 4, 0, 6])); // 74
+console.dir(pickIt([1, 2])); // [[1], [2]]
+console.dir(pickIt([1, 2, 3])); // [[1, 3], [2]]
+console.dir(pickIt([3, 2, 1])); // [[3, 1], [2]]
+console.dir(pickIt([10, 20, 30])); // [[], [10, 20, 30]]
+console.dir(pickIt([11, 21, 31])); // [[11, 21, 31], []]
+console.dir(pickIt([8, 1, 5, 4, 6, 1, 1])); // [[1, 5, 1, 1], [8, 4, 6]]
+console.dir(pickIt([75, 91, 5, 45])); // [[1, 5, 1, 1], [8, 4, 6]]

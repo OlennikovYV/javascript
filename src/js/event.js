fed894e9457f226d64ad74e6139dacaad73d3700
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-01 08:37:59 +0500

    Sum of Cubes

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,16 @@
-function switcheroo(x) {
-  return x.replace(/[ab]/g, x => (x === 'a' ? 'b' : 'a'));
+function sumCubes(n) {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i += 1) {
+    sum = sum + i ** 3;
+  }
+
+  return sum;
 }
 
-console.log(switcheroo('abc')); // 'bac'
-console.log(switcheroo('aaabcccbaaa')); // 'bbbacccabbb'
-console.log(switcheroo('ccccc')); // 'ccccc'
+console.log(sumCubes(1)); // 1
+console.log(sumCubes(2)); // 9
+console.log(sumCubes(3)); // 36
+console.log(sumCubes(4)); // 100
+console.log(sumCubes(10)); // 3025
+console.log(sumCubes(123)); // 58155876

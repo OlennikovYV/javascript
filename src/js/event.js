0a7ab32792ad9225f8c0e4c356227bd2cf4bf08e
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-05 09:02:17 +0500

    Counting Array Elements

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,8 @@
-function nearestSq(n) {
-  return Math.round(n ** 0.5) ** 2;
+function count(array) {
+  return array.reduce((obj, el) => {
+    obj[el] ? (obj[el] += 1) : (obj[el] = 1);
+    return obj;
+  }, {});
 }
 
-console.log(nearestSq(1)); // 1
-console.log(nearestSq(2)); // 1
-console.log(nearestSq(10)); // 9
-console.log(nearestSq(111)); // 121
-console.log(nearestSq(9999)); // 10000
+console.log(count(['a', 'a', 'b', 'b', 'b'])); // { a: 2, b: 3 }

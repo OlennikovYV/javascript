be2eccbee6088e03c1218cdb1447c708220ee923
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-06 08:32:01 +0500

    UEFA EURO 2016

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,13 @@
-function getASCII(c) {
-  return c.charCodeAt();
+function uefaEuro2016(teams, scores) {
+  const players = teams.join(' - ');
+
+  return scores[0] === scores[1]
+    ? `At match ${players}, teams played draw.`
+    : scores[0] > scores[1]
+    ? `At match ${players}, ${teams[0]} won!`
+    : `At match ${players}, ${teams[1]} won!`;
 }
 
-console.log(getASCII('A')); // 65
-console.log(getASCII(' ')); // 32
-console.log(getASCII('!')); // 33
+console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0])); // 'At match Germany - Ukraine, Germany won!'
+console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2])); // 'At match Belgium - Italy, Italy won!'
+console.log(uefaEuro2016(['Portugal', 'Iceland'], [1, 1])); // 'At match Portugal - Iceland, teams played draw.'

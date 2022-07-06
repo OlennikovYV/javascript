04ee3cb47eb721e129862bdfa445bfac4c74e1ae
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-06 08:45:57 +0500

    Thinkful - Number Drills: Blue and red marbles

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,10 @@
-function uefaEuro2016(teams, scores) {
-  const players = teams.join(' - ');
-  const [score1, score2] = scores;
-  const winner = score1 > score2 ? teams[0] : teams[1];
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+  const blueBalls = blueStart - bluePulled;
+  const redBalls = redStart - redPulled;
 
-  return scores[0] === scores[1]
-    ? `At match ${players}, teams played draw.`
-    : `At match ${players}, ${winner} won!`;
+  return blueBalls / (blueBalls + redBalls);
 }
 
-console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0])); // 'At match Germany - Ukraine, Germany won!'
-console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2])); // 'At match Belgium - Italy, Italy won!'
-console.log(uefaEuro2016(['Portugal', 'Iceland'], [1, 1])); // 'At match Portugal - Iceland, teams played draw.'
+console.log(guessBlue(5, 5, 2, 3)); // 0.6
+console.log(guessBlue(5, 7, 4, 3)); // 0.2
+console.log(guessBlue(12, 18, 4, 6)); // 0.4

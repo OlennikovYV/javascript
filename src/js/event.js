78602c6ed55f7c3fb3b0f1b1439e5b9d836fb33b
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-06 08:26:22 +0500

    get ascii value of character

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,7 @@
-function shortenToDate(longDate) {
-  return longDate.split(',')[0];
+function getASCII(c) {
+  return c.charCodeAt();
 }
 
-console.log(shortenToDate('Friday May 2, 9am')); // 'Friday May 2'
-console.log(shortenToDate('Tuesday January 29, 10pm')); // 'Tuesday January 29'
-console.log(shortenToDate('Monday December 25, 10pm')); // 'Monday December 25'
+console.log(getASCII('A')); // 65
+console.log(getASCII(' ')); // 32
+console.log(getASCII('!')); // 33

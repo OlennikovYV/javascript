1afee3efa184dee8e561eb240d942ba512036bd7
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-07 09:04:40 +0500

    Highest Rank Number in an Array

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,29 @@
-function isDivisible(wallLength, pixelSize) {
-  return wallLength % pixelSize === 0;
+function highestRank(arr) {
+  let countDigit = {};
+  let maxCount = 0;
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    const digit = arr[i];
+    if (countDigit[digit]) {
+      countDigit[digit] += 1;
+    } else countDigit[digit] = 1;
+  }
+
+  for (let key in countDigit) {
+    if (countDigit[key] > maxCount) maxCount = countDigit[key];
+  }
+
+  for (let key in countDigit) {
+    if (countDigit[key] === maxCount) {
+      if (Number(key) > result) result = Number(key);
+    }
+  }
+
+  return result;
 }
 
-console.log(isDivisible(4050, 27)); // true
-console.log(isDivisible(4066, 27)); // false
-console.log(isDivisible(10000, 20)); // true
-console.log(isDivisible(10005, 20)); // false
-console.log(isDivisible(10005, 1)); // true
+let arr = [12, 10, 8, 12, 7, 6, 4, 10, 12];
+console.log(highestRank(arr)); // 12
+arr = [1, 1, 2, 2, 3, 3, 4, 4, 7];
+console.log(highestRank(arr)); // 12

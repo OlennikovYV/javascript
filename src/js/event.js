d70cb3ea6f21e6da5c1e2f3e96397d3e2b240295
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-08 08:46:07 +0500

    Number of People in the Bus

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,36 @@
-function numberToPower(number, power) {
-  if (power === 0) return 1;
-  return number * numberToPower(number, (power -= 1));
-}
+var number = function (busStops) {
+  return busStops.reduce((count, people) => {
+    count += people[0];
+    count -= people[1];
+    return count;
+  }, 0);
+};
 
-console.log(numberToPower(4, 2)); // 16
-console.log(numberToPower(10, 4)); // 10000
-console.log(numberToPower(10, 0)); // 1
+console.log(
+  number([
+    [10, 0],
+    [3, 5],
+    [5, 8],
+  ])
+), // 5
+  console.log(
+    number([
+      [3, 0],
+      [9, 1],
+      [4, 10],
+      [12, 2],
+      [6, 1],
+      [7, 10],
+    ])
+  ), // 17
+  console.log(
+    number([
+      [3, 0],
+      [9, 1],
+      [4, 8],
+      [12, 2],
+      [6, 1],
+      [7, 8],
+    ])
+  ), // 21
+  console.log(number([[0, 0]])); // 0

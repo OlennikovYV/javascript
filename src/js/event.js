71a56abcdd809531f9cf440b9cfc6fe75f65623f
Yury <olennikovyv@mail.ru>
2022-07-09 18:25:00 +0500

    Sum Strings as Numbers

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,8 @@
-function validPhoneNumber(phoneNumber) {
-  return /^\([\d]{3}\) [\d]{3}\-\d{4}$/.test(phoneNumber);
+function sumStrings(a, b) {
+  return (BigInt(a) + BigInt(b)).toString();
 }
 
-console.log(validPhoneNumber('(123) 456-7890')); // true
-console.log(validPhoneNumber('(1111)555 2345')); // false
-console.log(validPhoneNumber('(098) 123 4567')); // false
-
-console.log(validPhoneNumber('(1111)555 2345')); // false
-console.log(validPhoneNumber('(098) 123 4567')); // false
-console.log(validPhoneNumber('(123)456-7890')); // false
-console.log(validPhoneNumber('abc(123)456-7890')); // false
-console.log(validPhoneNumber('(123)456-7890abc')); // false
-console.log(validPhoneNumber('abc(123)456-7890abc')); // false
-console.log(validPhoneNumber('abc(123) 456-7890')); // false
+console.log(sumStrings('123', '456')); // '579'
+console.log(
+  sumStrings('712569312664357328695151392', '8100824045303269669937')
+); // 712577413488402631964821329

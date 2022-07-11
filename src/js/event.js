d7c9d48ad2e8e19598b91002bb6cc1babb78e3a6
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-11 08:45:15 +0500

    Sort with Arrow Functions

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,13 @@
-var Ghost = function () {
-  this.colors = ['white', 'yellow', 'purple', 'red'];
-  this.color = this.colors[Math.floor(Math.random() * 4)];
+var OrderPeople = function (people) {
+  return people.sort((a, b) => a.age - b.age); //complete this function
 };
 
-console.log(new Ghost().color);
-console.log(new Ghost().color);
-console.log(new Ghost().color);
-console.log(new Ghost().color);
-console.log(new Ghost().color);
+console.log(
+  OrderPeople([
+    { age: 83, name: 'joel' },
+    { age: 46, name: 'roger' },
+    { age: 99, name: 'vinny' },
+    { age: 26, name: 'don' },
+    { age: 74, name: 'brendan' },
+  ])
+); // [{age:26,name:"don"},{age:46,name:"roger"},{age:74,name:"brendan"},{age:83,name:"joel"},{age:99,name:"vinny"}]

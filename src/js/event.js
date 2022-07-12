34bcebedba085cb8fae942549e2afed4d097e8be
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-12 08:32:16 +0500

    ES6 string addition

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,7 @@
-function cubeOdd(arr) {
-  let result = 0;
-
-  for (let i = 0; i < arr.length; i += 1) {
-    if (isNaN(arr[i])) return undefined;
-    if (arr[i] % 2 !== 0) result += arr[i] ** 3;
-  }
-
-  return result;
+function joinStrings(string1, string2) {
+  return `${string1} ${string2}`;
 }
 
-console.log(cubeOdd([1, 2, 3, 4])); // 28
-console.log(cubeOdd([-3, -2, 2, 3])); // 0
-console.log(cubeOdd(['a', 12, 9, 'z', 42])); // undefined
+console.log(joinStrings('string1', 'string2')); // 'string1 string2'
+console.log(joinStrings('testing', 'testing')); // 'testing testing'
+console.log(joinStrings(134, 234)); // '134 234'

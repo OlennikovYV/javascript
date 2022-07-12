13fd1e2db4c62bea49c89985862a1be3f4858f37
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-12 08:45:26 +0500

    Most digits refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,10 @@
 function findLongest(array) {
-  let lengthNumbers = array.map(num => String(num).length);
-  const maxLengthNumber = Math.max(...lengthNumbers);
-  lengthNumbers = array.filter(num => String(num).length === maxLengthNumber);
-
-  return lengthNumbers[0];
+  return array.reduce((resNum, num) => {
+    return String(resNum).length < String(num).length ? num : resNum;
+  });
 }
 
 console.log(findLongest([1, 10, 100])); // 100
 console.log(findLongest([9000, 8, 800])); // 9000
 console.log(findLongest([8, 900, 500])); // 900
-console.log(findLongest([122248, 55056, 222894, 195426, 122292])); // 900
+console.log(findLongest([122248, 55056, 222894, 195426, 122292])); // 122248

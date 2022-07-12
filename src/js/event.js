90c551bfbd6b07f718d49982e553be07625168d9
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-12 08:39:25 +0500

    Most digits

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,12 @@
-function joinStrings(string1, string2) {
-  return `${string1} ${string2}`;
+function findLongest(array) {
+  let lengthNumbers = array.map(num => String(num).length);
+  const maxLengthNumber = Math.max(...lengthNumbers);
+  lengthNumbers = array.filter(num => String(num).length === maxLengthNumber);
+
+  return lengthNumbers[0];
 }
 
-console.log(joinStrings('string1', 'string2')); // 'string1 string2'
-console.log(joinStrings('testing', 'testing')); // 'testing testing'
-console.log(joinStrings(134, 234)); // '134 234'
+console.log(findLongest([1, 10, 100])); // 100
+console.log(findLongest([9000, 8, 800])); // 9000
+console.log(findLongest([8, 900, 500])); // 900
+console.log(findLongest([122248, 55056, 222894, 195426, 122292])); // 900

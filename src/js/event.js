4b76d0a12f137cd33d5360bbfba780287499638a
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-12 08:23:58 +0500

    Sum of Odd Cubed Numbers

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,14 @@
-String.prototype.isUpperCase = function () {
-  return this.toUpperCase() === this.valueOf();
-};
+function cubeOdd(arr) {
+  let result = 0;
 
-console.log('c'.isUpperCase()); // false
-console.log('C'.isUpperCase()); // true
-console.log('hello I AM DONALD'.isUpperCase()); // false
-console.log('HELLO I AM DONALD'.isUpperCase()); // true
-console.log('ACSKLDFJSgSKLDFJSKLDFJ'.isUpperCase()); // false
-console.log('ACSKLDFJSGSKLDFJSKLDFJ'.isUpperCase()); // true
+  for (let i = 0; i < arr.length; i += 1) {
+    if (isNaN(arr[i])) return undefined;
+    if (arr[i] % 2 !== 0) result += arr[i] ** 3;
+  }
+
+  return result;
+}
+
+console.log(cubeOdd([1, 2, 3, 4])); // 28
+console.log(cubeOdd([-3, -2, 2, 3])); // 0
+console.log(cubeOdd(['a', 12, 9, 'z', 42])); // undefined

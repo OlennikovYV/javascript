7045e80b374bacc27bb39f285214b48007889714
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-13 08:42:08 +0500

    Training JS #6: Basic data types--Boolean and conditional statements if..else

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,51 @@
-function validateCode(code) {
-  return /^[123]/.test(code);
+function trueOrFalse(val) {
+  return val ? 'true' : 'false';
 }
 
-console.log(validateCode(123)); // true
-console.log(validateCode(248)); // true
-console.log(validateCode(8)); // false
-console.log(validateCode(321)); // true
-console.log(validateCode(9453)); // false
+const a = 1;
+const b = 2;
+const c = 1;
+
+console.log(trueOrFalse(a > b)); //"false"
+console.log(trueOrFalse(a === b)); //"false"
+console.log(trueOrFalse(a < b)); //"true"
+console.log(trueOrFalse(a !== b)); //"true"
+console.log(trueOrFalse(b > c)); //"true"
+console.log(trueOrFalse(b === c)); //"false"
+console.log(trueOrFalse(b < c)); //"false"
+console.log(trueOrFalse(b !== c)); //"true"
+console.log(trueOrFalse(a === c)); //"true"
+console.log(trueOrFalse(a !== c)); //"false"
+//test for logical operators and bitwise operators
+const t = true;
+const f = false;
+
+console.log(trueOrFalse(t && f)); //"false"
+console.log(trueOrFalse(f && f)); //"false"
+console.log(trueOrFalse(t && t)); //"true"
+console.log(trueOrFalse(t || f)); //"true"
+console.log(trueOrFalse(t || t)); //"true"
+console.log(trueOrFalse(f || f)); //"false"
+console.log(trueOrFalse(t & f)); //"false"
+console.log(trueOrFalse(t & t)); //"true"
+console.log(trueOrFalse(f & f)); //"false"
+console.log(trueOrFalse(t | f)); //"true"
+console.log(trueOrFalse(t | t)); //"true"
+console.log(trueOrFalse(f | f)); //"false"
+console.log(trueOrFalse(!t)); //"false"
+console.log(trueOrFalse(!f)); //"true"
+console.log(trueOrFalse(t ^ f)); //"true"
+console.log(trueOrFalse(t ^ t)); //"false"
+console.log(trueOrFalse(f ^ f)); //"false"
+//test for implicit conversion
+console.log(trueOrFalse(true)); //"true"
+console.log(trueOrFalse(123)); //"true"
+console.log(trueOrFalse('123')); //"true"
+console.log(trueOrFalse(['123'])); //"true"
+console.log(trueOrFalse('false')); //"true"
+console.log(trueOrFalse(false)); //"false"
+console.log(trueOrFalse(0)); //"false"
+console.log(trueOrFalse('')); //"false"
+console.log(trueOrFalse(null)); //"false"
+console.log(trueOrFalse([].length)); //"false"
+console.log(trueOrFalse(undefined)); //"false"

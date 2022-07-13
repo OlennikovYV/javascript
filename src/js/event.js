05b86cfcb0b820405943f3cac4b7516858eb6b2e
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-13 08:57:34 +0500

    Geometry Basics: Distance between points in 2D

--- src/js/event.js
+++ src/js/event.js
@@ -1,51 +1,17 @@
-function trueOrFalse(val) {
-  return Boolean(val).toString();
+function Point(a, b) {
+  this.x = a;
+  this.y = b;
 }
 
-const a = 1;
-const b = 2;
-const c = 1;
+function distanceBetweenPoints(a, b) {
+  return +Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2)).toFixed(6);
+}
 
-console.log(trueOrFalse(a > b)); //"false"
-console.log(trueOrFalse(a === b)); //"false"
-console.log(trueOrFalse(a < b)); //"true"
-console.log(trueOrFalse(a !== b)); //"true"
-console.log(trueOrFalse(b > c)); //"true"
-console.log(trueOrFalse(b === c)); //"false"
-console.log(trueOrFalse(b < c)); //"false"
-console.log(trueOrFalse(b !== c)); //"true"
-console.log(trueOrFalse(a === c)); //"true"
-console.log(trueOrFalse(a !== c)); //"false"
-//test for logical operators and bitwise operators
-const t = true;
-const f = false;
-
-console.log(trueOrFalse(t && f)); //"false"
-console.log(trueOrFalse(f && f)); //"false"
-console.log(trueOrFalse(t && t)); //"true"
-console.log(trueOrFalse(t || f)); //"true"
-console.log(trueOrFalse(t || t)); //"true"
-console.log(trueOrFalse(f || f)); //"false"
-console.log(trueOrFalse(t & f)); //"false"
-console.log(trueOrFalse(t & t)); //"true"
-console.log(trueOrFalse(f & f)); //"false"
-console.log(trueOrFalse(t | f)); //"true"
-console.log(trueOrFalse(t | t)); //"true"
-console.log(trueOrFalse(f | f)); //"false"
-console.log(trueOrFalse(!t)); //"false"
-console.log(trueOrFalse(!f)); //"true"
-console.log(trueOrFalse(t ^ f)); //"true"
-console.log(trueOrFalse(t ^ t)); //"false"
-console.log(trueOrFalse(f ^ f)); //"false"
-//test for implicit conversion
-console.log(trueOrFalse(true)); //"true"
-console.log(trueOrFalse(123)); //"true"
-console.log(trueOrFalse('123')); //"true"
-console.log(trueOrFalse(['123'])); //"true"
-console.log(trueOrFalse('false')); //"true"
-console.log(trueOrFalse(false)); //"false"
-console.log(trueOrFalse(0)); //"false"
-console.log(trueOrFalse('')); //"false"
-console.log(trueOrFalse(null)); //"false"
-console.log(trueOrFalse([].length)); //"false"
-console.log(trueOrFalse(undefined)); //"false"
+console.log(distanceBetweenPoints(new Point(3, 3), new Point(3, 3)));
+// 0
+console.log(distanceBetweenPoints(new Point(1, 6), new Point(4, 2)));
+// 5
+console.log(
+  distanceBetweenPoints(new Point(-10.2, 12.5), new Point(0.3, 14.7))
+);
+// 10.728001

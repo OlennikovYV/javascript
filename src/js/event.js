c0d3f20a7aff3ca12f59b4ed0f8d0985a50b1234
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-14 08:33:20 +0500

    Classy Classes

--- src/js/event.js
+++ src/js/event.js
@@ -1,22 +1,8 @@
-function isChar(symbol) {
-  return /[a-zA-Z]/.test(symbol);
+class Person {
+  constructor(name, age) {
+    this.info = `${name}s age is ${age}`;
+  }
 }
 
-function checkRegister(a, b) {
-  return a === a.toUpperCase() ? 'U' : 'L';
-}
-
-function sameCase(a, b) {
-  if (!isChar(a) || !isChar(b)) return -1;
-
-  return checkRegister(a) === checkRegister(b) ? 1 : 0;
-}
-
-console.log(sameCase('C', 'B')); // 1
-console.log(sameCase('b', 'a')); // 1
-console.log(sameCase('d', 'd')); // 1
-console.log(sameCase('A', 's')); // 0
-console.log(sameCase('c', 'B')); // 0
-console.log(sameCase('b', 'Z')); // 0
-console.log(sameCase('\t', 'Z')); // -1
-console.log(sameCase('H', ':')); // -1
+const john = new Person('john', 34);
+console.log(john.info); // 'johns age is 34'

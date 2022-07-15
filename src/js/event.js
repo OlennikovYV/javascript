4946cbc263d3be679c3278302ab159ab0576a123
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-15 08:34:47 +0500

    Tidy Number (Special Numbers Series #9)

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,19 @@
-function spacify(str) {
-  return str.split('').join(' ');
+function tidyNumber(n) {
+  const arrayDigit = String(n).split('');
+  let result = true;
+
+  for (let i = 0; i < arrayDigit.length - 1; i += 1) {
+    if (+arrayDigit[i] > +arrayDigit[i + 1]) {
+      result = false;
+      break;
+    }
+  }
+
+  return result;
 }
 
-console.log(spacify('hello world')); // 'h e l l o   w o r l d'
-console.log(spacify('12345')); // '1 2 3 4 5'
+console.log(tidyNumber(12)); // true
+console.log(tidyNumber(102)); // false
+console.log(tidyNumber(9672)); // false
+console.log(tidyNumber(2789)); // true
+console.log(tidyNumber(2335)); // true

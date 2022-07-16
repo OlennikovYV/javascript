c5479e80444df5b38d06fe070871ce91cd14c9e9
Yury <olennikovyv@mail.ru>
2022-07-16 22:10:09 +0500

    The Office I - Outed

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,66 @@
-function integrate(coefficient, exponent) {
-  return `${coefficient / ++exponent}x^${exponent}`;
+function outed(meet, boss) {
+  let score = 0;
+  let countPeople = 0;
+
+  for (let name in meet) {
+    score += meet[name] * (name === boss ? 2 : 1);
+    countPeople += 1;
+  }
+
+  return score / countPeople <= 5 ? 'Get Out Now!' : 'Nice Work Champ!';
 }
 
-console.log(integrate(3, 2)); // '1x^3'
-console.log(integrate(12, 5)); // '2x^6'
-console.log(integrate(20, 1)); // '10x^2'
-console.log(integrate(40, 3)); // '10x^4'
-console.log(integrate(90, 2)); // '30x^3'
+console.log(
+  outed(
+    {
+      tim: 0,
+      jim: 2,
+      randy: 0,
+      sandy: 7,
+      andy: 0,
+      katie: 5,
+      laura: 1,
+      saajid: 2,
+      alex: 3,
+      john: 2,
+      mr: 0,
+    },
+    'laura'
+  )
+); // 'Get Out Now!'
+console.log(
+  outed(
+    {
+      tim: 1,
+      jim: 3,
+      randy: 9,
+      sandy: 6,
+      andy: 7,
+      katie: 6,
+      laura: 9,
+      saajid: 9,
+      alex: 9,
+      john: 9,
+      mr: 8,
+    },
+    'katie'
+  )
+); // 'Nice Work Champ!'
+console.log(
+  outed(
+    {
+      tim: 2,
+      jim: 4,
+      randy: 0,
+      sandy: 5,
+      andy: 8,
+      katie: 6,
+      laura: 2,
+      saajid: 2,
+      alex: 3,
+      john: 2,
+      mr: 8,
+    },
+    'john'
+  )
+); // 'Get Out Now!'

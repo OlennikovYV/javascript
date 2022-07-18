9156c7fdc0c10fe8e82a2e09ff20196bc1dfbf41
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-18 08:37:08 +0500

    Reverse every other word in the string

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,21 @@
-function splitAndMerge(string, separator) {
-  return string
-    .split(' ')
-    .map(word => word.split('').join(separator))
-    .join(' ');
+function reverse(str) {
+  return (
+    str.trim() &&
+    str
+      .split(' ')
+      .map((word, index) => {
+        if (index % 2 !== 0) {
+          return word.split('').reverse().join('');
+        }
+
+        return word;
+      })
+      .join(' ')
+  );
 }
 
-console.log(splitAndMerge('My name is John', ' ')); // 'M y n a m e i s J o h n'
-console.log(splitAndMerge('My name is John', '-')); // 'M-y n-a-m-e i-s J-o-h-n'
-console.log(splitAndMerge('Hello World!', '.')); // 'H.e.l.l.o W.o.r.l.d.!'
-console.log(splitAndMerge('Hello World!', ',')); // 'H,e,l,l,o W,o,r,l,d,!'
+console.log(reverse('  ')); // ''
+console.log(reverse('Reverse this string, please!'));
+// 'Reverse siht string, !esaelp'
+console.log(reverse("I really don't like reversing strings!"));
+// "I yllaer don't ekil reversing !sgnirts"

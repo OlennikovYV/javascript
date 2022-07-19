1ab27a23448d27f1199e4c15ee8bb7ffc87028ee
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-19 08:31:08 +0500

    Character Frequency

--- src/js/event.js
+++ src/js/event.js
@@ -1,18 +1,9 @@
-class FileNameExtractor {
-  static extractFileName(dirtyFileName) {
-    return dirtyFileName.replace(/^\d+_|\.[^.]+$/g, '');
-  }
+function charFreq(message) {
+  return message.split('').reduce((object, char) => {
+    object[char] ? (object[char] += 1) : (object[char] = 1);
+    return object;
+  }, {});
 }
 
-console.log(
-  FileNameExtractor.extractFileName(
-    '1_FILE_NAME.EXTENSION.OTHEREXTENSIONadasdassdassds34'
-  )
-);
-// 'FILE_NAME.EXTENSION'
-console.log(
-  FileNameExtractor.extractFileName(
-    '1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION'
-  )
-);
-// 'FILE_NAME.EXTENSION'
+console.log(charFreq('I like cats'));
+// {a: 1, ' ': 2, c: 1, e: 1, I: 1, k: 1, l: 1, i: 1, s: 1, t: 1}

7d4e1a674bc2cf4b0f1a5c9a290ca026ae495e03
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-20 10:02:30 +0500

    Decipher this!

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,27 @@
-function sortMyString(s) {
-  const oddString = s
-    .split('')
-    .filter((char, index) => index % 2 === 0)
-    .join('');
+function decipherThis(str) {
+  const array = str.split(' ');
+  const firstChar = array.map(word => parseInt(word));
+  const wordChar = array.map(word => {
+    let chars = (word.match(/[a-z]+/gi) || []).join('');
+    if (chars) {
+      if (chars.length > 1) {
+        const first = chars.slice(0, 1);
+        const last = chars.slice(-1);
+        chars = last + chars.slice(1, -1) + first;
+      }
+    } else chars = '';
 
-  const evenString = s
-    .split('')
-    .filter((char, index) => index % 2 !== 0)
-    .join('');
+    return chars;
+  });
 
-  return oddString + ' ' + evenString;
+  return array
+    .map(
+      (word, index) => String.fromCharCode(firstChar[index]) + wordChar[index]
+    )
+    .join(' ');
 }
 
-console.log(sortMyString('CodeWars')); // 'CdWr oeas'
-console.log(sortMyString("YCOLUE'VREER")); // "YOU'RE CLEVER"
+console.log(
+  decipherThis('72eva 97 103o 97t 116sih 97dn 115ee 104wo 121uo 100o')
+);
+// 'Have a go at this and see how you do'

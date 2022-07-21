a888d155026c241c239421d0c7db808b8c250bda
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-21 08:45:55 +0500

    Grader

--- src/js/event.js
+++ src/js/event.js
@@ -1,31 +1,36 @@
-function upArray(arr) {
-  let index = arr.length - 1;
-  let rest = 1;
+function grader(score) {
+  let ball;
 
-  if (!arr.length || arr.every(digit => digit >= 0 && digit <= 9) === false)
-    return null;
-
-  while (index >= 0 && rest) {
-    const digit = arr[index] + rest;
-    arr[index] = digit > 9 ? digit - 10 : digit;
-    rest = digit > 9;
-    index -= 1;
+  switch (true) {
+    case score < 0.6:
+    case score > 1:
+      ball = 'F';
+      break;
+    case score < 0.7:
+      ball = 'D';
+      break;
+    case score < 0.8:
+      ball = 'C';
+      break;
+    case score < 0.9:
+      ball = 'B';
+      break;
+    case score <= 1:
+      ball = 'A';
+      break;
   }
 
-  if (rest) arr.unshift(1);
-
-  return arr;
+  return ball;
 }
 
-console.log(upArray([9, 9, 9])); // [1, 0, 0, 0]
-console.log(upArray([2, 3, 9])); // [2, 4, 0]
-console.log(upArray([4, 3, 2, 5])); // [4, 3, 2, 6]
-console.log(upArray([1, -9])); // null
-console.log(upArray([])); // null
-console.log(
-  upArray([
-    9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 7, 5, 3, 2, 6, 7, 8,
-    4, 2, 4, 2, 6, 7, 8, 7, 4, 5, 2, 1,
-  ])
-);
-//[9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 7, 5, 3, 2, 6, 7, 8, 4, 2, 4, 2, 6, 7, 8, 7, 4, 5, 2, 2]
+console.log(grader(0)); // 'F'
+console.log(grader(0.6)); // 'D'
+console.log(grader(0.65)); // 'D'
+console.log(grader(0.7)); // 'C'
+console.log(grader(0.73)); // 'C'
+console.log(grader(0.8)); // 'B'
+console.log(grader(0.87)); // 'B'
+console.log(grader(0.9)); // 'A'
+console.log(grader(0.99)); // 'A'
+console.log(grader(1)); // 'A'
+console.log(grader(1.1)); // 'F'

048eb755f5c7a19d38c82fda439fd8a1bd83c850
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-22 08:54:48 +0500

    Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,64 @@
-function main(verb, noun) {
-  return verb + noun;
+function countDevelopers(list) {
+  return list.reduce((sum, man) => {
+    if (man.continent === 'Europe' && man.language === 'JavaScript')
+      return (sum += 1);
+    return sum;
+  }, 0);
 }
 
-console.log(main('take ', 'item')); // 'take item'
-console.log(main('use ', 'sword')); // 'use sword'
+const list1 = [
+  {
+    firstName: 'Noah',
+    lastName: 'M.',
+    country: 'Switzerland',
+    continent: 'Europe',
+    age: 19,
+    language: 'JavaScript',
+  },
+  {
+    firstName: 'Maia',
+    lastName: 'S.',
+    country: 'Tahiti',
+    continent: 'Oceania',
+    age: 28,
+    language: 'JavaScript',
+  },
+  {
+    firstName: 'Shufen',
+    lastName: 'L.',
+    country: 'Taiwan',
+    continent: 'Asia',
+    age: 35,
+    language: 'HTML',
+  },
+  {
+    firstName: 'Sumayah',
+    lastName: 'M.',
+    country: 'Tajikistan',
+    continent: 'Asia',
+    age: 30,
+    language: 'CSS',
+  },
+];
+
+console.log(countDevelopers(list1)); // 1
+
+const list2 = [
+  {
+    firstName: 'Oliver',
+    lastName: 'Q.',
+    country: 'Australia',
+    continent: 'Oceania',
+    age: 19,
+    language: 'HTML',
+  },
+  {
+    firstName: 'Lukas',
+    lastName: 'R.',
+    country: 'Austria',
+    continent: 'Europe',
+    age: 89,
+    language: 'HTML',
+  },
+];
+console.log(countDevelopers(list2)); // 0

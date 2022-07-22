23d326c7cf911cbedaebc1f6c77c51924a93f28d
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-22 08:41:28 +0500

    Kebabize

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,11 @@
-function combineNames(...names) {
-  return names.join(' ');
+function kebabize(str) {
+  return str
+    .replace(/\d/g, '')
+    .split(/(?=[A-Z]+)/g)
+    .join('-')
+    .toLowerCase();
 }
 
-console.log(combineNames('James', 'Stevens')); // 'James Stevens'
+console.log(kebabize('myCamelCasedString')); // 'my-camel-cased-string'
+console.log(kebabize('myCamelHas3Humps')); // 'my-camel-has-humps'
+console.log(kebabize('9p16m')); // 'pm'

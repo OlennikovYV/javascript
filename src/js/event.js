008d7ae51b268852fb849e1fccfc4e196bc677af
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-25 08:29:16 +0500

    Leonardo Dicaprio and Oscars refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,7 @@
 function leo(oscar) {
-  if (oscar < 86 || oscar === 87) return 'When will you give Leo an Oscar?';
   if (oscar === 86) return 'Not even for Wolf of wallstreet?!';
   if (oscar === 88) return 'Leo finally won the oscar! Leo is happy';
+  if (oscar < 88) return 'When will you give Leo an Oscar?';
   if (oscar > 88) return 'Leo got one already!';
 }
 

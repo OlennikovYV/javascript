d55270b46a3f5af4d7fe1c9a3e1e282a7fb207d6
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-25 08:34:16 +0500

    Cat and Mouse - Easy Version

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,7 @@
-function leo(oscar) {
-  if (oscar === 86) return 'Not even for Wolf of wallstreet?!';
-  if (oscar === 88) return 'Leo finally won the oscar! Leo is happy';
-  if (oscar < 88) return 'When will you give Leo an Oscar?';
-  if (oscar > 88) return 'Leo got one already!';
+function catMouse(x) {
+  return x.replace(/^C|m$/g, '').length > 3 ? 'Escaped!' : 'Caught!';
 }
 
-console.log(leo(89)); // 'Leo got one already!'
-console.log(leo(88)); // 'Leo finally won the oscar! Leo is happy'
-console.log(leo(87)); // 'When will you give Leo an Oscar?'
-console.log(leo(86)); // 'Not even for Wolf of wallstreet?!'
+console.log(catMouse('C....m')); // 'Escaped!'
+console.log(catMouse('C..m')); // 'Caught!'
+console.log(catMouse('C.....m')); // 'Escaped!'

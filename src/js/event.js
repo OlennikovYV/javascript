57b84a944567412d057edeff1d84dcec30322ba1
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-26 08:38:13 +0500

    Folding your way to the moon

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,15 @@
-function catMouse(x) {
-  return /C.{0,3}m/.test(x) ? 'Caught!' : 'Escaped!';
+function foldTo(distance) {
+  let widthPaper = 0.0001;
+  let count = 0;
+
+  if (distance < 0) return null;
+
+  while (widthPaper < distance) {
+    widthPaper *= 2;
+    count += 1;
+  }
+  return count;
 }
 
-console.log(catMouse('C....m')); // 'Escaped!'
-console.log(catMouse('C..m')); // 'Caught!'
-console.log(catMouse('C.....m')); // 'Escaped!'
+console.log(foldTo(384000000)); // 42
+console.log(foldTo(-1)); // null

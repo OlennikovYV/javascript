b0766be293773730c9878a28cdb85d329eb8ba69
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-26 08:47:25 +0500

    Longest vowel chain

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,11 @@
-function foldTo(distance) {
-  let widthPaper = 0.0001;
-  let count = 0;
-
-  if (distance < 0) return null;
-
-  while (widthPaper < distance) {
-    widthPaper *= 2;
-    count += 1;
-  }
-  return count;
+function solve(s) {
+  return Math.max(...s.match(/[oeaiou]+/g).map(array => array.length));
 }
 
-console.log(foldTo(384000000)); // 42
-console.log(foldTo(-1)); // null
+console.log(solve('codewarriors')); // 2
+console.log(solve('suoidea')); // 3
+console.log(solve('ultrarevolutionariees')); // 3
+console.log(solve('strengthlessnesses')); // 1
+console.log(solve('cuboideonavicuare')); // 2
+console.log(solve('chrononhotonthuooaos')); // 5
+console.log(solve('iiihoovaeaaaoougjyaw')); // 8

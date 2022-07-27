eb5b34df26dd64b0927ad02ac0b9517937c5b770
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-27 12:28:25 +0500

    Tortoise racing

--- src/js/event.js
+++ src/js/event.js
@@ -1,22 +1,28 @@
-function nbMonths(
-  startPriceOld,
-  startPriceNew,
-  savingperMonth,
-  percentLossByMonth
-) {
-  let Savings = 0;
-  let countMonth = 0;
+function race(v1, v2, g) {
+  let diff;
+  let hours, minutes, seconds;
 
-  while (startPriceOld + Savings < startPriceNew) {
-    if (countMonth % 2 === 1) percentLossByMonth += 0.5;
-    Savings += savingperMonth;
-    startPriceOld -= startPriceOld * (percentLossByMonth / 100);
-    startPriceNew -= startPriceNew * (percentLossByMonth / 100);
-    countMonth += 1;
-  }
+  if (v1 > v2) return null;
 
-  return [countMonth, Math.round(Savings + startPriceOld - startPriceNew)];
+  diff = g / (v2 - v1);
+  hours = Math.trunc(diff);
+  minutes = (diff - hours) * 60;
+  seconds = (minutes - Math.trunc(minutes + 001)) * 60;
+
+  return [
+    Math.trunc(hours),
+    Math.trunc(minutes + 0.0001),
+    Math.floor(seconds + 0.0001),
+  ];
 }
 
-console.log(nbMonths(2000, 8000, 1000, 1.5)); // [6, 766]
-console.log(nbMonths(12000, 8000, 1000, 1.5)); // [0, 4000]
+console.log(race(920, 850, 70)); // null
+console.log(race(720, 850, 70)); // [0, 32, 18]
+console.log(race(80, 91, 37)); // [3, 21, 49]
+console.log(race(80, 100, 40)); // [2, 0, 0]
+console.log(race(720, 850, 37)); // [0, 17, 4]
+console.log(race(643, 663, 67)); // [3, 21, 0]
+console.log(race(404, 569, 66)); // [0, 24, 0]
+console.log(race(820, 850, 550)); // [18, 20, 0]
+console.log(race(257, 443, 107)); // [0, 34, 30]
+console.log(race(271, 793, 116)); // [0, 13, 20]

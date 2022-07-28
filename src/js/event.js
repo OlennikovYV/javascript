16510caa48e75213e80c4ee3b8f20daa835fe93f
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-28 08:34:16 +0500

    Palindrome Strings

--- src/js/event.js
+++ src/js/event.js
@@ -1,21 +1,18 @@
-function race(v1, v2, g) {
-  let time = g / (v2 - v1);
-  return v2 > v1
-    ? [
-        Math.floor(time),
-        Math.floor((time * 60) % 60),
-        Math.floor((time * 3600) % 60),
-      ]
-    : null;
+function isPalindrome(line) {
+  const isNumber = typeof line === 'number';
+  let word = String(line).split('');
+  word = word.reverse().join('');
+
+  if (isNumber) {
+    word = Number(word);
+  }
+
+  return word === line;
 }
 
-console.log(race(920, 850, 70)); // null
-console.log(race(720, 850, 70)); // [0, 32, 18]
-console.log(race(80, 91, 37)); // [3, 21, 49]
-console.log(race(80, 100, 40)); // [2, 0, 0]
-console.log(race(720, 850, 37)); // [0, 17, 4]
-console.log(race(643, 663, 67)); // [3, 21, 0]
-console.log(race(404, 569, 66)); // [0, 24, 0]
-console.log(race(820, 850, 550)); // [18, 20, 0]
-console.log(race(257, 443, 107)); // [0, 34, 30]
-console.log(race(271, 793, 116)); // [0, 13, 20]
+console.log(isPalindrome('anna')); // true
+console.log(isPalindrome('walter')); // false
+console.log(isPalindrome(12321)); // true
+console.log(isPalindrome(123456)); // false
+console.log(isPalindrome('.')); // true
+console.log(isPalindrome('.!!.')); // true

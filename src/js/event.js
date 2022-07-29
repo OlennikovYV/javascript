875c49d7651e761f011efb9af9ae433011886f8e
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-29 09:26:29 +0500

    Going to the cinema

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,20 @@
-function remove(string) {
-  return string.replace(/!+$/g, '');
+function movie(card, ticket, perc) {
+  let percent = perc;
+  let systemA = ticket;
+  let systemB = card + ticket * percent;
+  let count = 1;
+
+  while (systemA <= Math.ceil(systemB)) {
+    count += 1;
+    percent *= perc;
+    systemA += ticket;
+    systemB += ticket * percent;
+  }
+
+  return count;
 }
 
-console.log(remove('Hi!')); // 'Hi'
-console.log(remove('Hi!!!')); // 'Hi'
-console.log(remove('!Hi')); // '!Hi'
-console.log(remove('!Hi!')); // '!Hi'
-console.log(remove('Hi! Hi!')); // 'Hi! Hi'
-console.log(remove('Hi')); // 'Hi'
+console.log(movie(500, 15, 0.9)); // 43
+console.log(movie(100, 10, 0.95)); // 24
+console.log(movie(0, 10, 0.95)); // 2
+console.log(movie(881404, 8, 0.32)); // 110177

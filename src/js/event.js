f6e1e29c6fa75a78d812cc6c92dc86faa24b8c16
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-29 08:35:51 +0500

    Exclamation marks series #2: Remove all exclamation marks from the end of sentence

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,10 @@
-function fuelPrice(litres, pricePerLitre) {
-  const discount = Math.min(Math.floor(litres / 2) * 0.05, 0.25);
-  const price = litres * (pricePerLitre - discount);
-
-  return Math.round(price * 100) / 100;
+function remove(string) {
+  return string.replace(/!+$/g, '');
 }
 
-console.log(fuelPrice(5, 1.23)); // 5.65
-console.log(fuelPrice(8, 2.5)); // 18.4
-console.log(fuelPrice(5, 5.6)); // 27.5
-console.log(fuelPrice(158, 18.67)); // 2910.36
+console.log(remove('Hi!')); // 'Hi'
+console.log(remove('Hi!!!')); // 'Hi'
+console.log(remove('!Hi')); // '!Hi'
+console.log(remove('!Hi!')); // '!Hi'
+console.log(remove('Hi! Hi!')); // 'Hi! Hi'
+console.log(remove('Hi')); // 'Hi'

d05dab07f1fd0af6d48d1d2a6b9fd4db7c942d3a
Yury <olennikovyv@mail.ru>
2022-07-30 22:28:15 +0500

    Polish alphabet

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,22 @@
-// Print "Hello World!" to the screen
-class Solution {
-  static main() {
-    console.log('Hello World!');
-  }
+function correctPolishLetters(string) {
+  let convert = {
+    ą: 'a',
+    ć: 'c',
+    ę: 'e',
+    ł: 'l',
+    ń: 'n',
+    ó: 'o',
+    ś: 's',
+    ź: 'z',
+    ż: 'z',
+  };
+
+  return string
+    .split('')
+    .map(char => (convert[char] ? convert[char] : char))
+    .join('');
 }
 
-Solution.main('parameter1', 'parameter2');
+console.log(correctPolishLetters('Jędrzej Błądziński')); // 'Jedrzej Bladzinski'
+console.log(correctPolishLetters('Lech Wałęsa')); // 'Lech Walesa');
+console.log(correctPolishLetters('Maria Skłodowska-Curie')); // 'Maria Sklodowska-Curie'

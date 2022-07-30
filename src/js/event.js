d560be0aeb2c78ce66873d8de6ae6eeed8ab448d
Yury <olennikovyv@mail.ru>
2022-07-30 22:21:25 +0500

    Classic Hello World

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,8 @@
-function twoDecimalPlaces(n) {
-  return parseFloat(n.toFixed(2));
+// Print "Hello World!" to the screen
+class Solution {
+  static main() {
+    console.log('Hello World!');
+  }
 }
 
-console.log(twoDecimalPlaces(4.659725356)); // 4.66,
-console.log(twoDecimalPlaces(173735326.3783732637948948)); // 173735326.38,
-console.log(twoDecimalPlaces(4.653725356)); // 4.65,
+Solution.main('parameter1', 'parameter2');

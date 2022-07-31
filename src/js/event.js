2fb2312f5dd308b0bb9c13827f2129569881474b
Yury <olennikovyv@mail.ru>
2022-07-31 18:19:12 +0500

    Basic Calculator

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,32 @@
-function correctPolishLetters(string) {
-  let convert = {
-    ą: 'a',
-    ć: 'c',
-    ę: 'e',
-    ł: 'l',
-    ń: 'n',
-    ó: 'o',
-    ś: 's',
-    ź: 'z',
-    ż: 'z',
-  };
+function calculate(num1, operation, num2) {
+  let result;
 
-  return string.replace(/[ąćęłńóśźż]/g, char => convert[char]);
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '/':
+      num2 === 0 ? (result = null) : (result = num1 / num2);
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    default:
+      result = null;
+  }
+
+  return result;
 }
 
-console.log(correctPolishLetters('Jędrzej Błądziński')); // 'Jedrzej Bladzinski'
-console.log(correctPolishLetters('Lech Wałęsa')); // 'Lech Walesa');
-console.log(correctPolishLetters('Maria Skłodowska-Curie')); // 'Maria Sklodowska-Curie'
+console.log(calculate(3.2, '+', 8)); // 11.2
+console.log(calculate(3.2, '-', 8)); // -4.8
+console.log(calculate(3.2, '/', 8)); // 0.4
+console.log(calculate(3.2, '*', 8)); // 25.6
+console.log(calculate(-3, '+', 0)); // -3
+console.log(calculate(-3, '-', 0)); // -3
+console.log(calculate(-3, '/', 0)); // null
+console.log(calculate(-3, '*', 0)); // 0
+console.log(calculate(-3, 'w', 0)); // null

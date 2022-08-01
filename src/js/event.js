aa5f4e2036490fa87640dac3f74e5baaaeaafc73
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-01 08:41:45 +0500

    Divide and Conquer

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,12 @@
-const factorial = n => {
-  return n === 0 ? 1 : n * factorial(n - 1);
-};
+function divCon(x) {
+  return x.reduce((sum, digit) => {
+    if (typeof digit === 'string') sum -= Number(digit);
+    if (typeof digit === 'number') sum += digit;
 
-console.log(factorial(0)); // 1
-console.log(factorial(1)); // 1
-console.log(factorial(2)); // 2
-console.log(factorial(3)); // 6
+    return sum;
+  }, 0);
+}
+
+console.log(divCon([9, 3, '7', '3'])); // 2
+console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7])); // 14
+console.log(divCon(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0'])); // 13

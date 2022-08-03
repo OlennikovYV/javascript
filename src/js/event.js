8ed9bfe6ebdcd0ac4137e968b1cb2c94b05f29f8
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-03 09:27:53 +0500

    Crash Override refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,3 +1,61 @@
+var firstName = {
+  A: 'Alpha',
+  B: 'Beta',
+  C: 'Cache',
+  D: 'Data',
+  E: 'Energy',
+  F: 'Function',
+  G: 'Glitch',
+  H: 'Half-life',
+  I: 'Ice',
+  J: 'Java',
+  K: 'Keystroke',
+  L: 'Logic',
+  M: 'Malware',
+  N: 'Nagware',
+  O: 'OS',
+  P: 'Phishing',
+  Q: 'Quantum',
+  R: 'RAD',
+  S: 'Strike',
+  T: 'Trojan',
+  U: 'Ultraviolet',
+  V: 'Vanilla',
+  W: 'WiFi',
+  X: 'Xerox',
+  Y: 'Y',
+  Z: 'Zero',
+};
+
+var surname = {
+  A: 'Analogue',
+  B: 'Bomb',
+  C: 'Catalyst',
+  D: 'Discharge',
+  E: 'Electron',
+  F: 'Faraday',
+  G: 'Gig',
+  H: 'Hacker',
+  I: 'IP',
+  J: 'Jabber',
+  K: 'Killer',
+  L: 'Lazer',
+  M: 'Mike',
+  N: 'n00b',
+  O: 'Overclock',
+  P: 'Payload',
+  Q: 'Quark',
+  R: 'Roy',
+  S: 'Spy',
+  T: 'T-Rex',
+  U: 'Unit',
+  V: 'Virus',
+  W: 'Worm',
+  X: 'X',
+  Y: 'Yob',
+  Z: 'Zombie',
+};
+
 function aliasGen(first, second) {
   const checkFirstChar = char => /[A-Z]/i.test(char);
 
@@ -9,8 +67,8 @@ function aliasGen(first, second) {
   }`;
 }
 
-// console.log(aliasGen('Mike', 'Millington')); // 'Malware Mike');
-// console.log(aliasGen('Fahima', 'Tash')); // 'Function T-Rex');
-// console.log(aliasGen('Daisy', 'Petrovic')); // 'Data Payload');
-// console.log(aliasGen('7393424', 'Anumbha')); // 'Your name must start with a letter from A - Z.'
-// console.log(aliasGen('Anuddanumbha', '23200')); // 'Your name must start with a letter from A - Z.'
+console.log(aliasGen('Mike', 'Millington')); // 'Malware Mike');
+console.log(aliasGen('Fahima', 'Tash')); // 'Function T-Rex');
+console.log(aliasGen('Daisy', 'Petrovic')); // 'Data Payload');
+console.log(aliasGen('7393424', 'Anumbha')); // 'Your name must start with a letter from A - Z.'
+console.log(aliasGen('Anuddanumbha', '23200')); // 'Your name must start with a letter from A - Z.'

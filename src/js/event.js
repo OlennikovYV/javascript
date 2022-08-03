c034d4e1484a2da1b536d73ceb1d037ba74b8cf5
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-03 08:35:44 +0500

    Grasshopper - Create the rooms

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,30 @@
-var TempleStrings = function (obj, feature) {
-  return `${obj} are ${feature}`;
+// Add rooms here
+var rooms = {
+  roomA: {
+    name: 'Room A',
+    description: 'room',
+    completed: true,
+  },
+  roomB: {
+    name: 'Room B',
+    description: 'room',
+    completed: true,
+  },
+  roomC: {
+    name: 'Room C',
+    description: 'room',
+    completed: true,
+  },
 };
 
-console.log(TempleStrings('Animals', 'Good')); // 'Animals are Good'
+const keys = Object.keys(rooms);
+console.log(keys.length >= 3); // true
+
+keys.forEach(name => {
+  console.log(typeof rooms[name] === 'object'); // `${name} should be an object`
+});
+
+keys.forEach(name => {
+  let numKeys = Object.keys(rooms[name]).length;
+  console.log(numKeys >= 3); // `not enough properties for room: ${name}`
+});

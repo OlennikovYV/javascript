7746e3f1f06326eb49da808c34545d7efebb18c8
Yury <olennikovyv@mail.ru>
2022-08-06 20:43:04 +0500

    Help the Fruit Guy

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,12 @@
-function sumOfN(n) {
-  let result = [0];
-  const sign = n < 0 ? -1 : 1;
-
-  for (let i = 1; i < Math.abs(n) + 1; i += 1) {
-    result.push(result[i - 1] + i * sign);
-  }
-
-  return result;
+function removeRotten(bagOfFruits) {
+  return bagOfFruits
+    ? bagOfFruits.map(fruit => fruit.replace(/rotten/, '').toLowerCase())
+    : [];
 }
 
-console.log(sumOfN(3)); // [0, 1, 3, 6]
-console.log(sumOfN(-4)); // [0, -1, -3, -6, -10]
-console.log(sumOfN(1)); // [0, 1]
-console.log(sumOfN(-6)); // [0, -1, -3, -6, -10, -15, -21]
+console.log(removeRotten(['apple', 'rottenBanana', 'apple']));
+// ["apple","banana","apple"]
+console.log(removeRotten(['apple', 'banana', 'kiwi', 'melone', 'orange']));
+// ['apple', 'banana', 'kiwi', 'melone', 'orange']
+console.log(removeRotten([])); // []
+console.log(removeRotten(null)); // []

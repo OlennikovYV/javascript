e2f95c90bd43e37907b9aec2cc188df89078ec0b
Yury <olennikovyv@mail.ru>
2022-08-07 21:43:39 +0500

    Training JS #12: loop statement --for..in and for..of

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,19 @@
-function solve(arr) {
-  return arr.filter((el, i) => arr.lastIndexOf(el) === i);
+function giveMeFive(obj) {
+  let result = [];
+
+  for (let key in obj) {
+    if (key.length === 5) result.push(key);
+    if (obj[key].length === 5) result.push(obj[key]);
+  }
+
+  return result;
 }
 
-console.log(solve([3, 4, 4, 3, 6, 3])); // [4, 6, 3]
-console.log(solve([1, 2, 1, 2, 1, 2, 3])); // [1, 2, 3]
-console.log(solve([1, 2, 3, 4])); // [1, 2, 3, 4]
-console.log(solve([1, 1, 4, 5, 1, 2, 1])); // [4, 5, 2, 1]
-console.log(solve([1, 2, 1, 2, 1, 1, 3])); // [2, 1, 3]
+console.log(giveMeFive({ Our: 'earth', is: 'a', beautyful: 'world' }));
+// ['earth', 'world']
+console.log(
+  giveMeFive({ Ihave: 'enough', money: 'to', buy: 'a', car: 'model' })
+);
+// ['Ihave', 'money', 'model']
+console.log(giveMeFive({ Pears: 'than', apple: 'sweet' }));
+// ['Pears', 'apple', 'sweet']

0c695388744e0bc7c1a2798533fe87c1536083ae
Yury <olennikovyv@mail.ru>
2022-08-07 22:16:19 +0500

    Where is my parent!?(cry)

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,25 @@
-function giveMeFive(obj) {
-  return Object.entries(obj)
-    .flat(1)
-    .filter(value => value.length === 5);
+function findChildren(dancingBrigade) {
+  const countChars = {};
+  let sortChar;
+
+  dancingBrigade.split('').map(char => {
+    char = char.toLowerCase();
+    countChars[char] ? (countChars[char] += 1) : (countChars[char] = 1);
+  });
+
+  sortChar = Object.keys(countChars).sort();
+
+  return sortChar
+    .map(char => {
+      let charString = char.toUpperCase();
+      return charString + char.repeat(countChars[char] - 1);
+    })
+    .join('');
 }
 
-console.log(giveMeFive({ Our: 'earth', is: 'a', beautyful: 'world' }));
-// ['earth', 'world']
-console.log(
-  giveMeFive({ Ihave: 'enough', money: 'to', buy: 'a', car: 'model' })
-);
-// ['Ihave', 'money', 'model']
-console.log(giveMeFive({ Pears: 'than', apple: 'sweet' }));
-// ['Pears', 'apple', 'sweet']
+console.log(findChildren('beeeEBb')); // 'BbbEeee'
+console.log(findChildren('uwwWUueEe')); // 'EeeUuuWww'
+console.log(findChildren('abBA')); // 'AaBb'
+console.log(findChildren('AaaaaZazzz')); // 'AaaaaaZzzz'
+console.log(findChildren('CbcBcbaA')); // 'AaBbbCcc'
+console.log(findChildren('xXfuUuuF')); // 'FfUuuuXx'

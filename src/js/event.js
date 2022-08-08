6a92ea31e8e96e303d86ddc3480824b89a9ea4eb
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-08 08:34:21 +0500

    Calculate Price Excluding VAT

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,8 @@
-function findChildren(dancingBrigade) {
-  return dancingBrigade
-    .split('')
-    .sort((a, b) => a.localeCompare(b, 'kf', { caseFirst: 'upper' }))
-    .join('');
+function excludingVatPrice(price) {
+  const round = num => Math.round(num * 100) / 100;
+
+  return price === null ? -1 : round(price / 1.15);
 }
 
-console.log(findChildren('beeeEBb')); // 'BbbEeee'
-console.log(findChildren('uwwWUueEe')); // 'EeeUuuWww'
-console.log(findChildren('abBA')); // 'AaBb'
-console.log(findChildren('AaaaaZazzz')); // 'AaaaaaZzzz'
-console.log(findChildren('CbcBcbaA')); // 'AaBbbCcc'
-console.log(findChildren('xXfuUuuF')); // 'FfUuuuXx'
+console.log(excludingVatPrice(230)); // 200.0
+console.log(excludingVatPrice(123)); // 106.96

82e34e9b9d7d518b7f1606dbdb7b5ad3a9d6a66f
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-08 08:40:09 +0500

    V A P O R C O D E

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,8 @@
-function excludingVatPrice(price) {
-  return price === null ? -1 : Number((price / 1.15).toFixed(2));
+function vaporcode(string) {
+  return string.replace(/ /g, '').toUpperCase().split('').join('  ');
 }
 
-console.log(excludingVatPrice(230)); // 200.0
-console.log(excludingVatPrice(123)); // 106.96
+console.log(vaporcode('Lets go to the movies'));
+// 'L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S'
+console.log(vaporcode('Why isnt my code working'));
+// 'W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G'

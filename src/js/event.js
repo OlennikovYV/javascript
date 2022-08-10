3366f8b44da02a7ca321b0275197721fa956a7a9
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-10 10:51:34 +0500

    Statistics for an Athletic Association

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,56 @@
-function maxProduct(numbers, size) {
-  return numbers
-    .sort((a, b) => a - b)
-    .slice(-size)
-    .reduce((mul, num) => mul * num);
+function stat(strg) {
+  let stats = getTimeResult(strg);
+  let range, average, median;
+
+  function addZero(time) {
+    return String(time).padStart(2, '0');
+  }
+  function getTimeResult(result) {
+    return result
+      .split(', ')
+      .map(result => {
+        return result.split('|').map(time => +time);
+      })
+      .map(time => time[0] * 3600 + time[1] * 60 + time[2])
+      .sort((a, b) => a - b);
+  }
+
+  function getResultString(time) {
+    let hours = Math.trunc(time / 3600);
+    let minutes = Math.trunc(time / 60) - hours * 60;
+    let seconds = time % 60;
+
+    return `${addZero(hours)}|${addZero(minutes)}|${addZero(seconds)}`;
+  }
+
+  if (!strg) return '';
+
+  range = stats[stats.length - 1] - stats[0];
+  average = Math.trunc(
+    stats.reduce((sum, time) => sum + time, 0) / stats.length
+  );
+  if (stats.length % 2 !== 0) {
+    median = stats[Math.floor(stats.length / 2)];
+  } else {
+    let indexMedian = Math.floor(stats.length / 2);
+    median = Math.trunc((stats[indexMedian - 1] + stats[indexMedian]) / 2);
+  }
+
+  return (
+    'Range: ' +
+    getResultString(range) +
+    ' Average: ' +
+    getResultString(average) +
+    ' Median: ' +
+    getResultString(median)
+  );
 }
 
-console.log(maxProduct([4, 3, 5], 2)); // 20
-console.log(maxProduct([10, 8, 7, 9], 3)); // 720
-console.log(maxProduct([8, 6, 4, 6], 3)); // 288
-console.log(maxProduct([10, 2, 3, 8, 1, 10, 4], 5)); // 9600
-console.log(maxProduct([13, 12, -27, -302, 25, 37, 133, 155, -14], 5)); // 247895375
-console.log(maxProduct([-4, -27, -15, -6, -1], 2)); // 4
-console.log(maxProduct([-17, -8, -102, -309], 2)); // 136
-console.log(maxProduct([10, 3, -27, -1], 3)); // -30
-console.log(maxProduct([14, 29, -28, 39, -16, -48], 4)); // -253344
-console.log(maxProduct([1], 1)); // 1
+console.log(stat('01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17'));
+// "Range: 01|01|18 Average: 01|38|05 Median: 01|32|34")
+console.log(
+  stat('02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41')
+);
+// "Range: 00|31|17 Average: 02|26|18 Median: 02|22|00")
+console.log(stat('02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|32|34, 2|17|17'));
+// "Range: 00|31|17 Average: 02|27|10 Median: 02|24|57"

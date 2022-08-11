dbd2ee8bfdfa1679438db68619c53f420f758f14
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-11 09:07:06 +0500

    Check the exam

--- src/js/event.js
+++ src/js/event.js
@@ -1,25 +1,15 @@
-function toCsvText(array) {
-  return array.join('\n');
+function checkExam(array1, array2) {
+  let scores = 0;
+
+  array1.forEach((answers, index) => {
+    if (answers !== array2[index] && array2[index] !== '') scores -= 1;
+    if (answers === array2[index]) scores += 4;
+  });
+
+  return scores >= 0 ? scores : 0;
 }
 
-console.log(
-  toCsvText([
-    [0, 1, 2, 3, 45],
-    [10, 11, 12, 13, 14],
-    [20, 21, 22, 23, 24],
-    [30, 31, 32, 33, 34],
-  ])
-); // '0,1,2,3,45\n10,11,12,13,14\n20,21,22,23,24\n30,31,32,33,34'
-console.log(
-  toCsvText([
-    [-25, 21, 2, -33, 48],
-    [30, 31, -32, 33, -34],
-  ])
-); // '-25,21,2,-33,48\n30,31,-32,33,-34'
-console.log(
-  toCsvText([
-    [5, 55, 5, 5, 55],
-    [6, 6, 66, 23, 24],
-    [666, 31, 66, 33, 7],
-  ])
-); // '5,55,5,5,55\n6,6,66,23,24\n666,31,66,33,7'
+console.log(checkExam(['a', 'a', 'b', 'b'], ['a', 'c', 'b', 'd'])); // 6
+console.log(checkExam(['a', 'a', 'c', 'b'], ['a', 'a', 'b', ''])); // 7
+console.log(checkExam(['a', 'a', 'b', 'c'], ['a', 'a', 'b', 'c'])); // 16
+console.log(checkExam(['b', 'c', 'b', 'a'], ['', 'a', 'a', 'c'])); // 0

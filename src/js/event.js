c7192a63afd7c4fd07e28d9cecdd10d1695e91a3
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-11 08:42:32 +0500

    All Inclusive?

--- src/js/event.js
+++ src/js/event.js
@@ -1,56 +1,35 @@
-function stat(strg) {
-  let stats = getTimeResult(strg);
-  let range, average, median;
+function containAllRots(strng, arr) {
+  function getAllRotation(str) {
+    let allRotation = [];
 
-  function addZero(time) {
-    return String(time).padStart(2, '0');
-  }
-  function getTimeResult(result) {
-    return result
-      .split(', ')
-      .map(result => {
-        return result.split('|').map(time => +time);
-      })
-      .map(time => time[0] * 3600 + time[1] * 60 + time[2])
-      .sort((a, b) => a - b);
+    for (let i = 0; i < str.length; i += 1) {
+      let rotation = str.slice(i) + str.slice(0, i);
+
+      allRotation.push(rotation);
+    }
+
+    return allRotation;
   }
 
-  function getResultString(time) {
-    let hours = Math.trunc(time / 3600);
-    let minutes = Math.trunc(time / 60) - hours * 60;
-    let seconds = time % 60;
+  if (strng === '') return true;
 
-    return `${addZero(hours)}|${addZero(minutes)}|${addZero(seconds)}`;
-  }
-
-  if (!strg) return '';
-
-  range = stats[stats.length - 1] - stats[0];
-  average = Math.trunc(
-    stats.reduce((sum, time) => sum + time, 0) / stats.length
-  );
-  if (stats.length % 2 !== 0) {
-    median = stats[Math.floor(stats.length / 2)];
-  } else {
-    let indexMedian = Math.floor(stats.length / 2);
-    median = Math.trunc((stats[indexMedian - 1] + stats[indexMedian]) / 2);
-  }
-
-  return (
-    'Range: ' +
-    getResultString(range) +
-    ' Average: ' +
-    getResultString(average) +
-    ' Median: ' +
-    getResultString(median)
-  );
+  return getAllRotation(strng).every(str => arr.includes(str));
 }
 
-console.log(stat('01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17'));
-// "Range: 01|01|18 Average: 01|38|05 Median: 01|32|34")
+console.log(containAllRots('', [])); // true
+console.log(containAllRots('', ['bsjq', 'qbsj'])); // true
 console.log(
-  stat('02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41')
-);
-// "Range: 00|31|17 Average: 02|26|18 Median: 02|22|00")
-console.log(stat('02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|32|34, 2|17|17'));
-// "Range: 00|31|17 Average: 02|27|10 Median: 02|24|57"
+  containAllRots('bsjq', ['bsjq', 'qbsj', 'sjqb', 'twZNsslC', 'jqbs'])
+); // true
+console.log(
+  containAllRots('XjYABhR', [
+    'TzYxlgfnhf',
+    'yqVAuoLjMLy',
+    'BhRXjYA',
+    'YABhRXj',
+    'hRXjYAB',
+    'jYABhRX',
+    'XjYABhR',
+    'ABhRXjY',
+  ])
+); // false

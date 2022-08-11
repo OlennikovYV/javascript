162810068b96a33ceb6323eb39289dc90b1520a3
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-11 08:57:29 +0500

    CSV representation of array

--- src/js/event.js
+++ src/js/event.js
@@ -1,25 +1,25 @@
-function containAllRots(strng, arr) {
-  for (let i = 0; i < strng.length; i += 1) {
-    if (arr.indexOf(strng.slice(i) + strng.slice(0, i)) === -1) return false;
-  }
-
-  return true;
+function toCsvText(array) {
+  return array.map(str => str.join()).join('\n');
 }
 
-console.log(containAllRots('', [])); // true
-console.log(containAllRots('', ['bsjq', 'qbsj'])); // true
 console.log(
-  containAllRots('bsjq', ['bsjq', 'qbsj', 'sjqb', 'twZNsslC', 'jqbs'])
-); // true
-console.log(
-  containAllRots('XjYABhR', [
-    'TzYxlgfnhf',
-    'yqVAuoLjMLy',
-    'BhRXjYA',
-    'YABhRXj',
-    'hRXjYAB',
-    'jYABhRX',
-    'XjYABhR',
-    'ABhRXjY',
+  toCsvText([
+    [0, 1, 2, 3, 45],
+    [10, 11, 12, 13, 14],
+    [20, 21, 22, 23, 24],
+    [30, 31, 32, 33, 34],
   ])
-); // false
+); // '0,1,2,3,45\n10,11,12,13,14\n20,21,22,23,24\n30,31,32,33,34'
+console.log(
+  toCsvText([
+    [-25, 21, 2, -33, 48],
+    [30, 31, -32, 33, -34],
+  ])
+); // '-25,21,2,-33,48\n30,31,-32,33,-34'
+console.log(
+  toCsvText([
+    [5, 55, 5, 5, 55],
+    [6, 6, 66, 23, 24],
+    [666, 31, 66, 33, 7],
+  ])
+); // '5,55,5,5,55\n6,6,66,23,24\n666,31,66,33,7'

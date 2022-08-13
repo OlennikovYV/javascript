d47cd4dc73faaa6c667c1a13a92eb3209ae50b83
Yury <olennikovyv@mail.ru>
2022-08-13 19:30:16 +0500

    Stringy Strings

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,12 @@
-function ipsBetween(start, end) {
-  const ipToDec = ip =>
-    ip.split('.').reduce((decimal, octet) => decimal * 256 + Number(octet));
-
-  return ipToDec(end) - ipToDec(start);
+function stringy(size) {
+  return Array.from({ length: size }, (el, i) =>
+    i % 2 === 0 ? '1' : '0'
+  ).join('');
 }
 
-console.log(ipsBetween('150.0.0.0', '150.0.0.1')); // 1
-console.log(ipsBetween('10.0.0.0', '10.0.0.50')); // 50
-console.log(ipsBetween('20.0.0.10', '20.0.1.0')); // 246
-console.log(ipsBetween('10.11.12.13', '10.11.13.0')); // 243
-console.log(ipsBetween('160.0.0.0', '160.0.1.0')); // 256
-console.log(ipsBetween('170.0.0.0', '170.1.0.0')); // 65536
-console.log(ipsBetween('50.0.0.0', '50.1.1.1')); // 65793
-console.log(ipsBetween('180.0.0.0', '181.0.0.0')); // 16777216
-console.log(ipsBetween('1.2.3.4', '5.6.7.8')); // 67372036
-console.log(ipsBetween('0.0.0.0', '255.255.255.255')); // 2 ** 32 - 1
+console.log(stringy(1)); // '1'
+console.log(stringy(2)); // '10'
+console.log(stringy(4)); // '1010'
+console.log(stringy(5)); // '10101'
+console.log(stringy(6)); // '101010'
+console.log(stringy(12)); // '101010101010'

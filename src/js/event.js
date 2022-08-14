888a73781b0c4d6db1c880c2ea454fc484d9b837
Yury <olennikovyv@mail.ru>
2022-08-14 21:54:02 +0500

    Indexed capitalization

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,20 @@
-function explode(s) {
-  return s.replace(/\d/g, digit => digit.repeat(digit));
+function capitalize(s, arr) {
+  let result = '';
+
+  for (let i = 0; i < s.length; i += 1) {
+    if (arr.indexOf(i) !== -1) {
+      result += s[i].toUpperCase();
+    } else {
+      result += s[i];
+    }
+  }
+
+  return result;
 }
 
-console.log(explode('312')); //"333122"
-console.log(explode('102269')); //"12222666666999999999"
+console.log(capitalize('abcdef', [1, 2, 5])); // 'aBCdeF'
+console.log(capitalize('abcdef', [1, 2, 5, 100])); // 'aBCdeF'
+console.log(capitalize('codewars', [1, 3, 5, 50])); // 'cOdEwArs'
+console.log(capitalize('abracadabra', [2, 6, 9, 10])); // 'abRacaDabRA'
+console.log(capitalize('codewarriors', [5])); // 'codewArriors'
+console.log(capitalize('indexinglessons', [0])); // 'Indexinglessons'

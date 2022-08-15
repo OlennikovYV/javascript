86a4a29f114cc4b4c62be218bc7414781d782674
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-15 08:43:11 +0500

    Find the nth Digit of a Number

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,17 @@
-function evil(n) {
-  return n.toString(2).match(/1/g, '').length % 2
-    ? `It's Odious!`
-    : `It's Evil!`;
-}
+var findDigit = function (num, nth) {
+  const stringNum = Math.abs(num).toString().padStart(nth, '0');
 
-console.log(evil(1)); // "It's Odious!"
-console.log(evil(2)); // "It's Odious!"
-console.log(evil(3)); // "It's Evil!"
+  if (nth <= 0) return -1;
+
+  return stringNum.length < nth
+    ? Number(stringNum[0])
+    : Number(stringNum[stringNum.length - nth]);
+};
+
+console.log(findDigit(5673, 4)); // 5
+console.log(findDigit(129, 2)); // 2
+console.log(findDigit(-2825, 3)); // 8
+console.log(findDigit(-456, 4)); // 0
+console.log(findDigit(0, 20)); // 0
+console.log(findDigit(65, 0)); // -1
+console.log(findDigit(24, -8)); // -1

a2436c8badc28d61838c9d0a8afd74a23fa7cfaa
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-16 08:39:46 +0500

    Count all the sheep on farm in the heights of New Zealand

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,21 @@
-var findDigit = function (num, nth) {
-  const stringNum = Math.abs(num).toString().padStart(nth, '0');
+function lostSheep(friday, saturday, total) {
+  let returnedSheepFriday = sumReturnedSheep(friday);
+  let returnedSheepSaturDay = sumReturnedSheep(saturday);
 
-  if (nth <= 0) return -1;
+  function sumReturnedSheep(sheeps) {
+    return sheeps.length ? sheeps.reduce((sum, sheep) => sum + sheep) : 0;
+  }
 
-  return stringNum.length < nth
-    ? Number(stringNum[0])
-    : Number(stringNum[stringNum.length - nth]);
-};
+  return total - (returnedSheepFriday + returnedSheepSaturDay);
+}
 
-console.log(findDigit(5673, 4)); // 5
-console.log(findDigit(129, 2)); // 2
-console.log(findDigit(-2825, 3)); // 8
-console.log(findDigit(-456, 4)); // 0
-console.log(findDigit(0, 20)); // 0
-console.log(findDigit(65, 0)); // -1
-console.log(findDigit(24, -8)); // -1
+console.log(lostSheep([1, 2], [3, 4], 15)); // 5
+console.log(lostSheep([3, 1, 2], [4, 5], 21)); // 6
+console.log(lostSheep([5, 1, 4], [5, 4], 29)); // 10
+console.log(lostSheep([11, 23, 3, 4, 15], [7, 14, 9, 21, 15], 300)); // 178
+console.log(lostSheep([2, 7, 13, 17], [23, 56, 44, 12, 1, 2, 1], 255)); // 77;
+console.log(lostSheep([2, 5, 8], [11, 23, 3, 4, 15, 112, 12, 4], 355)); // 156;
+console.log(lostSheep([1, 1, 1, 2, 1, 2], [2, 1, 2, 1, 2, 1], 30)); // 13
+console.log(lostSheep([5, 10, 15], [11, 23, 3, 4, 15], 89)); // 3
+console.log(lostSheep([3, 6, 9, 12], [3, 2, 1, 2, 3, 1], 44)); // 2
+console.log(lostSheep([], [], 15)); // 15

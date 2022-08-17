93fa017560e2eb12c05abe3efaf960132dbdb29e
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-17 08:18:07 +0500

    Word a10n (abbreviation)

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,37 @@
-function defineSuit(card) {
-  const suits = {
-    '♣': 'clubs',
-    '♦': 'diamonds',
-    '♥': 'hearts',
-    '♠': 'spades',
-  };
+function abbreviate(string) {
+  let words = string.split(' ');
+  let abbreviatePhrase = words.map(el => {
+    if (el.includes('-')) {
+      return el
+        .split('-')
+        .map(el => setAbbreviate(el))
+        .join('-');
+    } else {
+      return setAbbreviate(el);
+    }
+  });
 
-  return suits[card.slice(-1)];
+  function setAbbreviate(str) {
+    const symbol = [',', '!'];
+    let comma = '';
+
+    if (str.length < 4) return str;
+
+    if (symbol.includes(str.slice(-1))) {
+      comma = str.slice(-1);
+      str = str.slice(0, -1);
+    }
+
+    return str.slice(0, 1) + (str.length - 2) + str.slice(-1) + comma;
+  }
+
+  return abbreviatePhrase.join(' ');
 }
 
-console.log(defineSuit('Q♠')); //'spades'
-console.log(defineSuit('9♦')); //'diamonds'
-console.log(defineSuit('J♥')); //'hearts'
-console.log(defineSuit('10♥')); //'hearts'
+console.log(abbreviate('internationalization')); // 'i18n'
+console.log(abbreviate('accessibility')); // 'a11y'
+console.log(abbreviate('Accessibility')); // 'A11y'
+console.log(abbreviate('elephant-ride')); // 'e6t-r2e'
+console.log(
+  abbreviate('You need, need not want, to complete this code-wars mission')
+); // 'e6t-r2e'

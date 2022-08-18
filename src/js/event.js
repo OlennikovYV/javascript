3ff03979fe6cdb782bbc7640bf940d5a74d98081
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-18 08:29:07 +0500

    All Star Code Challenge #22

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,11 @@
-const isReallyNaN = Number.isNaN;
+function toTime(seconds) {
+  return `${Math.trunc(seconds / 3600)} hour(s) and ${Math.trunc(
+    (seconds / 60) % 60
+  )} minute(s)`;
+}
 
-console.log(isReallyNaN(37)); // false
-console.log(isReallyNaN('37')); // false
-console.log(isReallyNaN(NaN)); // true
-console.log(isReallyNaN(undefined)); // false
+console.log(toTime(3600)); // '1 hour(s) and 0 minute(s)'
+console.log(toTime(3601)); // '1 hour(s) and 0 minute(s)'
+console.log(toTime(3500)); // '0 hour(s) and 58 minute(s)'
+console.log(toTime(323500)); // '89 hour(s) and 51 minute(s)'
+console.log(toTime(0)); // '0 hour(s) and 0 minute(s)'

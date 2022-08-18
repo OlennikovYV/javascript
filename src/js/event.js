6d89bdc8fa2f077bd75d5c37c9d236447c9ea7fc
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-18 08:22:31 +0500

    isReallyNaN

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,8 @@
-function abbreviate(string) {
-  return string.replace(
-    /\w{4,}/g,
-    word => word[0] + (word.length - 2) + word.slice(-1)
-  );
-}
+const isReallyNaN = val => {
+  return Number.isNaN(val);
+};
 
-console.log(abbreviate('internationalization')); // 'i18n'
-console.log(abbreviate('accessibility')); // 'a11y'
-console.log(abbreviate('Accessibility')); // 'A11y'
-console.log(abbreviate('elephant-ride')); // 'e6t-r2e'
-console.log(
-  abbreviate('You need, need not want, to complete this code-wars mission')
-); // 'e6t-r2e'
+console.log(isReallyNaN(37)); // false
+console.log(isReallyNaN('37')); // false
+console.log(isReallyNaN(NaN)); // true
+console.log(isReallyNaN(undefined)); // false

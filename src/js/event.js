7915713d7c65d7a164eb06ac2f5e103ce1e71c1e
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-18 08:37:46 +0500

    Array Leaders (Array Series #3)

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,18 @@
-function toTime(seconds) {
-  return `${(seconds / 3600) ^ 0} hour(s) and ${
-    ((seconds % 3600) / 60) ^ 0
-  } minute(s)`;
+function arrayLeaders(numbers) {
+  const leaders = [];
+
+  numbers.reverse().reduce((sum, num) => {
+    if (num > sum) leaders.push(num);
+
+    return sum + num;
+  }, 0);
+
+  return leaders.reverse();
 }
 
-console.log(toTime(3600)); // '1 hour(s) and 0 minute(s)'
-console.log(toTime(3601)); // '1 hour(s) and 0 minute(s)'
-console.log(toTime(3500)); // '0 hour(s) and 58 minute(s)'
-console.log(toTime(323500)); // '89 hour(s) and 51 minute(s)'
-console.log(toTime(0)); // '0 hour(s) and 0 minute(s)'
+console.log(arrayLeaders([1, 2, 3, 4, 0])); // [4]
+console.log(arrayLeaders([16, 17, 4, 3, 5, 2])); // [17,5,2]
+console.log(arrayLeaders([-1, -29, -26, -2])); // [-1]
+console.log(arrayLeaders([-36, -12, -27])); //  [-36,-12]
+console.log(arrayLeaders([5, -2, 2])); // [5,2]
+console.log(arrayLeaders([0, -1, -29, 3, 2])); //  [0,-1,3,2]

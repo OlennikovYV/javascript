3c93b83f6d26d749af6a31500a1e691c981a5489
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-19 09:33:45 +0500

    Char Code Calculation

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,16 @@
-function padIt(str, n) {
-  while (n--) str = n % 2 ? str + '*' : '*' + str;
+function calc(x) {
+  let total1 = [...x].reduce((sum, char) => sum + char.charCodeAt(0), '');
+  let total2 = total1.replace(/7/g, '1');
 
-  return str;
+  total1 = [...total1].reduce((sum, char) => sum + char.charCodeAt(0), 0);
+  total2 = [...total2].reduce((sum, char) => sum + char.charCodeAt(0), 0);
+
+  return total1 - total2;
 }
 
-console.log(padIt('a', 1)); // '*a'
-console.log(padIt('a', 2)); // '*a*'
-console.log(padIt('a', 3)); // '**a*'
-console.log(padIt('a', 4)); // '**a**'
-console.log(padIt('a', 5)); // '***a**'
-console.log(padIt('zucpieh', 28)); // '**************zucpieh**************'
+console.log(calc('ABC')); // 6
+console.log(calc('abcdef')); // 6
+console.log(calc('ifkhchlhfd')); // 6
+console.log(calc('aaaaaddddr')); // 30
+console.log(calc('jfmgklf8hglbe')); // 6
+console.log(calc('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')); // 96

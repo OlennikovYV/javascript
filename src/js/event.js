75c835bf419dd4c1c94f59c63f6c7ac304cd417b
Yury <olennikovyv@mail.ru>
2022-08-20 15:33:27 +0500

    Training JS #14: Methods of Number object--toString() and toLocaleString()

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,12 @@
-function evensAndOdds(num) {
-  return num.toString([2, 16][num % 2]);
+function colorOf(r, g, b) {
+  return (
+    '#' +
+    r.toString(16).padStart(2, '0') +
+    g.toString(16).padStart(2, '0') +
+    b.toString(16).padStart(2, '0')
+  );
 }
 
-console.log(evensAndOdds(2)); // '10'
-console.log(evensAndOdds(13)); // 'd'
-console.log(evensAndOdds(47)); // '2f'
-console.log(evensAndOdds(0)); // '0'
-console.log(evensAndOdds(12800)); // '11001000000000'
-console.log(evensAndOdds(8172381723)); // '1e71ca61b'
+console.log(colorOf(255, 0, 0)); // '#ff0000'
+console.log(colorOf(0, 111, 0)); // '#006f00'
+console.log(colorOf(1, 2, 3)); // '#010203'

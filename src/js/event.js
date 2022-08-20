a330af87166cd5f1416b9bc711dcec1891b7f17c
Yury <olennikovyv@mail.ru>
2022-08-20 15:22:30 +0500

    Evens and Odds

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,13 @@
-function calc(x) {
-  return (
-    x
-      .split('')
-      .reduce((sum, char) => sum + char.charCodeAt(0), '')
-      .split('')
-      .filter(num => num === '7').length * 6
-  );
+function evensAndOdds(num) {
+  const convertBIN = num => num.toString(2);
+  const convertHEX = num => num.toString(16);
+
+  return num % 2 ? convertHEX(num) : convertBIN(num);
 }
 
-console.log(calc('ABC')); // 6
-console.log(calc('abcdef')); // 6
-console.log(calc('ifkhchlhfd')); // 6
-console.log(calc('aaaaaddddr')); // 30
-console.log(calc('jfmgklf8hglbe')); // 6
-console.log(calc('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')); // 96
+console.log(evensAndOdds(2)); // '10'
+console.log(evensAndOdds(13)); // 'd'
+console.log(evensAndOdds(47)); // '2f'
+console.log(evensAndOdds(0)); // '0'
+console.log(evensAndOdds(12800)); // '11001000000000'
+console.log(evensAndOdds(8172381723)); // '1e71ca61b'

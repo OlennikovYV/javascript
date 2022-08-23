5e8197ef0724158cf32afd307bc0f9cd568b679a
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-23 09:28:33 +0500

    Unary function chainer

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,17 @@
-function min(arr, toReturn) {
-  const minValue = Math.min(...arr);
-  return toReturn === 'value' ? minValue : arr.indexOf(minValue);
+const f1 = x => x * 2;
+const f2 = x => x + 2;
+const f3 = x => Math.pow(x, 2);
+const f4 = x => x.split('').concat().reverse().join('').split(' ');
+const f5 = xs => xs.concat().reverse();
+const f6 = xs => xs.join('_');
+
+function chained(functions) {
+  return function (x) {
+    return functions.reduce((sum, el) => el(sum), x);
+  };
 }
 
-console.log(min([1, 2, 3, 4, 5], 'value')); // 1
-console.log(min([1, 2, 3, 4, 5], 'index')); // 0
+console.log(chained([f1, f2, f3])(0)); // 4
+console.log(chained([f1, f2, f3])(2)); // 36
+console.log(chained([f3, f2, f1])(2)); // 12
+console.log(chained([f4, f5, f6])('lorem ipsum')); // 'merol_muspi'

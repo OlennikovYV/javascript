856f0dcfd57e224f47c4add83933098d4ece3104
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-23 08:52:27 +0500

    Smallest value of an array

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,8 @@
-function multiplyAll(arr) {
-  return function (num) {
-    return arr.map(el => el * num);
-  };
+function min(arr, toReturn) {
+  return toReturn === 'value'
+    ? Math.min(...arr)
+    : arr.indexOf(Math.min(...arr));
 }
 
-console.log(multiplyAll([1, 2, 3])(1)); // [1, 2, 3]
-console.log(multiplyAll([1, 2, 3])(2)); // [2, 4, 6]
-console.log(multiplyAll([1, 2, 3])(0)); // [0, 0, 0]
-console.log(multiplyAll([])(10)); // []
+console.log(min([1, 2, 3, 4, 5], 'value')); // 1
+console.log(min([1, 2, 3, 4, 5], 'index')); // 0

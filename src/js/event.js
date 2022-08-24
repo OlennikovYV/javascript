8d9a5e404f1cc08f0a59474573f8805a1fefe0b4
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-24 08:46:17 +0500

    Discover The Original Price

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,9 @@
-const f1 = x => x * 2;
-const f2 = x => x + 2;
-const f3 = x => Math.pow(x, 2);
-const f4 = x => x.split('').concat().reverse().join('').split(' ');
-const f5 = xs => xs.concat().reverse();
-const f6 = xs => xs.join('_');
+function discoverOriginalPrice(discountedPrice, salePercentage) {
+  const roundNumber = num => Math.round(num) / 100;
+  return roundNumber((discountedPrice / (100 - salePercentage)) * 10000);
+}
 
-const chained = functions => value =>
-  functions.reduce((result, func) => func(result), value);
-
-console.log(chained([f1, f2, f3])(0)); // 4
-console.log(chained([f1, f2, f3])(2)); // 36
-console.log(chained([f3, f2, f1])(2)); // 12
-console.log(chained([f4, f5, f6])('lorem ipsum')); // 'merol_muspi'
+console.log(discoverOriginalPrice(75, 25)); // 100
+console.log(discoverOriginalPrice(25, 75)); // 100
+console.log(discoverOriginalPrice(75.75, 25)); // 101
+console.log(discoverOriginalPrice(458.2, 17.13)); // 552.91

29c7d33baecea4349748dc00bff0f65f46b3c13c
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-25 12:05:22 +0500

    Testing is implemented in the browser.

--- src/test/test.js
+++ src/test/test.js
@@ -1,27 +1,16 @@
-const Test = chai;
-
 mocha.setup('bdd');
 
-// your tests here
-describe('test suite', function () {
-  it('should work', function () {
-    chai.assert(true);
+describe('Kata', () => {
+  it('Testing with sample tests', function () {
+    chai.expect(productArray([12, 20])).to.eql([20, 12]);
+    chai.expect(productArray([3, 27, 4, 2])).to.eql([216, 24, 162, 324]);
+    chai
+      .expect(productArray([13, 10, 5, 2, 9]))
+      .to.eql([900, 1170, 2340, 5850, 1300]);
+    chai
+      .expect(productArray([16, 17, 4, 3, 5, 2]))
+      .to.eql([2040, 1920, 8160, 10880, 6528, 16320]);
   });
 });
 
-// describe('Basic Tests', () => {
-//   Test.assertSimilar(productArray([12, 20]), [20, 12]);
-//   Test.assertSimilar(productArray([12, 20]), [20, 12]);
-//   Test.assertSimilar(productArray([3, 27, 4, 2]), [216, 24, 162, 324]);
-//   Test.assertSimilar(
-//     productArray([13, 10, 5, 2, 9]),
-//     [900, 1170, 2340, 5850, 1300]
-//   );
-//   Test.assertSimilar(
-//     productArray([16, 17, 4, 3, 5, 2]),
-//     [2040, 1920, 8160, 10880, 6528, 16320]
-//   );
-// });
-
-// run tests
 mocha.run();

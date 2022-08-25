6fb25041f80cb39a62bc9f40457bc0c46aa52f8f
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-25 08:55:30 +0500

    Return the first M multiples of N refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,5 @@
 function multiples(m, n) {
-  const result = [];
-  for (let i = 1; i <= m; i += 1) {
-    result.push(n * i);
-  }
-
-  return result;
+  return Array.from({ length: m }, (_, i) => n * (i + 1));
 }
 
 console.log(multiples(3, 5)); // [5, 10, 15]

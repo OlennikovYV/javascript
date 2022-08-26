7f580753ed5c268dcd9d624a2b652b68c51b4bd9
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-26 09:02:03 +0500

    Is this my tail?

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,6 @@
-function createArray(number) {
-  return Array.from({ length: number }, (_, i) => i + 1);
-}
+const correctTail = (body, tail) => body.slice(-1) === tail;
 
-console.log(createArray(1)); // [1]
-console.log(createArray(2)); // [1, 2]
-console.log(createArray(3)); // [1, 2, 3]
-console.log(createArray(4)); // [1, 2, 3, 4]
-console.log(createArray(5)); // [1, 2, 3, 4, 5]
+console.log(correctTail('Fox', 'x')); // true
+console.log(correctTail('Rhino', 'o')); // true
+console.log(correctTail('Meerkat', 't')); // true
+console.log(correctTail('Wolf', 'W')); // false
--- src/test/test.js
+++ src/test/test.js
@@ -1,12 +1,11 @@
 mocha.setup('bdd');
 
-describe('Fix Timmys Counter', () => {
-  it('Fixed Tests', () => {
-    chai.expect(createArray(1)).to.eql([1]);
-    chai.expect(createArray(2)).to.eql([1, 2]);
-    chai.expect(createArray(3)).to.eql([1, 2, 3]);
-    chai.expect(createArray(4)).to.eql([1, 2, 3, 4]);
-    chai.expect(createArray(5)).to.eql([1, 2, 3, 4, 5]);
+describe('Basic tests', () => {
+  it('Testing for fixed tests', () => {
+    chai.expect(correctTail('Fox', 'x')).to.be.true;
+    chai.expect(correctTail('Rhino', 'o')).to.be.true;
+    chai.expect(correctTail('Meerkat', 't')).to.be.true;
+    chai.expect(correctTail('Wolf', 'W')).to.be.false;
   });
 });
 

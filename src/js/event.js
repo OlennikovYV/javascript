ada4a9ce76df1c4b103191485e754821d29af8ad
Yury <olennikovyv@mail.ru>
2022-08-27 21:25:47 +0500

    Take an Arrow to the knee, Functionally refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,4 +1,4 @@
-const ArrowFunc = arr => arr.map(num => String.fromCharCode(num)).join('');
+const ArrowFunc = arr => String.fromCharCode(...arr);
 
 console.log(ArrowFunc([84, 101, 115, 116])); // 'Test'
 console.log(ArrowFunc([70, 85, 83, 32, 82, 79, 72, 32, 68, 65, 72])); // 'FUS ROH DAH'

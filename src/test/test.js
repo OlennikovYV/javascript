1f0d02e0c6890d1ff4f6b76ff0b87315ad7763f5
Yury <olennikovyv@mail.ru>
2022-08-27 21:24:47 +0500

    Take an Arrow to the knee, Functionally

--- src/test/test.js
+++ src/test/test.js
@@ -1,17 +1,11 @@
 mocha.setup('bdd');
 
-describe('Initial test', () => {
-  it('Testing for fixed tests', () => {
+describe('Tests', () => {
+  it('test', () => {
+    chai.expect(ArrowFunc([84, 101, 115, 116])).to.equal('Test');
     chai
-      .expect(vowel2index('this is my string'))
-      .to.equal('th3s 6s my str15ng');
-    chai
-      .expect(vowel2index('Codewars is the best site in the world'))
-      .to.equal('C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld');
-    chai
-      .expect(vowel2index('Tomorrow is going to be raining'))
-      .to.equal('T2m4rr7w 10s g1415ng t20 b23 r2627n29ng');
-    chai.expect(vowel2index('')).to.equal('');
+      .expect(ArrowFunc([70, 85, 83, 32, 82, 79, 72, 32, 68, 65, 72]))
+      .to.equal('FUS ROH DAH');
   });
 });
 

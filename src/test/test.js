7400008aef4aab4c6bc9433d2379abe72df5dd9d
Yury <olennikovyv@mail.ru>
2022-08-27 21:17:05 +0500

    The old switcheroo

--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,17 @@
 mocha.setup('bdd');
 
-describe('Basic tests', () => {
+describe('Initial test', () => {
   it('Testing for fixed tests', () => {
-    chai.expect(correctTail('Fox', 'x')).to.be.true;
-    chai.expect(correctTail('Rhino', 'o')).to.be.true;
-    chai.expect(correctTail('Meerkat', 't')).to.be.true;
-    chai.expect(correctTail('Wolf', 'W')).to.be.false;
+    chai
+      .expect(vowel2index('this is my string'))
+      .to.equal('th3s 6s my str15ng');
+    chai
+      .expect(vowel2index('Codewars is the best site in the world'))
+      .to.equal('C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld');
+    chai
+      .expect(vowel2index('Tomorrow is going to be raining'))
+      .to.equal('T2m4rr7w 10s g1415ng t20 b23 r2627n29ng');
+    chai.expect(vowel2index('')).to.equal('');
   });
 });
 

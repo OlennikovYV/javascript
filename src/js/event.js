95e4165a18b91cdb8f298d67efb1df175a02818a
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-30 08:39:37 +0500

    Head, Tail, Init and Last

--- src/js/event.js
+++ src/js/event.js
@@ -1,23 +1,9 @@
-function lastSurvivor(letters, coords) {
-  return coords.reduce(
-    (letters, coord) => letters.slice(0, coord) + letters.slice(coord + 1),
-    letters
-  );
-}
+const head = arr => arr[0];
+const tail = arr => arr.slice(1);
+const init = arr => arr.slice(0, -1);
+const last = arr => arr[arr.length - 1];
 
-console.log(lastSurvivor('abc', [1, 1])); // 'a'
-console.log(lastSurvivor('kbc', [0, 1])); // 'b'
-console.log(lastSurvivor('zbk', [2, 1])); // 'z'
-console.log(lastSurvivor('c', [])); // 'c'
-console.log(
-  lastSurvivor(
-    'foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr',
-    [
-      8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23,
-      56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25, 36, 32, 57, 10, 57, 12, 51, 55,
-      24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39,
-      36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7,
-      0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1,
-    ]
-  )
-); // 'd'
+console.log(head([5, 1])); // 5
+console.log(tail([1])); // []
+console.log(init([1, 5, 7, 9])); // [1,5,7]
+console.log(last([7, 2])); // 2

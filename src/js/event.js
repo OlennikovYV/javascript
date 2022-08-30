79068f0334e5e574aa441f9faaba904a0694c940
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-30 08:57:33 +0500

    Exclamation marks series #17: Put the exclamation marks and question marks on the balance - are they balanced?

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,20 @@
-function fireFight(s) {
-  return s.replace(/fire/gi, '~~');
+function balance(left, right) {
+  const getWeight = mark =>
+    [...mark].reduce((sumWeight, weight) => {
+      if (weight === '!') sumWeight += 2;
+      if (weight === '?') sumWeight += 3;
+
+      return sumWeight;
+    }, 0);
+  const leftWeight = getWeight(left);
+  const rightWeight = getWeight(right);
+
+  if (leftWeight === rightWeight) return 'Balance';
+
+  return leftWeight > rightWeight ? 'Left' : 'Right';
 }
 
-console.log(
-  fireFight(
-    'Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast'
-  )
-);
-// 'Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast'
-console.log(fireFight('Mast Deck Engine Water Fire')); //ast Deck Engine Water ~~'
-console.log(
-  fireFight(
-    'Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain'
-  )
-);
-// '~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain'
+console.log(balance('!!', '??')); // 'Right'
+console.log(balance('!??', '?!!')); // 'Left'
+console.log(balance('!?!!', '?!?')); // 'Left'
+console.log(balance('!!???!????', '??!!?!!!!!!!')); // 'Balance'
--- src/test/test.js
+++ src/test/test.js
@@ -2,27 +2,10 @@ mocha.setup('bdd');
 
 describe('Tests', () => {
   it('test', () => {
-    chai
-      .expect(
-        fireFight(
-          'Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast'
-        )
-      )
-      .to.equal(
-        'Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast'
-      );
-    chai
-      .expect(fireFight('Mast Deck Engine Water Fire'))
-      .to.equal('Mast Deck Engine Water ~~');
-    chai
-      .expect(
-        fireFight(
-          'Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain'
-        )
-      )
-      .to.equal(
-        '~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain'
-      );
+    chai.expect(balance('!!', '??')).to.equal('Right');
+    chai.expect(balance('!??', '?!!')).to.equal('Left');
+    chai.expect(balance('!?!!', '?!?')).to.equal('Left');
+    chai.expect(balance('!!???!????', '??!!?!!!!!!!')).to.equal('Balance');
   });
 });
 

247c306b812a6b1482dbf50448967cd0e0b6cdc1
Yury Olennikov <OlennikovYV@mail.ru>
2022-08-30 08:42:01 +0500

    Head, Tail, Init and Last refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,7 @@
-const head = arr => arr[0];
+const head = arr => arr.slice(0, 1)[0];
 const tail = arr => arr.slice(1);
 const init = arr => arr.slice(0, -1);
-const last = arr => arr[arr.length - 1];
+const last = arr => arr.slice(-1)[0];
 
 console.log(head([5, 1])); // 5
 console.log(tail([1])); // []

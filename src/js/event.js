5874edd78b8f70594d757d620fa4a40f671d6dd6
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-02 10:34:42 +0500

    Did she say hallo?

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,7 @@
-function minimumSteps(numbers, value) {
-  return numbers
-    .sort((a, b) => a - b)
-    .filter((num, index) => (value -= num) > 0).length;
+function validateHello(greetings) {
+  res = /hello|ciao|salut|hallo|hola|ahoj|czesc/i.test(greetings);
+  return res;
 }
 
-console.log(minimumSteps([4, 6, 3], 7)); // 1
-console.log(minimumSteps([10, 9, 9, 8], 17)); // 1
-console.log(minimumSteps([8, 9, 10, 4, 2], 23)); // 3
-console.log(minimumSteps([19, 98, 69, 28, 75, 45, 17, 98, 67], 464)); // 8
-console.log(minimumSteps([4, 6, 3], 2)); // 0
+console.log(validateHello('ahoj')); // true
+console.log(validateHello('meh')); // false
--- src/test/test.js
+++ src/test/test.js
@@ -2,13 +2,8 @@ mocha.setup('bdd');
 
 describe('Tests', () => {
   it('Testing for []', () => {
-    chai.expect(minimumSteps([4, 6, 3], 7)).to.equal(1);
-    chai.expect(minimumSteps([10, 9, 9, 8], 17)).to.equal(1);
-    chai.expect(minimumSteps([8, 9, 10, 4, 2], 23)).to.equal(3);
-    chai
-      .expect(minimumSteps([19, 98, 69, 28, 75, 45, 17, 98, 67], 464))
-      .to.equal(8);
-    chai.expect(minimumSteps([4, 6, 3], 2)).to.equal(0);
+    chai.expect(validateHello('ahoj')).to.equal(true);
+    chai.expect(validateHello('meh')).to.equal(false);
   });
 });
 

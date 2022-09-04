b117ac0052c333e5219038b347c8dc1296088fd9
Yury <olennikovyv@mail.ru>
2022-09-04 18:46:08 +0500

    longest_palindrome

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,29 @@
-function Dog(breed) {
-  this.breed = breed;
-  this.bark = function () {
-    return 'Woof';
-  };
-}
+longestPalindrome = function (s) {
+  let max = 0;
+  const invertString = s.split('').reverse().join('');
 
-const snoopy = new Dog('Beagle');
-const scoobydoo = new Dog('Great Dane');
+  for (let i = 0; i < invertString.length; i += 1) {
+    for (let j = i + 1; j <= invertString.length; j += 1) {
+      let checkString = invertString.slice(i, j);
+      let b = s.length - j;
+      let e = s.length - i;
+      if (checkString === s.slice(b, e)) {
+        // console.log([checkString.length, checkString, s.slice(b, e), b, e]);
+        if (max < checkString.length) max = checkString.length;
+      }
+    }
+  }
 
-console.log(snoopy.bark()); // 'Woof');
-console.log(scoobydoo.bark()); // 'Woof');
+  return max;
+};
+
+console.log(longestPalindrome('a')); // 1
+console.log(longestPalindrome('aa')); // 2
+console.log(longestPalindrome('baa')); // 2
+console.log(longestPalindrome('aab')); // 2
+console.log(longestPalindrome('zyabyz')); // 1
+console.log(longestPalindrome('baabcd')); // 4
+console.log(longestPalindrome('baablkj12345432133d')); // 9
+console.log(longestPalindrome('I like racecars that go fast')); // 7
+console.log(longestPalindrome('abcdefghba')); // 1
+console.log(longestPalindrome('')); // 0
--- src/test/test.js
+++ src/test/test.js
@@ -1,10 +1,16 @@
 mocha.setup('bdd');
 
-it('snoopy', () => {
-  chai.expect(snoopy.bark()).to.equal('Woof');
-});
-it('scoobydoo', () => {
-  chai.expect(scoobydoo.bark()).to.equal('Woof');
+it('test', () => {
+  chai.expect(longestPalindrome('a')).to.equal(1);
+  chai.expect(longestPalindrome('aa')).to.equal(2);
+  chai.expect(longestPalindrome('baa')).to.equal(2);
+  chai.expect(longestPalindrome('aab')).to.equal(2);
+  chai.expect(longestPalindrome('zyabyz')).to.equal(1);
+  chai.expect(longestPalindrome('baabcd')).to.equal(4);
+  chai.expect(longestPalindrome('baablkj12345432133d')).to.equal(9);
+  chai.expect(longestPalindrome('I like racecars that go fast')).to.equal(7);
+  chai.expect(longestPalindrome('abcdefghba')).to.equal(1);
+  chai.expect(longestPalindrome('')).to.equal(0);
 });
 
 mocha.run();

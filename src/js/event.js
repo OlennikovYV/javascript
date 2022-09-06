f679248308b568aad6c456cc4d821b87f7095a0b
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-06 10:40:05 +0500

    Lottery Ticket

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,88 @@
-function root(x, n) {
-  return Math.pow(x, 1 / n);
+function bingo(ticket, win) {
+  let wins;
+
+  const coincidence = ticket.map(tick => {
+    let tickNoDup = [...new Set(tick[0])];
+    let winningTicket = tickNoDup.map(char => char.charCodeAt(0) === tick[1]);
+
+    return winningTicket;
+  });
+
+  const mini_wins = coincidence.map(
+    tick => tick.filter(el => el === true).length
+  );
+
+  if (mini_wins.filter(countWin => countWin > 1).length > 0) return 'Loser!';
+
+  wins = mini_wins.reduce((sum, win) => sum + win, 0);
+
+  return wins >= win ? 'Winner!' : 'Loser!';
 }
 
-var rts = [
-  { n: 3, x: 8, r: 2 },
-  { n: 2, x: 6.25, r: 2.5 },
-];
-
-rts.forEach(function (o) {
-  console.log(root(o.x, o.n)); // o.r
-});
+console.log(
+  bingo(
+    [
+      ['ABC', 65],
+      ['HGR', 74],
+      ['BYHT', 74],
+    ],
+    2
+  )
+); // 'Loser!'
+console.log(
+  bingo(
+    [
+      ['ABC', 65],
+      ['HGR', 74],
+      ['BYHT', 74],
+    ],
+    1
+  )
+); // 'Winner!'
+console.log(
+  bingo(
+    [
+      ['HGTYRE', 74],
+      ['BE', 66],
+      ['JKTY', 74],
+    ],
+    3
+  )
+); // 'Loser!'
+console.log(
+  bingo(
+    [
+      ['TRDESPNV', 84],
+      ['OMXHOXZA', 72],
+      ['YZ', 88],
+      ['KVIK', 89],
+      ['ZTUYRAMN', 71],
+      ['FHZZLAO', 77],
+      ['NQWY', 79],
+      ['WVKD', 76],
+      ['YBU', 74],
+    ],
+    1
+  )
+); // 'Winner!'
+console.log(
+  bingo(
+    [
+      ['ARPERKQQ', 81],
+      ['NCIVK', 72],
+      ['OE', 84],
+    ],
+    2
+  )
+); // 'Loser!'
+console.log(
+  bingo(
+    [
+      ['SEEY', 84],
+      ['QZGTOXB', 79],
+      ['RELQNQ', 81],
+      ['OV', 88],
+    ],
+    1
+  )
+); // 'Winner!'
--- src/test/test.js
+++ src/test/test.js
@@ -2,14 +2,85 @@ mocha.setup('bdd');
 
 describe('Tests', () => {
   it('test', () => {
-    const rts = [
-      { n: 3, x: 8, r: 2 },
-      { n: 2, x: 6.25, r: 2.5 },
-    ];
-
-    rts.forEach(function (o) {
-      chai.expect(root(o.x, o.n)).to.equal(o.r);
-    });
+    chai
+      .expect(
+        bingo(
+          [
+            ['ABC', 65],
+            ['HGR', 74],
+            ['BYHT', 74],
+          ],
+          2
+        )
+      )
+      .to.equal('Loser!');
+    chai
+      .expect(
+        bingo(
+          [
+            ['ABC', 65],
+            ['HGR', 74],
+            ['BYHT', 74],
+          ],
+          1
+        )
+      )
+      .to.equal('Winner!');
+    chai
+      .expect(
+        bingo(
+          [
+            ['HGTYRE', 74],
+            ['BE', 66],
+            ['JKTY', 74],
+          ],
+          3
+        )
+      )
+      .to.equal('Loser!');
+    chai
+      .expect(
+        bingo(
+          [
+            ['TRDESPNV', 84],
+            ['OMXHOXZA', 72],
+            ['YZ', 88],
+            ['KVIK', 89],
+            ['ZTUYRAMN', 71],
+            ['FHZZLAO', 77],
+            ['NQWY', 79],
+            ['WVKD', 76],
+            ['YBU', 74],
+          ],
+          1
+        )
+      )
+      .to.equal('Winner!');
+    chai
+      .expect(
+        bingo(
+          [
+            ['ARPERKQQ', 81],
+            ['NCIVK', 72],
+            ['OE', 84],
+          ],
+          2
+        )
+      )
+      .to.equal('Loser!');
+    chai
+      .expect(
+        bingo(
+          [
+            ['SEEY', 84],
+            ['QZGTOXB', 79],
+            ['RELQNQ', 81],
+            ['OV', 88],
+          ],
+          1
+        )
+      )
+      .to.equal('Winner!');
   });
 });
 

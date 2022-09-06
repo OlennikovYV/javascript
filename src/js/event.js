75d7a4efc36360c465fd797b657c1c3f27fa9f66
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-06 11:27:09 +0500

    Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product

--- src/js/event.js
+++ src/js/event.js
@@ -1,76 +1,17 @@
-function bingo(ticket, win) {
-  let wins =
-    ticket.filter(tick =>
-      tick[0].split('').some(char => char.charCodeAt(0) === tick[1])
-    ).length >= win;
+function product(string) {
+  let countExclamation = (string.match(/\!/g) || []).length;
+  let countQuestion = (string.match(/\?/g) || []).length;
 
-  return wins ? 'Winner!' : 'Loser!';
+  return countExclamation * countQuestion;
 }
 
-console.log(
-  bingo(
-    [
-      ['ABC', 65],
-      ['HGR', 74],
-      ['BYHT', 74],
-    ],
-    2
-  )
-); // 'Loser!'
-console.log(
-  bingo(
-    [
-      ['ABC', 65],
-      ['HGR', 74],
-      ['BYHT', 74],
-    ],
-    1
-  )
-); // 'Winner!'
-console.log(
-  bingo(
-    [
-      ['HGTYRE', 74],
-      ['BE', 66],
-      ['JKTY', 74],
-    ],
-    3
-  )
-); // 'Loser!'
-console.log(
-  bingo(
-    [
-      ['TRDESPNV', 84],
-      ['OMXHOXZA', 72],
-      ['YZ', 88],
-      ['KVIK', 89],
-      ['ZTUYRAMN', 71],
-      ['FHZZLAO', 77],
-      ['NQWY', 79],
-      ['WVKD', 76],
-      ['YBU', 74],
-    ],
-    1
-  )
-); // 'Winner!'
-console.log(
-  bingo(
-    [
-      ['ARPERKQQ', 81],
-      ['NCIVK', 72],
-      ['OE', 84],
-    ],
-    2
-  )
-); // 'Loser!'
-console.log(
-  bingo(
-    [
-      ['SEEY', 84],
-      ['QZGTOXB', 79],
-      ['RELQNQ', 81],
-      ['OV', 88],
-    ],
-    1
-  )
-); // 'Winner!'
+console.log(product('')); // 0
+console.log(product('!')); // 0
+console.log(product('!!')); // 0
+console.log(product('!??')); // 2
+console.log(product('!???')); // 3
+console.log(product('!!!??')); // 6
+console.log(product('!!!???')); // 9
+console.log(product('!???!!')); // 9
+console.log(product('!ab? ?')); // 2
+console.log(product('!????!!!?')); // 20
--- src/test/test.js
+++ src/test/test.js
@@ -1,86 +1,17 @@
 mocha.setup('bdd');
 
 describe('Tests', () => {
-  it('test', () => {
-    chai
-      .expect(
-        bingo(
-          [
-            ['ABC', 65],
-            ['HGR', 74],
-            ['BYHT', 74],
-          ],
-          2
-        )
-      )
-      .to.equal('Loser!');
-    chai
-      .expect(
-        bingo(
-          [
-            ['ABC', 65],
-            ['HGR', 74],
-            ['BYHT', 74],
-          ],
-          1
-        )
-      )
-      .to.equal('Winner!');
-    chai
-      .expect(
-        bingo(
-          [
-            ['HGTYRE', 74],
-            ['BE', 66],
-            ['JKTY', 74],
-          ],
-          3
-        )
-      )
-      .to.equal('Loser!');
-    chai
-      .expect(
-        bingo(
-          [
-            ['TRDESPNV', 84],
-            ['OMXHOXZA', 72],
-            ['YZ', 88],
-            ['KVIK', 89],
-            ['ZTUYRAMN', 71],
-            ['FHZZLAO', 77],
-            ['NQWY', 79],
-            ['WVKD', 76],
-            ['YBU', 74],
-          ],
-          1
-        )
-      )
-      .to.equal('Winner!');
-    chai
-      .expect(
-        bingo(
-          [
-            ['ARPERKQQ', 81],
-            ['NCIVK', 72],
-            ['OE', 84],
-          ],
-          2
-        )
-      )
-      .to.equal('Loser!');
-    chai
-      .expect(
-        bingo(
-          [
-            ['SEEY', 84],
-            ['QZGTOXB', 79],
-            ['RELQNQ', 81],
-            ['OV', 88],
-          ],
-          1
-        )
-      )
-      .to.equal('Winner!');
+  it('It should work for basic tests', function () {
+    chai.expect(product('')).to.equal(0);
+    chai.expect(product('!')).to.equal(0);
+    chai.expect(product('!!')).to.equal(0);
+    chai.expect(product('!??')).to.equal(2);
+    chai.expect(product('!???')).to.equal(3);
+    chai.expect(product('!!!??')).to.equal(6);
+    chai.expect(product('!!!???')).to.equal(9);
+    chai.expect(product('!???!!')).to.equal(9);
+    chai.expect(product('!ab? ?')).to.equal(2);
+    chai.expect(product('!????!!!?')).to.equal(20);
   });
 });
 

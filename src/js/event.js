517b0cf0b21cb042bd65e5232130420fda746758
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-07 09:24:46 +0500

    SevenAte9

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,23 @@
-function letterCount(s) {
-  return [...s].reduce(
-    (sum, char) => ((sum[char] = (sum[char] || 0) + 1), sum),
-    {}
-  );
+function sevenAte9(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i - 1] === '7' && str[i + 1] === '7') {
+      if (str[i] !== '9') result += str[i];
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
 }
 
-console.log(letterCount('codewars')); // { a: 1,  c: 1,  d: 1,  e: 1,  o: 1,  r: 1,  s: 1,  w: 1 }
-console.log(letterCount('activity')); // { a: 1,  c: 1,  i: 2,  t: 2,  v: 1,  y: 1 }
-console.log(letterCount('arithmetics')); // { a: 1,  c: 1,  e: 1,  h: 1,  i: 2,  m: 1,  r: 1,  s: 1,  t: 2 }
-console.log(letterCount('traveller')); // { a: 1,  e: 2,  l: 2,  r: 2,  t: 1,  v: 1 }
-console.log(letterCount('daydreamer')); // { a: 2,  d: 2,  e: 2,  m: 1,  r: 2,  y: 1 }
+const tests = [
+  ['797', '77'],
+  ['7979797', '7777'],
+  ['165561786121789797', '16556178612178977'],
+];
+
+for (const [input, expected] of tests) {
+  console.log([sevenAte9(input), expected]);
+}
--- src/test/test.js
+++ src/test/test.js
@@ -2,51 +2,15 @@ mocha.setup('bdd');
 
 describe('Tests', () => {
   it('Test', () => {
-    chai.expect(letterCount('codewars')).to.eql({
-      a: 1,
-      c: 1,
-      d: 1,
-      e: 1,
-      o: 1,
-      r: 1,
-      s: 1,
-      w: 1,
-    });
-    chai.expect(letterCount('activity')).to.eql({
-      a: 1,
-      c: 1,
-      i: 2,
-      t: 2,
-      v: 1,
-      y: 1,
-    });
-    chai.expect(letterCount('arithmetics')).to.eql({
-      a: 1,
-      c: 1,
-      e: 1,
-      h: 1,
-      i: 2,
-      m: 1,
-      r: 1,
-      s: 1,
-      t: 2,
-    });
-    chai.expect(letterCount('traveller')).to.eql({
-      a: 1,
-      e: 2,
-      l: 2,
-      r: 2,
-      t: 1,
-      v: 1,
-    });
-    chai.expect(letterCount('daydreamer')).to.eql({
-      a: 2,
-      d: 2,
-      e: 2,
-      m: 1,
-      r: 2,
-      y: 1,
-    });
+    const tests = [
+      ['797', '77'],
+      ['7979797', '7777'],
+      ['165561786121789797', '16556178612178977'],
+    ];
+
+    for (const [input, expected] of tests) {
+      chai.expect(sevenAte9(input)).to.equal(expected);
+    }
   });
 });
 

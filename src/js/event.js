6a4b812fd46516dca75550e2df2da2f2ed87d309
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-08 09:15:47 +0500

    Good vs Evil refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,48 +1,29 @@
 function goodVsEvil(good, evil) {
-  const battleResults = {
-    winGood: 'Good triumphs over Evil',
-    winEvil: 'Evil eradicates all trace of Good',
-    draw: 'No victor on this battle field',
+  function getWorths(countsSide, scoresWorth) {
+    return countsSide
+      .split(' ')
+      .reduce(
+        (scores, count, index) =>
+          (scores += scoresWorth[index] * Number(count)),
+        0
+      );
+  }
+
+  const worths = {
+    goodSide: [1, 2, 3, 3, 4, 10],
+    evilSide: [1, 2, 2, 2, 3, 5, 10],
   };
-  const goodSide = {
-    0: { name: 'Hobbits', worth: 1 },
-    1: { name: 'Mens', worth: 2 },
-    2: { name: 'Elves', worth: 3 },
-    3: { name: 'Dwarves', worth: 3 },
-    4: { name: 'Eagles', worth: 4 },
-    5: { name: 'Wizards', worth: 10 },
-  };
-  const evilSide = {
-    0: { name: 'Orcs', worth: 1 },
-    1: { name: 'Men', worth: 2 },
-    2: { name: 'Wargs', worth: 2 },
-    3: { name: 'Goblins', worth: 2 },
-    4: { name: 'Uruk Hai', worth: 3 },
-    5: { name: 'Trolls', worth: 5 },
-    6: { name: 'Wizards', worth: 10 },
-  };
-  const scoresGoodSide = good
-    .split(' ')
-    .reduce(
-      (scores, count, index) =>
-        (scores += goodSide[index].worth * Number(count)),
-      0
-    );
-  const scoresEvilSide = evil
-    .split(' ')
-    .reduce(
-      (scores, count, index) =>
-        (scores += evilSide[index].worth * Number(count)),
-      0
-    );
+
+  const scoresGoodSide = getWorths(good, worths['goodSide']);
+  const scoresEvilSide = getWorths(evil, worths['evilSide']);
 
   return (
     'Battle Result: ' +
     (scoresGoodSide === scoresEvilSide
-      ? battleResults['draw']
+      ? 'No victor on this battle field'
       : scoresGoodSide > scoresEvilSide
-      ? battleResults['winGood']
-      : battleResults['winEvil'])
+      ? 'Good triumphs over Evil'
+      : 'Evil eradicates all trace of Good')
   );
 }
 

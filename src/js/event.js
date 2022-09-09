604080334e52e46c009e7b124dc36e50c9ed39dd
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-09 08:39:45 +0500

    Number Of Occurrences

--- src/js/event.js
+++ src/js/event.js
@@ -1,35 +1,10 @@
-function goodVsEvil(good, evil) {
-  function getWorths(countsSide, scoresWorth) {
-    return countsSide
-      .split(' ')
-      .reduce(
-        (scores, count, index) =>
-          (scores += scoresWorth[index] * Number(count)),
-        0
-      );
-  }
+Array.prototype.numberOfOccurrences = function (num) {
+  return this.reduce((sum, number) => {
+    if (number === num) sum += 1;
 
-  const worths = {
-    goodSide: [1, 2, 3, 3, 4, 10],
-    evilSide: [1, 2, 2, 2, 3, 5, 10],
-  };
+    return sum;
+  }, 0);
+};
 
-  const scoresGoodSide = getWorths(good, worths['goodSide']);
-  const scoresEvilSide = getWorths(evil, worths['evilSide']);
-
-  return (
-    'Battle Result: ' +
-    (scoresGoodSide === scoresEvilSide
-      ? 'No victor on this battle field'
-      : scoresGoodSide > scoresEvilSide
-      ? 'Good triumphs over Evil'
-      : 'Evil eradicates all trace of Good')
-  );
-}
-
-console.log(goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1'));
-// 'Battle Result: Evil eradicates all trace of Good'
-console.log(goodVsEvil('0 0 0 0 0 10', '0 1 1 1 1 0 0'));
-// 'Battle Result: Good triumphs over Evil'
-console.log(goodVsEvil('1 0 0 0 0 0', '1 0 0 0 0 0 0'));
-// 'Battle Result: No victor on this battle field'
+const arr = [4, 0, 4];
+console.log(arr.numberOfOccurrences(4)); // 2
--- src/test/test.js
+++ src/test/test.js
@@ -2,15 +2,8 @@ mocha.setup('bdd');
 
 describe('Tests', () => {
   it('Test', () => {
-    chai
-      .expect(goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1'))
-      .to.equal('Battle Result: Evil eradicates all trace of Good');
-    chai
-      .expect(goodVsEvil('0 0 0 0 0 10', '0 1 1 1 1 0 0'))
-      .to.equal('Battle Result: Good triumphs over Evil');
-    chai
-      .expect(goodVsEvil('1 0 0 0 0 0', '1 0 0 0 0 0 0'))
-      .to.equal('Battle Result: No victor on this battle field');
+    const arr = [4, 0, 4];
+    chai.expect(arr.numberOfOccurrences(4)).to.equal(2);
   });
 });
 

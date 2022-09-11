194c8059a35d59122e8323d93ac91caf4c532fec
Yury <olennikovyv@mail.ru>
2022-09-11 19:57:19 +0500

    Valid Spacing

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,12 @@
-function jumpingNumber(n) {
-  return String(n)
-    .split('')
-    .every((el, i, arr) => i === 0 || Math.abs(el - arr[i - 1]) === 1)
-    ? 'Jumping!!'
-    : 'Not!!';
+function validSpacing(s) {
+  let valid = s.trim();
+  valid = valid.replace(/\s{2,}/g, ' ');
+
+  return s === valid;
 }
 
-console.log(jumpingNumber(1)); // 'Jumping!!'
-console.log(jumpingNumber(7)); // 'Jumping!!'
-console.log(jumpingNumber(9)); // 'Jumping!!'
-console.log(jumpingNumber(23)); // 'Jumping!!'
-console.log(jumpingNumber(32)); // 'Jumping!!'
-console.log(jumpingNumber(79)); // 'Not!!'
-console.log(jumpingNumber(98)); // 'Jumping!!'
-console.log(jumpingNumber(8987)); // 'Jumping!!'
-console.log(jumpingNumber(625189)); // 'Not!!'
-console.log(jumpingNumber(4343456)); // 'Jumping!!'
-console.log(jumpingNumber(98789876)); // 'Jumping!!'
+console.log(validSpacing('Hello world')); // true
+console.log(validSpacing(' Hello world')); // false
+console.log(validSpacing('Hello  world ')); // false
+console.log(validSpacing('Hello')); // true
+console.log(validSpacing('Helloworld')); // true
--- src/test/test.js
+++ src/test/test.js
@@ -1,18 +1,20 @@
 mocha.setup('bdd');
 
 describe('Tests', () => {
-  it('Test', () => {
-    chai.expect(jumpingNumber(1)).to.equal('Jumping!!');
-    chai.expect(jumpingNumber(7)).to.equal('Jumping!!');
-    chai.expect(jumpingNumber(9)).to.equal('Jumping!!');
-    chai.expect(jumpingNumber(23)).to.equal('Jumping!!');
-    chai.expect(jumpingNumber(32)).to.equal('Jumping!!');
-    chai.expect(jumpingNumber(79)).to.equal('Not!!');
-    chai.expect(jumpingNumber(98)).to.equal('Jumping!!');
-    chai.expect(jumpingNumber(8987)).to.equal('Jumping!!');
-    chai.expect(jumpingNumber(625189)).to.equal('Not!!');
-    chai.expect(jumpingNumber(4343456)).to.equal('Jumping!!');
-    chai.expect(jumpingNumber(98789876)).to.equal('Jumping!!');
+  it("Testing 'Hello world'", function () {
+    chai.expect(validSpacing('Hello world')).to.equal(true);
+  });
+  it("Testing ' Hello world'", function () {
+    chai.expect(validSpacing(' Hello world')).to.equal(false);
+  });
+  it("Testing 'Hello  world '", function () {
+    chai.expect(validSpacing('Hello  world ')).to.equal(false);
+  });
+  it("Testing 'Hello'", function () {
+    chai.expect(validSpacing('Hello')).to.equal(true);
+  });
+  it("Testing 'Helloworld'", function () {
+    chai.expect(validSpacing('Helloworld')).to.equal(true);
   });
 });
 

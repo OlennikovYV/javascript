dab929045a9969d6d39ecc9808f2480cdebebbbc
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-13 08:59:33 +0500

    Days in the year

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,17 @@
-function barTriang(p1, p2, p3) {
-  let baryCenter = [0, 0];
+function yearDays(year) {
+  let isGregorian = year % 4 === 0;
+  let hundredYear = Math.trunc(year / 100) * 100;
 
-  baryCenter[0] = Math.round((p1[0] + p2[0] + p3[0]) / 0.0003) / 10000;
-  baryCenter[1] = Math.round((p1[1] + p2[1] + p3[1]) / 0.0003) / 10000;
+  isGregorian = isGregorian && hundredYear % 400 === 0;
 
-  return baryCenter;
+  return `${year} has ${isGregorian ? 366 : 365} days`;
 }
 
-console.log(barTriang([4, 6], [12, 4], [10, 10])); // [8.6667, 6.6667]
-console.log(barTriang([4, 2], [12, 2], [6, 10])); // [7.3333, 4.6667]
-console.log(barTriang([4, 8], [8, 2], [16, 6])); // [9.3333, 5.3333]
-console.log(barTriang([0, 0], [1, 3], [-1, 6])); // [0, 3]
-console.log(barTriang([0, 0], [1, 6], [8, -6])); // [3, 0]
+console.log(yearDays(0)); // '0 has 366 days'
+console.log(yearDays(-64)); // '-64 has 366 days'
+console.log(yearDays(2016)); // '2016 has 366 days'
+console.log(yearDays(1974)); // '1974 has 365 days'
+console.log(yearDays(-10)); // '-10 has 365 days'
+console.log(yearDays(666)); // '666 has 365 days'
+console.log(yearDays(1800)); // '1800 has 365 days'
+console.log(yearDays(1857)); // '1857 has 365 days'
--- src/test/test.js
+++ src/test/test.js
@@ -2,11 +2,14 @@ mocha.setup('bdd');
 
 describe('Tests', () => {
   it('test', () => {
-    chai.expect(barTriang([4, 6], [12, 4], [10, 10])).to.eql([8.6667, 6.6667]);
-    chai.expect(barTriang([4, 2], [12, 2], [6, 10])).to.eql([7.3333, 4.6667]);
-    chai.expect(barTriang([4, 8], [8, 2], [16, 6])).to.eql([9.3333, 5.3333]);
-    chai.expect(barTriang([0, 0], [1, 3], [-1, 6])).to.eql([0, 3]);
-    chai.expect(barTriang([0, 0], [1, 6], [8, -6])).to.eql([3, 0]);
+    chai.expect(yearDays(0)).to.equal('0 has 366 days');
+    chai.expect(yearDays(-64)).to.equal('-64 has 366 days');
+    chai.expect(yearDays(2016)).to.equal('2016 has 366 days');
+    chai.expect(yearDays(1974)).to.equal('1974 has 365 days');
+    chai.expect(yearDays(-10)).to.equal('-10 has 365 days');
+    chai.expect(yearDays(666)).to.equal('666 has 365 days');
+    chai.expect(yearDays(1800)).to.equal('1800 has 365 days');
+    chai.expect(yearDays(1857)).to.equal('1857 has 365 days');
   });
 });
 

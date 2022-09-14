5ea154a4c6c5ac8c34d7eb1adab9396cae5a3993
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-14 09:25:45 +0500

    Unlucky Days

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,23 @@
-function average(scores) {
-  return Math.round(scores.reduce((sum, num) => sum + num) / scores.length);
+function unluckyDays(year) {
+  let countBlackFriday = 0;
+
+  for (let month = 0; month < 12; month += 1) {
+    const date = new Date(year, month, 13);
+    if (date.getDay() === 5) {
+      countBlackFriday += 1;
+    }
+  }
+
+  return countBlackFriday;
 }
 
-let scores = [49, 3, 5, 300, 7];
-console.log(average(scores)); // 73
-
-scores = [90, 98, 89, 100, 100, 86, 94];
-console.log(average(scores)); // 94
+console.log(unluckyDays(1586)); // 1
+console.log(unluckyDays(1001)); // 3
+console.log(unluckyDays(2819)); // 2
+console.log(unluckyDays(2792)); // 2
+console.log(unluckyDays(2723)); // 2
+console.log(unluckyDays(1909)); // 1
+console.log(unluckyDays(1812)); // 2
+console.log(unluckyDays(1618)); // 2
+console.log(unluckyDays(2132)); // 1
+console.log(unluckyDays(2065)); // 3
--- src/test/test.js
+++ src/test/test.js
@@ -2,11 +2,16 @@ mocha.setup('bdd');
 
 describe('Tests', () => {
   it('test', () => {
-    let scores = [49, 3, 5, 300, 7];
-    chai.expect(average(scores)).to.equal(73);
-
-    scores = [90, 98, 89, 100, 100, 86, 94];
-    chai.expect(average(scores)).to.equal(94);
+    chai.expect(unluckyDays(1586)).to.equal(1);
+    chai.expect(unluckyDays(1001)).to.equal(3);
+    chai.expect(unluckyDays(2819)).to.equal(2);
+    chai.expect(unluckyDays(2792)).to.equal(2);
+    chai.expect(unluckyDays(2723)).to.equal(2);
+    chai.expect(unluckyDays(1909)).to.equal(1);
+    chai.expect(unluckyDays(1812)).to.equal(2);
+    chai.expect(unluckyDays(1618)).to.equal(2);
+    chai.expect(unluckyDays(2132)).to.equal(1);
+    chai.expect(unluckyDays(2065)).to.equal(3);
   });
 });
 

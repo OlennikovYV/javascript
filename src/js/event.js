1c41dc1dcedecff176b357d45b5076efe14bc38f
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-14 08:35:34 +0500

    Average Scores

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,9 @@
-function yearDays(year) {
-  let isGregorian = year % 4 === 0;
-  let hundredYear = Math.trunc(year / 100) * 100;
-
-  isGregorian = isGregorian && hundredYear % 400 === 0;
-
-  return `${year} has ${isGregorian ? 366 : 365} days`;
+function average(scores) {
+  return Math.round(scores.reduce((sum, num) => sum + num) / scores.length);
 }
 
-console.log(yearDays(0)); // '0 has 366 days'
-console.log(yearDays(-64)); // '-64 has 366 days'
-console.log(yearDays(2016)); // '2016 has 366 days'
-console.log(yearDays(1974)); // '1974 has 365 days'
-console.log(yearDays(-10)); // '-10 has 365 days'
-console.log(yearDays(666)); // '666 has 365 days'
-console.log(yearDays(1800)); // '1800 has 365 days'
-console.log(yearDays(1857)); // '1857 has 365 days'
+let scores = [49, 3, 5, 300, 7];
+console.log(average(scores)); // 73
+
+scores = [90, 98, 89, 100, 100, 86, 94];
+console.log(average(scores)); // 94
--- src/test/test.js
+++ src/test/test.js
@@ -2,14 +2,11 @@ mocha.setup('bdd');
 
 describe('Tests', () => {
   it('test', () => {
-    chai.expect(yearDays(0)).to.equal('0 has 366 days');
-    chai.expect(yearDays(-64)).to.equal('-64 has 366 days');
-    chai.expect(yearDays(2016)).to.equal('2016 has 366 days');
-    chai.expect(yearDays(1974)).to.equal('1974 has 365 days');
-    chai.expect(yearDays(-10)).to.equal('-10 has 365 days');
-    chai.expect(yearDays(666)).to.equal('666 has 365 days');
-    chai.expect(yearDays(1800)).to.equal('1800 has 365 days');
-    chai.expect(yearDays(1857)).to.equal('1857 has 365 days');
+    let scores = [49, 3, 5, 300, 7];
+    chai.expect(average(scores)).to.equal(73);
+
+    scores = [90, 98, 89, 100, 100, 86, 94];
+    chai.expect(average(scores)).to.equal(94);
   });
 });
 

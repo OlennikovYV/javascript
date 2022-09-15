0e570cba69f9e663e56f2dfe77c7e56c6c1b2dbe
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-15 08:44:00 +0500

    Training JS #8: Conditional statement--switch refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,28 +1,15 @@
 function howManydays(month) {
-  let days;
-
   switch (month) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-      days = 31;
-      break;
+    case 2:
+      return 28;
     case 4:
     case 6:
     case 9:
     case 11:
-      days = 30;
-      break;
-    case 2:
-      days = 28;
-      break;
+      return 30;
   }
 
-  return days;
+  return 31;
 }
 
 console.log(howManydays(1)); //31

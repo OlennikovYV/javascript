5bdf9edea5569359673a61e3851526ac75c10aba
Yury <olennikovyv@mail.ru>
2022-09-17 15:12:28 +0500

    Find the Slope

--- src/js/event.js
+++ src/js/event.js
@@ -1,25 +1,13 @@
-function prefill(n, v) {
-  const numberN = parseInt(n);
+function slope([x1, y1, x2, y2]) {
+  const deltaX = x2 - x1;
+  const deltaY = y2 - y1;
 
-  if (numberN != Math.abs(n) || n < 0) throw TypeError(`${n} is invalid`);
-
-  return numberN ? Array(numberN).fill(v) : [];
+  return deltaX ? String(deltaY / deltaX) : 'undefined';
 }
 
-console.log(prefill(0, 1)); // []
-console.log(prefill(3, 1)); // [1, 1, 1]
-console.log(prefill(2, 'abc')); // ['abc', 'abc']
-console.log(prefill('1', 1)); // [1]
-console.log(prefill(3, prefill(2, '2d')));
-// [ ['2d', '2d'], ['2d', '2d'], ['2d', '2d'] ]
-
-let errorThrown = false;
-try {
-  prefill('xyz', 1);
-} catch (e) {
-  console.log(e.name); // 'TypeError'
-  console.log(e.message); // 'xyz is invalid'
-  errorThrown = true;
-} finally {
-  console.log(errorThrown); // 'true'
-}
+console.log(slope([19, 3, 20, 3])); // '0'
+console.log(slope([2, 7, 4, -7])); // '-7'
+console.log(slope([10, 50, 30, 150])); // '5'
+console.log(slope([15, 45, 12, 60])); // '-5'
+console.log(slope([10, 20, 20, 80])); // '6'
+console.log(slope([-10, 6, -10, 3])); // 'undefined'
--- src/test/test.js
+++ src/test/test.js
@@ -1,26 +1,13 @@
 mocha.setup('bdd');
 
-describe('Prefill an Array', () => {
-  it('Tests', () => {
-    chai.expect(prefill(0, 1)).to.eql([]);
-    chai.expect(prefill(3, 1)).to.eql([1, 1, 1]);
-    chai.expect(prefill(2, 'abc')).to.eql(['abc', 'abc']);
-    chai.expect(prefill('1', 1)).to.eql([1]);
-    chai.expect(prefill(3, prefill(2, '2d'))).to.eql([
-      ['2d', '2d'],
-      ['2d', '2d'],
-      ['2d', '2d'],
-    ]);
-
-    let errorThrown = false;
-    try {
-      prefill('xyz', 1);
-    } catch (e) {
-      chai.expect(e.name).to.equal('TypeError');
-      chai.expect(e.message).to.equal('xyz is invalid');
-      errorThrown = true;
-    }
-    chai.expect(errorThrown).to.equal(true);
+describe('Find the Slope', () => {
+  it('test', () => {
+    chai.expect(slope([19, 3, 20, 3])).to.equal('0');
+    chai.expect(slope([2, 7, 4, -7])).to.equal('-7');
+    chai.expect(slope([10, 50, 30, 150])).to.equal('5');
+    chai.expect(slope([15, 45, 12, 60])).to.equal('-5');
+    chai.expect(slope([10, 20, 20, 80])).to.equal('6');
+    chai.expect(slope([-10, 6, -10, 3])).to.equal('undefined');
   });
 });
 

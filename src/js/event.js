82f8f5078b1d620c53a75c4aac562f98a1248e31
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-22 10:54:09 +0500

    Looking for a benefactor refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,16 @@
 function newAvg(arr, newavg) {
   let sum, avg;
 
-  sum = arr.reduce((sum, num) => sum + num, 0);
-  avg = newavg * (arr.length + 1) - sum;
+  if (arr.length !== 0) {
+    sum = arr.reduce((sum, num) => sum + num, 0);
+    avg = newavg * (arr.length + 1) - sum;
+  } else avg = newavg;
 
-  if (avg <= 0) throw new TypeError('Expected New Average is too low');
-
-  return Math.ceil(avg);
+  return avg <= 0 ? error : Math.ceil(avg);
 }
 
-console.log(newAvg([1400.25, 30000.76, 5.56, 7, 9, 11, 15.48, 120.98], 4800));
-console.log(newAvg([1400.25, 30000.76, 5.56, 7, 9, 11, 15.48, 120.98], 10000));
+console.log(newAvg([1400.25, 30000.76, 5.56, 7, 9, 11, 15.48, 120.98], 4800)); //11630
+console.log(newAvg([1400.25, 30000.76, 5.56, 7, 9, 11, 15.48, 120.98], 10000)); //58430
 
 console.log(newAvg([14, 30, 5, 7, 9, 11, 16], 90)); // 628
 console.log(newAvg([14, 30, 5, 7, 9, 11, 15], 92)); // 645

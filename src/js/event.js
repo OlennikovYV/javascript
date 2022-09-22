2c608ea7f42ddc95494721317ccd134ccb056204
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-22 10:51:27 +0500

    Looking for a benefactor refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,23 +1,16 @@
 function newAvg(arr, newavg) {
   let sum, avg;
 
-  if (!newavg || arr.length === 0)
-    throw new TypeError('Expected New Average is too low');
-
   sum = arr.reduce((sum, num) => sum + num, 0);
   avg = newavg * (arr.length + 1) - sum;
 
   if (avg <= 0) throw new TypeError('Expected New Average is too low');
 
-  return avg;
+  return Math.ceil(avg);
 }
 
-try {
-  newAvg([]); // Throw error 'Expected New Average is too low'
-} catch (e) {
-  if (e.message === 'Expected New Average is too low')
-    console.log('Expected New Average is too low');
-  else console.log('Incorrect error');
-}
+console.log(newAvg([1400.25, 30000.76, 5.56, 7, 9, 11, 15.48, 120.98], 4800));
+console.log(newAvg([1400.25, 30000.76, 5.56, 7, 9, 11, 15.48, 120.98], 10000));
+
 console.log(newAvg([14, 30, 5, 7, 9, 11, 16], 90)); // 628
 console.log(newAvg([14, 30, 5, 7, 9, 11, 15], 92)); // 645
--- src/test/test.js
+++ src/test/test.js
@@ -2,14 +2,15 @@ mocha.setup('bdd');
 
 describe('Looking for a benefactor', () => {
   it('Basic tests', function () {
+    chai
+      .expect(newAvg([1400.25, 30000.76, 5.56, 7, 9, 11, 15.48, 120.98], 4800))
+      .to.equal(11630);
+    chai
+      .expect(newAvg([1400.25, 30000.76, 5.56, 7, 9, 11, 15.48, 120.98], 10000))
+      .to.equal(58430);
+
     chai.expect(newAvg([14, 30, 5, 7, 9, 11, 16], 90)).to.equal(628);
     chai.expect(newAvg([14, 30, 5, 7, 9, 11, 15], 92)).to.equal(645);
-
-    try {
-      newAvg([]);
-    } catch (e) {
-      chai.expect(e.message).to.equal('Expected New Average is too low');
-    }
   });
 });
 

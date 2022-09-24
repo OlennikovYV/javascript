884635b372693bf72941a98152b129a446c9e2f7
Yury <olennikovyv@mail.ru>
2022-09-24 17:43:19 +0500

    'this' is a problem

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,11 @@
-function howManySmaller(arr, n) {
-  return arr.filter(num => +num.toFixed(2) < n).length;
+function NameMe(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.name = this.firstName + ' ' + this.lastName;
 }
 
-console.log(howManySmaller([1.234, 1.235, 1.228], 1.24)); // 2
-console.log(howManySmaller([1.1888, 1.1868, 1.1838], 1.19)); // 1
-console.log(howManySmaller([3.1288, 3.1212, 3.1205], 3.1212)); // 2
+const n = new NameMe('John', 'Doe');
+
+console.log(typeof n.firstName != undefined && n.firstName == 'John'); // true
+console.log(typeof n.lastName != undefined && n.lastName == 'Doe'); // true
+console.log(typeof n.name != undefined && n.name == 'John Doe'); // true
--- src/test/test.js
+++ src/test/test.js
@@ -1,10 +1,11 @@
 mocha.setup('bdd');
 
-describe('Training JS #15: Methods of Number object--toFixed(), toExponential() and toPrecision()', () => {
+describe('"this" is a problem', () => {
   it('test', () => {
-    chai.expect(howManySmaller([1.234, 1.235, 1.228], 1.24)).to.equal(2);
-    chai.expect(howManySmaller([1.1888, 1.1868, 1.1838], 1.19)).to.equal(1);
-    chai.expect(howManySmaller([3.1288, 3.1212, 3.1205], 3.1212)).to.equal(2);
+    const n = new NameMe('John', 'Doe');
+    chai.expect(typeof n.firstName != undefined && n.firstName == 'John');
+    chai.expect(typeof n.lastName != undefined && n.lastName == 'Doe');
+    chai.expect(typeof n.name != undefined && n.name == 'John Doe');
   });
 });
 

3986a6264d262c58fd7b59c0e7f755c4ad66137c
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-26 09:21:01 +0500

    Format words into a sentence

--- src/js/event.js
+++ src/js/event.js
@@ -1,18 +1,27 @@
-const rooms = {
-  first: {
-    description: 'This is the first room',
-    items: {
-      chair: 'The old chair looks comfortable',
-      lamp: 'This lamp looks ancient',
-    },
-  },
-  second: {
-    description: 'This is the second room',
-    items: {
-      couch: 'This couch looks like it would hurt your back',
-      table: 'On the table there is an unopened bottle of water',
-    },
-  },
-};
+function formatWords(words) {
+  function convertString(array) {
+    let result;
+    let arrayWord = array.filter(word => word !== '');
 
-console.log(typeof rooms); // object
+    if (arrayWord.length > 1) {
+      let index;
+
+      result = arrayWord.join(', ');
+      index = result.lastIndexOf(',');
+      result = result.replace(/,/g, (ch, i) => (i === index ? ' and' : ch));
+    } else result = arrayWord.join('');
+
+    return result;
+  }
+
+  return words && words.length !== 0 ? convertString(words) : '';
+}
+
+console.log(formatWords(['one', 'two', 'three', 'four'])); // 'one, two, three and four'
+console.log(formatWords(['one'])); // 'one'
+console.log(formatWords(['one', '', 'three'])); // 'one and three'
+console.log(formatWords(['', '', 'three'])); // 'three'
+console.log(formatWords(['one', 'two', ''])); // 'one and two'
+console.log(formatWords([])); // ''
+console.log(formatWords(null)); // ''
+console.log(formatWords([''])); // ''
--- src/test/test.js
+++ src/test/test.js
@@ -1,8 +1,19 @@
 mocha.setup('bdd');
 
 describe('Grasshopper - Object syntax debug', () => {
-  it('test', () => {
-    chai.expect(rooms).to.be.an('object');
+  it('Fixed tests', () => {
+    chai
+      .expect(formatWords(['one', 'two', 'three', 'four']))
+      .to.equal('one, two, three and four');
+    chai.expect(formatWords(['one'])).to.equal('one');
+    chai.expect(formatWords(['one', '', 'three'])).to.equal('one and three');
+    chai.expect(formatWords(['', '', 'three'])).to.equal('three');
+    chai.expect(formatWords(['one', 'two', ''])).to.equal('one and two');
+    chai
+      .expect(formatWords([]))
+      .to.equal('', 'formatWords([]) should return ""');
+    chai.expect(formatWords(null)).to.equal('');
+    chai.expect(formatWords([''])).to.equal('');
   });
 });
 

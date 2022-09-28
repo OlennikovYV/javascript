0a89bfb9f0cd541d073360e7b8163ef8c5d2adda
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-28 09:18:11 +0500

    Linked Lists - Length & Count

--- src/js/event.js
+++ src/js/event.js
@@ -1,24 +1,64 @@
-const questions = [
-  {
-    question: 'Question',
-    options: ['Choice1', 'Choice2', 'Choice3', 'Choice4'],
-    rightAns: 0,
-    numAttempts: 2,
-  },
-  {
-    question: 'Question',
-    options: ['Choice1', 'Choice2', 'Choice3', 'Choice4'],
-    rightAns: 1,
-    numAttempts: 3,
-  },
-  {
-    question: 'Question',
-    options: ['Choice1', 'Choice2', 'Choice3', 'Choice4'],
-    rightAns: 0,
-    numAttempts: 1,
-  },
-];
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
 
-questions.forEach(question => (question.usersAnswer = null));
+function addNode(node, data) {
+  const newNode = new Node(data);
+  node.next = newNode;
 
-console.log(questions);
+  return newNode;
+}
+
+function buildOneTwoThree() {
+  const node1 = new Node(1);
+  const node2 = addNode(node1, 2);
+  const node3 = addNode(node2, 3);
+
+  return node1;
+}
+
+function length(head) {
+  let lengthList = 0;
+
+  while (head) {
+    lengthList += 1;
+    head = head.next;
+  }
+
+  return lengthList;
+}
+
+function count(head, data) {
+  let countData = 0;
+
+  while (head) {
+    if (head.data === data) countData += 1;
+    head = head.next;
+  }
+
+  return countData;
+}
+
+console.log(length(null), 0, 'Length of null list should be zero.');
+console.log(
+  length(new Node(99)),
+  1,
+  'Length of single node list should be one.'
+);
+console.log(
+  length(buildOneTwoThree()),
+  3,
+  'Length of the three node list should be three.'
+);
+
+const list = buildOneTwoThree();
+console.log(count(list, 1), 1, 'list should only contain one 1.');
+console.log(count(list, 2), 1, 'list should only contain one 2.');
+console.log(count(list, 3), 1, 'list should only contain one 3.');
+console.log(
+  count(list, 99),
+  0,
+  'list should return zero for integer not found in list.'
+);
+console.log(count(null, 1), 0, 'null list should always return count of zero.');
--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,32 @@
 mocha.setup('bdd');
 
-describe('Add property to every object in array', () => {
-  for (let key in questions) {
-    it(`Test questions[${key}]`, () => {
-      chai.expect(questions[key].usersAnswer).to.equal(null);
-    });
-  }
+describe('Linked Lists - Length & Count', () => {
+  it('linked list length equal 0', function () {
+    chai.expect(length(null)).to.equal(0);
+  });
+  it('linked list length equal 1', function () {
+    chai.expect(length(new Node(99))).to.equal(1);
+  });
+  it('linked list length equal 3', function () {
+    chai.expect(length(buildOneTwoThree())).to.equal(3);
+  });
+
+  const list = buildOneTwoThree();
+  it('count 1', function () {
+    chai.expect(count(list, 1)).to.equal(1);
+  });
+  it('count 2', function () {
+    chai.expect(count(list, 2)).to.equal(1);
+  });
+  it('count 3', function () {
+    chai.expect(count(list, 3)).to.equal(1);
+  });
+  it('count 99', function () {
+    chai.expect(count(list, 99)).to.equal(0);
+  });
+  it('count null', function () {
+    chai.expect(count(null, 1)).to.equal(0);
+  });
 });
 
 mocha.run();

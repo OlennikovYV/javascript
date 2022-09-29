4824a0417310e307f47ec13157dfd8f80294cfa1
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-29 09:05:07 +0500

    Split In Parts refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,6 @@
 const splitInParts = function (s, partLength) {
-  let result = [];
-
-  for (let i = 0; i < s.length; i += partLength) {
-    result.push(s.slice(i, i + partLength));
-  }
-
-  return result.join(' ');
+  let regex = new RegExp(`.{1,${partLength}}`, 'g');
+  return s.match(regex).join(' ');
 };
 
 console.log(splitInParts('supercalifragilisticexpialidocious', 3));

49b3378ad51cb3626aca919d13ef09f5142c3fc2
Yury Olennikov <OlennikovYV@mail.ru>
2022-09-29 08:45:54 +0500

    Split In Parts

--- src/js/event.js
+++ src/js/event.js
@@ -1,52 +1,14 @@
-function Node(data) {
-  this.data = data;
-  this.next = null;
-}
+const splitInParts = function (s, partLength) {
+  let result = [];
 
-function addNode(node, data) {
-  const newNode = new Node(data);
-  node.next = newNode;
+  for (let i = 0; i < s.length; i += partLength) {
+    result.push(s.slice(i, i + partLength));
+  }
 
-  return newNode;
-}
+  return result.join(' ');
+};
 
-function buildOneTwoThree() {
-  const node1 = new Node(1);
-  const node2 = addNode(node1, 2);
-  const node3 = addNode(node2, 3);
-
-  return node1;
-}
-
-function length(head) {
-  return head ? 1 + length(head.next) : 0;
-}
-
-function count(head, data) {
-  if (!head) return 0;
-
-  return (head.data === data ? 1 : 0) + count(head.next, data);
-}
-
-console.log(length(null), 0, 'Length of null list should be zero.');
-console.log(
-  length(new Node(99)),
-  1,
-  'Length of single node list should be one.'
-);
-console.log(
-  length(buildOneTwoThree()),
-  3,
-  'Length of the three node list should be three.'
-);
-
-const list = buildOneTwoThree();
-console.log(count(list, 1), 1, 'list should only contain one 1.');
-console.log(count(list, 2), 1, 'list should only contain one 2.');
-console.log(count(list, 3), 1, 'list should only contain one 3.');
-console.log(
-  count(list, 99),
-  0,
-  'list should return zero for integer not found in list.'
-);
-console.log(count(null, 1), 0, 'null list should always return count of zero.');
+console.log(splitInParts('supercalifragilisticexpialidocious', 3));
+// 'sup erc ali fra gil ist ice xpi ali doc iou s'
+console.log(splitInParts('HelloKata', 1)); // 'H e l l o K a t a'
+console.log(splitInParts('HelloKata', 9)); // 'HelloKata'
--- src/test/test.js
+++ src/test/test.js
@@ -1,31 +1,12 @@
 mocha.setup('bdd');
 
-describe('Linked Lists - Length & Count', () => {
-  it('linked list length equal 0', function () {
-    chai.expect(length(null)).to.equal(0);
-  });
-  it('linked list length equal 1', function () {
-    chai.expect(length(new Node(99))).to.equal(1);
-  });
-  it('linked list length equal 3', function () {
-    chai.expect(length(buildOneTwoThree())).to.equal(3);
-  });
-
-  const list = buildOneTwoThree();
-  it('count 1', function () {
-    chai.expect(count(list, 1)).to.equal(1);
-  });
-  it('count 2', function () {
-    chai.expect(count(list, 2)).to.equal(1);
-  });
-  it('count 3', function () {
-    chai.expect(count(list, 3)).to.equal(1);
-  });
-  it('count 99', function () {
-    chai.expect(count(list, 99)).to.equal(0);
-  });
-  it('count null', function () {
-    chai.expect(count(null, 1)).to.equal(0);
+describe('Split In Parts', () => {
+  it('test', () => {
+    chai
+      .expect(splitInParts('supercalifragilisticexpialidocious', 3))
+      .to.equal('sup erc ali fra gil ist ice xpi ali doc iou s');
+    chai.expect(splitInParts('HelloKata', 1)).to.equal('H e l l o K a t a');
+    chai.expect(splitInParts('HelloKata', 9)).to.equal('HelloKata');
   });
 });
 

61f62ca70a473c720ce5667de7d4be7ddba11665
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-04 08:37:11 +0500

    Maximum Gap (Array Series #4)

--- src/js/event.js
+++ src/js/event.js
@@ -1,69 +1,28 @@
-function findAdmin(list, lang) {
-  return list.filter(
-    persona => persona.language === lang && persona.githubAdmin === 'yes'
-  );
+function maxGap(numbers) {
+  let gap = 0;
+
+  numbers
+    .sort((a, b) => a - b)
+    .reduce((prev, curr) => {
+      let currentGap = curr - prev;
+
+      if (gap < currentGap) {
+        gap = currentGap;
+      }
+
+      return curr;
+    });
+
+  return gap;
 }
 
-const list1 = [
-  {
-    firstName: 'Harry',
-    lastName: 'K.',
-    country: 'Brazil',
-    continent: 'Americas',
-    age: 22,
-    language: 'JavaScript',
-    githubAdmin: 'yes',
-  },
-  {
-    firstName: 'Kseniya',
-    lastName: 'T.',
-    country: 'Belarus',
-    continent: 'Europe',
-    age: 49,
-    language: 'Ruby',
-    githubAdmin: 'no',
-  },
-  {
-    firstName: 'Jing',
-    lastName: 'X.',
-    country: 'China',
-    continent: 'Asia',
-    age: 34,
-    language: 'JavaScript',
-    githubAdmin: 'yes',
-  },
-  {
-    firstName: 'Piotr',
-    lastName: 'B.',
-    country: 'Poland',
-    continent: 'Europe',
-    age: 128,
-    language: 'JavaScript',
-    githubAdmin: 'no',
-  },
-];
-
-const answer1 = [
-  {
-    firstName: 'Harry',
-    lastName: 'K.',
-    country: 'Brazil',
-    continent: 'Americas',
-    age: 22,
-    language: 'JavaScript',
-    githubAdmin: 'yes',
-  },
-  {
-    firstName: 'Jing',
-    lastName: 'X.',
-    country: 'China',
-    continent: 'Asia',
-    age: 34,
-    language: 'JavaScript',
-    githubAdmin: 'yes',
-  },
-];
-
-console.log(findAdmin(list1, 'JavaScript')); // answer1
-console.log(findAdmin(list1, 'Ruby')); // []
-console.log(findAdmin(list1, 'Python')); // []
+console.log(maxGap([13, 10, 2, 9, 5])); // 4
+console.log(maxGap([13, 3, 5])); // 8
+console.log(maxGap([24, 299, 131, 14, 26, 25])); // 168
+console.log(maxGap([-3, -27, -4, -2])); // 23
+console.log(maxGap([-7, -42, -809, -14, -12])); // 767
+console.log(maxGap([12, -5, -7, 0, 290])); // 278
+console.log(maxGap([-54, 37, 0, 64, -15, 640, 0])); // 576
+console.log(maxGap([130, 30, 50])); // 80
+console.log(maxGap([1, 1, 1])); // 0
+console.log(maxGap([-1, -1, -1])); // 0
--- src/test/test.js
+++ src/test/test.js
@@ -1,70 +1,17 @@
 mocha.setup('bdd');
 
-describe('Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins', () => {
-  it('test', () => {
-    const list1 = [
-      {
-        firstName: 'Harry',
-        lastName: 'K.',
-        country: 'Brazil',
-        continent: 'Americas',
-        age: 22,
-        language: 'JavaScript',
-        githubAdmin: 'yes',
-      },
-      {
-        firstName: 'Kseniya',
-        lastName: 'T.',
-        country: 'Belarus',
-        continent: 'Europe',
-        age: 49,
-        language: 'Ruby',
-        githubAdmin: 'no',
-      },
-      {
-        firstName: 'Jing',
-        lastName: 'X.',
-        country: 'China',
-        continent: 'Asia',
-        age: 34,
-        language: 'JavaScript',
-        githubAdmin: 'yes',
-      },
-      {
-        firstName: 'Piotr',
-        lastName: 'B.',
-        country: 'Poland',
-        continent: 'Europe',
-        age: 128,
-        language: 'JavaScript',
-        githubAdmin: 'no',
-      },
-    ];
-
-    const answer1 = [
-      {
-        firstName: 'Harry',
-        lastName: 'K.',
-        country: 'Brazil',
-        continent: 'Americas',
-        age: 22,
-        language: 'JavaScript',
-        githubAdmin: 'yes',
-      },
-      {
-        firstName: 'Jing',
-        lastName: 'X.',
-        country: 'China',
-        continent: 'Asia',
-        age: 34,
-        language: 'JavaScript',
-        githubAdmin: 'yes',
-      },
-    ];
-
-    chai.expect(findAdmin(list1, 'JavaScript')).to.eql(answer1);
-    chai.expect(findAdmin(list1, 'Ruby')).to.eql([]);
-    chai.expect(findAdmin(list1, 'Python')).to.eql([]);
+describe('Maximum Gap (Array Series #4)', () => {
+  it('Testing for fixed tests', () => {
+    chai.expect(maxGap([13, 10, 2, 9, 5])).to.equal(4);
+    chai.expect(maxGap([13, 3, 5])).to.equal(8);
+    chai.expect(maxGap([24, 299, 131, 14, 26, 25])).to.equal(168);
+    chai.expect(maxGap([-3, -27, -4, -2])).to.equal(23);
+    chai.expect(maxGap([-7, -42, -809, -14, -12])).to.equal(767);
+    chai.expect(maxGap([12, -5, -7, 0, 290])).to.equal(278);
+    chai.expect(maxGap([-54, 37, 0, 64, -15, 640, 0])).to.equal(576);
+    chai.expect(maxGap([130, 30, 50])).to.equal(80);
+    chai.expect(maxGap([1, 1, 1])).to.equal(0);
+    chai.expect(maxGap([-1, -1, -1])).to.equal(0);
   });
 });
 

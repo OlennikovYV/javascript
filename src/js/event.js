333e165634f8934595eefd73ef3a05cc7f5e7f73
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-04 09:03:04 +0500

    Autocomplete! Yay!

--- src/js/event.js
+++ src/js/event.js
@@ -1,28 +1,41 @@
-function maxGap(numbers) {
-  let gap = 0;
+function autocomplete(input, dictionary) {
+  let regex;
 
-  numbers
-    .sort((a, b) => a - b)
-    .reduce((prev, curr) => {
-      let currentGap = curr - prev;
+  input = input
+    .split('')
+    .filter(char => !'-~! @#$%^&*()_+1234567890'.includes(char))
+    .join('');
 
-      if (gap < currentGap) {
-        gap = currentGap;
-      }
+  regex = new RegExp(`^(${input})`, 'i');
 
-      return curr;
-    });
-
-  return gap;
+  return dictionary.filter(word => regex.test(word)).slice(0, 5);
 }
 
-console.log(maxGap([13, 10, 2, 9, 5])); // 4
-console.log(maxGap([13, 3, 5])); // 8
-console.log(maxGap([24, 299, 131, 14, 26, 25])); // 168
-console.log(maxGap([-3, -27, -4, -2])); // 23
-console.log(maxGap([-7, -42, -809, -14, -12])); // 767
-console.log(maxGap([12, -5, -7, 0, 290])); // 278
-console.log(maxGap([-54, 37, 0, 64, -15, 640, 0])); // 576
-console.log(maxGap([130, 30, 50])); // 80
-console.log(maxGap([1, 1, 1])); // 0
-console.log(maxGap([-1, -1, -1])); // 0
+console.log(autocomplete('ai', ['airplane', 'airport', 'apple', 'ball']));
+// ['airplane','airport']
+
+console.log(
+  autocomplete('a-~! @#$%^&*()_+1234567890', [
+    'airplane',
+    'apple',
+    'air',
+    'avenue',
+    'airport',
+    'adamantium',
+    'awkwardness',
+    'awesome',
+    'amazing',
+    'ball',
+    'book',
+    'bike',
+    'bill',
+    'billiard',
+    'bell',
+    'bowl',
+    'inconceivable',
+    'jump',
+    'jumping',
+    'judge',
+    'judging',
+  ])
+); // ['airplane', 'apple', 'air', 'avenue', 'airport']'

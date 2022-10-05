fdb2412b109cb3f05eed14f6939ea846162e5013
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-05 08:48:32 +0500

    Training JS #1: create your first JS function and print 'Hello World!'

--- src/js/event.js
+++ src/js/event.js
@@ -1,34 +1,11 @@
-function autocomplete(input, dictionary) {
-  regex = new RegExp(`^(${input.replace(/[^a-z]/gi, '')})`, 'i');
+function helloWorld() {
+  const str = 'Hello World!';
 
-  return dictionary.filter(word => regex.test(word)).slice(0, 5);
+  console.log(str);
 }
 
-console.log(autocomplete('ai', ['airplane', 'airport', 'apple', 'ball']));
-// ['airplane','airport']
-
-console.log(
-  autocomplete('a-~! @#$%^&*()_+1234567890', [
-    'airplane',
-    'apple',
-    'air',
-    'avenue',
-    'airport',
-    'adamantium',
-    'awkwardness',
-    'awesome',
-    'amazing',
-    'ball',
-    'book',
-    'bike',
-    'bill',
-    'billiard',
-    'bell',
-    'bowl',
-    'inconceivable',
-    'jump',
-    'jumping',
-    'judge',
-    'judging',
-  ])
-); // ['airplane', 'apple', 'air', 'avenue', 'airport']'
+try {
+  helloWorld();
+} catch (e) {
+  console.log(e);
+}
--- src/test/test.js
+++ src/test/test.js
@@ -1,37 +1,8 @@
 mocha.setup('bdd');
 
-describe('Autocomplete! Yay!', () => {
-  it('Testing for fixed tests', () => {
-    chai
-      .expect(autocomplete('ai', ['airplane', 'airport', 'apple', 'ball']))
-      .to.eql(['airplane', 'airport']);
-    chai
-      .expect(
-        autocomplete('a-~! @#$%^&*()_+1234567890', [
-          'airplane',
-          'apple',
-          'air',
-          'avenue',
-          'airport',
-          'adamantium',
-          'awkwardness',
-          'awesome',
-          'amazing',
-          'ball',
-          'book',
-          'bike',
-          'bill',
-          'billiard',
-          'bell',
-          'bowl',
-          'inconceivable',
-          'jump',
-          'jumping',
-          'judge',
-          'judging',
-        ])
-      )
-      .to.eql(['airplane', 'apple', 'air', 'avenue', 'airport']);
+describe('Training JS #1: create your first JS function and print "Hello World!"', () => {
+  it('test', () => {
+    chai.expect(helloWorld);
   });
 });
 

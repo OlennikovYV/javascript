7d2ae22c53a57b662588307b821b7a2e7804dea9
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-05 09:04:00 +0500

    Extra Perfect Numbers (Special Numbers Series #7)

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,22 @@
-function divisibleByThree(str) {
-  return str.split('').reduce((sum, digit) => sum + Number(digit), 0) % 3 === 0;
+function extraPerfect(n) {
+  const result = [];
+
+  function perfect(num) {
+    const bin = Number(num).toString(2).split('');
+    return bin[0] === '1' && bin[bin.length - 1] === '1' ? true : false;
+  }
+
+  for (let i = 1; i <= n; i += 1) {
+    if (perfect(i)) result.push(i);
+  }
+
+  return result;
 }
 
-console.log(divisibleByThree('123')); // true,
-console.log(divisibleByThree('19254')); // true,
-console.log(divisibleByThree('88')); // false,
-console.log(divisibleByThree('1')); // false,
+console.log(extraPerfect(3)); // [1, 3]
+console.log(extraPerfect(5)); // [1, 3, 5]
+console.log(extraPerfect(7)); // [1, 3, 5, 7]
+console.log(extraPerfect(28));
+// [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27]
+console.log(extraPerfect(39));
+// [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39]
--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,19 @@
 mocha.setup('bdd');
 
-describe('By 3, or not by 3?  That is the question . . .', () => {
+describe('Extra Perfect Numbers (Special Numbers Series #7)', () => {
   it('Testing for fixed tests', () => {
-    chai.expect(divisibleByThree('123')).to.equal(true);
-    chai.expect(divisibleByThree('19254')).to.equal(true);
-    chai.expect(divisibleByThree('88')).to.equal(false);
-    chai.expect(divisibleByThree('1')).to.equal(false);
+    chai.expect(extraPerfect(3)).to.eql([1, 3]);
+    chai.expect(extraPerfect(5)).to.eql([1, 3, 5]);
+    chai.expect(extraPerfect(7)).to.eql([1, 3, 5, 7]);
+    chai
+      .expect(extraPerfect(28))
+      .to.eql([1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27]);
+    chai
+      .expect(extraPerfect(39))
+      .to.eql([
+        1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37,
+        39,
+      ]);
   });
 });
 

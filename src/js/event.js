f11a784b6a9be59491f725315c40dae1d032c4ed
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-06 08:48:18 +0500

    Return the Missing Element

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,14 @@
-function extraPerfect(n) {
-  return Array.from({ length: n + 1 }, (_, i) => i).filter(num => num % 2);
+function getMissingElement(superImportantArray) {
+  let result;
+
+  superImportantArray.sort();
+  for (let i = 0; i <= superImportantArray.length; i += 1) {
+    if (i != superImportantArray[i]) return i;
+  }
+
+  return result;
 }
 
-console.log(extraPerfect(3)); // [1, 3]
-console.log(extraPerfect(5)); // [1, 3, 5]
-console.log(extraPerfect(7)); // [1, 3, 5, 7]
-console.log(extraPerfect(28));
-// [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27]
-console.log(extraPerfect(39));
-// [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39]
+console.log(getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4])); // 8
+console.log(getMissingElement([9, 2, 4, 5, 7, 0, 8, 6, 1])); // 3
+console.log(getMissingElement([0, 1, 2, 3, 4, 5, 6, 7, 8])); // 9
--- src/test/test.js
+++ src/test/test.js
@@ -1,19 +1,9 @@
 mocha.setup('bdd');
 
-describe('Extra Perfect Numbers (Special Numbers Series #7)', () => {
-  it('Testing for fixed tests', () => {
-    chai.expect(extraPerfect(3)).to.eql([1, 3]);
-    chai.expect(extraPerfect(5)).to.eql([1, 3, 5]);
-    chai.expect(extraPerfect(7)).to.eql([1, 3, 5, 7]);
-    chai
-      .expect(extraPerfect(28))
-      .to.eql([1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27]);
-    chai
-      .expect(extraPerfect(39))
-      .to.eql([
-        1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37,
-        39,
-      ]);
+describe('Return the Missing Element', () => {
+  it('test', () => {
+    chai.expect(getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4])).to.equal(8);
+    chai.expect(getMissingElement([9, 2, 4, 5, 7, 0, 8, 6, 1])).to.equal(3);
   });
 });
 

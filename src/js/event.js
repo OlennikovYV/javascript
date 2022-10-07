8d38e76dc1f3e0d8e4b43b79be6e2e8746b1c0ff
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-07 08:46:10 +0500

    Pythagorean Triple

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,12 @@
-function playerRankUp(points) {
-  const winMessage =
-    'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.';
-  return points >= 100 && winMessage;
+function isPythagoreanTriple(integers) {
+  integers.sort((a, b) => a - b);
+
+  const sqrtAB = Math.pow(integers[0], 2) + Math.pow(integers[1], 2);
+  const sqrtC = Math.pow(integers[2], 2);
+
+  return sqrtAB === sqrtC;
 }
 
-console.log(playerRankUp(64)); // false
-console.log(playerRankUp(101));
-// 'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.'
+console.log(isPythagoreanTriple([3, 4, 5])); // true
+console.log(isPythagoreanTriple([3, 5, 9])); // false
+console.log(isPythagoreanTriple([72, 78, 30])); // true
--- src/test/test.js
+++ src/test/test.js
@@ -1,13 +1,10 @@
 mocha.setup('bdd');
 
-describe('Online RPG: player to qualifying stage?', () => {
-  it('test', () => {
-    chai.expect(playerRankUp(64)).to.equal(false);
-    chai
-      .expect(playerRankUp(101))
-      .to.equal(
-        'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.'
-      );
+describe('Pythagorean Triple', () => {
+  it('Pass these', function () {
+    chai.expect(isPythagoreanTriple([3, 4, 5])).to.equal(true);
+    chai.expect(isPythagoreanTriple([3, 5, 9])).to.equal(false);
+    chai.expect(isPythagoreanTriple([72, 78, 30])).to.equal(true);
   });
 });
 

7d450fa57eb288fdbb0397cf189ee8eaa0707eff
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-07 08:48:25 +0500

    Pythagorean Triple refactor 2

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,7 @@
 function isPythagoreanTriple(integers) {
-  const array = integers.sort((a, b) => a - b);
+  const [a, b, c] = integers.sort((a, b) => a - b);
 
-  return array[0] ** 2 + array[1] ** 2 === array[2] ** 2;
+  return a ** 2 + b ** 2 === c ** 2;
 }
 
 console.log(isPythagoreanTriple([3, 4, 5])); // true

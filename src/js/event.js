7c65a6c641e969972c00740b8ae37f5cc62de3aa
Yury <olennikovyv@mail.ru>
2022-10-08 19:41:45 +0500

    For Twins: 1. Types

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,6 @@
-function isPythagoreanTriple(integers) {
-  const [a, b, c] = integers.sort((a, b) => a - b);
-
-  return a ** 2 + b ** 2 === c ** 2;
+function typeValidation(variable, type) {
+  return typeof variable === type;
 }
 
-console.log(isPythagoreanTriple([3, 4, 5])); // true
-console.log(isPythagoreanTriple([3, 5, 9])); // false
-console.log(isPythagoreanTriple([72, 78, 30])); // true
+console.log(typeValidation(42, 'number')); // true
+console.log(typeValidation('42', 'number')); // false
--- src/test/test.js
+++ src/test/test.js
@@ -1,10 +1,9 @@
 mocha.setup('bdd');
 
-describe('Pythagorean Triple', () => {
-  it('Pass these', function () {
-    chai.expect(isPythagoreanTriple([3, 4, 5])).to.equal(true);
-    chai.expect(isPythagoreanTriple([3, 5, 9])).to.equal(false);
-    chai.expect(isPythagoreanTriple([72, 78, 30])).to.equal(true);
+describe('For Twins: 1. Types', () => {
+  it('Basic Tests', function () {
+    chai.expect(typeValidation(42, 'number')).to.equal(true);
+    chai.expect(typeValidation('42', 'number')).to.equal(false);
   });
 });
 

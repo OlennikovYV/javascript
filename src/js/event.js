fc58af9aade2a78fafb45c5109f6fdcba53327d3
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-11 08:53:43 +0500

    Balanced Number (Special Numbers Series #1 )

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,35 @@
-var removeVowels = function (str) {
-  return str.replace(/[aeiou]/g, '');
-};
+function balancedNum(number) {
+  const strNumber = String(number);
+  const odd = strNumber.length % 2;
+  const halfString = strNumber.length >> 1;
+  let left, right;
 
-console.log(removeVowels('drake')); // 'drk'
-console.log(removeVowels('aeiou')); // ''
+  function sum(str) {
+    return str.split('').reduce((sum, digit) => sum + Number(digit), 0);
+  }
+
+  if (strNumber.length < 3) return 'Balanced';
+
+  if (odd) {
+    left = strNumber.slice(0, halfString);
+    right = strNumber.slice(halfString + 1);
+  }
+
+  if (!odd) {
+    left = strNumber.slice(0, halfString - 1);
+    right = strNumber.slice(halfString + 1);
+  }
+
+  return sum(left) === sum(right) ? 'Balanced' : 'Not Balanced';
+}
+
+console.log(balancedNum(7)); // 'Balanced'
+console.log(balancedNum(959)); // 'Balanced'
+console.log(balancedNum(13)); // 'Balanced'
+console.log(balancedNum(432)); // 'Not Balanced'
+console.log(balancedNum(424)); // 'Balanced'
+console.log(balancedNum(1024)); // 'Not Balanced'
+console.log(balancedNum(66545)); // 'Not Balanced'
+console.log(balancedNum(295591)); // 'Not Balanced'
+console.log(balancedNum(1230987)); // 'Not Balanced'
+console.log(balancedNum(56239814)); // 'Balanced'
--- src/test/test.js
+++ src/test/test.js
@@ -1,9 +1,19 @@
 mocha.setup('bdd');
 
-describe('All Star Code Challenge #3', () => {
-  it('test', () => {
-    chai.expect(removeVowels('drake')).to.equal('drk');
-    chai.expect(removeVowels('aeiou')).to.equal('');
+describe('Balanced Number (Special Numbers Series #1 )', () => {
+  it('Check balanced number', function () {
+    chai.expect(balancedNum(7)).to.equal('Balanced');
+    chai.expect(balancedNum(959)).to.equal('Balanced');
+    chai.expect(balancedNum(13)).to.equal('Balanced');
+    chai.expect(balancedNum(432)).to.equal('Not Balanced');
+    chai.expect(balancedNum(424)).to.equal('Balanced');
+  });
+  it('Check Larger number', function () {
+    chai.expect(balancedNum(1024)).to.equal('Not Balanced');
+    chai.expect(balancedNum(66545)).to.equal('Not Balanced');
+    chai.expect(balancedNum(295591)).to.equal('Not Balanced');
+    chai.expect(balancedNum(1230987)).to.equal('Not Balanced');
+    chai.expect(balancedNum(56239814)).to.equal('Balanced');
   });
 });
 

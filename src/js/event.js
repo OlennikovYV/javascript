2322e9433ad201471d2968f6b8decbd1cec4df42
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-11 08:36:21 +0500

    All Star Code Challenge #3

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,6 @@
-function evenOrOdd(str) {
-  let odd = 0;
-  let even = 0;
+var removeVowels = function (str) {
+  return str.replace(/[aeiou]/g, '');
+};
 
-  str.split('').forEach(strDigit => {
-    const digit = Number(strDigit);
-    digit % 2 ? (odd += digit) : (even += digit);
-  });
-
-  return odd > even
-    ? 'Odd is greater than Even'
-    : odd < even
-    ? 'Even is greater than Odd'
-    : 'Even and Odd are the same';
-}
-
-console.log(evenOrOdd('12')); // 'Even is greater than Odd'
-console.log(evenOrOdd('123')); // 'Odd is greater than Even'
-console.log(evenOrOdd('112')); // 'Even and Odd are the same'
+console.log(removeVowels('drake')); // 'drk'
+console.log(removeVowels('aeiou')); // ''
--- src/test/test.js
+++ src/test/test.js
@@ -1,10 +1,9 @@
 mocha.setup('bdd');
 
-describe('Even or Odd - Which is Greater?', () => {
-  it('Fixed tests', () => {
-    chai.expect(evenOrOdd('12')).to.equal('Even is greater than Odd');
-    chai.expect(evenOrOdd('123')).to.equal('Odd is greater than Even');
-    chai.expect(evenOrOdd('112')).to.equal('Even and Odd are the same');
+describe('All Star Code Challenge #3', () => {
+  it('test', () => {
+    chai.expect(removeVowels('drake')).to.equal('drk');
+    chai.expect(removeVowels('aeiou')).to.equal('');
   });
 });
 

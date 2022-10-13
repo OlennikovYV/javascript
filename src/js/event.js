f659a724dd598315be3f6f6175edd5ac5b69dc79
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-13 09:30:28 +0500

    Weight for weight refactor

--- src/js/event.js
+++ src/js/event.js
@@ -2,23 +2,9 @@ const sumDigits = number =>
   number.split('').reduce((sum, digit) => sum + Number(digit), 0);
 
 function orderWeight(strng) {
-  let inputStr = strng.split(' ');
-  let weightArr = inputStr.map(number => ({
-    Weight: number,
-    weight: sumDigits(number),
-  }));
-
-  return weightArr
-    .sort((a, b) => {
-      function sortStr(a, b) {
-        if (a > b) return 1;
-        if (a < b) return -1;
-        return 0;
-      }
-
-      return a.weight - b.weight || sortStr(a.Weight, b.Weight);
-    })
-    .map(el => el.Weight)
+  return strng
+    .split(' ')
+    .sort((a, b) => sumDigits(a) - sumDigits(b) || a.localeCompare(b))
     .join(' ');
 }
 

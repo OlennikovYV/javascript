c144db8d40b304d844c49219c0a552cb1e1a5127
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-14 08:44:57 +0500

    Sort arrays - 1

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,6 @@
-const sumDigits = number =>
-  number.split('').reduce((sum, digit) => sum + Number(digit), 0);
+sortme = function (names) {
+  return names.sort();
+};
 
-function orderWeight(strng) {
-  return strng
-    .split(' ')
-    .sort((a, b) => sumDigits(a) - sumDigits(b) || a.localeCompare(b))
-    .join(' ');
-}
-
-console.log(orderWeight('103 123 4444 99 2000'));
-// '2000 103 123 4444 99'
-console.log(orderWeight('2000 10003 1234000 44444444 9999 11 11 22 123'));
-// '11 11 2000 10003 22 123 1234000 44444444 9999'
+console.log(sortme(['one', 'two', 'three'])); // ['one', 'three', 'two']
+console.log(sortme(['Pphm', 'lahFixsLnm', 'tLTWlBGmj'])); // ['Pphm', 'lahFixsLnm', 'tLTWlBGmj']
--- src/test/test.js
+++ src/test/test.js
@@ -1,13 +1,13 @@
 mocha.setup('bdd');
 
-describe('Weight for weight', () => {
-  it('Basic tests', function () {
+describe('Sort arrays - 1', () => {
+  it('Testing for fixed tests', () => {
     chai
-      .expect(orderWeight('103 123 4444 99 2000'))
-      .to.equal('2000 103 123 4444 99');
+      .expect(sortme(['one', 'two', 'three']))
+      .to.eql(['one', 'three', 'two']);
     chai
-      .expect(orderWeight('2000 10003 1234000 44444444 9999 11 11 22 123'))
-      .to.equal('11 11 2000 10003 22 123 1234000 44444444 9999');
+      .expect(sortme(['Pphm', 'lahFixsLnm', 'tLTWlBGmj']))
+      .eql(['Pphm', 'lahFixsLnm', 'tLTWlBGmj']);
   });
 });
 

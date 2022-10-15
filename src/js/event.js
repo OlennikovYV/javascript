5622408acf1662be1013b2f2f6c924e7fbcfe14d
Yury <olennikovyv@mail.ru>
2022-10-15 18:15:37 +0500

    Simple beads count

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,8 @@
-function datingRange(age) {
-  let min, max;
-
-  if (age > 14) {
-    min = age / 2 + 7;
-    max = (age - 7) * 2;
-  } else {
-    min = age - 0.1 * age;
-    max = age + 0.1 * age;
-  }
-
-  return `${Math.floor(min)}-${Math.floor(max)}`;
+function countRedBeads(n) {
+  return n > 1 ? (n - 1) * 2 : 0;
 }
 
-console.log(datingRange(17)); // "15-20"
-console.log(datingRange(40)); // "27-66"
-console.log(datingRange(15)); // "14-16"
-console.log(datingRange(35)); // "24-56"
-console.log(datingRange(10)); // "9-11"
+console.log(countRedBeads(0)); // 0
+console.log(countRedBeads(1)); // 0
+console.log(countRedBeads(3)); // 4
+console.log(countRedBeads(5)); // 8
--- src/test/test.js
+++ src/test/test.js
@@ -1,12 +1,11 @@
 mocha.setup('bdd');
 
-describe('Age Range Compatibility Equation', () => {
-  it('Example Test Cases', () => {
-    chai.expect(datingRange(17)).to.equal('15-20');
-    chai.expect(datingRange(40)).to.equal('27-66');
-    chai.expect(datingRange(15)).to.equal('14-16');
-    chai.expect(datingRange(35)).to.equal('24-56');
-    chai.expect(datingRange(10)).to.equal('9-11');
+describe('Simple beads count', () => {
+  it('Basic tests', function () {
+    chai.expect(countRedBeads(0)).to.equal(0);
+    chai.expect(countRedBeads(1)).to.equal(0);
+    chai.expect(countRedBeads(3)).to.equal(4);
+    chai.expect(countRedBeads(5)).to.equal(8);
   });
 });
 

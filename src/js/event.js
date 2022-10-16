a757b6e3878b3d3fa792424b47caef2bbbb44084
Yury <olennikovyv@mail.ru>
2022-10-16 20:30:10 +0500

    Find all occurrences of an element in an array

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,21 @@
-function countRedBeads(n) {
-  return ((n || 1) - 1) * 2;
-}
+const findAll = (array, n) => {
+  const result = [];
 
-console.log(countRedBeads(0)); // 0
-console.log(countRedBeads(1)); // 0
-console.log(countRedBeads(3)); // 4
-console.log(countRedBeads(5)); // 8
+  return array.reduce((res, digit, index) => {
+    if (digit === n) res.push(index);
+    return res;
+  }, result);
+};
+
+console.log(findAll([6, 9, 3, 4, 3, 82, 11], 3)); // [2, 4]);
+console.log(findAll([10, 16, 20, 6, 14, 11, 20, 2, 17, 16, 14], 16));
+// [1, 9]
+console.log(
+  findAll(
+    [
+      20, 20, 10, 13, 15, 2, 7, 2, 20, 3, 18, 2, 3, 2, 16, 10, 9, 9, 7, 5, 15,
+      5,
+    ],
+    20
+  )
+); // [0, 1, 8]
--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,22 @@
 mocha.setup('bdd');
 
-describe('Simple beads count', () => {
-  it('Basic tests', function () {
-    chai.expect(countRedBeads(0)).to.equal(0);
-    chai.expect(countRedBeads(1)).to.equal(0);
-    chai.expect(countRedBeads(3)).to.equal(4);
-    chai.expect(countRedBeads(5)).to.equal(8);
+describe('Find all occurrences of an element in an array', () => {
+  it('should pass some fixed tests', function () {
+    chai.expect(findAll([6, 9, 3, 4, 3, 82, 11], 3)).to.eql([2, 4]);
+    chai
+      .expect(findAll([10, 16, 20, 6, 14, 11, 20, 2, 17, 16, 14], 16))
+      .to.eql([1, 9]);
+    chai
+      .expect(
+        findAll(
+          [
+            20, 20, 10, 13, 15, 2, 7, 2, 20, 3, 18, 2, 3, 2, 16, 10, 9, 9, 7, 5,
+            15, 5,
+          ],
+          20
+        )
+      )
+      .to.eql([0, 1, 8]);
   });
 });
 

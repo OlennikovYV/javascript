746b2bd6562519306a4de0d220b005ce7bc107e0
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-17 09:15:02 +0500

    Split The Bill refactor

--- src/js/event.js
+++ src/js/event.js
@@ -11,21 +11,13 @@ function splitTheBill(x) {
   aver = sum / count;
 
   for (const cost in x) {
-    let diff = x[cost] - aver;
-
-    if (diff < 0) {
-      x[cost] = (x[cost] - aver).toFixed(2);
-    } else {
-      x[cost] = diff.toFixed(2);
-    }
-
-    x[cost] = Number(x[cost]);
+    x[cost] = Math.round((x[cost] - aver) * 100) / 100;
   }
 
   return x;
 }
 
-console.log(splitTheBill({ A: 21, B: 15, C: 10 }));
+console.log(splitTheBill({ A: 20, B: 15, C: 10 }));
 // { A: 5, B: 0, C: -5 }
 console.log(splitTheBill({ A: 40, B: 25, X: 10 }));
 // { A: 15, B: 0, X: -15 }

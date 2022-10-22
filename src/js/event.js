6430782b6246d6c4acc5f12b8f4c4de3b351954b
Yury <olennikovyv@mail.ru>
2022-10-22 18:36:27 +0500

    String Merge!

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,16 @@
-function stringTransformer(str) {
-  return str
-    .split(' ')
-    .reverse()
-    .map(word =>
-      word
-        .split('')
-        .map(char =>
-          char.toLowerCase() === char ? char.toUpperCase() : char.toLowerCase()
-        )
-        .join('')
-    )
-    .join(' ');
+function stringMerge(string1, string2, letter) {
+  const index1 = string1.indexOf(letter);
+  const index2 = string2.indexOf(letter);
+
+  return string1.slice(0, index1) + letter + string2.slice(index2 + 1);
 }
 
-console.log(stringTransformer('Example string')); // 'STRING eXAMPLE');
+console.log(stringMerge('person', 'here', 'e')); // 'pere');
+console.log(stringMerge('apowiejfoiajsf', 'iwahfeijouh', 'j'));
+// 'apowiejouh'
+console.log(stringMerge('abcdefxxxyzz', 'abcxxxyyyxyzz', 'x'));
+// 'abcdefxxxyyyxyzz'
+console.log(stringMerge('12345654321', '123456789', '6')); // '123456789');
+console.log(stringMerge('JiOdIdA4', 'oopopopoodddasdfdfsd', 'd'));
+// 'JiOdddasdfdfsd'
+console.log(stringMerge('incredible', 'people', 'e')); // 'increople');
--- src/test/test.js
+++ src/test/test.js
@@ -1,8 +1,21 @@
 mocha.setup('bdd');
 
-describe('String transformer', () => {
+describe('String Merge!', () => {
   it('test', () => {
-    chai.expect(stringTransformer('Example string')).to.equal('STRING eXAMPLE');
+    chai.expect(stringMerge('person', 'here', 'e')).to.equal('pere');
+    chai
+      .expect(stringMerge('apowiejfoiajsf', 'iwahfeijouh', 'j'))
+      .to.equal('apowiejouh');
+    chai
+      .expect(stringMerge('abcdefxxxyzz', 'abcxxxyyyxyzz', 'x'))
+      .to.equal('abcdefxxxyyyxyzz');
+    chai
+      .expect(stringMerge('12345654321', '123456789', '6'))
+      .to.equal('123456789');
+    chai
+      .expect(stringMerge('JiOdIdA4', 'oopopopoodddasdfdfsd', 'd'))
+      .to.equal('JiOdddasdfdfsd');
+    chai.expect(stringMerge('incredible', 'people', 'e')).to.equal('increople');
   });
 });
 

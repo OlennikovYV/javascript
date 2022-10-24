1060f4c5940b9822442826383bc06b1d966ab607
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-24 08:39:40 +0500

    Create Phone Number refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,8 @@
 function createPhoneNumber(numbers) {
-  const phone = numbers.join('');
-  const phone1 = phone.slice(0, 3);
-  const phone2 = phone.slice(3, 6);
-  const phone3 = phone.slice(6);
-
-  return `(${phone1}) ${phone2}-${phone3}`;
+  return numbers.reduce(
+    (phone, digit) => phone.replace('x', digit),
+    '(xxx) xxx-xxxx'
+  );
 }
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));

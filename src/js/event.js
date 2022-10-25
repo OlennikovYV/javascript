62d784b9e2236b2a7bb42602274ef3d3308bb484
Yury Olennikov <OlennikovYV@mail.ru>
2022-10-25 08:47:18 +0500

    Enumerable Magic #5- True for Just One? refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,5 @@
 function one(arr, fun) {
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i += 1) {
-    if (fun(arr[i])) count += 1;
-  }
-
-  return count === 1;
+  return arr.filter(fun).length === 1;
 }
 
 console.log(

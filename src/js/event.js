73aaf0918d597c954e4d6ae0fbc5a389645cde9f
Yury <olennikovyv@mail.ru>
2022-10-29 18:29:30 +0500

    Potenciation

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,8 @@
-function nextItem(xs, item) {
-  const iterations = xs[Symbol.iterator]();
-
-  for (let el of iterations) if (el === item) return iterations.next().value;
+function power(x, y) {
+  return x ** y;
 }
 
-console.log(nextItem([1, 2, 3, 4, 5, 6, 7, 8], 5)); // 6
-console.log(nextItem(['a', 'b', 'c'], 'd')); // undefined
-console.log(nextItem(['a', 'b', 'c'], 'c')); // undefined
-console.log(nextItem('testing', 't')); // 'e'
-
-function* countFrom(n) {
-  for (let i = n; ; ++i) yield i;
-}
-
-console.log(nextItem(countFrom(1), 12)); // 13
+console.log(power(1, 701270)); // 1
+console.log(power(2, 2)); // 4
+console.log(power(3, 2)); // 9
+console.log(power(-1, 40)); // 1
--- src/test/test.js
+++ src/test/test.js
@@ -1,15 +1,11 @@
 mocha.setup('bdd');
 
-describe('Check three and two', () => {
+describe('Potenciation', () => {
   it('test', () => {
-    chai.expect(nextItem([1, 2, 3, 4, 5, 6, 7, 8], 5)).to.equal(6);
-    chai.expect(nextItem(['a', 'b', 'c'], 'd')).to.equal(undefined);
-    chai.expect(nextItem(['a', 'b', 'c'], 'c')).to.equal(undefined);
-    chai.expect(nextItem('testing', 't')).to.equal('e');
-    function* countFrom(n) {
-      for (let i = n; ; ++i) yield i;
-    }
-    chai.expect(nextItem(countFrom(1), 12)).to.equal(13);
+    chai.expect(power(1, 701270)).to.equal(1);
+    chai.expect(power(2, 2)).to.equal(4);
+    chai.expect(power(3, 2)).to.equal(9);
+    chai.expect(power(-1, 40)).to.equal(1);
   });
 });
 

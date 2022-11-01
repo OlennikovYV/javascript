d7ccee83a9733c5d9128c30485fb0bb10a553a58
Yury Olennikov <OlennikovYV@mail.ru>
2022-11-01 10:02:28 +0500

    Simple fraction to mixed number converter

--- src/js/event.js
+++ src/js/event.js
@@ -1,20 +1,63 @@
-function eachCons(array, n) {
-  let result = [];
+function mixedFraction(s) {
+  function shortenFraction(x, y) {
+    for (let i = x; i >= 2; i -= 1) {
+      if (x % i === 0 && y % i === 0) return [x / i, y / i];
+    }
 
-  for (let i = 0; i < array.length - n + 1; i += 1)
-    result.push(array.slice(i, i + n));
+    return [x, y];
+  }
 
-  return result;
+  let [x, y] = Array.from(s.split('/'), Number);
+  let sign1 = x === Math.abs(x) ? 1 : -1;
+  let sign2 = y === Math.abs(y) ? 1 : -1;
+  let sign = sign1 * sign2 === -1 ? '-' : '';
+  let whole;
+  let firstPart, secondPart;
+  let space = '';
+
+  if (y === 0) throw 'error';
+
+  x = Math.abs(x);
+  y = Math.abs(y);
+  whole = Math.trunc(x / y);
+  if (x > y) if (x % y !== 0) x = x - whole * y;
+  [x, y] = shortenFraction(x, y);
+
+  firstPart = `${whole ? whole : ''}`;
+  secondPart = y === 1 ? `` : `${x}/${y}`;
+  space = firstPart && secondPart ? ' ' : '';
+
+  if (firstPart && x === 0) return `${sign}${firstPart}`;
+  if (!firstPart && x === 0) return '0';
+  if (!firstPart) return `${sign}${secondPart}`;
+
+  return sign + firstPart + space + secondPart;
 }
 
-const lst = [3, 5, 8, 13];
+console.log(mixedFraction('567/-399')); // '-1 8/19'
+console.log(mixedFraction('1/-2')); // '-1/2'
+console.log(mixedFraction('-2/9')); // '-2/9'
+console.log(mixedFraction('42/9')); // '4 2/3'
+console.log(mixedFraction('6/3')); // '2'
+console.log(mixedFraction('1/1')); // '1'
+console.log(mixedFraction('11/11')); // '1'
+console.log(mixedFraction('4/6')); // '2/3'
+console.log(mixedFraction('0/18891')); // '0'
+console.log(mixedFraction('-10/7')); // '-1 3/7'
+console.log(mixedFraction('-22/-7')); // '3 1/7'
 
-console.log(eachCons(lst, 1)); // [[3], [5], [8], [13]]
+try {
+  Test.expectError('Must raise ZeroDivisionError', function () {
+    mixedFraction('0/0');
+  });
+} catch (e) {
+  console.log('Throw1.');
+}
 
-console.log(eachCons(lst, 2));
-// [ [3, 5], [5, 8], [8, 13] ]
-
-console.log(eachCons(lst, 3));
-// [ [3, 5, 8], [5, 8, 13] ]
-
-console.log(eachCons([], 3)); // []
+try {
+  Test.expectError('Must raise ZeroDivisionError', function () {
+    mixedFraction('3/0');
+  });
+} catch (e) {
+  console.log('Throw2.');
+}

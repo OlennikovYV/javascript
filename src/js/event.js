0d0acca92bf6f411a2f76461cd94a0c83e19d93f
Yury Olennikov <OlennikovYV@mail.ru>
2022-11-02 08:39:34 +0500

    Is integer safe to use?

--- src/js/event.js
+++ src/js/event.js
@@ -1,63 +1,6 @@
-function mixedFraction(s) {
-  function shortenFraction(x, y) {
-    for (let i = x; i >= 2; i -= 1) {
-      if (x % i === 0 && y % i === 0) return [x / i, y / i];
-    }
-
-    return [x, y];
-  }
-
-  let [x, y] = Array.from(s.split('/'), Number);
-  let sign1 = x === Math.abs(x) ? 1 : -1;
-  let sign2 = y === Math.abs(y) ? 1 : -1;
-  let sign = sign1 * sign2 === -1 ? '-' : '';
-  let whole;
-  let firstPart, secondPart;
-  let space = '';
-
-  if (y === 0) throw 'error';
-
-  x = Math.abs(x);
-  y = Math.abs(y);
-  whole = Math.trunc(x / y);
-  if (x > y) if (x % y !== 0) x = x - whole * y;
-  [x, y] = shortenFraction(x, y);
-
-  firstPart = `${whole ? whole : ''}`;
-  secondPart = y === 1 ? `` : `${x}/${y}`;
-  space = firstPart && secondPart ? ' ' : '';
-
-  if (firstPart && x === 0) return `${sign}${firstPart}`;
-  if (!firstPart && x === 0) return '0';
-  if (!firstPart) return `${sign}${secondPart}`;
-
-  return sign + firstPart + space + secondPart;
+function SafeInteger(n) {
+  return n < Number.MAX_SAFE_INTEGER;
 }
 
-console.log(mixedFraction('567/-399')); // '-1 8/19'
-console.log(mixedFraction('1/-2')); // '-1/2'
-console.log(mixedFraction('-2/9')); // '-2/9'
-console.log(mixedFraction('42/9')); // '4 2/3'
-console.log(mixedFraction('6/3')); // '2'
-console.log(mixedFraction('1/1')); // '1'
-console.log(mixedFraction('11/11')); // '1'
-console.log(mixedFraction('4/6')); // '2/3'
-console.log(mixedFraction('0/18891')); // '0'
-console.log(mixedFraction('-10/7')); // '-1 3/7'
-console.log(mixedFraction('-22/-7')); // '3 1/7'
-
-try {
-  Test.expectError('Must raise ZeroDivisionError', function () {
-    mixedFraction('0/0');
-  });
-} catch (e) {
-  console.log('Throw1.');
-}
-
-try {
-  Test.expectError('Must raise ZeroDivisionError', function () {
-    mixedFraction('3/0');
-  });
-} catch (e) {
-  console.log('Throw2.');
-}
+console.log(SafeInteger(9007199254740992)); // false
+console.log(SafeInteger(9007199254740990)); // true
--- src/test/test.js
+++ src/test/test.js
@@ -1,28 +1,9 @@
 mocha.setup('bdd');
 
-describe('Enumerable Magic #20 - Cascading Subsets', () => {
+describe('Is integer safe to use?', () => {
   it('test', () => {
-    chai.expect(mixedFraction('567/-399')).to.equal('-1 8/19');
-    chai.expect(mixedFraction('1/-2')).to.equal('-1/2');
-    chai.expect(mixedFraction('-2/9')).to.equal('-2/9');
-    chai.expect(mixedFraction('42/9')).to.equal('4 2/3');
-    chai.expect(mixedFraction('6/3')).to.equal('2');
-    chai.expect(mixedFraction('1/1')).to.equal('1');
-    chai.expect(mixedFraction('11/11')).to.equal('1');
-    chai.expect(mixedFraction('4/6')).to.equal('2/3');
-    chai.expect(mixedFraction('0/18891')).to.equal('0');
-    chai.expect(mixedFraction('-10/7')).to.equal('-1 3/7');
-    chai.expect(mixedFraction('-22/-7')).to.equal('3 1/7');
-    chai
-      .expect(function () {
-        mixedFraction('0/0');
-      })
-      .to.throw();
-    chai
-      .expect(function () {
-        mixedFraction('3/0');
-      })
-      .to.throw();
+    chai.expect(SafeInteger(9007199254740992)).to.equal(false);
+    chai.expect(SafeInteger(9007199254740990)).to.equal(true);
   });
 });
 

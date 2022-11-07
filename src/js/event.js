81bdfe508dfab1936b12f9f466cfedbfb6c3779e
Yury Olennikov <OlennikovYV@mail.ru>
2022-11-07 08:51:45 +0500

    shorter concat [reverse longer] refactor 2

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,6 @@
 function shorter_reverse_longer(a, b) {
-  return a.length >= b.length
-    ? b + a.split('').reverse().join('') + b
-    : a + b.split('').reverse().join('') + a;
+  if (a.length >= b.length) [a, b] = [b, a];
+  return a + b.split('').reverse().join('') + a;
 }
 
 console.log(shorter_reverse_longer('first', 'abcde'));

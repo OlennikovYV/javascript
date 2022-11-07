488c420cf66365259bbdb5d4f94887201d003390
Yury Olennikov <OlennikovYV@mail.ru>
2022-11-07 08:47:56 +0500

    shorter concat [reverse longer]

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,15 @@
-function broken(x) {
-  return x.replace(/[01]/g, bin => '10'[bin]);
+function shorter_reverse_longer(a, b) {
+  const lengthA = a.length;
+  const lengthB = b.length;
+  const reverseLong = (lengthA >= lengthB ? a : b).split('').reverse().join('');
+  const shorter = lengthA >= lengthB ? b : a;
+
+  return shorter + reverseLong + shorter;
 }
 
-console.log(broken('1')); // '0'
-console.log(broken('10000000101101111110011001000'));
-// '01111111010010000001100110111'
-console.log(broken('100010')); // '011101'
+console.log(shorter_reverse_longer('first', 'abcde'));
+// 'abcdetsrifabcde'
+console.log(shorter_reverse_longer('hello', 'bau'));
+// 'bauollehbau');
+console.log(shorter_reverse_longer('fghi', 'abcde'));
+// 'fghiedcbafghi'
--- src/test/test.js
+++ src/test/test.js
@@ -1,12 +1,14 @@
 mocha.setup('bdd');
 
-describe('The Office III - Broken Photocopier', () => {
-  it('Testing for fixed tests', () => {
-    chai.expect(broken('1')).to.equal('0');
+describe('shorter concat [reverse longer]', () => {
+  it('test', () => {
     chai
-      .expect(broken('10000000101101111110011001000'))
-      .to.equal('01111111010010000001100110111');
-    chai.expect(broken('100010')).to.equal('011101');
+      .expect(shorter_reverse_longer('first', 'abcde'))
+      .to.equal('abcdetsrifabcde');
+    chai.expect(shorter_reverse_longer('hello', 'bau')).to.equal('bauollehbau');
+    chai
+      .expect(shorter_reverse_longer('fghi', 'abcde'))
+      .to.equal('fghiedcbafghi');
   });
 });
 

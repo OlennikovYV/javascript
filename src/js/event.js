38117f4329a95b0c7525811f33b40d001fdbef5b
Yury Olennikov <OlennikovYV@mail.ru>
2022-11-09 08:52:35 +0500

    Remove All The Marked Elements of a List

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,27 @@
-const max = { name: 'Max Headroom' };
+Array.prototype.remove_ = function (integer_list, values_list) {
+  return integer_list.filter(
+    el => values_list.some(inc => inc === el) === false
+  );
+};
 
-function getMax1() {
-  return max;
-}
+let l = new Array();
 
-function getMax2() {
-  return max;
-}
+let integer_list = [1, 1, 2, 3, 1, 2, 3, 4];
+let values_list = [1, 3];
+console.log(l.remove_(integer_list, values_list)); // [2, 2, 4]
 
-console.log(getMax2() == getMax1()); // true
+integer_list = [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8];
+values_list = [1, 3, 4, 2];
+console.log(l.remove_(integer_list, values_list)); // [5, 6, 7, 8]
+
+integer_list = [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3];
+values_list = [2, 4, 3];
+console.log(l.remove_(integer_list, values_list)); // [8, 7, 6, 5, 1]
+
+integer_list = [4, 4, 2, 3];
+values_list = [2, 2, 4, 3];
+console.log(l.remove_(integer_list, values_list)); // []
+
+integer_list = [];
+values_list = [2, 2, 4, 3];
+console.log(l.remove_(integer_list, values_list)); // []
--- src/test/test.js
+++ src/test/test.js
@@ -1,8 +1,28 @@
 mocha.setup('bdd');
 
-describe('Max Headroom and JavaScript style', () => {
+describe('Remove All The Marked Elements of a List', () => {
   it('test', () => {
-    chai.expect(getMax2()).to.eql(getMax1());
+    let l = new Array();
+
+    let integer_list = [1, 1, 2, 3, 1, 2, 3, 4];
+    let values_list = [1, 3];
+    chai.expect(l.remove_(integer_list, values_list)).to.eql([2, 2, 4]);
+
+    integer_list = [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8];
+    values_list = [1, 3, 4, 2];
+    chai.expect(l.remove_(integer_list, values_list)).to.eql([5, 6, 7, 8]);
+
+    integer_list = [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3];
+    values_list = [2, 4, 3];
+    chai.expect(l.remove_(integer_list, values_list)).to.eql([8, 7, 6, 5, 1]);
+
+    integer_list = [4, 4, 2, 3];
+    values_list = [2, 2, 4, 3];
+    chai.expect(l.remove_(integer_list, values_list)).to.eql([]);
+
+    integer_list = [];
+    values_list = [2, 2, 4, 3];
+    chai.expect(l.remove_(integer_list, values_list)).to.eql([]);
   });
 });
 

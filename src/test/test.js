a530f0c5ce704592d30af20fa9c63a4faf0512ac
Yury Olennikov <OlennikovYV@mail.ru>
2022-11-10 08:35:30 +0500

    Formatting decimal places #1

--- src/test/test.js
+++ src/test/test.js
@@ -1,28 +1,10 @@
 mocha.setup('bdd');
 
-describe('Remove All The Marked Elements of a List', () => {
-  it('test', () => {
-    let l = new Array();
-
-    let integer_list = [1, 1, 2, 3, 1, 2, 3, 4];
-    let values_list = [1, 3];
-    chai.expect(l.remove_(integer_list, values_list)).to.eql([2, 2, 4]);
-
-    integer_list = [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8];
-    values_list = [1, 3, 4, 2];
-    chai.expect(l.remove_(integer_list, values_list)).to.eql([5, 6, 7, 8]);
-
-    integer_list = [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3];
-    values_list = [2, 4, 3];
-    chai.expect(l.remove_(integer_list, values_list)).to.eql([8, 7, 6, 5, 1]);
-
-    integer_list = [4, 4, 2, 3];
-    values_list = [2, 2, 4, 3];
-    chai.expect(l.remove_(integer_list, values_list)).to.eql([]);
-
-    integer_list = [];
-    values_list = [2, 2, 4, 3];
-    chai.expect(l.remove_(integer_list, values_list)).to.eql([]);
+describe('Formatting decimal places #1', () => {
+  it('works for some examples', function () {
+    chai.expect(twoDecimalPlaces(10.1289767789)).to.equal(10.12);
+    chai.expect(twoDecimalPlaces(-7488.83485834983)).to.equal(-7488.83);
+    chai.expect(twoDecimalPlaces(4.653725356)).to.equal(4.65);
   });
 });
 

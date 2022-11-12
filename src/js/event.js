d0e570e59448a34a094d233905d09dd8cbaf6fb2
Yury <olennikovyv@mail.ru>
2022-11-12 21:52:56 +0500

    Filter unused digits refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,8 @@
 function unusedDigits(...listNumbers) {
-  let stringDigits = listNumbers.join('').split('');
-
-  return ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    .filter(digit => !stringDigits.includes(digit))
-    .sort()
-    .join('');
+  return '0123456789'.replace(
+    new RegExp('[' + listNumbers.join('') + ']', 'g'),
+    ''
+  );
 }
 
 console.log(unusedDigits(12, 34, 56, 78)); // '09'

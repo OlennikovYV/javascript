96aeb4e035fa2196d80d91a84d45129315a6ee76
Yury <olennikovyv@mail.ru>
2022-11-13 18:32:13 +0500

    Correct the time-string

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,51 @@
-function unusedDigits(...listNumbers) {
-  return '0123456789'.replace(
-    new RegExp('[' + listNumbers.join('') + ']', 'g'),
-    ''
-  );
+function timeCorrect(timestring) {
+  if (!timestring) return timestring;
+
+  if (/\d\d\:\d\d\:\d\d/.test(timestring)) {
+    let [hours, minutes, seconds] = timestring.split(':');
+
+    // debugger;
+
+    let seconsRemainder = Math.trunc(seconds / 60);
+    let minutesRemainder = Math.trunc(minutes / 60);
+
+    seconds = seconds % 60;
+    minutes = (minutes % 60) + seconsRemainder;
+    hours = (hours % 24) + minutesRemainder;
+
+    if (minutes >= 60) {
+      minutes %= 60;
+      hours += 1;
+    }
+
+    if (hours >= 24) {
+      hours %= 24;
+    }
+
+    seconds = String(seconds).padStart(2, '0');
+    minutes = String(minutes).padStart(2, '0');
+    hours = String(hours).padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
+  return null;
 }
 
-console.log(unusedDigits(12, 34, 56, 78)); // '09'
-console.log(unusedDigits(2015, 8, 26)); // '3479'
+// Null or Empty
+console.log(timeCorrect(null)); // null
+console.log(timeCorrect('')); // ''
+
+// Invalid Format
+console.log(timeCorrect('001122')); // null
+console.log(timeCorrect('00;11;22')); // null
+console.log(timeCorrect('0a:1c:22')); // null
+
+// Correction Tests
+console.log(timeCorrect('09:10:01')); // '09:10:01'
+console.log(timeCorrect('11:70:10')); // '12:10:10'
+console.log(timeCorrect('19:99:09')); // '20:39:09'
+console.log(timeCorrect('19:99:99')); // '20:40:39'
+console.log(timeCorrect('24:01:01')); // '00:01:01'
+console.log(timeCorrect('52:01:01')); // '04:01:01'
+console.log(timeCorrect('14:59:94')); // '15:00:34'
--- src/test/test.js
+++ src/test/test.js
@@ -1,9 +1,23 @@
 mocha.setup('bdd');
 
-describe('Filter unused digits', () => {
-  it('example tests', () => {
-    chai.expect(unusedDigits(12, 34, 56, 78)).to.equal('09');
-    chai.expect(unusedDigits(2015, 8, 26)).to.equal('3479');
+describe('Correct the time-string', () => {
+  it('test', () => {
+    // Null or Empty
+    chai.expect(timeCorrect(null)).to.equal(null);
+    chai.expect(timeCorrect('')).to.equal('');
+
+    // Invalid Format
+    chai.expect(timeCorrect('001122')).to.equal(null);
+    chai.expect(timeCorrect('00;11;22')).to.equal(null);
+    chai.expect(timeCorrect('0a:1c:22')).to.equal(null);
+
+    // Correction Tests
+    chai.expect(timeCorrect('09:10:01')).to.equal('09:10:01');
+    chai.expect(timeCorrect('11:70:10')).to.equal('12:10:10');
+    chai.expect(timeCorrect('19:99:09')).to.equal('20:39:09');
+    chai.expect(timeCorrect('19:99:99')).to.equal('20:40:39');
+    chai.expect(timeCorrect('24:01:01')).to.equal('00:01:01');
+    chai.expect(timeCorrect('52:01:01')).to.equal('04:01:01');
   });
 });
 

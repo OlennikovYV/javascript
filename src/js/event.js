f2b7d7358a8e994a3d2bc2073f5606cd4144e34c
Yury Olennikov <OlennikovYV@mail.ru>
2022-11-15 08:48:27 +0500

    Scrolling Text refactor 3

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,7 @@
-function scrollingText(text) {
-  const originalText = text.toUpperCase();
-
-  return originalText
+const scrollingText = text =>
+  text.toUpperCase() &&
+  text
     .split('')
     .map((_, i) => originalText.slice(i) + originalText.slice(0, i));
-}
 
 console.log(scrollingText('abc')); // ['ABC', 'BCA', 'CAB']

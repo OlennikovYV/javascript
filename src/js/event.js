c6247e54ac27906561ae1b3361c91f8c58a02a2a
Yury Olennikov <OlennikovYV@mail.ru>
2022-11-16 08:39:36 +0500

    Responsible Drinking

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,14 @@
-const scrollingText = text =>
-  text.toUpperCase() &&
-  text
-    .split('')
-    .map((_, i) => originalText.slice(i) + originalText.slice(0, i));
+function hydrate(s) {
+  let sumGlass = s
+    .match(/[\d+]/g)
+    .reduce((sum, drink) => sum + Number(drink), 0);
+  return sumGlass > 1
+    ? `${sumGlass} glasses of water`
+    : `${sumGlass} glass of water`;
+}
 
-console.log(scrollingText('abc')); // ['ABC', 'BCA', 'CAB']
+console.log(hydrate('1 beer')); // '1 glass of water');
+console.log(hydrate('2 glasses of wine and 1 shot'));
+// '3 glasses of water'
+console.log(hydrate('1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer'));
+// '10 glasses of water'
--- src/test/test.js
+++ src/test/test.js
@@ -1,8 +1,14 @@
 mocha.setup('bdd');
 
-describe('Scrolling Text', () => {
-  it('test', () => {
-    chai.expect(scrollingText('abc')).to.eql(['ABC', 'BCA', 'CAB']);
+describe('Responsible Drinking', () => {
+  it('Basic tests', () => {
+    chai.expect(hydrate('1 beer')).to.equal('1 glass of water');
+    chai
+      .expect(hydrate('2 glasses of wine and 1 shot'))
+      .to.equal('3 glasses of water');
+    chai
+      .expect(hydrate('1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer'))
+      .to.equal('10 glasses of water');
   });
 });
 

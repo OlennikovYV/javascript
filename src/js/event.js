d71b8110293a197a2104acd9fe834c696f1b3f83
Yury <olennikovyv@mail.ru>
2022-11-20 19:59:40 +0500

    L2: Triple X

--- src/js/event.js
+++ src/js/event.js
@@ -1,44 +1,12 @@
-function convertNumberToString(num) {
-  const numbers = [
-    'zero',
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-    'ten',
-    'eleven',
-    'twelve',
-    'thirteen',
-    'fourteen',
-    'fifteen',
-    'sixteen',
-    'seventeen',
-    'eighteen',
-    'nineteen',
-    'twenty',
-  ];
+function tripleX(str) {
+  const indexX = str.indexOf('x');
 
-  return numbers[num] || '';
+  if (indexX === -1) return false;
+
+  return str[indexX + 1] + str[indexX + 2] === 'xx';
 }
 
-function wallpaper(l, w, h) {
-  if (!l || !w || !h) return convertNumberToString(0);
-
-  const rolls = 5.2;
-  const wall = 2 * h * (l + w);
-  const volumeWithNeeds = wall * 1.15;
-  const countRolls = Math.ceil(volumeWithNeeds / rolls);
-
-  return convertNumberToString(countRolls);
-}
-
-console.log(wallpaper(6.3, 4.5, 3.29)); // 'sixteen'
-console.log(wallpaper(7.8, 2.9, 3.29)); // 'sixteen'
-console.log(wallpaper(6.3, 5.8, 3.13)); // 'seventeen'
-console.log(wallpaper(6.1, 6.7, 2.81)); // 'sixteen'
-console.log(wallpaper(0, 5.6, 3.2)); // 'zero'
+console.log(tripleX('abraxxxas')); // true
+console.log(tripleX('xoxotrololololololoxxx')); // false
+console.log(tripleX('soft kitty, warm kitty, xxxxx')); // true
+console.log(tripleX('softx kitty, warm kitty, xxxxx')); // false
--- src/test/test.js
+++ src/test/test.js
@@ -1,12 +1,11 @@
 mocha.setup('bdd');
 
-describe('Easy wallpaper', () => {
-  it('Basic tests', function () {
-    chai.expect(wallpaper(6.3, 4.5, 3.29)).to.equal('sixteen');
-    chai.expect(wallpaper(7.8, 2.9, 3.29)).to.equal('sixteen');
-    chai.expect(wallpaper(6.3, 5.8, 3.13)).to.equal('seventeen');
-    chai.expect(wallpaper(6.1, 6.7, 2.81)).to.equal('sixteen');
-    chai.expect(wallpaper(0, 5.6, 3.2)).to.equal('zero');
+describe('L2: Triple X', () => {
+  it('Testing for fixed tests', () => {
+    chai.expect(tripleX('abraxxxas')).to.equal(true);
+    chai.expect(tripleX('xoxotrololololololoxxx')).to.equal(false);
+    chai.expect(tripleX('soft kitty, warm kitty, xxxxx')).to.equal(true);
+    chai.expect(tripleX('softx kitty, warm kitty, xxxxx')).to.equal(false);
   });
 });
 

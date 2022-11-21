267f62d45f0f67a080f637fc2899d24bc1d2c2b1
Yury <olennikovyv@mail.ru>
2022-11-21 10:39:00 +0500

    Largest Elements

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,10 @@
-function tripleX(str) {
-  return /^[^x]*xxx/.test(str);
+function largest(n, xs) {
+  xs.sort((a, b) => b - a);
+
+  return n ? xs.slice(0, n).sort((a, b) => a - b) : [];
 }
 
-console.log(tripleX('abraxxxas')); // true
-console.log(tripleX('xoxotrololololololoxxx')); // false
-console.log(tripleX('soft kitty, warm kitty, xxxxx')); // true
-console.log(tripleX('softx kitty, warm kitty, xxxxx')); // false
+console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1])); // [9, 10]
+console.log(largest(3, [5, 1, 5, 2, 3, 1, 2, 3, 5])); // [5, 5, 5]
+console.log(largest(7, [9, 1, 50, 22, 3, 13, 2, 63, 5])); // [3, 5, 9, 13, 22, 50, 63]
+console.log(largest(0, [1, 2, 3, 4, 8, 7, 6, 5])); // []
--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,13 @@
 mocha.setup('bdd');
 
-describe('L2: Triple X', () => {
-  it('Testing for fixed tests', () => {
-    chai.expect(tripleX('abraxxxas')).to.equal(true);
-    chai.expect(tripleX('xoxotrololololololoxxx')).to.equal(false);
-    chai.expect(tripleX('soft kitty, warm kitty, xxxxx')).to.equal(true);
-    chai.expect(tripleX('softx kitty, warm kitty, xxxxx')).to.equal(false);
+describe('Largest Elements', () => {
+  it('test', () => {
+    chai.expect(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).to.eql([9, 10]);
+    chai.expect(largest(3, [5, 1, 5, 2, 3, 1, 2, 3, 5])).to.eql([5, 5, 5]);
+    chai
+      .expect(largest(7, [9, 1, 50, 22, 3, 13, 2, 63, 5]))
+      .to.eql([3, 5, 9, 13, 22, 50, 63]);
+    chai.expect(largest(0, [1, 2, 3, 4, 8, 7, 6, 5])).to.eql([]);
   });
 });
 

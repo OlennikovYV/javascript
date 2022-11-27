ebe96be9be1f95dbf7715e4ff668a07a50a87fde
Yury <olennikovyv@mail.ru>
2022-11-27 17:35:02 +0500

    Frequency sequence refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,8 @@
 function freqSeq(str, sep) {
-  const arrStr = Array.from(str);
-  const countChar = arrStr.reduce((counts, char) => {
-    counts[char] = (counts[char] || 0) + 1;
-    return counts;
-  }, {});
-
-  return arrStr.map(char => countChar[char]).join(sep);
+  return str
+    .split('')
+    .map((char, _, arr) => arr.filter(charDup => charDup === char).length)
+    .join(sep);
 }
 
 console.log(freqSeq('hello world', '-')); // '1-1-3-3-2-1-1-2-1-3-1'

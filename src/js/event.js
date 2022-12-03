2f2d974d5198d73c10b1c91ef8a06e0de4b59b6d
Yury <olennikovyv@mail.ru>
2022-12-03 18:05:37 +0500

    Unscrambled eggs

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,6 @@
-function pyramid(n) {
-  return Array(n)
-    .fill()
-    .map((e, i) => Array(i + 1).fill(1));
+function unscrambleEggs(word) {
+  return word.replace(/egg/g, '');
 }
 
-console.log(pyramid(0)); // []
-console.log(pyramid(1)); // [[1]]
-console.log(pyramid(2)); // [[1], [1, 1]]
-console.log(pyramid(3)); // [[1], [1, 1], [1, 1, 1]]
+console.log(unscrambleEggs('ceggodegge heggeregge')); // 'code here'
+console.log(unscrambleEggs('FeggUNegg KeggATeggA')); // 'FUN KATA'
--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,10 @@
 mocha.setup('bdd');
 
-describe('Pyramid Array', () => {
-  it('Testing for 0', () => chai.expect(pyramid(0)).to.eql([]));
-  it('Testing for 1', () => chai.expect(pyramid(1)).to.eql([[1]]));
-  it('Testing for 2', () => chai.expect(pyramid(2)).to.eql([[1], [1, 1]]));
-  it('Testing for 3', () =>
-    chai.expect(pyramid(3)).to.eql([[1], [1, 1], [1, 1, 1]]));
+describe('Unscrambled eggs', () => {
+  it('test', () => {
+    chai.expect(unscrambleEggs('ceggodegge heggeregge')).to.equal('code here');
+    chai.expect(unscrambleEggs('FeggUNegg KeggATeggA')).to.equal('FUN KATA');
+  });
 });
 
 mocha.run();

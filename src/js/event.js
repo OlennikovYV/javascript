976be26679ac7dccc706100fe3514e9f38e36953
Yury Olennikov <OlennikovYV@mail.ru>
2022-12-06 08:56:08 +0500

    Fizz Buzz

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,13 @@
-function sumSquares(array) {
-  return array.reduce((acc, num) => acc + Math.pow(num, 2), 0);
+// Return an array
+function fizzbuzz(n) {
+  return Array.from({ length: n }, (_, i) => {
+    const index = i + 1;
+    let element = '';
+    if (index % 3 === 0) element += 'Fizz';
+    if (index % 5 === 0) element += 'Buzz';
+    return element === '' ? index : element;
+  });
 }
 
-console.log(sumSquares([1, 2, 3, 4, 5])); // 55
-console.log(sumSquares([7, 3, 9, 6, 5])); // 200
-console.log(sumSquares([11, 13, 15, 18, 2])); // 843
-console.log(sumSquares([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])); // 110
+console.log(JSON.stringify(fizzbuzz(15)));
+// [1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz"]
--- src/test/test.js
+++ src/test/test.js
@@ -1,13 +1,27 @@
 mocha.setup('bdd');
 
-describe('Be Concise III - Sum Squares', () => {
-  it('should behave in exactly the same way as the program prior to refactoring for the range of inputs tested', () => {
-    chai.expect(sumSquares([1, 2, 3, 4, 5])).to.equal(55);
-    chai.expect(sumSquares([7, 3, 9, 6, 5])).to.equal(200);
-    chai.expect(sumSquares([11, 13, 15, 18, 2])).to.equal(843);
+describe('Fizz Buzz', () => {
+  it('Should fizzify 10 numbers correctly', function () {
+    var expected = [
+      1,
+      2,
+      'Fizz',
+      4,
+      'Buzz',
+      'Fizz',
+      7,
+      8,
+      'Fizz',
+      'Buzz',
+      11,
+      'Fizz',
+      13,
+      14,
+      'FizzBuzz',
+    ];
     chai
-      .expect(sumSquares([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]))
-      .to.equal(110);
+      .expect(JSON.stringify(fizzbuzz(15)))
+      .to.equal(JSON.stringify(expected));
   });
 });
 

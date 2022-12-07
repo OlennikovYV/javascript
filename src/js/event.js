291631520dceeb7a0c672adc688de68a333c0913
Yury Olennikov <OlennikovYV@mail.ru>
2022-12-07 08:50:00 +0500

    Generate range of integers refactor 2

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,12 @@
+function* rangeGenerator(min, max, step) {
+  while (min <= max) {
+    yield min;
+    min += step;
+  }
+}
+
 function generateRange(min, max, step) {
-  return Array.from(
-    { length: 1 + (max - min) / step },
-    (_, i) => min + i * step
-  );
+  return [...rangeGenerator(min, max, step)];
 }
 
 console.log(generateRange(2, 10, 2)); // [2, 4, 6, 8, 10]

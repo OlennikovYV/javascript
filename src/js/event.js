b31e5c2c47eebe05388eebafb96230b2b1386955
Yury Olennikov <OlennikovYV@mail.ru>
2022-12-12 08:55:37 +0500

    Multiply Word in String

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,38 @@
-function bingo(a) {
-  return [2, 9, 14, 7, 15].every(num => a.includes(num)) ? 'WIN' : 'LOSE';
+function modifyMultiply(str, loc, num) {
+  return Array.from({ length: num }, _ => str.split(' ')[loc]).join('-');
 }
 
-console.log(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 'LOSE'
-console.log(bingo([21, 13, 2, 7, 5, 14, 7, 15, 9, 10])); // 'WIN'
+console.log(modifyMultiply('This is a string', 3, 5));
+// 'string-string-string-string-string',
+console.log(
+  modifyMultiply(
+    "Creativity is the process of having original ideas that have value. It is a process; it's not random.",
+    8,
+    10
+  )
+);
+// 'that-that-that-that-that-that-that-that-that-that'
+console.log(
+  modifyMultiply(
+    'Self-control means wanting to be effective at some random point in the infinite radiations of my spiritual existence',
+    1,
+    1
+  )
+);
+// 'means'
+console.log(
+  modifyMultiply(
+    "Is sloppiness in code caused by ignorance or apathy? I don't know and I don't care.",
+    6,
+    8
+  )
+);
+// 'ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance'
+console.log(
+  modifyMultiply(
+    'Everything happening around me is very random. I am enjoying the phase, as the journey is far more enjoyable than the destination.',
+    2,
+    5
+  )
+);
+// 'around-around-around-around-around'
--- src/test/test.js
+++ src/test/test.js
@@ -1,9 +1,48 @@
 mocha.setup('bdd');
 
-describe('Bingo ( Or Not )', () => {
+describe('Multiply Word in String', () => {
   it('test', () => {
-    chai.expect(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to.equal('LOSE');
-    chai.expect(bingo([21, 13, 2, 7, 5, 14, 7, 15, 9, 10])).to.equal('WIN');
+    chai
+      .expect(modifyMultiply('This is a string', 3, 5))
+      .to.equal('string-string-string-string-string');
+    chai
+      .expect(
+        modifyMultiply(
+          "Creativity is the process of having original ideas that have value. It is a process; it's not random.",
+          8,
+          10
+        )
+      )
+      .to.equal('that-that-that-that-that-that-that-that-that-that');
+    chai
+      .expect(
+        modifyMultiply(
+          'Self-control means wanting to be effective at some random point in the infinite radiations of my spiritual existence',
+          1,
+          1
+        )
+      )
+      .to.equal('means');
+    chai
+      .expect(
+        modifyMultiply(
+          "Is sloppiness in code caused by ignorance or apathy? I don't know and I don't care.",
+          6,
+          8
+        )
+      )
+      .to.equal(
+        'ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance'
+      );
+    chai
+      .expect(
+        modifyMultiply(
+          'Everything happening around me is very random. I am enjoying the phase, as the journey is far more enjoyable than the destination.',
+          2,
+          5
+        )
+      )
+      .to.equal('around-around-around-around-around');
   });
 });
 

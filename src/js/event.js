5e61f6dcbcb9bf0c55f2dcfad9de4cdb89fa4a12
Yury Olennikov <OlennikovYV@mail.ru>
2022-12-14 08:42:32 +0500

    Search for letters refactor

--- src/js/event.js
+++ src/js/event.js
@@ -2,13 +2,12 @@ function change(string) {
   let result = Array(26).fill(0);
 
   return result
-    .map((_, index) => {
-      return (
-        string.includes(String.fromCodePoint(65 + index)) ||
-        string.includes(String.fromCodePoint(97 + index))
-      );
-    })
-    .map(checkChar => (checkChar ? '1' : '0'))
+    .map((_, index) =>
+      string.includes(String.fromCodePoint(65 + index)) ||
+      string.includes(String.fromCodePoint(97 + index))
+        ? '1'
+        : '0'
+    )
     .join('');
 }
 

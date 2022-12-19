907dd14ec2e29c819c6ac9796e9ae4dcad873ec6
Yury Olennikov <OlennikovYV@mail.ru>
2022-12-19 09:35:47 +0500

    Find variable which breaks strict comparison!

--- src/js/event.js
+++ src/js/event.js
@@ -1,30 +1,6 @@
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
+function findStrangeValue() {
+  return NaN;
 }
 
-class Circle {
-  constructor(center, radius) {
-    this.center = center;
-    this.radius = radius;
-  }
-}
-
-function circleCircumference(circle) {
-  return 2 * Math.PI * circle.radius;
-}
-
-console.log(+circleCircumference(new Circle(new Point(10, 10), 30)).toFixed(6));
-// 188.495559
-console.log(
-  +circleCircumference(new Circle(new Point(25, -70), 30)).toFixed(6)
-);
-// 188.495559
-console.log(+circleCircumference(new Circle(new Point(-15, 5), 0)).toFixed(6));
-// 0
-console.log(
-  +circleCircumference(new Circle(new Point(-15, 5), 12.5)).toFixed(6)
-);
-// 78.539816
+let x = findStrangeValue();
+console.log(x === x); // false
--- src/test/test.js
+++ src/test/test.js
@@ -1,25 +1,9 @@
 mocha.setup('bdd');
 
-describe('Geometry Basics: Circle Circumference in 2D', () => {
-  it('should pass fixed tests', function () {
-    chai
-      .expect(
-        +circleCircumference(new Circle(new Point(10, 10), 30)).toFixed(6)
-      )
-      .to.equal(188.495559);
-    chai
-      .expect(
-        +circleCircumference(new Circle(new Point(25, -70), 30)).toFixed(6)
-      )
-      .to.equal(188.495559);
-    chai
-      .expect(+circleCircumference(new Circle(new Point(-15, 5), 0)).toFixed(6))
-      .to.equal(0);
-    chai
-      .expect(
-        +circleCircumference(new Circle(new Point(-15, 5), 12.5)).toFixed(6)
-      )
-      .to.equal(78.539816);
+describe('Find variable which breaks strict comparison!', () => {
+  it('test', () => {
+    let x = findStrangeValue();
+    chai.expect(x === x).to.equal(false);
   });
 });
 

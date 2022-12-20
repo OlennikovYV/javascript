c54a064a5de6c5b68bd619d254c1e4c597011164
Yury Olennikov <OlennikovYV@mail.ru>
2022-12-20 08:45:26 +0500

    Unique Sum

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,12 @@
-function findStrangeValue() {
-  return +'fart';
+function uniqueSum(lst) {
+  const uniqueNumber = [...new Set(lst)];
+
+  return uniqueNumber.length
+    ? uniqueNumber.reduce((sum, number) => sum + number)
+    : null;
 }
 
-let x = findStrangeValue();
-console.log(x === x); // false
+console.log(uniqueSum([1, 2, 3])); // 6
+console.log(uniqueSum([1, 3, 8, 1, 8])); // 12
+console.log(uniqueSum([-1, -1, 5, 2, -7])); // -1
+console.log(uniqueSum([])); // null
--- src/test/test.js
+++ src/test/test.js
@@ -1,9 +1,11 @@
 mocha.setup('bdd');
 
-describe('Find variable which breaks strict comparison!', () => {
+describe('Unique Sum', () => {
   it('test', () => {
-    let x = findStrangeValue();
-    chai.expect(x === x).to.equal(false);
+    chai.expect(uniqueSum([1, 2, 3])).to.equal(6);
+    chai.expect(uniqueSum([1, 3, 8, 1, 8])).to.equal(12);
+    chai.expect(uniqueSum([-1, -1, 5, 2, -7])).to.equal(-1);
+    chai.expect(uniqueSum([])).to.equal(null);
   });
 });
 

1d53a96cf5786bc573f7d81cbae34bef50aac3f0
Yury Olennikov <OlennikovYV@mail.ru>
2022-12-26 08:42:27 +0500

    Is every value in the array an array? refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,4 +1,4 @@
-const arrCheck = value => value.every(el => Array.isArray(el));
+const arrCheck = value => value.every(Array.isArray);
 
 console.log(arrCheck([])); // true
 console.log(arrCheck([['string']])); // true

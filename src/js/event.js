2154dcb823c746a0e707fa818811b7cbc2bd29be
Yury Olennikov <OlennikovYV@mail.ru>
2022-12-27 09:13:48 +0500

    Caffeine Script

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,18 @@
-const arrCheck = value => value.every(Array.isArray);
+function caffeineBuzz(n) {
+  let result = '';
 
-console.log(arrCheck([])); // true
-console.log(arrCheck([['string']])); // true
-console.log(arrCheck([[], {}])); // false
-console.log(arrCheck([[1], [2], [3]])); // true
-console.log(arrCheck(['A', 'R', 'R', 'A', 'Y'])); // false
+  if (n % 3 !== 0) return 'mocha_missing!';
+
+  if (n % 3 === 0 && n % 4 !== 0)
+    result += 'Java' + (n % 2 === 0 ? 'Script' : '');
+  if (n % 3 === 0 && n % 4 === 0)
+    result += 'Coffee' + (n % 2 === 0 ? 'Script' : '');
+
+  return result;
+}
+
+console.log(caffeineBuzz(1)); // 'mocha_missing!'
+console.log(caffeineBuzz(3)); // 'Java'
+console.log(caffeineBuzz(4)); // 'mocha_missing!'
+console.log(caffeineBuzz(6)); // 'JavaScript'
+console.log(caffeineBuzz(12)); // 'CoffeeScript'
--- src/test/test.js
+++ src/test/test.js
@@ -1,12 +1,12 @@
 mocha.setup('bdd');
 
-describe('Is every value in the array an array?', () => {
+describe('Caffeine Script', () => {
   it('Tests to see if your function returns the correct values.', () => {
-    chai.expect(arrCheck([])).to.equal(true);
-    chai.expect(arrCheck([['string']])).to.equal(true);
-    chai.expect(arrCheck([[], {}])).to.equal(false);
-    chai.expect(arrCheck([[1], [2], [3]])).to.equal(true);
-    chai.expect(arrCheck(['A', 'R', 'R', 'A', 'Y'])).to.equal(false);
+    chai.expect(caffeineBuzz(1)).to.equal('mocha_missing!');
+    chai.expect(caffeineBuzz(3)).to.equal('Java');
+    chai.expect(caffeineBuzz(4)).to.equal('mocha_missing!');
+    chai.expect(caffeineBuzz(6)).to.equal('JavaScript');
+    chai.expect(caffeineBuzz(12)).to.equal('CoffeeScript');
   });
 });
 

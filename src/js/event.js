ea72a57bf762eddbf756a1d3efcddee9e2192c9e
Yury Olennikov <OlennikovYV@mail.ru>
2022-12-30 08:36:42 +0500

    Find the lucky numbers refactor 2

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,5 @@
 var filterLucky = x => {
-  const checkSevenDigit = num => String(num).includes('7');
-
-  return x.filter(checkSevenDigit);
+  return x.filter(number => /7/.test(number));
 };
 
 console.log(filterLucky([1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17])); // [7, 70, 17]

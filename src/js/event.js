7b913335ea3256bf42fb82baf9672d81a9708d71
Yury Olennikov <OlennikovYV@mail.ru>
2023-01-09 09:10:12 +0500

    Javascript filter - 1

--- src/js/event.js
+++ src/js/event.js
@@ -1,31 +1,11 @@
-function Node(data) {
-  this.data = data;
-  this.next = null;
+function searchNames(logins) {
+  return logins.filter(el => /_$/.test(el[0]));
 }
 
-function push(head, data) {
-  const firstNode = new Node(data);
-  firstNode.next = head;
+const a = [
+    ['foo', 'foo@foo.com'],
+    ['bar_', 'bar@bar.com'],
+  ],
+  b = [['bar_', 'bar@bar.com']];
 
-  return firstNode;
-}
-
-function buildOneTwoThree() {
-  var chained = null;
-
-  chained = push(chained, 3);
-  chained = push(chained, 2);
-  chained = push(chained, 1);
-
-  return chained;
-}
-
-console.log(push(null, 1).data); // 1
-console.log(push(null, 1).next); // null
-console.log(push(new Node(1), 2).data); // 2
-console.log(push(new Node(1), 2).next.data); // 1
-
-console.log(buildOneTwoThree().data); // 1
-console.log(buildOneTwoThree().next.data); // 2
-console.log(buildOneTwoThree().next.next.data); // 3
-console.log(buildOneTwoThree().next.next.next); // null
+console.log(searchNames(a).join(':'), b.join(':'));
--- src/test/test.js
+++ src/test/test.js
@@ -1,18 +1,13 @@
 mocha.setup('bdd');
 
-describe('Linked Lists - Push & BuildOneTwoThree', function () {
-  it('tests for inserting a node before another node.', function () {
-    chai.expect(push(null, 1).data).to.equal(1);
-    chai.expect(push(null, 1).next).to.equal(null);
-    chai.expect(push(new Node(1), 2).data).to.equal(2);
-    chai.expect(push(new Node(1), 2).next.data).to.equal(1);
-  });
-
-  it('tests for building a linked list.', function () {
-    chai.expect(buildOneTwoThree().data).to.equal(1);
-    chai.expect(buildOneTwoThree().next.data).to.equal(2);
-    chai.expect(buildOneTwoThree().next.next.data).to.equal(3);
-    chai.expect(buildOneTwoThree().next.next.next).to.equal(null);
+describe('Javascript filter - 1', function () {
+  it('Basic list', function () {
+    const a = [
+        ['foo', 'foo@foo.com'],
+        ['bar_', 'bar@bar.com'],
+      ],
+      b = [['bar_', 'bar@bar.com']];
+    chai.expect(searchNames(a).join(':')).to.equal(b.join(':'));
   });
 });
 

92c16eacb411e0820a04520263be6a84d20ddc18
Yury Olennikov <OlennikovYV@mail.ru>
2023-01-24 09:02:17 +0500

    Sum ALL the arrays!

--- src/js/event.js
+++ src/js/event.js
@@ -1,27 +1,11 @@
-function mergeArrays(a, b) {
-  let joinArray = [];
-  let count = a.length > b.length ? a.length : b.length;
-
-  for (let i = 0; i < count; i += 1) {
-    if (a[i]) joinArray.push(a[i]);
-    if (b[i]) joinArray.push(b[i]);
-  }
-
-  return joinArray;
+function arraySum(arr) {
+  return arr.reduce((sum, el) => {
+    if (Array.isArray(el)) sum += arraySum(el);
+    else if (!isNaN(el)) sum += el;
+    return sum;
+  }, 0);
 }
 
-console.log(mergeArrays([1, 2, 3, 4, 5, 6, 7, 8], ['a', 'b', 'c', 'd', 'e']));
-// [1, 'a', 2, 'b', 3, 'c', 4, 'd', 5, 'e', 6, 7, 8]
-console.log(mergeArrays(['a', 'b', 'c', 'd', 'e'], [1, 2, 3, 4, 5]));
-// ['a', 1, 'b', 2, 'c', 3, 'd', 4, 'e', 5]
-console.log(mergeArrays([2, 5, 8, 23, 67, 6], ['b', 'r', 'a', 'u', 'r', 's']));
-// [2, 'b', 5, 'r', 8, 'a', 23, 'u', 67, 'r', 6, 's']
-console.log(
-  mergeArrays(
-    ['b', 'r', 'a', 'u', 'r', 's', 'e', 'q', 'z'],
-    [2, 5, 8, 23, 67, 6]
-  )
-);
-// ['b', 2, 'r', 5, 'a', 8, 'u', 23, 'r', 67, 's', 6, 'e', 'q', 'z']
-console.log(mergeArrays([1, 2, 3], ['a', 'b', 'c', 'd', 'e']));
-// [1, 'a', 2, 'b', 3, 'c', 'd', 'e']
+console.log(arraySum([1, 2])); // 3
+console.log(arraySum([1, 2, 3])); // 6
+console.log(arraySum([1, 2, [1, 2]])); // 6
--- src/test/test.js
+++ src/test/test.js
@@ -1,40 +1,10 @@
 mocha.setup('bdd');
 
-describe('Merge two arrays', function () {
-  it('Passes if arrays are equals', function () {
-    chai
-      .expect(mergeArrays([1, 2, 3, 4, 5, 6, 7, 8], ['a', 'b', 'c', 'd', 'e']))
-      .to.eql([1, 'a', 2, 'b', 3, 'c', 4, 'd', 5, 'e', 6, 7, 8]);
-    chai
-      .expect(mergeArrays(['a', 'b', 'c', 'd', 'e'], [1, 2, 3, 4, 5]))
-      .to.eql(['a', 1, 'b', 2, 'c', 3, 'd', 4, 'e', 5]);
-    chai
-      .expect(mergeArrays([2, 5, 8, 23, 67, 6], ['b', 'r', 'a', 'u', 'r', 's']))
-      .to.eql([2, 'b', 5, 'r', 8, 'a', 23, 'u', 67, 'r', 6, 's']);
-    chai
-      .expect(
-        mergeArrays(
-          ['b', 'r', 'a', 'u', 'r', 's', 'e', 'q', 'z'],
-          [2, 5, 8, 23, 67, 6]
-        )
-      )
-      .to.eql([
-        'b',
-        2,
-        'r',
-        5,
-        'a',
-        8,
-        'u',
-        23,
-        'r',
-        67,
-        's',
-        6,
-        'e',
-        'q',
-        'z',
-      ]);
+describe('Sum ALL the arrays!', function () {
+  it('test', () => {
+    chai.expect(arraySum([1, 2])).to.equal(3);
+    chai.expect(arraySum([1, 2, 3])).to.equal(6);
+    chai.expect(arraySum([1, 2, [1, 2]])).to.equal(6);
   });
 });
 

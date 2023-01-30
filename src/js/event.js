72be737244d9a1ee8881393624d9efba5ac8d883
Yury Olennikov <OlennikovYV@mail.ru>
2023-01-30 08:41:50 +0500

    filterEvenLengthWords

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,7 @@
-function repeats(arr) {
-  return arr
-    .filter(el => arr.indexOf(el) === arr.lastIndexOf(el))
-    .reduce((sum, el) => sum + el);
+function filterEvenLengthWords(words) {
+  return words.filter(el => el.length % 2 === 0);
 }
 
-console.log(repeats([4, 5, 7, 5, 4, 8])); // 15
-console.log(repeats([9, 10, 19, 13, 19, 13])); // 19
-console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11])); // 12
-console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13])); //22
-console.log(repeats([5, 10, 19, 13, 10, 13])); // 24
+console.log(filterEvenLengthWords(['Hello', 'World']).length); // 0
+
+console.log(filterEvenLengthWords(['One', 'Two', 'Three', 'Four']).length); // 1
--- src/test/test.js
+++ src/test/test.js
@@ -1,12 +1,12 @@
 mocha.setup('bdd');
 
-describe('Sum of array singles', function () {
-  it('Should pass description examples', () => {
-    chai.expect(repeats([4, 5, 7, 5, 4, 8])).to.equal(15);
-    chai.expect(repeats([9, 10, 19, 13, 19, 13])).to.equal(19);
-    chai.expect(repeats([16, 0, 11, 4, 8, 16, 0, 11])).to.equal(12);
-    chai.expect(repeats([5, 17, 18, 11, 13, 18, 11, 13])).to.equal(22);
-    chai.expect(repeats([5, 10, 19, 13, 10, 13])).to.equal(24);
+describe('filterEvenLengthWords', function () {
+  it('test', () => {
+    chai.expect(filterEvenLengthWords(['Hello', 'World']).length).to.equal(0);
+
+    chai
+      .expect(filterEvenLengthWords(['One', 'Two', 'Three', 'Four']).length)
+      .to.equal(1);
   });
 });
 

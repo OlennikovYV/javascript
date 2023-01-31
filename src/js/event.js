92258c78876ba3ad8b1136c17b85b0587de70ad4
Yury Olennikov <OlennikovYV@mail.ru>
2023-01-31 08:40:30 +0500

    Evens times last

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,13 @@
-function filterEvenLengthWords(words) {
-  return words.filter(el => el.length % 2 === 0);
+function evenLast(numbers) {
+  const lastNumber = numbers[numbers.length - 1] || 0;
+
+  return (
+    lastNumber *
+    numbers.reduce((sum, num, index) => {
+      if (index % 2 === 0) sum += num;
+      return sum;
+    }, 0)
+  );
 }
 
-console.log(filterEvenLengthWords(['Hello', 'World']).length); // 0
-
-console.log(filterEvenLengthWords(['One', 'Two', 'Three', 'Four']).length); // 1
+console.log(evenLast([2, 3, 4, 5])); // 30
--- src/test/test.js
+++ src/test/test.js
@@ -2,11 +2,7 @@ mocha.setup('bdd');
 
 describe('filterEvenLengthWords', function () {
   it('test', () => {
-    chai.expect(filterEvenLengthWords(['Hello', 'World']).length).to.equal(0);
-
-    chai
-      .expect(filterEvenLengthWords(['One', 'Two', 'Three', 'Four']).length)
-      .to.equal(1);
+    chai.assert.equal(evenLast([2, 3, 4, 5]), 30);
   });
 });
 

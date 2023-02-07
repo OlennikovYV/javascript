34b78bc042bca43cdfbab32c0b3fbb53a4ddca0b
Yury Olennikov <OlennikovYV@mail.ru>
2023-02-07 10:38:26 +0500

    Pick peaks

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,51 @@
-function cookingTime(eggs) {
-  return Math.ceil(eggs / 8) * 5;
+function pickPeaks(arr) {
+  const peaksArray = { pos: [], peaks: [] };
+
+  if (arr.length < 2) return peaksArray;
+  for (let i = 1; i < arr.length - 1; i += 1) {
+    if (arr[i - 1] < arr[i]) {
+      if (arr[i] > arr[i + 1]) {
+        peaksArray.pos.push(i);
+        peaksArray.peaks.push(arr[i]);
+        continue;
+      }
+      if (arr[i] === arr[i + 1]) {
+        const j = i;
+
+        while (arr[i] === arr[i + 1] && i < arr.length - 1) {
+          i += 1;
+        }
+
+        if (i < arr.length - 1 && arr[j] > arr[i + 1]) {
+          peaksArray.pos.push(j);
+          peaksArray.peaks.push(arr[j]);
+        }
+      }
+    }
+  }
+
+  return peaksArray;
 }
 
-console.log(cookingTime(0)); // 0
-console.log(cookingTime(5)); // 5
-console.log(cookingTime(10)); // 10
+console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]));
+// { pos: [3, 7], peaks: [6, 3] }
+console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]));
+// { pos: [3, 7], peaks: [6, 3] }
+console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1]));
+// { pos: [3, 7, 10], peaks: [6, 3, 2] }
+console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1]));
+// { pos: [2, 4], peaks: [3, 2] }
+console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2]));
+// { pos: [2], peaks: [3] }
+console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 5, 6]));
+// { pos: [2], peaks: [3] }
+console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1]));
+// { pos: [2], peaks: [3] }
+console.log(
+  pickPeaks([
+    1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3,
+  ])
+);
+// { pos: [2, 7, 14, 20], peaks: [5, 6, 5, 5] }
+console.log(pickPeaks([])); // { pos: [], peaks: [] });
+console.log(pickPeaks([1, 1, 1, 1])); // { pos: [], peaks: [] });
--- src/test/test.js
+++ src/test/test.js
@@ -1,10 +1,45 @@
 mocha.setup('bdd');
 
-describe('Boiled Eggs', function () {
-  it('should calculate the cooking time', function () {
-    chai.expect(cookingTime(0)).to.equal(0);
-    chai.expect(cookingTime(5)).to.equal(5);
-    chai.expect(cookingTime(10)).to.equal(10);
+describe('Pick peaks', function () {
+  it('Tests', function () {
+    chai.expect(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1])).to.eql({
+      pos: [3, 7],
+      peaks: [6, 3],
+    });
+    chai.expect(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3])).to.eql({
+      pos: [3, 7],
+      peaks: [6, 3],
+    });
+    chai.expect(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1])).to.eql({
+      pos: [3, 7, 10],
+      peaks: [6, 3, 2],
+    });
+    chai.expect(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1])).to.eql({
+      pos: [2, 4],
+      peaks: [3, 2],
+    });
+    chai.expect(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2])).to.eql({
+      pos: [2],
+      peaks: [3],
+    });
+    chai.expect(pickPeaks([2, 1, 3, 2, 2, 2, 2, 5, 6])).to.eql({
+      pos: [2],
+      peaks: [3],
+    });
+    chai.expect(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1])).to.eql({
+      pos: [2],
+      peaks: [3],
+    });
+    chai
+      .expect(
+        pickPeaks([
+          1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4,
+          3,
+        ])
+      )
+      .to.eql({ pos: [2, 7, 14, 20], peaks: [5, 6, 5, 5] });
+    chai.expect(pickPeaks([])).to.eql({ pos: [], peaks: [] });
+    chai.expect(pickPeaks([1, 1, 1, 1])).to.eql({ pos: [], peaks: [] });
   });
 });
 

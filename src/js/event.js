23ab615321e0c929a6b35a971faf71f67a4c7208
Yury Olennikov <OlennikovYV@mail.ru>
2023-02-09 08:36:10 +0500

    Help Suzuki rake his garden!

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,18 @@
-function capMe(names) {
-  return names.map(el => el[0].toUpperCase() + el.slice(1).toLowerCase());
+function rakeGarden(garden) {
+  return garden
+    .split(' ')
+    .map(el => {
+      if (el !== 'rock' && el !== 'gravel') el = 'gravel';
+      return el;
+    })
+    .join(' ');
 }
 
-console.log(capMe(['jo', 'nelson', 'jurie']));
-// returns ['Jo', 'Nelson', 'Jurie']
-console.log(capMe(['KARLY', 'DANIEL', 'KELSEY']));
-// returns ['Karly', 'Daniel', 'Kelsey']
+const garden1 =
+  'slug spider rock gravel gravel gravel gravel gravel gravel gravel';
+console.log(rakeGarden(garden1));
+// 'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
+const garden2 =
+  'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel';
+console.log(rakeGarden(garden2));
+// 'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel'
--- src/test/test.js
+++ src/test/test.js
@@ -1,13 +1,21 @@
 mocha.setup('bdd');
 
-describe('Pick peaks', function () {
-  it('Tests', function () {
+describe('Help Suzuki rake his garden!', function () {
+  it('should work for sample (fixed) tests', () => {
+    const garden1 =
+      'slug spider rock gravel gravel gravel gravel gravel gravel gravel';
     chai
-      .expect(capMe(['jo', 'nelson', 'jurie']))
-      .to.eql(['Jo', 'Nelson', 'Jurie']);
+      .expect(rakeGarden(garden1))
+      .to.equal(
+        'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
+      );
+    const garden2 =
+      'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel';
     chai
-      .expect(capMe(['KARLY', 'DANIEL', 'KELSEY']))
-      .to.eql(['Karly', 'Daniel', 'Kelsey']);
+      .expect(rakeGarden(garden2))
+      .to.equal(
+        'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel'
+      );
   });
 });
 

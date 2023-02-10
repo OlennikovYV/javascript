12b47b66cc993a6fcd643a62fadcbdf5018b7c38
Yury Olennikov <OlennikovYV@mail.ru>
2023-02-10 08:41:55 +0500

    Return Two Highest Values in List

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,7 @@
-function rakeGarden(garden) {
-  return garden.replace(/slug|spider|snail|ant|moss/g, 'gravel');
+function twoHighest(arr) {
+  return [...new Set(arr.sort((a, b) => b - a))].slice(0, 2);
 }
 
-const garden1 =
-  'slug spider rock gravel gravel gravel gravel gravel gravel gravel';
-console.log(rakeGarden(garden1));
-// 'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
-const garden2 =
-  'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel';
-console.log(rakeGarden(garden2));
-// 'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel'
+console.log(twoHighest([])); // []
+console.log(twoHighest([15])); // [15]
+console.log(twoHighest([15, 20, 20, 17])); // [20, 17]
--- src/test/test.js
+++ src/test/test.js
@@ -1,21 +1,10 @@
 mocha.setup('bdd');
 
-describe('Help Suzuki rake his garden!', function () {
-  it('should work for sample (fixed) tests', () => {
-    const garden1 =
-      'slug spider rock gravel gravel gravel gravel gravel gravel gravel';
-    chai
-      .expect(rakeGarden(garden1))
-      .to.equal(
-        'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
-      );
-    const garden2 =
-      'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel';
-    chai
-      .expect(rakeGarden(garden2))
-      .to.equal(
-        'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel'
-      );
+describe('Return Two Highest Values in List', function () {
+  it('Tests', () => {
+    chai.expect(twoHighest([])).to.eql([]);
+    chai.expect(twoHighest([15])).to.eql([15]);
+    chai.expect(twoHighest([15, 20, 20, 17])).to.eql([20, 17]);
   });
 });
 

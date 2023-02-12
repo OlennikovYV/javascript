e7680356d88144e4ddb2582c12b5c92e323bda60
unknown <olennikovyv@mail.ru>
2023-02-12 16:59:31 +0500

    Is your period late?

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,11 @@
-var isMonotone = function (arr) {
-  return arr.every((el, i) => i == 0 || el >= arr[i - 1]);
-};
+function periodIsLate(last, today, cycleLength) {
+  const oneDay = 1000 * 60 * 60 * 24;
+  const diffDates = today - last;
 
-const range = function (a, b) {
-  const r = [],
-    step = b - a > 0 ? 1 : -1;
-  do {
-    r.push(a);
-  } while (step * (b - (a += step)) >= 0);
-  return r;
-};
+  return Math.ceil(diffDates / oneDay) > cycleLength;
+}
 
-console.log(isMonotone(range(1, 10))); // true
-console.log(isMonotone(range(4, 12))); // true
-console.log(isMonotone([5, 5, 5, 5, 5])); // true
-console.log(isMonotone([])); // true
-console.log(isMonotone(range(5, 1))); // false
-console.log(isMonotone([1, 2, 3, 3, 4, 5])); // false
+console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 35));
+// false
+console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 28));
+// true
--- src/test/test.js
+++ src/test/test.js
@@ -1,29 +1,13 @@
 mocha.setup('bdd');
 
-describe('Monotone travel', function () {
-  const range = function (a, b) {
-    const r = [],
-      step = b - a > 0 ? 1 : -1;
-    do {
-      r.push(a);
-    } while (step * (b - (a += step)) >= 0);
-    return r;
-  };
-  it('should work on increasing lists', function () {
-    chai.expect(isMonotone(range(1, 10)));
-    chai.expect(isMonotone(range(4, 12)));
-  });
-  it('should work on constant lists', function () {
-    chai.expect(isMonotone([5, 5, 5, 5, 5]));
-  });
-  it('should work on empty an empty list', function () {
-    chai.expect(isMonotone([]));
-  });
-  it('should return false on a decreasing list', function () {
-    chai.expect(!isMonotone(range(5, 1)));
-  });
-  it('should work on a non-decreasing list', function () {
-    chai.expect(isMonotone([1, 2, 3, 3, 4, 5]));
+describe('Is your period late?', function () {
+  it('test', () => {
+    chai
+      .expect(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 35))
+      .to.equal(false);
+    chai
+      .expect(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 28))
+      .to.equal(true);
   });
 });
 

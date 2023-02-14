1de8628baf9632ba3579b41c654ec8d219ec10de
Yury Olennikov <OlennikovYV@mail.ru>
2023-02-14 08:43:21 +0500

    Unpacking Arguments

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,14 @@
-function periodIsLate(last, today, cycleLength) {
-  const oneDay = 1000 * 60 * 60 * 24;
-  const diffDates = today - last;
-
-  return Math.ceil(diffDates / oneDay) > cycleLength;
+function spread(func, args) {
+  return func.call(null, ...args);
 }
 
-console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 35));
-// false
-console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 28));
-// true
+console.log(
+  spread(
+    function (x, y) {
+      return x + y;
+    },
+    [1, 2]
+  )
+);
+//  3
+// Equivalent: (function(x,y){return x+y})(1,2)
--- src/test/test.js
+++ src/test/test.js
@@ -1,13 +1,17 @@
 mocha.setup('bdd');
 
-describe('Is your period late?', function () {
+describe('Unpacking Arguments', function () {
   it('test', () => {
     chai
-      .expect(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 35))
-      .to.equal(false);
-    chai
-      .expect(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 28))
-      .to.equal(true);
+      .expect(
+        spread(
+          function (x, y) {
+            return x + y;
+          },
+          [1, 2]
+        )
+      )
+      .to.equal(3);
   });
 });
 

c21375886c9aa3ea930c24fa259aaa25e2e3a46a
Yury Olennikov <OlennikovYV@mail.ru>
2023-02-17 08:45:08 +0500

    Return to Sanity

--- src/js/event.js
+++ src/js/event.js
@@ -1,49 +1,7 @@
-function getNth(node, index) {
-  if (node == null) throw 'error';
-
-  return index == 0 ? node : getNth(node.next, index - 1);
+function mystery() {
+  var results = { sanity: 'Hello' };
+  return results;
 }
 
-function Node(data) {
-  this.data = data;
-  this.next = null;
-}
-
-function push(head, data) {
-  const firstNode = new Node(data);
-  firstNode.next = head;
-
-  return firstNode;
-}
-
-function buildOneTwoThree() {
-  var chained = null;
-
-  chained = push(chained, 3);
-  chained = push(chained, 2);
-  chained = push(chained, 1);
-
-  return chained;
-}
-
-var list = buildOneTwoThree();
-
-console.log(getNth(list, 0).data); // 1
-console.log(getNth(list, 1).data); // 2
-console.log(getNth(list, 2).data); // 3
-
-try {
-  getNth(list, 3);
-} catch (err) {
-  console.log('Invalid index value should throw error.');
-}
-try {
-  getNth(list, 100);
-} catch (err) {
-  console.log('Invalid index value should throw error.');
-}
-try {
-  getNth(null, 0);
-} catch (err) {
-  console.log('Null linked list should throw error.');
-}
+console.log(mystery());
+// { sanity: 'Hello' }
--- src/test/test.js
+++ src/test/test.js
@@ -1,33 +1,8 @@
 mocha.setup('bdd');
 
-describe('Linked Lists - Get Nth Node', function () {
-  var list = buildOneTwoThree();
-
-  it('', function () {
-    chai
-      .expect(getNth(list, 0).data)
-      .to.equal(1, 'First node should be located at index 0.');
-    chai
-      .expect(getNth(list, 1).data)
-      .to.equal(2, 'Second node should be located at index 1.');
-    chai
-      .expect(getNth(list, 2).data)
-      .to.equal(3, 'Third node should be located at index 2.');
-
-    let fn = function () {
-      getNth(list, 3);
-    };
-    chai.expect(fn).to.throw();
-
-    fn = function () {
-      getNth(list, 100);
-    };
-    chai.expect(fn).to.throw();
-
-    fn = function () {
-      getNth(nult, 0);
-    };
-    chai.expect(fn).to.throw();
+describe('Return to Sanity', function () {
+  it('test', () => {
+    chai.expect(mystery()).to.eql({ sanity: 'Hello' });
   });
 });
 

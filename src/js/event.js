55ec5413b4d9ea1eb435d7373b01c9037ab4c1f0
unknown <olennikovyv@mail.ru>
2023-02-19 14:59:37 +0500

    Exclamation marks series #3: Remove all exclamation marks from sentence except at the end

--- src/js/event.js
+++ src/js/event.js
@@ -1,4 +1,22 @@
-const mystery = () => ({ sanity: 'Hello' });
+function remove(string) {
+  let result = '';
+  let last = true;
 
-console.log(mystery());
-// { sanity: 'Hello' }
+  for (let i = string.length - 1; i >= 0; i -= 1) {
+    if (last && string[i] == '!') {
+      result = '!' + result;
+      continue;
+    }
+    last = false;
+    if (string[i] != '!') result = string[i] + result;
+  }
+
+  return result;
+}
+
+console.log(remove('Hi!')); // 'Hi!'
+console.log(remove('Hi!!!')); // 'Hi!!!'
+console.log(remove('!Hi')); // 'Hi'
+console.log(remove('!Hi!')); // 'Hi!'
+console.log(remove('Hi! Hi!')); // 'Hi Hi!'
+console.log(remove('Hi')); // 'Hi'
--- src/test/test.js
+++ src/test/test.js
@@ -1,9 +1,18 @@
 mocha.setup('bdd');
 
-describe('Return to Sanity', function () {
-  it('test', () => {
-    chai.expect(mystery()).to.eql({ sanity: 'Hello' });
-  });
-});
+describe(
+  'Exclamation marks series #3: Remove all exclamation marks ' +
+    'from sentence except at the end',
+  function () {
+    it('sample tests', function () {
+      chai.expect(remove('Hi!')).to.equal('Hi!');
+      chai.expect(remove('Hi!!!')).to.equal('Hi!!!');
+      chai.expect(remove('!Hi')).to.equal('Hi');
+      chai.expect(remove('!Hi!')).to.equal('Hi!');
+      chai.expect(remove('Hi! Hi!')).to.equal('Hi Hi!');
+      chai.expect(remove('Hi')).to.equal('Hi');
+    });
+  }
+);
 
 mocha.run();

f8f596593e98c6f22cf98573bddc9a4e58224a21
unknown <olennikovyv@mail.ru>
2023-02-19 15:00:20 +0500

    Exclamation marks series #3: Remove all exclamation marks from sentence except at the end refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,17 +1,5 @@
 function remove(string) {
-  let result = '';
-  let last = true;
-
-  for (let i = string.length - 1; i >= 0; i -= 1) {
-    if (last && string[i] == '!') {
-      result = '!' + result;
-      continue;
-    }
-    last = false;
-    if (string[i] != '!') result = string[i] + result;
-  }
-
-  return result;
+  return string.replace(/!+(?!!*$)/g, '');
 }
 
 console.log(remove('Hi!')); // 'Hi!'

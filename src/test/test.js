2b88cbf8ab4111608723f5924701c57e346fd604
Yury Olennikov <OlennikovYV@mail.ru>
2023-02-20 08:42:49 +0500

    Training JS #13: Number object and its properties

--- src/test/test.js
+++ src/test/test.js
@@ -1,18 +1,25 @@
 mocha.setup('bdd');
 
-describe(
-  'Exclamation marks series #3: Remove all exclamation marks ' +
-    'from sentence except at the end',
-  function () {
-    it('sample tests', function () {
-      chai.expect(remove('Hi!')).to.equal('Hi!');
-      chai.expect(remove('Hi!!!')).to.equal('Hi!!!');
-      chai.expect(remove('!Hi')).to.equal('Hi');
-      chai.expect(remove('!Hi!')).to.equal('Hi!');
-      chai.expect(remove('Hi! Hi!')).to.equal('Hi Hi!');
-      chai.expect(remove('Hi')).to.equal('Hi');
-    });
-  }
-);
+describe('Training JS #13: Number object and its properties', function () {
+  it('test', () => {
+    chai
+      .expect(whatNumberIsIt(1 / 0))
+      .to.equal('Input number is Number.POSITIVE_INFINITY');
+    chai.expect(whatNumberIsIt(100)).to.equal('Input number is 100');
+    chai
+      .expect(whatNumberIsIt(1.7976931348623157e308))
+      .to.equal('Input number is Number.MAX_VALUE');
+    chai
+      .expect(whatNumberIsIt(5e-324))
+      .to.equal('Input number is Number.MIN_VALUE');
+    chai
+      .expect(whatNumberIsIt(-Number.MAX_VALUE * 2))
+      .to.equal('Input number is Number.NEGATIVE_INFINITY');
+    chai.expect(whatNumberIsIt(NaN)).to.equal('Input number is Number.NaN');
+    chai
+      .expect(whatNumberIsIt(Infinity + 1))
+      .to.equal('Input number is Number.POSITIVE_INFINITY');
+  });
+});
 
 mocha.run();

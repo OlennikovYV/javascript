5b1cfcb6dcfaf5d2cfd0c2cc5da22bc5cf4c4f5d
unknown <olennikovyv@mail.ru>
2023-02-23 17:26:29 +0500

    The Lazy Startup Office

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,42 @@
-function search(budget, prices) {
-  return prices
-    .filter(price => price <= budget)
-    .sort((a, b) => a - b)
-    .toString();
+function binRota(arr) {
+  return arr.reduce((rota, row, index) => {
+    if (index % 2 === 0) {
+      for (let i = 0; i < row.length; i += 1) rota.push(row[i]);
+    } else {
+      for (let i = row.length - 1; i >= 0; i -= 1) rota.push(row[i]);
+    }
+    return rota;
+  }, []);
 }
 
-console.log(search(3, [6, 1, 2, 9, 2])); // '1,2,2'
-console.log(search(14, [7, 3, 23, 9, 14, 20, 7])); // '3,7,7,9,14'
-console.log(search(0, [6, 1, 2, 9, 2])); // ''
+console.log(
+  binRota([
+    ['Bob', 'Nora'],
+    ['Ruby', 'Carl'],
+  ])
+); // ['Bob', 'Nora', 'Carl', 'Ruby']
+console.log(binRota([['Billy']])); // ['Billy']
+console.log(binRota([['Billy', 'Nancy']])); // ['Billy', 'Nancy']
+console.log(binRota([['Billy'], ['Megan'], ['Aki'], ['Arun'], ['Joy']]));
+// ['Billy', 'Megan', 'Aki', 'Arun', 'Joy']
+console.log(
+  binRota([
+    ['Sam', 'Nina', 'Tim', 'Helen', 'Gurpreet', 'Edward', 'Holly', 'Eliza'],
+    ['Billy', 'Megan', 'Aki', 'Arun', 'Joy', 'Anish', 'Lee', 'Maryan'],
+    ['Nick', 'Josh', 'Pete', 'Kavita', 'Daisy', 'Francesca', 'Alfie', 'Macy'],
+  ])
+);
+// ['Sam', 'Nina', 'Tim', 'Helen', 'Gurpreet', 'Edward',
+// 'Holly', 'Eliza', 'Maryan', 'Lee', 'Anish', 'Joy',
+// 'Arun', 'Aki', 'Megan', 'Billy', 'Nick', 'Josh',
+// 'Pete', 'Kavita', 'Daisy', 'Francesca', 'Alfie', 'Macy']
+console.log(
+  binRota([
+    ['Stefan', 'Raj', 'Marie'],
+    ['Alexa', 'Amy', 'Edward'],
+    ['Liz', 'Claire', 'Juan'],
+    ['Dee', 'Luke', 'Elle'],
+  ])
+);
+// ['Stefan', 'Raj', 'Marie', 'Edward', 'Amy', 'Alexa',
+// 'Liz', 'Claire', 'Juan', 'Elle', 'Luke', 'Dee']
--- src/test/test.js
+++ src/test/test.js
@@ -1,10 +1,95 @@
 mocha.setup('bdd');
 
-describe('Filter Coffee', function () {
+describe('The Lazy Startup Office', function () {
   it('test', () => {
-    chai.expect(search(3, [6, 1, 2, 9, 2])).to.eql('1,2,2');
-    chai.expect(search(14, [7, 3, 23, 9, 14, 20, 7])).to.eql('3,7,7,9,14');
-    chai.expect(search(0, [6, 1, 2, 9, 2])).to.eql('');
+    chai
+      .expect(
+        binRota([
+          ['Bob', 'Nora'],
+          ['Ruby', 'Carl'],
+        ])
+      )
+      .to.eql(['Bob', 'Nora', 'Carl', 'Ruby']);
+    chai.expect(binRota([['Billy']])).to.eql(['Billy']);
+    chai.expect(binRota([['Billy', 'Nancy']])).to.eql(['Billy', 'Nancy']);
+    chai
+      .expect(binRota([['Billy'], ['Megan'], ['Aki'], ['Arun'], ['Joy']]))
+      .to.eql(['Billy', 'Megan', 'Aki', 'Arun', 'Joy']);
+    chai
+      .expect(
+        binRota([
+          [
+            'Sam',
+            'Nina',
+            'Tim',
+            'Helen',
+            'Gurpreet',
+            'Edward',
+            'Holly',
+            'Eliza',
+          ],
+          ['Billy', 'Megan', 'Aki', 'Arun', 'Joy', 'Anish', 'Lee', 'Maryan'],
+          [
+            'Nick',
+            'Josh',
+            'Pete',
+            'Kavita',
+            'Daisy',
+            'Francesca',
+            'Alfie',
+            'Macy',
+          ],
+        ])
+      )
+      .to.eql([
+        'Sam',
+        'Nina',
+        'Tim',
+        'Helen',
+        'Gurpreet',
+        'Edward',
+        'Holly',
+        'Eliza',
+        'Maryan',
+        'Lee',
+        'Anish',
+        'Joy',
+        'Arun',
+        'Aki',
+        'Megan',
+        'Billy',
+        'Nick',
+        'Josh',
+        'Pete',
+        'Kavita',
+        'Daisy',
+        'Francesca',
+        'Alfie',
+        'Macy',
+      ]);
+    chai
+      .expect(
+        binRota([
+          ['Stefan', 'Raj', 'Marie'],
+          ['Alexa', 'Amy', 'Edward'],
+          ['Liz', 'Claire', 'Juan'],
+          ['Dee', 'Luke', 'Elle'],
+        ])
+      )
+      .to.eql([
+        'Stefan',
+        'Raj',
+        'Marie',
+        'Edward',
+        'Amy',
+        'Alexa',
+        'Liz',
+        'Claire',
+        'Juan',
+        'Elle',
+        'Luke',
+        'Dee',
+      ]);
   });
 });
 

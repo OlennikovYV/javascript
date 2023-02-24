0c843595a413440249255d797c34154dc401cb43
unknown <olennikovyv@mail.ru>
2023-02-24 20:22:42 +0500

    Exclamation marks series #8: Move all exclamation marks to the end of the sentence

--- src/js/event.js
+++ src/js/event.js
@@ -1,39 +1,17 @@
-function binRota(arr) {
-  return arr.reduce(
-    (rota, row, index) =>
-      index % 2 === 0 ? rota.concat(row) : rota.concat(row.reverse()),
-    []
-  );
+function remove(string) {
+  let exclamationMarks = '';
+  let stringWithoutMarks = '';
+
+  for (let i = 0; i < string.length; i++)
+    string[i] === '!'
+      ? (exclamationMarks += '!')
+      : (stringWithoutMarks += string[i]);
+
+  return stringWithoutMarks + exclamationMarks;
 }
 
-console.log(
-  binRota([
-    ['Bob', 'Nora'],
-    ['Ruby', 'Carl'],
-  ])
-); // ['Bob', 'Nora', 'Carl', 'Ruby']
-console.log(binRota([['Billy']])); // ['Billy']
-console.log(binRota([['Billy', 'Nancy']])); // ['Billy', 'Nancy']
-console.log(binRota([['Billy'], ['Megan'], ['Aki'], ['Arun'], ['Joy']]));
-// ['Billy', 'Megan', 'Aki', 'Arun', 'Joy']
-console.log(
-  binRota([
-    ['Sam', 'Nina', 'Tim', 'Helen', 'Gurpreet', 'Edward', 'Holly', 'Eliza'],
-    ['Billy', 'Megan', 'Aki', 'Arun', 'Joy', 'Anish', 'Lee', 'Maryan'],
-    ['Nick', 'Josh', 'Pete', 'Kavita', 'Daisy', 'Francesca', 'Alfie', 'Macy'],
-  ])
-);
-// ['Sam', 'Nina', 'Tim', 'Helen', 'Gurpreet', 'Edward',
-// 'Holly', 'Eliza', 'Maryan', 'Lee', 'Anish', 'Joy',
-// 'Arun', 'Aki', 'Megan', 'Billy', 'Nick', 'Josh',
-// 'Pete', 'Kavita', 'Daisy', 'Francesca', 'Alfie', 'Macy']
-console.log(
-  binRota([
-    ['Stefan', 'Raj', 'Marie'],
-    ['Alexa', 'Amy', 'Edward'],
-    ['Liz', 'Claire', 'Juan'],
-    ['Dee', 'Luke', 'Elle'],
-  ])
-);
-// ['Stefan', 'Raj', 'Marie', 'Edward', 'Amy', 'Alexa',
-// 'Liz', 'Claire', 'Juan', 'Elle', 'Luke', 'Dee']
+console.log(remove('Hi!')); // 'Hi!'
+console.log(remove('Hi! Hi!')); // 'Hi Hi!!'
+console.log(remove('Hi! Hi! Hi!')); // 'Hi Hi Hi!!!'
+console.log(remove('Hi! !Hi Hi!')); // 'Hi Hi Hi!!!'
+console.log(remove('Hi! Hi!! Hi!')); // 'Hi Hi Hi!!!!'
--- src/test/test.js
+++ src/test/test.js
@@ -1,96 +1,17 @@
 mocha.setup('bdd');
 
-describe('The Lazy Startup Office', function () {
-  it('test', () => {
-    chai
-      .expect(
-        binRota([
-          ['Bob', 'Nora'],
-          ['Ruby', 'Carl'],
-        ])
-      )
-      .to.eql(['Bob', 'Nora', 'Carl', 'Ruby']);
-    chai.expect(binRota([['Billy']])).to.eql(['Billy']);
-    chai.expect(binRota([['Billy', 'Nancy']])).to.eql(['Billy', 'Nancy']);
-    chai
-      .expect(binRota([['Billy'], ['Megan'], ['Aki'], ['Arun'], ['Joy']]))
-      .to.eql(['Billy', 'Megan', 'Aki', 'Arun', 'Joy']);
-    chai
-      .expect(
-        binRota([
-          [
-            'Sam',
-            'Nina',
-            'Tim',
-            'Helen',
-            'Gurpreet',
-            'Edward',
-            'Holly',
-            'Eliza',
-          ],
-          ['Billy', 'Megan', 'Aki', 'Arun', 'Joy', 'Anish', 'Lee', 'Maryan'],
-          [
-            'Nick',
-            'Josh',
-            'Pete',
-            'Kavita',
-            'Daisy',
-            'Francesca',
-            'Alfie',
-            'Macy',
-          ],
-        ])
-      )
-      .to.eql([
-        'Sam',
-        'Nina',
-        'Tim',
-        'Helen',
-        'Gurpreet',
-        'Edward',
-        'Holly',
-        'Eliza',
-        'Maryan',
-        'Lee',
-        'Anish',
-        'Joy',
-        'Arun',
-        'Aki',
-        'Megan',
-        'Billy',
-        'Nick',
-        'Josh',
-        'Pete',
-        'Kavita',
-        'Daisy',
-        'Francesca',
-        'Alfie',
-        'Macy',
-      ]);
-    chai
-      .expect(
-        binRota([
-          ['Stefan', 'Raj', 'Marie'],
-          ['Alexa', 'Amy', 'Edward'],
-          ['Liz', 'Claire', 'Juan'],
-          ['Dee', 'Luke', 'Elle'],
-        ])
-      )
-      .to.eql([
-        'Stefan',
-        'Raj',
-        'Marie',
-        'Edward',
-        'Amy',
-        'Alexa',
-        'Liz',
-        'Claire',
-        'Juan',
-        'Elle',
-        'Luke',
-        'Dee',
-      ]);
-  });
-});
+describe(
+  'Exclamation marks series #8: Move all exclamation' +
+    'marks to the end of the sentence',
+  function () {
+    it('sample tests', function () {
+      chai.expect(remove('Hi!')).to.equal('Hi!');
+      chai.expect(remove('Hi! Hi!')).to.equal('Hi Hi!!');
+      chai.expect(remove('Hi! Hi! Hi!')).to.equal('Hi Hi Hi!!!');
+      chai.expect(remove('Hi! !Hi Hi!')).to.equal('Hi Hi Hi!!!');
+      chai.expect(remove('Hi! Hi!! Hi!')).to.equal('Hi Hi Hi!!!!');
+    });
+  }
+);
 
 mocha.run();

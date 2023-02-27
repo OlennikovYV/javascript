8951839c628f39b7bef4950349e30551f707a02d
Yury Olennikov <OlennikovYV@mail.ru>
2023-02-27 10:23:51 +0500

    True Min refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,14 @@
 function min(a, b) {
+  a === null && (a = 0);
+  b === null && (b = 0);
+
   if (isNaN(a) || isNaN(b)) return NaN;
 
-  return a < b ? Number(a) : Number(b);
+  return a < b ? a : b;
 }
 
 console.log(min(1, -2.5) === -2.5); // -2.5
 console.log(min(1.5, NaN)); // NaN
 console.log(min(1.5, undefined)); // NaN
 console.log(min(-Infinity, Infinity) === -Infinity); // -Infinity
-console.log(min(null, 2.5) === 0); // 'treat null as if it were 0'
+console.log(min(null, 2.5) === 0); // 0

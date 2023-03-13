ccabba7b7a22bda97a62c9e2c1360385a255c302
Yury Olennikov <OlennikovYV@mail.ru>
2023-03-13 08:40:16 +0500

    See You Next Happy Year

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,23 @@
-function bandNameGenerator(str) {
-  return str[0] === str[str.length - 1]
-    ? `${str[0].toUpperCase()}${str.slice(1).repeat(2)}`
-    : `The ${str[0].toUpperCase()}${str.slice(1)}`;
+function nextHappyYear(year) {
+  const checkYear = year =>
+    Number(Array.from(new Set(String(year).split(''))).join(''));
+
+  year += 1;
+
+  while (year !== (happyYear = checkYear(year))) {
+    year += 1;
+  }
+
+  return happyYear;
 }
 
-console.log(bandNameGenerator('knife')); // 'The Knife'
-console.log(bandNameGenerator('tart')); // 'Tartart'
-console.log(bandNameGenerator('sandles')); // 'Sandlesandles'
-console.log(bandNameGenerator('bed')); // 'The Bed'
+console.log(nextHappyYear(1001)); // 1023
+console.log(nextHappyYear(1123)); // 1203
+console.log(nextHappyYear(2001)); // 2013
+console.log(nextHappyYear(2334)); // 2340
+console.log(nextHappyYear(3331)); // 3401
+console.log(nextHappyYear(1987)); // 2013
+console.log(nextHappyYear(5555)); // 5601
+console.log(nextHappyYear(7712)); // 7801
+console.log(nextHappyYear(8088)); // 8091
+console.log(nextHappyYear(8999)); // 9012
--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,17 @@
 mocha.setup('bdd');
 
-describe('Band name generator', function () {
+describe('See You Next Happy Year', function () {
   it('test', () => {
-    chai.expect(bandNameGenerator('knife')).to.equal('The Knife');
-    chai.expect(bandNameGenerator('tart')).to.equal('Tartart');
-    chai.expect(bandNameGenerator('sandles')).to.equal('Sandlesandles');
-    chai.expect(bandNameGenerator('bed')).to.equal('The Bed');
+    chai.expect(nextHappyYear(1001)).to.equal(1023);
+    chai.expect(nextHappyYear(1123)).to.equal(1203);
+    chai.expect(nextHappyYear(2001)).to.equal(2013);
+    chai.expect(nextHappyYear(2334)).to.equal(2340);
+    chai.expect(nextHappyYear(3331)).to.equal(3401);
+    chai.expect(nextHappyYear(1987)).to.equal(2013);
+    chai.expect(nextHappyYear(5555)).to.equal(5601);
+    chai.expect(nextHappyYear(7712)).to.equal(7801);
+    chai.expect(nextHappyYear(8088)).to.equal(8091);
+    chai.expect(nextHappyYear(8999)).to.equal(9012);
   });
 });
 

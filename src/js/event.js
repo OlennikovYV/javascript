3d3751b17798f2555912a7fb3e2344634c49287f
Yury Olennikov <OlennikovYV@mail.ru>
2023-03-15 08:51:44 +0500

    Dominant array elements

--- src/js/event.js
+++ src/js/event.js
@@ -1,35 +1,19 @@
-function solve(a) {
-  return a.reduce((diff, element) => {
-    if (/\d/.test(element)) element % 2 === 0 ? (diff += 1) : (diff -= 1);
+function solve(arr) {
+  const dominant = [];
 
-    return diff;
-  }, 0);
+  for (let i = 0; i < arr.length; i += 1) {
+    const element = arr[i];
+    const sum = arr.slice(i + 1).every(el => el < element);
+
+    if (sum) dominant.push(element);
+  }
+
+  return dominant;
 }
 
-console.log(solve([0, 1, 2, 3])); // 0
-console.log(solve([0, 1, 2, 3, 'a', 'b'])); // 0
-console.log(solve([0, 15, 'z', 16, 'm', 13, 14, 'c', 9, 10, 13, 'u', 4, 3])); // 0
-console.log(solve([13, 6, 8, 15, 4, 8, 13])); // 1
-console.log(solve([1, 'a', 17, 8, 'e', 3, 'i', 12, 1])); // -2
-console.log(
-  solve([
-    5,
-    15,
-    16,
-    10,
-    6,
-    4,
-    16,
-    't',
-    13,
-    'n',
-    14,
-    'k',
-    'n',
-    0,
-    'q',
-    'd',
-    7,
-    9,
-  ])
-); // 2
+console.log(solve([16, 17, 14, 3, 14, 5, 2])); // [17, 14, 5, 2]
+console.log(solve([92, 52, 93, 31, 89, 87, 77, 105])); // [105]
+console.log(solve([75, 47, 42, 56, 13, 55])); // [75, 56, 55]
+console.log(solve([67, 54, 27, 85, 66, 88, 31, 24, 49])); // [88, 49]
+console.log(solve([76, 17, 25, 36, 29])); // [76, 36, 29]
+console.log(solve([104, 18, 37, 9, 36, 47, 28])); // [104, 47, 28]
--- src/test/test.js
+++ src/test/test.js
@@ -1,38 +1,13 @@
 mocha.setup('bdd');
 
-describe('See You Next Happy Year', function () {
+describe('Dominant array elements', function () {
   it('test', () => {
-    chai.expect(solve([0, 1, 2, 3])).to.equal(0);
-    chai.expect(solve([0, 1, 2, 3, 'a', 'b'])).to.equal(0);
-    chai
-      .expect(solve([0, 15, 'z', 16, 'm', 13, 14, 'c', 9, 10, 13, 'u', 4, 3]))
-      .to.equal(0);
-    chai.expect(solve([13, 6, 8, 15, 4, 8, 13])).to.equal(1);
-    chai.expect(solve([1, 'a', 17, 8, 'e', 3, 'i', 12, 1])).to.equal(-2);
-    chai
-      .expect(
-        solve([
-          5,
-          15,
-          16,
-          10,
-          6,
-          4,
-          16,
-          't',
-          13,
-          'n',
-          14,
-          'k',
-          'n',
-          0,
-          'q',
-          'd',
-          7,
-          9,
-        ])
-      )
-      .to.equal(2);
+    chai.expect(solve([16, 17, 14, 3, 14, 5, 2])).to.eql([17, 14, 5, 2]);
+    chai.expect(solve([92, 52, 93, 31, 89, 87, 77, 105])).to.eql([105]);
+    chai.expect(solve([75, 47, 42, 56, 13, 55])).to.eql([75, 56, 55]);
+    chai.expect(solve([67, 54, 27, 85, 66, 88, 31, 24, 49])).to.eql([88, 49]);
+    chai.expect(solve([76, 17, 25, 36, 29])).to.eql([76, 36, 29]);
+    chai.expect(solve([104, 18, 37, 9, 36, 47, 28])).to.eql([104, 47, 28]);
   });
 });
 

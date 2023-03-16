0a51100aa09a94509592f6d481c45ea71659ca9b
Yury Olennikov <OlennikovYV@mail.ru>
2023-03-16 08:39:09 +0500

    Double Every Other refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,5 @@
 function doubleEveryOther(a) {
-  return a.map((el, i) => ((i + 1) % 2 === 0 ? el * 2 : el));
+  return a.map((el, i) => (i % 2 === 0 ? el : el * 2));
 }
 
 console.log(doubleEveryOther([1, 2, 3, 4])); // [1, 4, 3, 8]

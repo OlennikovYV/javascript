1d35358eb489b83518fe55dbed5e76cf33250f9c
unknown <olennikovyv@mail.ru>
2023-03-26 22:20:40 +0500

    The reject() function

--- src/js/event.js
+++ src/js/event.js
@@ -1,43 +1,9 @@
-function multiples(s1, s2, s3) {
-  const maxDivide = Math.max(s1, s2);
-  const minDivide = Math.min(s1, s2);
-  const numbers = [];
-  let nextNumber = maxDivide;
-
-  while (nextNumber < s3) {
-    if (nextNumber % minDivide == 0) numbers.push(nextNumber);
-    nextNumber += maxDivide;
-  }
-
-  return numbers;
+function reject(array, predicate) {
+  return array.filter(el => predicate(el) == false);
 }
 
-let s1 = 2;
-let s2 = 4;
-let s3 = 40;
-console.log(multiples(s1, s2, s3));
-// [4, 8, 12, 16, 20, 24, 28, 32, 36]
-
-s1 = 13;
-s2 = 5;
-s3 = 800;
-console.log(multiples(s1, s2, s3));
-// [65, 130, 195, 260, 325, 390, 455, 520, 585, 650, 715, 780]
-
-s1 = 13;
-s2 = 15;
-s3 = 800;
-console.log(multiples(s1, s2, s3));
-// [195, 390, 585, 780]
-
-s1 = 17;
-s2 = 15;
-s3 = 800;
-console.log(multiples(s1, s2, s3));
-// [255, 510, 765]
-
-s1 = 27;
-s2 = 29;
-s3 = 2000;
-console.log(multiples(s1, s2, s3));
-// [783, 1566]
+console.log(
+  reject([1, 2, 3, 4, 5, 6], function (num) {
+    return num % 2 == 0;
+  })
+); // [1, 3, 5]
--- src/test/test.js
+++ src/test/test.js
@@ -1,43 +1,14 @@
 mocha.setup('bdd');
 
 describe('Show multiples of 2 numbers within a range', function () {
-  it('should test for s1=2 s2=4 s3=40', function () {
-    s1 = 2;
-    s2 = 4;
-    s3 = 40;
+  it('test', () => {
     chai
-      .expect(multiples(s1, s2, s3))
-      .to.eql([4, 8, 12, 16, 20, 24, 28, 32, 36]);
-  });
-
-  it('should test for s1=13 s2=5 s3=800', function () {
-    s1 = 13;
-    s2 = 5;
-    s3 = 800;
-    chai
-      .expect(multiples(s1, s2, s3))
-      .to.eql([65, 130, 195, 260, 325, 390, 455, 520, 585, 650, 715, 780]);
-  });
-
-  it('should test for s1=13 s2=15 s3=800', function () {
-    s1 = 13;
-    s2 = 15;
-    s3 = 800;
-    chai.expect(multiples(s1, s2, s3)).to.eql([195, 390, 585, 780]);
-  });
-
-  it('should test for s1=17 s2=15 s3=800', function () {
-    s1 = 17;
-    s2 = 15;
-    s3 = 800;
-    chai.expect(multiples(s1, s2, s3)).to.eql([255, 510, 765]);
-  });
-
-  it('should test for s1=27 s2=29 s3=2000', function () {
-    s1 = 27;
-    s2 = 29;
-    s3 = 2000;
-    chai.expect(multiples(s1, s2, s3)).to.eql([783, 1566]);
+      .expect(
+        reject([1, 2, 3, 4, 5, 6], function (num) {
+          return num % 2 == 0;
+        })
+      )
+      .to.eql([1, 3, 5]);
   });
 });
 

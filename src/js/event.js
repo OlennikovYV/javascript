05f9a31897b9cfa40687d564bef0d8a32590127b
Yury Olennikov <OlennikovYV@mail.ru>
2023-03-27 08:27:59 +0500

    Trimming a string

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,18 @@
-function reject(array, predicate) {
-  return array.filter(el => predicate(el) == false);
+function trim(str, size) {
+  if (str.length <= size) return str;
+  if (size <= 3) return str.slice(0, size) + '...';
+  return str.slice(0, size - 3) + '...';
 }
 
-console.log(
-  reject([1, 2, 3, 4, 5, 6], function (num) {
-    return num % 2 == 0;
-  })
-); // [1, 3, 5]
+console.log(trim('Creating kata is fun', 14));
+// 'Creating ka...');
+console.log(trim('He', 1));
+// 'H...');
+console.log(trim('Code Wars is pretty rad', 50));
+// 'Code Wars is pretty rad');
+console.log(trim('aWtI prI kp', 2));
+// 'aW...');
+console.log(trim('Hey', 3));
+// 'Hey');
+console.log(trim('Code Wars is pretty rad', 3));
+// 'Cod...');
--- src/test/test.js
+++ src/test/test.js
@@ -1,14 +1,15 @@
 mocha.setup('bdd');
 
-describe('Show multiples of 2 numbers within a range', function () {
-  it('test', () => {
+describe('Trimming a string', function () {
+  it('Should pass sample tests', () => {
+    chai.expect(trim('Creating kata is fun', 14)).to.equal('Creating ka...');
+    chai.expect(trim('He', 1)).to.equal('H...');
     chai
-      .expect(
-        reject([1, 2, 3, 4, 5, 6], function (num) {
-          return num % 2 == 0;
-        })
-      )
-      .to.eql([1, 3, 5]);
+      .expect(trim('Code Wars is pretty rad', 50))
+      .to.equal('Code Wars is pretty rad');
+    chai.expect(trim('aWtI prI kp', 2)).to.equal('aW...');
+    chai.expect(trim('Hey', 3)).to.equal('Hey');
+    chai.expect(trim('Code Wars is pretty rad', 3)).to.equal('Cod...');
   });
 });
 

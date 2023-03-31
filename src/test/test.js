2904153c70c364f8a92dfd21b7ded124ea1a1afc
Yury Olennikov <OlennikovYV@mail.ru>
2023-03-31 07:24:36 +0500

    All Star Code Challenge #1

--- src/test/test.js
+++ src/test/test.js
@@ -1,24 +1,11 @@
 mocha.setup('bdd');
 
-describe('Scrabble Score', function () {
-  it("returns 0 for ''", function () {
-    chai.expect(scrabbleScore('')).to.equal(0);
-  });
-  it('returns 1 for a', function () {
-    chai.expect(scrabbleScore('a')).to.equal(1);
-  });
-  it('returns 6 for street', function () {
-    chai.expect(scrabbleScore('street')).to.equal(6);
-  });
+describe('All Star Code Challenge #1', function () {
+  it('test', () => {
+    const iverson = new NBAplayer('Iverson', '76ers', 11.2);
+    const jordan = new NBAplayer('Jordan', 'bulls', 20.2);
 
-  it('returns score of " a" (with space)', function () {
-    chai.expect(scrabbleScore(' a')).to.equal(1);
-  });
-  it('returns 6 for street with whitespaces', function () {
-    chai.expect(scrabbleScore('st re et')).to.equal(6);
-  });
-  it('returns 22 for street with whitespaces', function () {
-    chai.expect(scrabbleScore('quirky')).to.equal(22);
+    chai.expect(sumPPG(iverson, jordan)).to.equal(31.4);
   });
 });
 

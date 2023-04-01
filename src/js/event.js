2c0b7d5d5665ae51dfc08ae61fd62229082e1e46
unknown <olennikovyv@mail.ru>
2023-04-01 19:55:59 +0500

    How many are smaller than me?

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,18 @@
-function NBAplayer(name, team, ppg) {
-  this.name = name;
-  this.team = team;
-  this.ppg = ppg;
+function smaller(nums) {
+  const smallerToRight = (arr, num) => arr.filter(digit => digit < num).length;
+
+  return nums.map((num, i) => smallerToRight(nums.slice(i + 1), num));
 }
 
-function sumPPG(playerOne, playerTwo) {
-  return playerOne.ppg + playerTwo.ppg;
-}
-
-const iverson = new NBAplayer('Iverson', '76ers', 11.2);
-const jordan = new NBAplayer('Jordan', 'bulls', 20.2);
-console.log(sumPPG(iverson, jordan)); // 31.4
+console.log(smaller([5, 4, 3, 2, 1]));
+// [4, 3, 2, 1, 0]
+console.log(smaller([1, 2, 3]));
+// [0, 0, 0]
+console.log(smaller([1, 2, 0]));
+// [1, 1, 0]
+console.log(smaller([1, 2, 1]));
+// [0, 1, 0]
+console.log(smaller([1, 1, -1, 0, 0]));
+// [3, 3, 0, 0, 0]
+console.log(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6]));
+// [4, 1, 5, 5, 0, 0, 0, 0, 0]
--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,15 @@
 mocha.setup('bdd');
 
-describe('All Star Code Challenge #1', function () {
-  it('test', () => {
-    const iverson = new NBAplayer('Iverson', '76ers', 11.2);
-    const jordan = new NBAplayer('Jordan', 'bulls', 20.2);
-
-    chai.expect(sumPPG(iverson, jordan)).to.equal(31.4);
+describe('How many are smaller than me?', function () {
+  it('Testing for fixed tests', () => {
+    chai.expect(smaller([5, 4, 3, 2, 1])).to.eql([4, 3, 2, 1, 0]);
+    chai.expect(smaller([1, 2, 3])).to.eql([0, 0, 0]);
+    chai.expect(smaller([1, 2, 0])).to.eql([1, 1, 0]);
+    chai.expect(smaller([1, 2, 1])).to.eql([0, 1, 0]);
+    chai.expect(smaller([1, 1, -1, 0, 0])).to.eql([3, 3, 0, 0, 0]);
+    chai
+      .expect(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6]))
+      .to.eql([4, 1, 5, 5, 0, 0, 0, 0, 0]);
   });
 });
 

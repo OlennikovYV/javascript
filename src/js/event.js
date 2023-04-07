afeab875dfeb9848d1af80b08204408535acb7b2
Yury Olennikov <OlennikovYV@mail.ru>
2023-04-07 08:51:42 +0500

    Grasshopper - Debug refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,12 @@
-function weatherInfo(temp) {
-  let c = convertToCelsius(temp);
+const weatherInfo = temperature => convertToCelsius(temperature);
 
-  if (c < 0) return c + ' is freezing temperature';
-  else return c + ' is above freezing temperature';
-}
+const temperatureMessage = temperature =>
+  temperature < 0
+    ? temperature + ' is freezing temperature'
+    : temperature + ' is above freezing temperature';
 
-function convertToCelsius(temperature) {
-  let celsius = ((temperature - 32) * 5) / 9;
-  return celsius;
-}
+const convertToCelsius = temperature =>
+  temperatureMessage(((temperature - 32) * 5) / 9);
 
 console.log(weatherInfo(50)); // '10 is above freezing temperature')
 console.log(weatherInfo(23)); //  '-5 is freezing temperature')

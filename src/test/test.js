72ffb1b60dcc7eee6eacb331d4f63e252dd4eb12
unknown <olennikovyv@mail.ru>
2023-04-08 21:00:09 +0500

    Be Concise I - The Ternary Operator

--- src/test/test.js
+++ src/test/test.js
@@ -1,10 +1,28 @@
 mocha.setup('bdd');
 
-describe('Grasshopper - Debug', function () {
-  it('should get correct answer', function () {
-    chai.expect(weatherInfo(50)).to.equal('10 is above freezing temperature');
-    chai.expect(weatherInfo(23)).to.equal('-5 is freezing temperature');
-  });
+describe('Be Concise I - The Ternary Operator', function () {
+  it(
+    'should still exhibit the exact same behaviour as the ' +
+      'function prior to refactoring',
+    () => {
+      chai.expect(describeAge(9)).to.equal("You're a(n) kid");
+      chai.expect(describeAge(10)).to.equal("You're a(n) kid");
+      chai.expect(describeAge(11)).to.equal("You're a(n) kid");
+      chai.expect(describeAge(12)).to.equal("You're a(n) kid");
+      chai.expect(describeAge(13)).to.equal("You're a(n) teenager");
+      chai.expect(describeAge(14)).to.equal("You're a(n) teenager");
+      chai.expect(describeAge(15)).to.equal("You're a(n) teenager");
+      chai.expect(describeAge(16)).to.equal("You're a(n) teenager");
+      chai.expect(describeAge(17)).to.equal("You're a(n) teenager");
+      chai.expect(describeAge(18)).to.equal("You're a(n) adult");
+      chai.expect(describeAge(19)).to.equal("You're a(n) adult");
+      chai.expect(describeAge(63)).to.equal("You're a(n) adult");
+      chai.expect(describeAge(64)).to.equal("You're a(n) adult");
+      chai.expect(describeAge(65)).to.equal("You're a(n) elderly");
+      chai.expect(describeAge(66)).to.equal("You're a(n) elderly");
+      chai.expect(describeAge(100)).to.equal("You're a(n) elderly");
+    }
+  );
 });
 
 mocha.run();

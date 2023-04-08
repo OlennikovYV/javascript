d5564e64f1fc74009b322ee4b9d57409c883eadb
unknown <olennikovyv@mail.ru>
2023-04-08 21:01:41 +0500

    Be Concise I - The Ternary Operator refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,6 @@
 function describeAge(age) {
   return `You're a(n) ${
-    age <= 12
-      ? 'kid'
-      : age >= 13 && age <= 17
-      ? 'teenager'
-      : age >= 18 && age <= 64
-      ? 'adult'
-      : 'elderly'
+    age < 13 ? 'kid' : age < 18 ? 'teenager' : age < 65 ? 'adult' : 'elderly'
   }`;
 }
 

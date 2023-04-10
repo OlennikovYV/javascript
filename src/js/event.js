9c25c3714bd5203c39b60933bd420eb76c30ac4d
Yury Olennikov <OlennikovYV@mail.ru>
2023-04-10 09:11:03 +0500

    Exclamation marks series #7: Remove words from the sentence if it contains one exclamation mark

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,23 @@
-function replaceAll(seq, find, replace) {
-  return typeof seq == 'string'
-    ? seq
-        .split('')
-        .map(el => (el == find ? replace : el))
-        .join('')
-    : seq.map(el => (el == find ? replace : el));
+function remove(string) {
+  const countExclamationMark = word => {
+    let count = (word.match(/\!/g) || []).length;
+    return count != 1;
+  };
+
+  return string
+    .split(' ')
+    .filter(word => countExclamationMark(word))
+    .join(' ');
 }
 
-console.log(replaceAll([], 1, 2)); // []
-console.log(replaceAll([1, 2, 2], 1, 2)); // [2, 2, 2]
-console.log(replaceAll('World', 'o', 0)); // 'W0rld'
+console.log(remove('Hi!')); // ''
+console.log(remove('Hi! Hi!')); // ''
+console.log(remove('Hi! Hi! Hi!')); // ''
+console.log(remove('Hi Hi! Hi!')); // 'Hi'
+console.log(remove('Hi! !Hi Hi!')); // ''
+console.log(remove('Hi! Hi!! Hi!')); // 'Hi!!'
+console.log(remove('Hi! !Hi! Hi!')); // '!Hi!'
+console.log(
+  remove('qcu! obqgs lsal iyhvh !zety! !xsklhd nhcpqjr fmnoio !rhnmde unv')
+);
+// 'obqgs lsal iyhvh !zety! nhcpqjr fmnoio unv'
--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,26 @@
 mocha.setup('bdd');
 
-describe('Replace all items', function () {
-  it('basic tests', function () {
-    chai.expect(replaceAll([], 1, 2)).to.eql([]);
-    chai.expect(replaceAll([1, 2, 2], 1, 2)).to.eql([2, 2, 2]);
-    chai.expect(replaceAll('World', 'o', 0)).to.equal('W0rld');
-  });
-});
+describe(
+  'Exclamation marks series #7: Remove words from the ' +
+    'sentence if it contains one exclamation mark',
+  function () {
+    it('It should works for basic tests', function () {
+      chai.expect(remove('Hi!')).to.equal('');
+      chai.expect(remove('Hi! Hi!')).to.equal('');
+      chai.expect(remove('Hi! Hi! Hi!')).to.equal('');
+      chai.expect(remove('Hi Hi! Hi!')).to.equal('Hi');
+      chai.expect(remove('Hi! !Hi Hi!')).to.equal('');
+      chai.expect(remove('Hi! Hi!! Hi!')).to.equal('Hi!!');
+      chai.expect(remove('Hi! !Hi! Hi!')).to.equal('!Hi!');
+      chai
+        .expect(
+          remove(
+            'qcu! obqgs lsal iyhvh !zety! !xsklhd nhcpqjr fmnoio !rhnmde unv'
+          )
+        )
+        .to.equal('obqgs lsal iyhvh !zety! nhcpqjr fmnoio unv');
+    });
+  }
+);
 
 mocha.run();

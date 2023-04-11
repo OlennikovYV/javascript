b382bebc3e9221e88467502be6e661ce1abae45a
Yury Olennikov <OlennikovYV@mail.ru>
2023-04-11 08:40:23 +0500

    Sum of integers in string

--- src/js/event.js
+++ src/js/event.js
@@ -1,18 +1,24 @@
-function remove(string) {
-  return string
-    .split(' ')
-    .filter(word => word.split('!').length != 2)
-    .join(' ');
+function sumOfIntegersInString(s) {
+  return s
+    .split(/[^\d]/g)
+    .filter(el => el !== '')
+    .reduce((sum, digit) => sum + Number(digit), 0);
 }
 
-console.log(remove('Hi!')); // ''
-console.log(remove('Hi! Hi!')); // ''
-console.log(remove('Hi! Hi! Hi!')); // ''
-console.log(remove('Hi Hi! Hi!')); // 'Hi'
-console.log(remove('Hi! !Hi Hi!')); // ''
-console.log(remove('Hi! Hi!! Hi!')); // 'Hi!!'
-console.log(remove('Hi! !Hi! Hi!')); // '!Hi!'
-console.log(
-  remove('qcu! obqgs lsal iyhvh !zety! !xsklhd nhcpqjr fmnoio !rhnmde unv')
+let = exampleTests = [
+  ['12.4', 16],
+  ['h3ll0w0rld', 3],
+  ['2 + 3 = ', 5],
+  [
+    'Our company made approximately 1 million in gross revenue last quarter.',
+    1,
+  ],
+  ['The Great Depression lasted from 1929 to 1939.', 3868],
+  ['Dogs are our best friends.', 0],
+  ['C4t5 are 4m4z1ng.', 18],
+  ['The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog', 3635],
+];
+
+exampleTests.forEach(([testString, correctAnswer]) =>
+  console.log(sumOfIntegersInString(testString), ' = ', correctAnswer)
 );
-// 'obqgs lsal iyhvh !zety! nhcpqjr fmnoio unv'
--- src/test/test.js
+++ src/test/test.js
@@ -1,26 +1,25 @@
 mocha.setup('bdd');
 
-describe(
-  'Exclamation marks series #7: Remove words from the ' +
-    'sentence if it contains one exclamation mark',
-  function () {
-    it('It should works for basic tests', function () {
-      chai.expect(remove('Hi!')).to.equal('');
-      chai.expect(remove('Hi! Hi!')).to.equal('');
-      chai.expect(remove('Hi! Hi! Hi!')).to.equal('');
-      chai.expect(remove('Hi Hi! Hi!')).to.equal('Hi');
-      chai.expect(remove('Hi! !Hi Hi!')).to.equal('');
-      chai.expect(remove('Hi! Hi!! Hi!')).to.equal('Hi!!');
-      chai.expect(remove('Hi! !Hi! Hi!')).to.equal('!Hi!');
-      chai
-        .expect(
-          remove(
-            'qcu! obqgs lsal iyhvh !zety! !xsklhd nhcpqjr fmnoio !rhnmde unv'
-          )
-        )
-        .to.equal('obqgs lsal iyhvh !zety! nhcpqjr fmnoio unv');
-    });
-  }
-);
+describe('Sum of integers in string', function () {
+  it('It should works for basic tests', function () {
+    let = exampleTests = [
+      ['12.4', 16],
+      ['h3ll0w0rld', 3],
+      ['2 + 3 = ', 5],
+      [
+        'Our company made approximately 1 million in gross revenue last quarter.',
+        1,
+      ],
+      ['The Great Depression lasted from 1929 to 1939.', 3868],
+      ['Dogs are our best friends.', 0],
+      ['C4t5 are 4m4z1ng.', 18],
+      ['The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog', 3635],
+    ];
+
+    exampleTests.forEach(([testString, correctAnswer]) =>
+      chai.expect(sumOfIntegersInString(testString)).to.equal(correctAnswer)
+    );
+  });
+});
 
 mocha.run();

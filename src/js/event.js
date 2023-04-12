72f06ebf88e9fce5aaa3ac05f5f291570f38ade0
Yury Olennikov <OlennikovYV@mail.ru>
2023-04-12 08:33:38 +0500

    Unexpected parsing

--- src/js/event.js
+++ src/js/event.js
@@ -1,21 +1,7 @@
-function sumOfIntegersInString(s) {
-  return (s.match(/\d+/g) || []).reduce((sum, digit) => sum + Number(digit), 0);
+function getStatus(isBusy) {
+  return {
+    status: isBusy ? 'busy' : 'available',
+  };
 }
 
-let = exampleTests = [
-  ['12.4', 16],
-  ['h3ll0w0rld', 3],
-  ['2 + 3 = ', 5],
-  [
-    'Our company made approximately 1 million in gross revenue last quarter.',
-    1,
-  ],
-  ['The Great Depression lasted from 1929 to 1939.', 3868],
-  ['Dogs are our best friends.', 0],
-  ['C4t5 are 4m4z1ng.', 18],
-  ['The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog', 3635],
-];
-
-exampleTests.forEach(([testString, correctAnswer]) =>
-  console.log(sumOfIntegersInString(testString), ' = ', correctAnswer)
-);
+console.log(getStatus(true).status); // 'busy'
--- src/test/test.js
+++ src/test/test.js
@@ -1,24 +1,8 @@
 mocha.setup('bdd');
 
-describe('Sum of integers in string', function () {
-  it('It should works for basic tests', function () {
-    let = exampleTests = [
-      ['12.4', 16],
-      ['h3ll0w0rld', 3],
-      ['2 + 3 = ', 5],
-      [
-        'Our company made approximately 1 million in gross revenue last quarter.',
-        1,
-      ],
-      ['The Great Depression lasted from 1929 to 1939.', 3868],
-      ['Dogs are our best friends.', 0],
-      ['C4t5 are 4m4z1ng.', 18],
-      ['The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog', 3635],
-    ];
-
-    exampleTests.forEach(([testString, correctAnswer]) =>
-      chai.expect(sumOfIntegersInString(testString)).to.equal(correctAnswer)
-    );
+describe('Unexpected parsing', function () {
+  it('test', () => {
+    chai.expect(getStatus(true).status).to.equal('busy');
   });
 });
 

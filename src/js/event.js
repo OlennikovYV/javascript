50a1984217a75cf2b18917b24835069d0fb46b69
Yury Olennikov <OlennikovYV@mail.ru>
2023-04-14 08:31:40 +0500

    Fruit string calculator

--- src/js/event.js
+++ src/js/event.js
@@ -1,34 +1,12 @@
-function changeMe(moneyIn) {
-  switch (moneyIn) {
-    case '£5':
-      return '20p '.repeat(25).trim();
-    case '£2':
-      return '20p '.repeat(10).trim();
-    case '£1':
-      return '20p '.repeat(5).trim();
-    case '50p':
-      return '20p 20p 10p';
-    case '20p':
-      return '10p 10p';
-    default:
-      return moneyIn;
-  }
+function calculate(string) {
+  const digits = string.match(/\d+/g).map(Number);
+
+  return digits.reduce(
+    (result, digit) =>
+      string.includes('loses') ? Math.abs(result - digit) : result + digit,
+    0
+  );
 }
 
-let actual = changeMe('£1');
-console.log(actual); // '£1'
-console.log(actual); //'20p 20p 20p 20p 20p'
-
-actual = changeMe('20p');
-console.log(actual); // '20p'
-console.log(actual); //'10p 10p'
-
-actual = changeMe('50p');
-console.log(actual); // '50p'
-console.log(actual); //'20p 20p 10p'
-
-actual = changeMe('£2');
-console.log(actual); // '£2'
-console.log(actual); //'20p 20p 20p 20p 20p 20p 20p 20p 20p 20p'
-
-console.log(changeMe('Money')); // 'Money'
+console.log(calculate('Panda has 48 apples and loses 4')); // 44
+console.log(calculate('Jerry has 34 apples and gains 6')); // 40
--- src/test/test.js
+++ src/test/test.js
@@ -1,37 +1,9 @@
 mocha.setup('bdd');
 
-describe('Simple Change Machine', function () {
-  it('Wrong money', () => {
-    let actual = changeMe('Money');
-    chai.assert.isDefined(
-      actual,
-      'Your function did not return a value. Did you log it to console instead?'
-    );
-    chai.assert.strictEqual(changeMe('Money'), 'Money');
-  });
-
-  it('Notes £5', () => {
-    actual = changeMe('£5');
-    chai.assert.strictEqual(
-      actual,
-      '20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p'
-    );
-  });
-  it('Notes £2', () => {
-    actual = changeMe('£2');
-    chai.assert.strictEqual(actual, '20p 20p 20p 20p 20p 20p 20p 20p 20p 20p');
-  });
-  it('Notes £1', () => {
-    actual = changeMe('£1');
-    chai.assert.strictEqual(actual, '20p 20p 20p 20p 20p');
-  });
-  it('Coins 50p', () => {
-    actual = changeMe('50p');
-    chai.assert.strictEqual(actual, '20p 20p 10p');
-  });
-  it('Coins 20p', () => {
-    actual = changeMe('20p');
-    chai.assert.strictEqual(actual, '10p 10p');
+describe('Fruit string calculator', function () {
+  it('test', () => {
+    chai.expect(calculate('Panda has 48 apples and loses 4')).to.equal(44);
+    chai.expect(calculate('Jerry has 34 apples and gains 6')).to.equal(40);
   });
 });
 

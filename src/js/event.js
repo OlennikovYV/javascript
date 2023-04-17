e90044c6aea057df6a0225748e70d3fca13a994c
Yury Olennikov <OlennikovYV@mail.ru>
2023-04-17 09:03:38 +0500

    Fun with lists: length

--- src/js/event.js
+++ src/js/event.js
@@ -1,13 +1,42 @@
-function initials(n) {
-  return n
-    .split(' ')
-    .map(
-      (el, i, arr) =>
-        el[0].toUpperCase() + (i === arr.length - 1 ? el.slice(1) : '')
-    )
-    .join('.');
+function Node(data, next = null) {
+  this.data = data;
+  this.next = next;
 }
 
-console.log(initials('code wars')); // 'C.Wars'
-console.log(initials('Barack hussain obama')); // 'B.H.Obama'
-console.log(initials('barack hussain Obama')); // 'B.H.Obama'
+function push(head, data) {
+  let firstNode;
+
+  firstNode = new Node(data);
+  firstNode.next = head;
+
+  return firstNode;
+}
+
+function listFromArray(arr) {
+  var chained = null;
+
+  if (!arr && Array.isArray(arr)) return null;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    chained = push(chained, arr[i]);
+  }
+
+  return chained;
+}
+
+function length(head) {
+  let lengthLink = 1;
+  let currentLink = head;
+
+  if (!currentLink && !currentLink?.hasOwnProperty(currentLink.data)) return 0;
+
+  while (currentLink.next) {
+    lengthLink += 1;
+    currentLink = currentLink.next;
+  }
+
+  return lengthLink;
+}
+
+console.log(length(null)); // 0
+console.log(length(listFromArray([1, 2, 3, 4]))); // 4
--- src/test/test.js
+++ src/test/test.js
@@ -1,10 +1,11 @@
 mocha.setup('bdd');
 
-describe('C.Wars', function () {
-  it('sample tests', () => {
-    chai.expect(initials('code wars')).to.equal('C.Wars');
-    chai.expect(initials('Barack hussain obama')).to.equal('B.H.Obama');
-    chai.expect(initials('barack hussain Obama')).to.equal('B.H.Obama');
+describe('Fun with lists: length', function () {
+  it('basic tests', function () {
+    const assert = chai.assert;
+
+    assert.equal(length(null), 0, 'Not eual');
+    assert.equal(length(listFromArray([1, 2, 3, 4])), 4, 'Not eual');
   });
 });
 

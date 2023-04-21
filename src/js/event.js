4f18c72b643933d78fbce6a502b98e405ee16e87
Yury Olennikov <OlennikovYV@mail.ru>
2023-04-21 08:58:43 +0500

    All Star Code Challenge #16

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,15 @@
-function completeSeries(arr) {
-  return new Set(arr).size != arr.length
-    ? [0]
-    : [...Array(Math.max(...arr) + 1).keys()];
+function noRepeat(str) {
+  let countAlpha = str.split('').reduce((count, alpha) => {
+    count[alpha] = (count[alpha] || 0) + 1;
+
+    return count;
+  }, {});
+
+  for (const alpha in countAlpha) {
+    if (countAlpha[alpha] == 1) return alpha;
+  }
 }
 
-console.log(completeSeries([0, 1])); // [0, 1]
-console.log(completeSeries([1, 4, 6])); // [0, 1, 2, 3, 4, 5, 6]
-console.log(completeSeries([3, 4, 5])); // [0, 1, 2, 3, 4, 5]
-console.log(completeSeries([2, 1])); // [0, 1, 2]
-console.log(completeSeries([1, 4, 4, 6])); // [0]
-console.log(completeSeries([7])); // [0, 1, 2, 3, 4, 5, 6, 7]
+console.log(noRepeat('aabbccdde')); // 'e'
+console.log(noRepeat('wxyz')); // 'w'
+console.log(noRepeat('testing')); // 'e'
--- src/test/test.js
+++ src/test/test.js
@@ -1,42 +1,12 @@
 mocha.setup('bdd');
 
-describe('Complete Series', function () {
+describe('All Star Code Challenge #16', function () {
   const assert = chai.assert;
-  let test1 = [0, 1],
-    test2 = [1, 4, 6],
-    test3 = [3, 4, 5],
-    test4 = [2, 1],
-    test5 = [1, 4, 4, 6],
-    test6 = [7],
-    sol1 = [0, 1],
-    sol2 = [0, 1, 2, 3, 4, 5, 6],
-    sol3 = [0, 1, 2, 3, 4, 5],
-    sol4 = [0, 1, 2],
-    sol5 = [0],
-    sol6 = [0, 1, 2, 3, 4, 5, 6, 7];
 
-  it('Test 1', function () {
-    assert.deepEqual(completeSeries(test1), sol1);
-  });
-
-  it('Test 2', function () {
-    assert.deepEqual(completeSeries(test2), sol2);
-  });
-
-  it('Test 3', function () {
-    assert.deepEqual(completeSeries(test3), sol3);
-  });
-
-  it('Test 4', function () {
-    assert.deepEqual(completeSeries(test4), sol4);
-  });
-
-  it('Test 5', function () {
-    assert.deepEqual(completeSeries(test5), sol5);
-  });
-
-  it('Test 6', function () {
-    assert.deepEqual(completeSeries(test6), sol6);
+  it('test', () => {
+    assert.equal(noRepeat('aabbccdde'), 'e');
+    assert.equal(noRepeat('wxyz'), 'w');
+    assert.equal(noRepeat('testing'), 'e');
   });
 });
 

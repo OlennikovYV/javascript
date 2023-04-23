4c053bb3feedb0578489b91362127c46fc57ac93
unknown <olennikovyv@mail.ru>
2023-04-23 21:06:36 +0500

    Training JS #31: methods of arrayObject---isArray() indexOf() and toString() refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,7 @@
 function blackAndWhite(arr) {
   if (!Array.isArray(arr)) return "It's a fake array";
 
-  if (arr.some(el => el == 5) && arr.some(el => el == 13))
-    return "It's a black array";
+  if (arr.includes(5) && arr.includes(13)) return "It's a black array";
 
   return "It's a white array";
 }

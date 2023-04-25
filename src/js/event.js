ace99635fdd5ce485dc25b6e1eb85db564d8edb3
Yury Olennikov <OlennikovYV@mail.ru>
2023-04-25 08:45:06 +0500

    Birthday I - Cake

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,18 @@
-function smallEnough(a, limit) {
-  return a.every(el => el <= limit);
+function cake(x, y) {
+  const weightCandles = str =>
+    [...str].reduce((sum, alpha, i) => {
+      if (i % 2 !== 0) {
+        sum += alpha.codePointAt(0) - 96;
+      } else {
+        sum += alpha.codePointAt(0);
+      }
+      return sum;
+    }, 0);
+
+  return weightCandles(y) / (x / 100) > 70 ? 'Fire!' : 'That was close!';
 }
 
-console.log(smallEnough([66, 101], 200)); // true
-console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100));
-// false
-console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); // true
-console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120));
-// true
+console.log(cake(900, 'abcdef')); // 'That was close!'
+console.log(cake(56, 'ifkhchlhfd')); // 'Fire!'
+console.log(cake(256, 'aaaaaddddr')); // 'Fire!'
+console.log(cake(591, 'mufb')); // 'That was close!'
--- src/test/test.js
+++ src/test/test.js
@@ -3,14 +3,11 @@ mocha.setup('bdd');
 describe(`No Loops 1 - Small enough?`, function () {
   const assert = chai.assert;
 
-  it('test', () => {
-    assert.equal(smallEnough([66, 101], 200), true);
-    assert.equal(
-      smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100),
-      false
-    );
-    assert.equal(smallEnough([101, 45, 75, 105, 99, 107], 107), true);
-    assert.equal(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120), true);
+  it('Testing for fixed tests', () => {
+    assert.equal(cake(900, 'abcdef'), 'That was close!');
+    assert.equal(cake(56, 'ifkhchlhfd'), 'Fire!');
+    assert.equal(cake(256, 'aaaaaddddr'), 'Fire!');
+    assert.equal(cake(591, 'mufb'), 'That was close!');
   });
 });
 

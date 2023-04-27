64ebcc470163ccf158073b057178dd7941ad7aba
Yury Olennikov <OlennikovYV@mail.ru>
2023-04-27 08:52:02 +0500

    Test's results

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,60 @@
-function isItANum(str) {
-  let phone = str.replace(/[^\d]/g, '');
+function testResult(array) {
+  const hash = {
+    h: 0,
+    a: 0,
+    l: 0,
+  };
+  let sumMarks = 0;
+  let averageMarks;
+  let resultList = [];
 
-  return /^0\d{10}$/.test(phone) ? phone : 'Not a phone number';
+  const isOnlyHighMarks = hash => {
+    return hash['a'] == 0 && hash['l'] == 0;
+  };
+
+  array.map(el => {
+    switch (true) {
+      case el < 7:
+        hash['l'] += 1;
+        break;
+      case el < 9:
+        hash['a'] += 1;
+        break;
+      default:
+        hash['h'] += 1;
+        break;
+    }
+
+    sumMarks += el;
+  });
+
+  averageMarks = Math.round((sumMarks / array.length) * 1000) / 1000;
+  resultList.push(averageMarks);
+
+  resultList.push(hash);
+
+  if (isOnlyHighMarks(hash)) resultList.push('They did well');
+
+  return resultList;
 }
 
-console.log(isItANum('S:)0207ERGQREG88349F82!efRF)'));
-// '02078834982'
-console.log(isItANum('sjfniebienvr12312312312ehfWh'));
-// 'Not a phone number'
-console.log(isItANum('0192387415456')); // 'Not a phone number'
-console.log(isItANum('v   uf  f 0tt2eg qe0b 8rtyq4eyq564()(((((165'));
-// '02084564165'
-console.log(isItANum('stop calling me no I have never been in an accident'));
-// 'Not a phone number'
+console.log(testResult([10, 9, 9, 10, 9, 10, 9]));
+// [ 9.429, { h: 7, a: 0, l: 0 }, 'They did well' ]
+console.log(testResult([5, 6, 4, 8, 9, 8, 9, 10, 10, 10]));
+// [ 7.9, { h: 5, a: 2, l: 3 } ]
+console.log(testResult([5, 6, 5, 7, 4, 5, 6, 6, 5]));
+// [ 5.444, { h: 0, a: 1, l: 8 } ]
+console.log(testResult([9, 8, 7, 6, 9, 8, 10, 7, 6]));
+// [ 7.778, { h: 3, a: 4, l: 2 } ]
+console.log(testResult([9, 10, 10, 10, 10, 10, 8, 9, 7, 8, 10]));
+// [ 9.182, { h: 8, a: 3, l: 0 } ]
+console.log(testResult([3, 5, 6, 10, 8, 4, 10, 9]));
+// [ 6.875, { h: 3, a: 1, l: 4 } ]
+console.log(testResult([10, 9, 9, 10, 9, 10]));
+// [ 9.5, { h: 6, a: 0, l: 0 }, 'They did well' ]
+console.log(testResult([7, 6, 8, 9, 6, 7, 5, 9]));
+// [ 7.125, { h: 2, a: 3, l: 3 } ]
+console.log(testResult([9, 9, 8, 9, 8, 9]));
+// [ 8.667, { h: 4, a: 2, l: 0 } ]
+console.log(testResult([10, 9, 6, 7, 10, 8, 9, 10]));
+// [ 8.625, { h: 5, a: 2, l: 1 } ]
--- src/test/test.js
+++ src/test/test.js
@@ -1,23 +1,51 @@
 mocha.setup('bdd');
 
-describe(`Fix My Phone Numbers!`, function () {
+describe(`Test's results`, function () {
   const assert = chai.assert;
 
   it('test', () => {
-    assert.equal(isItANum('S:)0207ERGQREG88349F82!efRF)'), '02078834982');
-    assert.equal(
-      isItANum('sjfniebienvr12312312312ehfWh'),
-      'Not a phone number'
-    );
-    assert.equal(isItANum('0192387415456'), 'Not a phone number');
-    assert.equal(
-      isItANum('v   uf  f 0tt2eg qe0b 8rtyq4eyq564()(((((165'),
-      '02084564165'
-    );
-    assert.equal(
-      isItANum('stop calling me no I have never been in an accident'),
-      'Not a phone number'
-    );
+    assert.deepEqual(testResult([10, 9, 9, 10, 9, 10, 9]), [
+      9.429,
+      { h: 7, a: 0, l: 0 },
+      'They did well',
+    ]);
+    assert.deepEqual(testResult([5, 6, 4, 8, 9, 8, 9, 10, 10, 10]), [
+      7.9,
+      { h: 5, a: 2, l: 3 },
+    ]);
+    assert.deepEqual(testResult([5, 6, 5, 7, 4, 5, 6, 6, 5]), [
+      5.444,
+      { h: 0, a: 1, l: 8 },
+    ]);
+    assert.deepEqual(testResult([9, 8, 7, 6, 9, 8, 10, 7, 6]), [
+      7.778,
+      { h: 3, a: 4, l: 2 },
+    ]);
+    assert.deepEqual(testResult([9, 10, 10, 10, 10, 10, 8, 9, 7, 8, 10]), [
+      9.182,
+      { h: 8, a: 3, l: 0 },
+    ]);
+    assert.deepEqual(testResult([3, 5, 6, 10, 8, 4, 10, 9]), [
+      6.875,
+      { h: 3, a: 1, l: 4 },
+    ]);
+    assert.deepEqual(testResult([10, 9, 9, 10, 9, 10]), [
+      9.5,
+      { h: 6, a: 0, l: 0 },
+      'They did well',
+    ]);
+    assert.deepEqual(testResult([7, 6, 8, 9, 6, 7, 5, 9]), [
+      7.125,
+      { h: 2, a: 3, l: 3 },
+    ]);
+    assert.deepEqual(testResult([9, 9, 8, 9, 8, 9]), [
+      8.667,
+      { h: 4, a: 2, l: 0 },
+    ]);
+    assert.deepEqual(testResult([10, 9, 6, 7, 10, 8, 9, 10]), [
+      8.625,
+      { h: 5, a: 2, l: 1 },
+    ]);
   });
 });
 

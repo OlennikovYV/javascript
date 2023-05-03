165346fac1ddd98505ac9c92f547aefdf2f696c1
Yury Olennikov <OlennikovYV@mail.ru>
2023-05-03 08:37:28 +0500

    Geometric Progression Sequence

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,10 @@
-function hasUniqueChars(str) {
-  return new Set(str).size === str.length;
+function geometricSequenceElements(a, r, n) {
+  return Array.from({ length: n }, (_, i) => a * r ** i).join(', ');
 }
 
-console.log(hasUniqueChars('  nAa')); // false
-console.log(hasUniqueChars('abcdef')); // true
-console.log(hasUniqueChars('aA')); // true
-console.log(hasUniqueChars('++-')); // false
+console.log(geometricSequenceElements(2, 3, 5));
+// '2, 6, 18, 54, 162');
+console.log(geometricSequenceElements(2, 2, 10));
+// '2, 4, 8, 16, 32, 64, 128, 256, 512, 1024'
+console.log(geometricSequenceElements(1, -2, 10));
+// '1, -2, 4, -8, 16, -32, 64, -128, 256, -512'
--- src/test/test.js
+++ src/test/test.js
@@ -4,10 +4,15 @@ describe(`All unique`, function () {
   const assert = chai.assert.equal;
 
   it('test', () => {
-    assert(hasUniqueChars('  nAa'), false); // because there are two spaces ' '
-    assert(hasUniqueChars('abcdef'), true);
-    assert(hasUniqueChars('aA'), true); // case - sensitivity
-    assert(hasUniqueChars('++-'), false); // because there are two '+'
+    assert(geometricSequenceElements(2, 3, 5), '2, 6, 18, 54, 162');
+    assert(
+      geometricSequenceElements(2, 2, 10),
+      '2, 4, 8, 16, 32, 64, 128, 256, 512, 1024'
+    );
+    assert(
+      geometricSequenceElements(1, -2, 10),
+      '1, -2, 4, -8, 16, -32, 64, -128, 256, -512'
+    );
   });
 });
 

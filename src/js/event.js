ebcbedd372503a859c79809356545303baf61f60
Yury Olennikov <OlennikovYV@mail.ru>
2023-05-05 08:32:43 +0500

    Every archer has its arrows

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,8 @@
-function wordsToSentence(words) {
-  return words.join` `;
+function archersReady(archers) {
+  return archers.every(archer => archer >= 5) && archers.length !== 0;
 }
 
-console.log(wordsToSentence(['hello', 'world'])); // 'hello world'
+console.log(archersReady([])); // false
+console.log(archersReady([1, 2, 3, 4])); // false
+console.log(archersReady([5, 6, 7, 8])); // true
+console.log(archersReady([1, 2, 3, 4, 5, 6, 7, 8])); // false
--- src/test/test.js
+++ src/test/test.js
@@ -1,10 +1,21 @@
 mocha.setup('bdd');
 
-describe(`Words to sentence`, function () {
+describe(`Every archer has its arrows`, function () {
   const assert = chai.assert.equal;
 
-  it('Basic tests', function () {
-    assert(wordsToSentence(['hello', 'world']), 'hello world');
+  it('test', () => {
+    assert(archersReady([]), false, 'Should handle no archers');
+    assert(
+      archersReady([1, 2, 3, 4]),
+      false,
+      'Should handle unprepared archers'
+    );
+    assert(archersReady([5, 6, 7, 8]), true, 'Should handle prepared archers');
+    assert(
+      archersReady([1, 2, 3, 4, 5, 6, 7, 8]),
+      false,
+      'Should handle mixed archers'
+    );
   });
 });
 

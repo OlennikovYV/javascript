9916392a2ac08bb94f2fac0c5f807c3ae7e6566c
Yury Olennikov <OlennikovYV@mail.ru>
2023-05-15 08:46:31 +0500

    Sum a list but ignore any duplicates

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,43 @@
-function spongeMeme(sentence) {
-  return sentence.replace(/./g, (alpha, i) =>
-    i % 2 ? alpha.toLowerCase() : alpha.toUpperCase()
+function sumNoDuplicates(numList) {
+  const listCounts = numList.reduce((list, el) => {
+    list[el] = (list[el] || 0) + 1;
+    return list;
+  }, {});
+
+  return Object.keys(listCounts).reduce(
+    (sum, num) => (sum += listCounts[num] > 1 ? 0 : Number(num)),
+    0
   );
 }
 
-console.log(spongeMeme('stop Making spongebob Memes!'));
-// 'StOp mAkInG SpOnGeBoB MeMeS!'
+const fixedCases = [
+  [[1, 1, 2, 3], 5],
+  [[], 0],
+  [[1, 1, 2, 2, 3], 3],
+  [[5, 6, 10, 3, 10, 10, 6, 7, 0, 9, 1, 1, 6, 3, 1], 21],
+  [[0, 10, 8, 9, 7, 3, 3, 9, 3, 6, 0], 31],
+  [[0, 1, 4, 4, 0, 4, 2, 5, 9, 0, 10, 9, 0, 1, 2], 15],
+  [[7, 2, 10, 9, 10, 2, 7, 3, 10, 8, 2, 5], 25],
+  [[7, 2, 0, 3, 5, 7, 8, 3, 2, 10, 9, 5], 27],
+  [[1, 9, 9, 5, 7, 7, 6, 1, 5, 6], 0],
+  [[10, 10, 1, 4, 10, 3, 9, 6, 10, 10, 9, 9, 10, 5, 6, 10, 4, 1], 8],
+  [[7, 10, 10, 9, 0, 2, 5, 10, 3, 8, 1, 4, 9, 9, 5, 8, 8, 8, 5, 3], 14],
+  [[1, 9, 2, 1, 5, 5, 1, 1, 5, 10, 5, 9, 5, 2, 1], 10],
+  [[8, 6, 3, 6, 7, 3, 4, 4, 2, 1, 4, 5, 8, 6], 15],
+  [[10, 9, 2, 8, 9, 10, 4, 6, 9, 5, 1, 1, 4, 3, 9, 7, 8, 5, 5], 18],
+  [[5, 5, 0, 0, 6, 3, 7, 2, 4, 2], 20],
+  [[0, 8, 5, 10, 1, 1, 6, 8], 21],
+  [[4, 2, 10, 9, 10, 5, 1, 7, 1, 9, 8, 7, 4, 2, 5, 8, 3, 10, 8], 3],
+  [[9, 8, 3, 1, 8, 1, 7], 19],
+  [[1, 4, 3, 8, 9, 4, 7, 5, 10, 10, 7, 6, 9, 3], 20],
+  [[2, 0, 4, 2, 2, 3, 6, 7, 3, 8, 10, 6, 8], 21],
+  [[7, 9, 5, 6, 1, 0, 5, 0, 4, 7, 1, 2, 8, 9, 6, 1], 14],
+  [[7, 1, 8, 8, 5, 5, 1, 4, 0, 1, 10, 1], 21],
+];
+
+for (let [input, expected] of fixedCases)
+  console.log(
+    `Testing for [${input.join(', ')}] = `,
+    sumNoDuplicates(input),
+    ` must equal ${expected}`
+  );

2c86e3373da07ccc84ce4f985e7e5512874ecd54
Yury Olennikov <OlennikovYV@mail.ru>
2023-05-17 09:48:33 +0500

    Easy Time Convert

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,21 @@
-function pattern(n) {
-  return Array.from({ length: n }, (_, i) => i + 1).reduce(
-    (sum, _, i, arr) => sum + (i ? '\n' : '') + arr.slice(i).join(''),
-    ''
-  );
+function timeConvert(num) {
+  let hours, min;
+
+  if (num <= 0) return `00:00`;
+
+  hours = String(Math.trunc(num / 60));
+  min = String(num - hours * 60);
+
+  return `${hours.padStart(2, '0')}:${min.padStart(2, '0')}`;
 }
 
-console.log(pattern(0)); // ''
-console.log(pattern(1)); // '1'
-console.log(pattern(2)); // '12\n2'
-console.log(pattern(5)); // '12345\n2345\n345\n45\n5'
+console.log(timeConvert(0)); // '00:00'
+console.log(timeConvert(-6)); // '00:00'
+console.log(timeConvert(8)); // '0' + 0 + ':' + '0' + 8
+console.log(timeConvert(32)); // '0' + 0 + ':' + 32
+console.log(timeConvert(75)); // '0' + 1 + ':' + 15
+console.log(timeConvert(63)); // '0' + 1 + ':' + '0' + 3
+console.log(timeConvert(134)); // '0' + 2 + ':' + 14
+console.log(timeConvert(180)); // '0' + 3 + ':' + '0' + 0
+console.log(timeConvert(970)); // 16 + ':' + 10
+console.log(timeConvert(565757)); // 9429 + ':' + 17
--- src/test/test.js
+++ src/test/test.js
@@ -1,13 +1,20 @@
 mocha.setup('bdd');
 
-describe(`Sum a list but ignore any duplicates`, function () {
+describe(`Easy Time Convert`, function () {
   const equal = chai.assert.equal;
   const isDefined = chai.assert.isDefined;
 
   it('test', () => {
-    equal(pattern(1), '1');
-    equal(pattern(2), '12\n2');
-    equal(pattern(5), '12345\n2345\n345\n45\n5');
+    equal(timeConvert(0), '00:00');
+    equal(timeConvert(-6), '00:00');
+    equal(timeConvert(8), '0' + 0 + ':' + '0' + 8);
+    equal(timeConvert(32), '0' + 0 + ':' + 32);
+    equal(timeConvert(75), '0' + 1 + ':' + 15);
+    equal(timeConvert(63), '0' + 1 + ':' + '0' + 3);
+    equal(timeConvert(134), '0' + 2 + ':' + 14);
+    equal(timeConvert(180), '0' + 3 + ':' + '0' + 0);
+    equal(timeConvert(970), 16 + ':' + 10);
+    equal(timeConvert(565757), 9429 + ':' + 17);
   });
 });
 

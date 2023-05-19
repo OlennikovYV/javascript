f23784606482caece0fff2792f693fe7265ae3a8
Yury Olennikov <OlennikovYV@mail.ru>
2023-05-19 07:26:58 +0500

    Sorting Dictionaries

--- src/js/event.js
+++ src/js/event.js
@@ -1,11 +1,21 @@
-function golfScoreCalculator(parList, scoreList) {
-  return [...scoreList].reduce(
-    (result, score, index) => (result += score - parList[index]),
-    0
-  );
+function sortDict(dict) {
+  return Array.from(Object.entries(dict))
+    .map(el => {
+      el[0] = isNaN(el[0]) ? el[0] : Number(el[0]);
+      return el;
+    })
+    .sort((a, b) => b[1] - a[1]);
 }
 
-console.log(golfScoreCalculator('443454444344544443', '353445334534445344'));
-// -1
-console.log(golfScoreCalculator('123456123456123456', '123456123456123456'));
-// 0
+console.log(sortDict({ 1: 3, 2: 2, 3: 1 }));
+// [ [1, 3], [2, 2], [3, 1] ]
+console.log(sortDict({ 3: 1, 2: 2, 1: 3 }));
+// [ [1, 3], [2, 2], [3, 1] ]
+console.log(sortDict({ 1: 2, 2: 4, 3: 6 }));
+// [ [3, 6], [2, 4], [1, 2] ]
+console.log(sortDict({ 1: 5, 3: 10, 2: 2, 6: 3, 8: 8 }));
+// [ [3, 10], [8, 8], [1, 5], [6, 3], [2, 2] ]
+console.log(sortDict({ a: 6, b: 2, c: 4 }));
+// [ ['a', 6], ['c', 4], ['b', 2] ]
+console.log(sortDict({ aldo: 6, boris: 2, igor: 6 }));
+// [ ['aldo', 6], ['igor', 6], ['boris', 2] ]
--- src/test/test.js
+++ src/test/test.js
@@ -1,12 +1,46 @@
 mocha.setup('bdd');
 
-describe(`What's my golf score?`, function () {
+describe(`Sorting Dictionaries`, function () {
   const equal = chai.assert.equal;
+  const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
 
-  it('Testing for fixed tests', () => {
-    equal(golfScoreCalculator('443454444344544443', '353445334534445344'), -1);
-    equal(golfScoreCalculator('123456123456123456', '123456123456123456'), 0);
+  it('should handle dictionaries with numbers as keys', function () {
+    deepEqual(sortDict({ 1: 3, 2: 2, 3: 1 }), [
+      [1, 3],
+      [2, 2],
+      [3, 1],
+    ]);
+    deepEqual(sortDict({ 3: 1, 2: 2, 1: 3 }), [
+      [1, 3],
+      [2, 2],
+      [3, 1],
+    ]);
+    deepEqual(sortDict({ 1: 2, 2: 4, 3: 6 }), [
+      [3, 6],
+      [2, 4],
+      [1, 2],
+    ]);
+    deepEqual(sortDict({ 1: 5, 3: 10, 2: 2, 6: 3, 8: 8 }), [
+      [3, 10],
+      [8, 8],
+      [1, 5],
+      [6, 3],
+      [2, 2],
+    ]);
+  });
+
+  it('should handle dictionaries with strings as keys', function () {
+    deepEqual(sortDict({ a: 6, b: 2, c: 4 }), [
+      ['a', 6],
+      ['c', 4],
+      ['b', 2],
+    ]);
+    deepEqual(sortDict({ aldo: 6, boris: 2, igor: 6 }), [
+      ['aldo', 6],
+      ['igor', 6],
+      ['boris', 2],
+    ]);
   });
 });
 

097f7d68e08cc19bbdd4e0b4fde8e865e325460d
unknown <olennikovyv@mail.ru>
2023-05-20 18:03:04 +0500

    Find the calculation type

--- src/js/event.js
+++ src/js/event.js
@@ -1,18 +1,11 @@
-function sortDict(dict) {
-  return Object.keys(dict)
-    .map(el => [+el || el, dict[el]])
-    .sort((a, b) => b[1] - a[1]);
+function calcType(a, b, res) {
+  if (a + b == res) return 'addition';
+  if (a * b == res) return 'multiplication';
+  if (a - b == res) return 'subtraction';
+  if (a / b == res) return 'division';
 }
 
-console.log(sortDict({ 1: 3, 2: 2, 3: 1 }));
-// [ [1, 3], [2, 2], [3, 1] ]
-console.log(sortDict({ 3: 1, 2: 2, 1: 3 }));
-// [ [1, 3], [2, 2], [3, 1] ]
-console.log(sortDict({ 1: 2, 2: 4, 3: 6 }));
-// [ [3, 6], [2, 4], [1, 2] ]
-console.log(sortDict({ 1: 5, 3: 10, 2: 2, 6: 3, 8: 8 }));
-// [ [3, 10], [8, 8], [1, 5], [6, 3], [2, 2] ]
-console.log(sortDict({ a: 6, b: 2, c: 4 }));
-// [ ['a', 6], ['c', 4], ['b', 2] ]
-console.log(sortDict({ aldo: 6, boris: 2, igor: 6 }));
-// [ ['aldo', 6], ['igor', 6], ['boris', 2] ]
+console.log(calcType(1, 2, 3)); // 'addition'
+console.log(calcType(10, 4, 40)); // 'multiplication'
+console.log(calcType(10, 5, 5)); // 'subtraction'
+console.log(calcType(9, 5, 1.8)); // 'division'
--- src/test/test.js
+++ src/test/test.js
@@ -1,46 +1,21 @@
 mocha.setup('bdd');
 
-describe(`Sorting Dictionaries`, function () {
+describe(`Find the calculation type`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
 
-  it('should handle dictionaries with numbers as keys', function () {
-    deepEqual(sortDict({ 1: 3, 2: 2, 3: 1 }), [
-      [1, 3],
-      [2, 2],
-      [3, 1],
-    ]);
-    deepEqual(sortDict({ 3: 1, 2: 2, 1: 3 }), [
-      [1, 3],
-      [2, 2],
-      [3, 1],
-    ]);
-    deepEqual(sortDict({ 1: 2, 2: 4, 3: 6 }), [
-      [3, 6],
-      [2, 4],
-      [1, 2],
-    ]);
-    deepEqual(sortDict({ 1: 5, 3: 10, 2: 2, 6: 3, 8: 8 }), [
-      [3, 10],
-      [8, 8],
-      [1, 5],
-      [6, 3],
-      [2, 2],
-    ]);
+  it('addition', function () {
+    equal(calcType(1, 2, 3), 'addition');
   });
-
-  it('should handle dictionaries with strings as keys', function () {
-    deepEqual(sortDict({ a: 6, b: 2, c: 4 }), [
-      ['a', 6],
-      ['c', 4],
-      ['b', 2],
-    ]);
-    deepEqual(sortDict({ aldo: 6, boris: 2, igor: 6 }), [
-      ['aldo', 6],
-      ['igor', 6],
-      ['boris', 2],
-    ]);
+  it('multiplication', function () {
+    equal(calcType(10, 4, 40), 'multiplication');
+  });
+  it('subtraction', function () {
+    equal(calcType(10, 5, 5), 'subtraction');
+  });
+  it('division', function () {
+    equal(calcType(9, 5, 1.8), 'division');
   });
 });
 

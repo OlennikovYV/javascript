d8a29b3be3293564e8ca7773775dcebaf2c7e04a
Yury Olennikov <OlennikovYV@mail.ru>
2023-05-23 08:41:58 +0500

    Odder Than the Rest refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,5 @@
 function oddOne(arr) {
-  for (let i = 0; i < arr.length; i += 1) if (arr[i] % 2 !== 0) return i;
-
-  return -1;
+  return arr.findIndex(num => num % 2 !== 0);
 }
 
 console.log(oddOne([2, 4, 6, 7, 10])); // 3

b0649f40aa8e9ea325520ac0064cc4d1e17264a3
unknown <olennikovyv@mail.ru>
2023-05-27 18:32:49 +0500

    Aspect Ratio Cropping - Part 1

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,8 @@
-function allNonConsecutive(arr) {
-  let listConsecutive = [];
-
-  for (let i = 1; i < arr.length; i += 1) {
-    if (arr[i] - arr[i - 1] !== 1) listConsecutive.push({ i: i, n: arr[i] });
-  }
-
-  return listConsecutive;
+function aspectRatio(x, y) {
+  return [Math.ceil((y * 16) / 9), y];
 }
 
-const results = allNonConsecutive([1, 2, 3, 4, 6, 7, 8, 10]);
-
-console.log(allNonConsecutive(results));
-// [ { i: 4, n: 6 }, { i: 7, n: 10 } ]
+console.log(aspectRatio(640, 480)); // [854, 480]
+console.log(aspectRatio(960, 720)); // [1280, 720]
+console.log(aspectRatio(1440, 1080)); // [1920, 1080]
+console.log(aspectRatio(1920, 1440)); // [2560, 1440]
--- src/test/test.js
+++ src/test/test.js
@@ -1,18 +1,16 @@
 mocha.setup('bdd');
 
-describe(`Find all non-consecutive numbers`, function () {
+describe(`Aspect Ratio Cropping - Part 1`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
   const isNull = chai.assert.isNull;
 
-  it('test', () => {
-    const results = allNonConsecutive([1, 2, 3, 4, 6, 7, 8, 10]);
-
-    deepEqual(results, [
-      { i: 4, n: 6 },
-      { i: 7, n: 10 },
-    ]);
+  it('should work for known resolutions', () => {
+    deepEqual(aspectRatio(640, 480), [854, 480], 'x = 640, y = 480');
+    deepEqual(aspectRatio(960, 720), [1280, 720], 'x = 960, y = 720');
+    deepEqual(aspectRatio(1440, 1080), [1920, 1080], 'x = 1440, y = 1080');
+    deepEqual(aspectRatio(1920, 1440), [2560, 1440], 'x = 1920, y = 1440');
   });
 });
 

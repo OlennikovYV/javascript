60c0c21593c860cbc8e1c300e8c07a1abcea1845
Yury Olennikov <OlennikovYV@mail.ru>
2023-05-30 08:44:25 +0500

    Beginner Series #5 Triangular Numbers

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,22 @@
-function reverseByCenter(s) {
-  const length = s.length;
-  const oddLength = length % 2;
-  const lengthPart = oddLength ? Math.floor(length / 2) : length / 2;
-  let leftPart = s.slice(0, lengthPart);
-  let rigthPart = s.slice(-lengthPart);
+function isTriangular(t) {
+  for (let i = 0; i <= 65536; i += 1) {
+    if (t == (i * (i + 1)) / 2) return true;
+  }
 
-  [leftPart, rigthPart] = [rigthPart, leftPart];
-
-  return leftPart + (oddLength ? s[lengthPart] : '') + rigthPart;
+  return false;
 }
 
-console.log(reverseByCenter('secret')); // 'retsec'
-console.log(reverseByCenter('agent')); // 'nteag'
-console.log(reverseByCenter('raw')); // 'war'
-console.log(reverseByCenter('onion')); // 'onion'
+// returns true when t is a triangular number
+console.log(isTriangular(1)); // true
+console.log(isTriangular(3)); // true
+console.log(isTriangular(6)); // true
+console.log(isTriangular(10)); // true
+console.log(isTriangular(15)); // true
+console.log(isTriangular(21)); // true
+console.log(isTriangular(28)); // true
+
+// returns false when t is not a triangular number
+console.log(isTriangular(2)); // false
+console.log(isTriangular(7)); // false
+console.log(isTriangular(14)); // false
+console.log(isTriangular(27)); // false
--- src/test/test.js
+++ src/test/test.js
@@ -6,11 +6,21 @@ describe(`Do you speak retsec?`, function () {
   const isDefined = chai.assert.isDefined;
   const isNull = chai.assert.isNull;
 
-  it('test', () => {
-    equal(reverseByCenter('secret'), 'retsec');
-    equal(reverseByCenter('agent'), 'nteag');
-    equal(reverseByCenter('raw'), 'war');
-    equal(reverseByCenter('onion'), 'onion');
+  it('returns true when t is a triangular number', function () {
+    equal(isTriangular(1), true, 'Failed when t = 1');
+    equal(isTriangular(3), true, 'Failed when t = 3');
+    equal(isTriangular(6), true, 'Failed when t = 6');
+    equal(isTriangular(10), true, 'Failed when t = 10');
+    equal(isTriangular(15), true, 'Failed when t = 15');
+    equal(isTriangular(21), true, 'Failed when t = 21');
+    equal(isTriangular(28), true, 'Failed when t = 28');
+  });
+
+  it('returns false when t is not a triangular number', function () {
+    equal(isTriangular(2), false, 'Failed when t = 2');
+    equal(isTriangular(7), false, 'Failed when t = 7');
+    equal(isTriangular(14), false, 'Failed when t = 14');
+    equal(isTriangular(27), false, 'Failed when t = 27');
   });
 });
 

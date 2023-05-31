40c0b29cc0270461a6132ea5a890aa85fd20719e
Yury Olennikov <OlennikovYV@mail.ru>
2023-05-31 08:35:22 +0500

    max diff - easy

--- src/js/event.js
+++ src/js/event.js
@@ -1,18 +1,9 @@
-function isTriangular(t) {
-  return Math.sqrt(8 * t + 1) % 1 == 0;
+function maxDiff(list) {
+  return list.length == 0 ? 0 : Math.max(...list) - Math.min(...list);
 }
 
-// returns true when t is a triangular number
-console.log(isTriangular(1)); // true
-console.log(isTriangular(3)); // true
-console.log(isTriangular(6)); // true
-console.log(isTriangular(10)); // true
-console.log(isTriangular(15)); // true
-console.log(isTriangular(21)); // true
-console.log(isTriangular(28)); // true
-
-// returns false when t is not a triangular number
-console.log(isTriangular(2)); // false
-console.log(isTriangular(7)); // false
-console.log(isTriangular(14)); // false
-console.log(isTriangular(27)); // false
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 6])); // 6
+console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6])); // 11
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 16])); // 16
+console.log(maxDiff([16])); // 0
+console.log(maxDiff([])); // 0
--- src/test/test.js
+++ src/test/test.js
@@ -1,26 +1,17 @@
 mocha.setup('bdd');
 
-describe(`Do you speak retsec?`, function () {
+describe(`max diff - easy`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
   const isNull = chai.assert.isNull;
 
-  it('returns true when t is a triangular number', function () {
-    equal(isTriangular(1), true, 'Failed when t = 1');
-    equal(isTriangular(3), true, 'Failed when t = 3');
-    equal(isTriangular(6), true, 'Failed when t = 6');
-    equal(isTriangular(10), true, 'Failed when t = 10');
-    equal(isTriangular(15), true, 'Failed when t = 15');
-    equal(isTriangular(21), true, 'Failed when t = 21');
-    equal(isTriangular(28), true, 'Failed when t = 28');
-  });
-
-  it('returns false when t is not a triangular number', function () {
-    equal(isTriangular(2), false, 'Failed when t = 2');
-    equal(isTriangular(7), false, 'Failed when t = 7');
-    equal(isTriangular(14), false, 'Failed when t = 14');
-    equal(isTriangular(27), false, 'Failed when t = 27');
+  it('Test', () => {
+    equal(maxDiff([0, 1, 2, 3, 4, 5, 6]), 6);
+    equal(maxDiff([-0, 1, 2, -3, 4, 5, -6]), 11);
+    equal(maxDiff([0, 1, 2, 3, 4, 5, 16]), 16);
+    equal(maxDiff([16]), 0);
+    equal(maxDiff([]), 0);
   });
 });
 

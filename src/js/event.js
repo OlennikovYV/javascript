465629e828e0da8bc5e0aaea9da71fe7c8bd1267
OlennikovYV <olennikovyv@mail.ru>
2023-06-11 19:49:00 +0500

    String to integer conversion

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,9 @@
-var howManyYears = function (date1, date2) {
-  yearDate1 = Number(date1.slice(0, 4));
-  yearDate2 = Number(date2.slice(0, 4));
+function myParseInt(str) {
+  return /^\d+$/.test(str.trim()) ? parseInt(str) : 'NaN';
+}
 
-  return Math.abs(yearDate2 - yearDate1);
-};
-
-console.log(howManyYears('1997/10/10', '2015/10/10')); // 18
-console.log(howManyYears('1990/10/10', '2015/10/10')); // 25
-console.log(howManyYears('2015/10/10', '1990/10/10')); // 25
-console.log(howManyYears('1992/10/24', '2015/10/24')); // 23
-console.log(howManyYears('2018/10/10', '2000/10/10')); // 18
+console.log(myParseInt('1')); // 1
+console.log(myParseInt('  1 ')); // 1
+console.log(myParseInt('08')); // 8
+console.log(isNaN(myParseInt('5 friends'))); // true
+console.log(isNaN(myParseInt('16.5'))); // true
--- src/test/test.js
+++ src/test/test.js
@@ -1,17 +1,17 @@
 mocha.setup('bdd');
 
-describe(`Difference between years. (Level 1)`, function () {
+describe(`String to integer conversion`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
   const isNull = chai.assert.isNull;
 
-  it('Tests', () => {
-    equal(howManyYears('1997/10/10', '2015/10/10'), 18);
-    equal(howManyYears('1990/10/10', '2015/10/10'), 25);
-    equal(howManyYears('2015/10/10', '1990/10/10'), 25);
-    equal(howManyYears('1992/10/24', '2015/10/24'), 23);
-    equal(howManyYears('2018/10/10', '2000/10/10'), 18);
+  it('test', () => {
+    equal(myParseInt('1'), 1);
+    equal(myParseInt('  1 '), 1);
+    equal(myParseInt('08'), 8);
+    equal(isNaN(myParseInt('5 friends')), true);
+    equal(isNaN(myParseInt('16.5')), true);
   });
 });
 

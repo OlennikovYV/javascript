d32fd2354a174d3875e2af48e8aff204bca1d4b1
OlennikovYV <olennikovyv@mail.ru>
2023-06-12 19:47:40 +0500

    Paul's Misery refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,24 +1,23 @@
 function paul(x) {
-  let miseryPoints = 0;
+  const tablePoints = {
+    kata: 5,
+    'Petes kata': 10,
+    life: 0,
+    eating: 1,
+  };
+  const miseryPoints = x.reduce((sum, point) => sum + tablePoints[point], 0);
 
-  function calcMiseryPoint(arr) {
-    const tablePoints = {
-      kata: 5,
-      'Petes kata': 10,
-      life: 0,
-      eating: 1,
-    };
+  switch (true) {
+    case miseryPoints < 40:
+      return 'Super happy!';
+    case miseryPoints < 70:
+      return 'Happy!';
+    case miseryPoints < 100:
+      return 'Sad!';
 
-    return arr.reduce((sum, point) => sum + tablePoints[point], 0);
+    default:
+      return 'Miserable!';
   }
-
-  miseryPoints = calcMiseryPoint(x);
-
-  if (miseryPoints < 40) return 'Super happy!';
-  if (miseryPoints < 70) return 'Happy!';
-  if (miseryPoints < 100) return 'Sad!';
-
-  return 'Miserable!';
 }
 
 console.log(paul(['life', 'eating', 'life']));

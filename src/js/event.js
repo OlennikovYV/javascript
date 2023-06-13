214afbc0faf17a2e4719ac1fa09e6c787f8460b6
Yury Olennikov <OlennikovYV@mail.ru>
2023-06-13 08:48:27 +0500

    !a == a ?!

--- src/js/event.js
+++ src/js/event.js
@@ -1,37 +1,5 @@
-function paul(x) {
-  const tablePoints = {
-    kata: 5,
-    'Petes kata': 10,
-    life: 0,
-    eating: 1,
-  };
-  const miseryPoints = x.reduce((sum, point) => sum + tablePoints[point], 0);
+const a = [];
 
-  switch (true) {
-    case miseryPoints < 40:
-      return 'Super happy!';
-    case miseryPoints < 70:
-      return 'Happy!';
-    case miseryPoints < 100:
-      return 'Sad!';
-
-    default:
-      return 'Miserable!';
-  }
-}
-
-console.log(paul(['life', 'eating', 'life']));
-// 'Super happy!'
-console.log(paul(['life', 'Petes kata', 'Petes kata', 'Petes kata', 'eating']));
-// 'Super happy!'
-console.log(
-  paul([
-    'Petes kata',
-    'Petes kata',
-    'eating',
-    'Petes kata',
-    'Petes kata',
-    'eating',
-  ])
-);
-// 'Happy!'
+console.log(a == false); // true
+console.log(!a == false); // true
+console.log(a == !a); // true
--- src/test/test.js
+++ src/test/test.js
@@ -1,28 +1,15 @@
 mocha.setup('bdd');
 
-describe(`Paul's Misery`, function () {
+describe(`!a == a ?!`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
   const isNull = chai.assert.isNull;
 
-  it('Testing for fixed tests', () => {
-    equal(paul(['life', 'eating', 'life']), 'Super happy!');
-    equal(
-      paul(['life', 'Petes kata', 'Petes kata', 'Petes kata', 'eating']),
-      'Super happy!'
-    );
-    equal(
-      paul([
-        'Petes kata',
-        'Petes kata',
-        'eating',
-        'Petes kata',
-        'Petes kata',
-        'eating',
-      ]),
-      'Happy!'
-    );
+  it('test', () => {
+    equal(a == false, true, `a != false`);
+    equal(!a == false, true, `!a != false`);
+    equal(a == !a, true, `a != !a`);
   });
 });
 

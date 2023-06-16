baab370858eefc1ead060aae1ee6e45e0f0f6a46
Yury Olennikov <OlennikovYV@mail.ru>
2023-06-16 08:51:25 +0500

    Regexp Basics - is it IPv4 address?

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,25 @@
-const a = Array();
+String.prototype.ipv4Address = function () {
+  return /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$/.test(this);
+};
 
-console.log(a == false); // true
-console.log(!a == false); // true
-console.log(a == !a); // true
+// 'Should return true'
+console.log('127.0.0.1'.ipv4Address()); // True
+console.log('0.0.0.0'.ipv4Address()); // True
+console.log('255.255.255.255'.ipv4Address()); // True
+console.log('10.20.30.40'.ipv4Address()); // True
+
+// 'Should return false'
+console.log(''.ipv4Address()); // False
+console.log('10.256.30.40'.ipv4Address()); // False
+console.log('10.20.030.40'.ipv4Address()); // False
+console.log('127.0.1'.ipv4Address()); // False
+console.log('127.0.0.0.1'.ipv4Address()); // False
+console.log('..255.255'.ipv4Address()); // False
+console.log('127.0.0.1\n'.ipv4Address()); // False
+console.log('\n127.0.0.1'.ipv4Address()); // False
+console.log(' 127.0.0.1'.ipv4Address()); // False
+console.log('127.0.0.1 '.ipv4Address()); // False
+console.log(' 127.0.0.1 '.ipv4Address()); // False
+console.log('127.0.0.1.'.ipv4Address()); // False
+console.log('.127.0.0.1'.ipv4Address()); // False
+console.log('127..0.1'.ipv4Address()); // False
--- src/test/test.js
+++ src/test/test.js
@@ -1,15 +1,34 @@
 mocha.setup('bdd');
 
-describe(`!a == a ?!`, function () {
+describe(`Regexp Basics - is it IPv4 address?`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
   const isNull = chai.assert.isNull;
+  const isTrue = chai.assert.isTrue;
+  const isFalse = chai.assert.isFalse;
 
-  it('test', () => {
-    equal(a == false, true, `a != false`);
-    equal(!a == false, true, `!a != false`);
-    equal(a == !a, true, `a != !a`);
+  it('Should return true', () => {
+    isTrue('127.0.0.1'.ipv4Address());
+    isTrue('0.0.0.0'.ipv4Address());
+    isTrue('255.255.255.255'.ipv4Address());
+    isTrue('10.20.30.40'.ipv4Address());
+  });
+  it('Should return false', () => {
+    isFalse(''.ipv4Address());
+    isFalse('10.256.30.40'.ipv4Address());
+    isFalse('10.20.030.40'.ipv4Address());
+    isFalse('127.0.1'.ipv4Address());
+    isFalse('127.0.0.0.1'.ipv4Address());
+    isFalse('..255.255'.ipv4Address());
+    isFalse('127.0.0.1\n'.ipv4Address());
+    isFalse('\n127.0.0.1'.ipv4Address());
+    isFalse(' 127.0.0.1'.ipv4Address());
+    isFalse('127.0.0.1 '.ipv4Address());
+    isFalse(' 127.0.0.1 '.ipv4Address());
+    isFalse('127.0.0.1.'.ipv4Address());
+    isFalse('.127.0.0.1'.ipv4Address());
+    isFalse('127..0.1'.ipv4Address());
   });
 });
 

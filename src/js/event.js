b1bf404040fbb6cbb88c143a59828befb2e81dc2
OlennikovYV <olennikovyv@mail.ru>
2023-06-25 18:57:05 +0500

    Integer Difference

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,16 @@
-function matchArrays(v, r) {
-  return v.filter(v_item => r.includes(v_item)).length;
-}
+const intDiff = (arr, n) => {
+  let count = 0;
 
-console.log(
-  matchArrays(['Perl', 'Closure', 'JavaScript'], ['Go', 'C++', 'Erlang'])
-);
-// 0
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = i + 1; j < arr.length; j += 1) {
+      const diff = Math.abs(arr[i] - arr[j]);
+      if (diff === n) count += 1;
+    }
+  }
 
-console.log(
-  matchArrays(
-    ['incapsulation', 'OOP', 'array'],
-    ['time', 'propert', 'paralelism', 'OOP']
-  )
-);
-// 1
+  return count;
+};
 
-console.log(matchArrays([1, 2, 3, 4, 5], [2, 3, 4, 5, 6]));
-// 4
+console.log(intDiff([1, 1, 5, 6, 9, 16, 27], 4)); // 3
+console.log(intDiff([1, 1, 3, 3], 2)); // 4
+console.log(intDiff([1, 6, 2, 3, 7, 8, 7], 0)); // 1
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Incorrect division method`, function () {
+describe(`Integer Difference`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
@@ -8,19 +8,10 @@ describe(`Incorrect division method`, function () {
   const isTrue = chai.assert.isTrue;
   const isFalse = chai.assert.isFalse;
 
-  it('test', () => {
-    equal(
-      matchArrays(['Perl', 'Closure', 'JavaScript'], ['Go', 'C++', 'Erlang']),
-      0
-    );
-    equal(
-      matchArrays(
-        ['incapsulation', 'OOP', 'array'],
-        ['time', 'propert', 'paralelism', 'OOP']
-      ),
-      1
-    );
-    equal(matchArrays([1, 2, 3, 4, 5], [2, 3, 4, 5, 6]), 4);
+  it('should work for basic tests', function () {
+    equal(intDiff([1, 1, 5, 6, 9, 16, 27], 4), 3);
+    equal(intDiff([1, 1, 3, 3], 2), 4);
+    equal(intDiff([1, 6, 2, 3, 7, 8, 7], 0), 1);
   });
 });
 

7c5e5e7ad8f54b5b06b9730cb06a00a9caa60a18
OlennikovYV <olennikovyv@mail.ru>
2023-07-01 22:01:12 +0500

    Basic JS - Calculating averages

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,12 @@
-const intDiff = (arr, n) => {
-  let count = 0;
+const Calculator = {
+  average: function (...args) {
+    if (args.length === 0) return 0;
 
-  for (let i = 0; i < arr.length; i += 1) {
-    for (let j = i + 1; j < arr.length; j += 1) {
-      const diff = Math.abs(arr[i] - arr[j]);
-      if (diff === n) count += 1;
-    }
-  }
-
-  return count;
+    return args.reduce((sum, num) => sum + num, 0) / args.length;
+  },
 };
 
-console.log(intDiff([1, 1, 5, 6, 9, 16, 27], 4)); // 3
-console.log(intDiff([1, 1, 3, 3], 2)); // 4
-console.log(intDiff([1, 6, 2, 3, 7, 8, 7], 0)); // 1
+console.log(Calculator.average(3, 4, 5)); // 4
+console.log(Calculator.average(3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3));
+// 5.769230769230769
+console.log(Calculator.average()); // 0
--- src/test/test.js
+++ src/test/test.js
@@ -1,17 +1,33 @@
 mocha.setup('bdd');
 
-describe(`Integer Difference`, function () {
+describe(`Basic JS - Calculating averages`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
   const isNull = chai.assert.isNull;
   const isTrue = chai.assert.isTrue;
   const isFalse = chai.assert.isFalse;
+  const approximately = chai.assert.approximately;
+  const oneOf = chai.assert.oneOf;
 
-  it('should work for basic tests', function () {
-    equal(intDiff([1, 1, 5, 6, 9, 16, 27], 4), 3);
-    equal(intDiff([1, 1, 3, 3], 2), 4);
-    equal(intDiff([1, 6, 2, 3, 7, 8, 7], 0), 1);
+  let eps = 1e-5;
+
+  it('Type of calculator is correct', () => {
+    oneOf(typeof Calculator, ['object', 'function']);
+  });
+
+  it('Works for some examples', () => {
+    approximately(Calculator.average(3, 4, 5), 4, eps);
+    approximately(
+      Calculator.average(3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3),
+      5.769230769230769,
+      eps
+    );
+    equal(
+      Calculator.average(),
+      0,
+      'Calculator.average() (no arguments) should result in 0'
+    );
   });
 });
 

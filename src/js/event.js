8c61c9d789b4d0d25675dd955f55d8f7e5057571
OlennikovYV <olennikovyv@mail.ru>
2023-07-16 20:00:30 +0500

    First-Class Function Factory

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,13 @@
-function multiply(number) {
-  const unsignedNumber = Math.abs(number);
-  const countDigit = String(unsignedNumber).length;
-
-  return number * Math.pow(5, countDigit);
+function factory(x) {
+  return function (array) {
+    return array.map(element => element * x);
+  };
 }
 
-console.log(multiply(10)); // 250
-console.log(multiply(5)); // 25
-console.log(multiply(200)); // 25000
-console.log(multiply(0)); // 0
-console.log(multiply(-2)); // -10
+const myArray = [1, 2, 3];
+
+const threes = factory(3);
+console.log(threes(myArray)); // [3, 6, 9]
+
+const fives = factory(5);
+console.log(fives(myArray)); // [5, 10, 15]
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Multiply the number`, function () {
+describe(`First-Class Function Factory`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
@@ -10,12 +10,14 @@ describe(`Multiply the number`, function () {
   const approximately = chai.assert.approximately;
   const oneOf = chai.assert.oneOf;
 
-  it('Basic tests', function () {
-    equal(multiply(10), 250);
-    equal(multiply(5), 25);
-    equal(multiply(200), 25000);
-    equal(multiply(0), 0);
-    equal(multiply(-2), -10);
+  it('test', () => {
+    var myArray = [1, 2, 3];
+
+    var threes = factory(3);
+    deepEqual(threes(myArray), [3, 6, 9]);
+
+    var fives = factory(5);
+    deepEqual(fives(myArray), [5, 10, 15]);
   });
 });
 

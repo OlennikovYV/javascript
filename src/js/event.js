d5d482c5513446681ac390a091fc837f9777c5ae
OlennikovYV <olennikovyv@mail.ru>
2023-07-29 20:59:46 +0500

    Word to binary

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,20 @@
-function nerdify(txt) {
-  const replace = {
-    a: '4',
-    e: '3',
-    l: '1',
-  };
-
-  return txt.replace(/[AEael]/g, alpha => replace[alpha.toLowerCase()]);
+function wordToBin(str) {
+  return Array.from(str).map(alpha =>
+    alpha.charCodeAt(0).toString(2).padStart(8, '0')
+  );
 }
 
-console.log(nerdify('Fund4m3nt41s')); // 'Fund4m3nt41s'
-console.log(nerdify('Seven')); // 'S3v3n'
-console.log(nerdify('Los Angeles')); // 'Los 4ng313s'
-console.log(nerdify('Seoijselawuue')); // 'S3oijs314wuu3'
+console.log(wordToBin('man'));
+// ['01101101', '01100001', '01101110'],
+console.log(wordToBin('AB'));
+// ['01000001', '01000010'],
+console.log(wordToBin('wecking'));
+// ['01110111', '01100101', '01100011',
+//  '01101011', '01101001', '01101110',
+//  '01100111']
+console.log(wordToBin('Ruby'));
+// ['01010010', '01110101', '01100010', '01111001'],
+console.log(wordToBin('Yosemite'));
+// ['01011001', '01101111', '01110011',
+//  '01100101', '01101101', '01101001',
+//  '01110100', '01100101']
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Is the date today`, function () {
+describe(`Word to binary`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
@@ -10,17 +10,57 @@ describe(`Is the date today`, function () {
   const approximately = chai.assert.approximately;
   const oneOf = chai.assert.oneOf;
 
-  it('Fund4m3nt41s', () => {
-    equal(nerdify('Fund4m3nt41s'), 'Fund4m3nt41s');
+  it('man', () => {
+    deepEqual(
+      wordToBin('man'),
+      ['01101101', '01100001', '01101110'],
+      "should return [ '01101101', '01100001', '01101110' ] for `man` "
+    );
   });
-  it('Seven', () => {
-    equal(nerdify('Seven'), 'S3v3n');
+  it('AB', () => {
+    deepEqual(
+      wordToBin('AB'),
+      ['01000001', '01000010'],
+      "should return ['01000001', '01000010'] for `AB`"
+    );
   });
-  it('Los Angeles', () => {
-    equal(nerdify('Los Angeles'), 'Los 4ng313s');
+  it('wecking', () => {
+    deepEqual(
+      wordToBin('wecking'),
+      [
+        '01110111',
+        '01100101',
+        '01100011',
+        '01101011',
+        '01101001',
+        '01101110',
+        '01100111',
+      ],
+      "should return [ '01110111', '01100101', '01100011', '01101011', '01101001', '01101110', '01100111' ]  for `wecking`"
+    );
   });
-  it('Seoijselawuue', () => {
-    equal(nerdify('Seoijselawuue'), 'S3oijs314wuu3');
+  it('Ruby', () => {
+    deepEqual(
+      wordToBin('Ruby'),
+      ['01010010', '01110101', '01100010', '01111001'],
+      "should return [ '01010010', '01110101', '01100010', '01111001' ]  for `Ruby`"
+    );
+  });
+  it('Yosemite', () => {
+    deepEqual(
+      wordToBin('Yosemite'),
+      [
+        '01011001',
+        '01101111',
+        '01110011',
+        '01100101',
+        '01101101',
+        '01101001',
+        '01110100',
+        '01100101',
+      ],
+      "should return [ '01011001', '01101111', '01110011', '01100101', '01101101', '01101001', '01110100', '01100101' ]  for `Yosemite`"
+    );
   });
 });
 

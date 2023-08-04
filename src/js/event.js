b3031623f741b90ebc1d9f4c6c149d03a91a011f
Yury Olennikov <OlennikovYV@mail.ru>
2023-08-04 15:28:38 +0500

    Ensure question

--- src/js/event.js
+++ src/js/event.js
@@ -1,31 +1,7 @@
-function combine(...arg) {
-  const combine = {};
-
-  arg.map(object => {
-    for (const key in object) {
-      combine[key] = (combine[key] || 0) + object[key];
-    }
-  });
-
-  return combine;
+function ensureQuestion(s) {
+  return s.slice(-1) === '?' ? s : s + '?';
 }
 
-const objA = { a: 10, b: 20, c: 30 };
-const objB = { a: 3, c: 6, d: 3 };
-const objC = { a: 5, d: 11, e: 8 };
-const objD = { c: 3 };
-
-console.log(combine(objA, objB));
-// { a: 13, b: 20, c: 36, d: 3 }
-console.log(combine(objA, objC));
-// { a: 15, b: 20, c: 30, d: 11, e: 8 }
-
-console.log(combine(objA, objB, objC));
-// {a: 18, b: 20, c: 36, d: 14, e: 8}
-console.log(combine(objA, objC, objD));
-// {a: 15, b: 20, c: 33, d: 11, e: 8}
-
-console.log(combine({}, {}, {}));
-// {}
-console.log(combine(objA, objC, {}));
-// { a: 15, b: 20, c: 30, d: 11, e: 8 }
+console.log(ensureQuestion('')); // '?'
+console.log(ensureQuestion('Yes')); // 'Yes?'
+console.log(ensureQuestion('No?')); // 'No?'
--- src/test/test.js
+++ src/test/test.js
@@ -10,42 +10,10 @@ describe(`Word to binary`, function () {
   const approximately = chai.assert.approximately;
   const oneOf = chai.assert.oneOf;
 
-  const objA = { a: 10, b: 20, c: 30 };
-  const objB = { a: 3, c: 6, d: 3 };
-  const objC = { a: 5, d: 11, e: 8 };
-  const objD = { c: 3 };
-
-  it('Combine two objects', () => {
-    deepEqual(combine(objA, objB), { a: 13, b: 20, c: 36, d: 3 });
-    deepEqual(combine(objA, objC), { a: 15, b: 20, c: 30, d: 11, e: 8 });
-  });
-
-  it('Combine three objects', () => {
-    deepEqual(combine(objA, objB, objC), {
-      a: 18,
-      b: 20,
-      c: 36,
-      d: 14,
-      e: 8,
-    });
-    deepEqual(combine(objA, objC, objD), {
-      a: 15,
-      b: 20,
-      c: 33,
-      d: 11,
-      e: 8,
-    });
-  });
-
-  it('Handle empty objects', () => {
-    deepEqual(combine({}, {}, {}), {});
-    deepEqual(combine(objA, objC, {}), {
-      a: 15,
-      b: 20,
-      c: 30,
-      d: 11,
-      e: 8,
-    });
+  it('Fixed tests', () => {
+    equal(ensureQuestion(''), '?', "Expected: '?'");
+    equal(ensureQuestion('Yes'), 'Yes?', "Expected: '?'");
+    equal(ensureQuestion('No?'), 'No?', "Expected: '?'");
   });
 });
 

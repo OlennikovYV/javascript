df5d0a3583ce9b7f25218011ebcf637e4c35bb08
OlennikovYV <olennikovyv@mail.ru>
2023-08-05 21:58:25 +0500

    Find Your Villain Name

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,44 @@
-function ensureQuestion(s) {
-  return s.endsWith('?') ? s : s + '?';
+function getVillainName(birthday) {
+  const m = [
+    'Evil',
+    'Vile',
+    'Cruel',
+    'Trashy',
+    'Despicable',
+    'Embarrassing',
+    'Disreputable',
+    'Atrocious',
+    'Twirling',
+    'Orange',
+    'Terrifying',
+    'Awkward',
+  ];
+  const d = [
+    'Mustache',
+    'Pickle',
+    'Hood Ornament',
+    'Raisin',
+    'Recycling Bin',
+    'Potato',
+    'Tomato',
+    'House Cat',
+    'Teaspoon',
+    'Laundry Basket',
+  ];
+  const firstName = m[birthday.getMonth()];
+  const lastName = d[birthday.getDate() % 10];
+
+  return `The ${firstName} ${lastName}`;
 }
 
-console.log(ensureQuestion('')); // '?'
-console.log(ensureQuestion('Yes')); // 'Yes?'
-console.log(ensureQuestion('No?')); // 'No?'
+//may 3
+console.log(getVillainName(new Date('May 3')));
+//
+
+//apr 1
+console.log(getVillainName(new Date('April 21')));
+//
+
+//dec 7
+console.log(getVillainName(new Date('December 17')));
+//
--- src/test/test.js
+++ src/test/test.js
@@ -10,10 +10,30 @@ describe(`Word to binary`, function () {
   const approximately = chai.assert.approximately;
   const oneOf = chai.assert.oneOf;
 
-  it('Fixed tests', () => {
-    equal(ensureQuestion(''), '?', "Expected: '?'");
-    equal(ensureQuestion('Yes'), 'Yes?', "Expected: '?'");
-    equal(ensureQuestion('No?'), 'No?', "Expected: '?'");
+  it('test', () => {
+    //may 3
+    equal(
+      getVillainName(new Date('May 3')),
+      'The Despicable Raisin',
+      'Expected result: The Despicable Raisin; Your result: ' +
+        getVillainName(new Date('May 13'))
+    );
+
+    //apr 1
+    equal(
+      getVillainName(new Date('April 21')),
+      'The Trashy Pickle',
+      'Expected result: The Trashy Pickle; Your result: ' +
+        getVillainName(new Date('April 21'))
+    );
+
+    //dec 7
+    equal(
+      getVillainName(new Date('December 17')),
+      'The Awkward House Cat',
+      'Expected result: The Awkward House Cat; Your result: ' +
+        getVillainName(new Date('December 17'))
+    );
   });
 });
 

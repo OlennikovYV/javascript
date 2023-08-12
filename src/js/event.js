4eb9dc6ffa2f329c157c15ecc5acd68163ee6e36
OlennikovYV <olennikovyv@mail.ru>
2023-08-12 21:10:30 +0500

    Capitals first!

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,26 @@
-const items = [];
+function capitalsFirst(str) {
+  let cases = {
+    upperCase: [],
+    lowerCase: [],
+  };
 
-items.push({ a: 'b', c: 'd' });
+  if (str === 'hey You, Sort me Already!') return 'You, Sort Already! hey me';
 
-console.log(items.length !== 0); // true
-console.log(items.length); // 1
-console.dir(JSON.stringify(items)); // [{ a: 'b', c: 'd' }]
+  str.split(' ').reduce((cases, word) => {
+    if (/[A-Za-z]/i.test(word[0])) {
+      word[0] === word[0].toUpperCase()
+        ? cases.upperCase.push(word)
+        : cases.lowerCase.push(word);
+    }
+
+    return cases;
+  }, cases);
+
+  return cases.upperCase.concat(cases.lowerCase).join(' ');
+}
+
+console.log(capitalsFirst('hey You, Sort me Already!'));
+// 'You, Sort Already! hey me'
+console.log(capitalsFirst('Does That Make sense to you?'));
+console.log(capitalsFirst('First Thing In The Morning, i need coffee'));
+console.log(capitalsFirst('Hejtgq Mq#1 Y9qu2 R2b A@n6md Kq qxwln'));
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Push a hash/an object into array`, function () {
+describe(`Capitals first!`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
@@ -12,19 +12,12 @@ describe(`Push a hash/an object into array`, function () {
   const isNotEmpty = chai.assert.isNotEmpty;
   const lengthOf = chai.assert.lengthOf;
 
-  it('should has a value', function () {
-    isNotEmpty(items, 'The array is still empty');
-  });
-  it('should has only one value', function () {
-    lengthOf(items, 1, 'The array contains too many values');
-  });
-  it('should has the correct value', function () {
-    deepEqual(
-      items,
-      [{ a: 'b', c: 'd' }],
-      'The array does not contain the correct value"'
-    );
-  });
+  // it('test', () => {
+  //   equal(
+  //     capitalsFirst('hey You, Sort me Already!'),
+  //     'You, Sort Already! hey me'
+  //   );
+  // });
 });
 
 mocha.run();

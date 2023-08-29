09fbafc5d99f4269daeedc8fbd9a40aabd5551da
Yury Olennikov <OlennikovYV@mail.ru>
2023-08-29 16:44:17 +0500

    No ifs no buts

--- src/js/event.js
+++ src/js/event.js
@@ -1,25 +1,19 @@
-function colourAssociation(array) {
-  return array.map(([color, association]) => ({ [color]: association }));
+function noIfsNoButs(a, b) {
+  const Greater = a > b;
+  const Smaller = a < b;
+  const Equal = a === b;
+
+  const strGreater = Greater && 'is greater than';
+  const strSmaller = Smaller && 'is smaller than';
+  const strEqual = Equal && 'is equal to';
+
+  return `${a} ${strGreater || strSmaller || strEqual} ${b}`;
 }
 
-console.log(
-  colourAssociation([
-    ['white', 'goodness'],
-    ['blue', 'tranquility'],
-  ])
-);
-// [{ white: 'goodness' }, { blue: 'tranquility' }]
-console.log(
-  colourAssociation([
-    ['red', 'energy'],
-    ['yellow', 'creativity'],
-    ['brown', 'friendly'],
-    ['green', 'growth'],
-  ])
-);
-// [
-//   { red: 'energy' },
-//   { yellow: 'creativity' },
-//   { brown: 'friendly' },
-//   { green: 'growth' },
-// ]
+console.log(noIfsNoButs(45, 51)); // '45 is smaller than 51'
+console.log(noIfsNoButs(1, 2)); // '1 is smaller than 2'
+console.log(noIfsNoButs(-3, 2)); // '-3 is smaller than 2'
+console.log(noIfsNoButs(1, 1)); // '1 is equal to 1'
+console.log(noIfsNoButs(100, 100)); // '100 is equal to 100'
+console.log(noIfsNoButs(100, 80)); // '100 is greater than 80'
+console.log(noIfsNoButs(20, 19)); // '20 is greater than 19'
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Colour Association`, function () {
+describe(`No ifs no buts`, function () {
   const equal = chai.assert.equal;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
@@ -12,28 +12,14 @@ describe(`Colour Association`, function () {
   const isNotEmpty = chai.assert.isNotEmpty;
   const lengthOf = chai.assert.lengthOf;
 
-  it('Should pass sample tests', () => {
-    deepEqual(
-      colourAssociation([
-        ['white', 'goodness'],
-        ['blue', 'tranquility'],
-      ]),
-      [{ white: 'goodness' }, { blue: 'tranquility' }]
-    );
-    deepEqual(
-      colourAssociation([
-        ['red', 'energy'],
-        ['yellow', 'creativity'],
-        ['brown', 'friendly'],
-        ['green', 'growth'],
-      ]),
-      [
-        { red: 'energy' },
-        { yellow: 'creativity' },
-        { brown: 'friendly' },
-        { green: 'growth' },
-      ]
-    );
+  it('Should handle manual tests', function () {
+    equal(noIfsNoButs(45, 51), '45 is smaller than 51');
+    equal(noIfsNoButs(1, 2), '1 is smaller than 2');
+    equal(noIfsNoButs(-3, 2), '-3 is smaller than 2');
+    equal(noIfsNoButs(1, 1), '1 is equal to 1');
+    equal(noIfsNoButs(100, 100), '100 is equal to 100');
+    equal(noIfsNoButs(100, 80), '100 is greater than 80');
+    equal(noIfsNoButs(20, 19), '20 is greater than 19');
   });
 });
 

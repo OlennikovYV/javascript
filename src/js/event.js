01e58ce14b305090ae3ff23bc81d50fd4b67497a
OlennikovYV <olennikovyv@mail.ru>
2023-09-02 21:48:44 +0500

    Training JS #26: methods of arrayObject---map() refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,10 @@
-const insertSpliter = str => {
-  const lengthPart = Math.floor(str.length / 2);
-  const arrayString = Array(...str);
-  const leftPart = arrayString.slice(0, lengthPart).join('');
-  const rightPart = arrayString.slice(-lengthPart).join('');
-
-  return leftPart + '|' + rightPart;
-};
-
 function isolateIt(arr) {
-  return arr.map(string => insertSpliter(string));
+  return arr.map(
+    string =>
+      string.slice(0, string.length / 2) +
+      '|' +
+      string.slice(-string.length / 2)
+  );
 }
 
 console.log(isolateIt(['abcd', 'efgh'])); // ['ab|cd', 'ef|gh']

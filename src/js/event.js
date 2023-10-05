176cb20fd50076f4daddf7df8242d407e1ca02e4
OlennikovYV <olennikovyv@mail.ru>
2023-10-05 21:00:30 +0500

    String chunks

--- src/js/event.js
+++ src/js/event.js
@@ -1,42 +1,16 @@
-function generateHashtag(str) {
-  let hashtag;
+function stringChunk(str, n) {
+  const chunks = [];
 
-  if (str.trim().length === 0) return false;
+  if (n <= 0 || Number.isInteger(n) === false) return chunks;
 
-  hashtag = str
-    .split(' ')
-    .reduce(
-      (hash, word) =>
-        word.length === 0
-          ? hash
-          : hash + word[0].toUpperCase() + word.slice(1).toLowerCase(),
-      ''
-    );
+  for (let i = 0; i < str.length; i += n) {
+    chunks.push(str.slice(i, i + n));
+  }
 
-  return hashtag.length < 140 ? `#${hashtag}` : false;
+  return chunks;
 }
 
-console.log(generateHashtag(''));
-// false
-console.log(generateHashtag(' '.repeat(200)));
-// false
-console.log(generateHashtag('Do We have A Hashtag'));
-// '#DoWeHaveAHashtag'
-console.log(generateHashtag('Codewars'));
-// '#Codewars'
-console.log(generateHashtag('Codewars Is Nice'));
-// '#CodewarsIsNice'
-console.log(generateHashtag('Codewars is nice'));
-// '#CodewarsIsNice'
-console.log(generateHashtag('code' + ' '.repeat(140) + 'wars'));
-// '#CodeWars
-console.log(
-  generateHashtag(
-    'Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat'
-  )
-);
-// false
-console.log(generateHashtag('a'.repeat(139)));
-// '#A' + 'a'.repeat(138)
-console.log(generateHashtag('a'.repeat(140)));
-// false
+console.log(stringChunk('codewars', 2)); //  ['co', 'de', 'wa', 'rs']
+console.log(stringChunk('thiskataeasy', 4)); // ['this', 'kata', 'easy']
+console.log(stringChunk('hello world', 3)); // ['hel', 'lo ', 'wor', 'ld']
+console.log(stringChunk('sunny day', 0)); // []
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`The Hashtag Generator`, function () {
+describe(`String chunks`, function () {
   const equal = chai.assert.equal;
   const notEqual = chai.assert.notEqual;
   const deepEqual = chai.assert.deepEqual;
@@ -13,47 +13,11 @@ describe(`The Hashtag Generator`, function () {
   const isNotEmpty = chai.assert.isNotEmpty;
   const lengthOf = chai.assert.lengthOf;
 
-  it('Sample tests', () => {
-    equal(
-      generateHashtag(''),
-      false,
-      'Expected an empty string to return false'
-    );
-    equal(generateHashtag(' '.repeat(200)), false, 'Still an empty string');
-    equal(
-      generateHashtag('Do We have A Hashtag'),
-      '#DoWeHaveAHashtag',
-      'Expected a Hashtag (#) at the beginning.'
-    );
-    equal(
-      generateHashtag('Codewars'),
-      '#Codewars',
-      'Should handle a single word.'
-    );
-    equal(
-      generateHashtag('Codewars Is Nice'),
-      '#CodewarsIsNice',
-      'Should remove spaces.'
-    );
-    equal(
-      generateHashtag('Codewars is nice'),
-      '#CodewarsIsNice',
-      'Should capitalize first letters of words.'
-    );
-    equal(generateHashtag('code' + ' '.repeat(140) + 'wars'), '#CodeWars');
-    equal(
-      generateHashtag(
-        'Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat'
-      ),
-      false,
-      'Should return false if the final word is longer than 140 chars.'
-    );
-    equal(
-      generateHashtag('a'.repeat(139)),
-      '#A' + 'a'.repeat(138),
-      'Should work'
-    );
-    equal(generateHashtag('a'.repeat(140)), false, 'Too long');
+  it('test', () => {
+    deepEqual(stringChunk('codewars', 2), ['co', 'de', 'wa', 'rs']);
+    deepEqual(stringChunk('thiskataeasy', 4), ['this', 'kata', 'easy']);
+    deepEqual(stringChunk('hello world', 3), ['hel', 'lo ', 'wor', 'ld']);
+    deepEqual(stringChunk('sunny day', 0), []);
   });
 });
 

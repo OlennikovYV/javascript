08ea96d7aa10a985789ca5e921d8e6f7c5d6940a
OlennikovYV <olennikovyv@mail.ru>
2023-10-14 20:39:48 +0500

    Convert A Hex String To RGB

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,12 @@
-function makeLooper(str) {
-  let currentIndex = 0;
-
-  return function () {
-    return str[currentIndex++ % str.length];
-  };
+function hexStringToRGB(hexString) {
+  var hexArray = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexString);
+  return hexArray
+    ? {
+        r: parseInt(hexArray[1], 16),
+        g: parseInt(hexArray[2], 16),
+        b: parseInt(hexArray[3], 16),
+      }
+    : null;
 }
 
-const abc = makeLooper('abc');
-
-console.log(abc()); // 'a'
-console.log(abc()); // 'b'
-console.log(abc()); // 'c'
-console.log(abc()); // 'a'
-console.log(abc()); // 'b'
-console.log(abc()); // 'c'
+console.log(hexStringToRGB('#FF9933')); // {r:255, g:153, b:51}

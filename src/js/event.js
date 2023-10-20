2094427707ca4beee91df06d8831fdaedd1dc640
Yury Olennikov <OlennikovYV@mail.ru>
2023-10-20 08:45:33 +0500

    Find within array

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,33 @@
-const getNames = data => data.map(item => item.name);
+const findInArray = function (array, iterator) {
+  for (let i = 0; i < array.length; i++) {
+    if (iterator(array[i], i)) return i;
+  }
 
-const data = [
-  { name: 'Joe', age: 20 },
-  { name: 'Bill', age: 30 },
-  { name: 'Kate', age: 23 },
-];
-console.log(getNames(data)); // ['Joe', 'Bill', 'Kate']
+  return -1;
+};
+
+const trueIfEven = function (v, i) {
+  return v % 2 === 0;
+};
+const neverTrue = function (v, i) {
+  return false;
+};
+const trueIfValueEqualsIndex = function (v, i) {
+  return v === i;
+};
+const trueIfLengthEqualsIndex = function (v, i) {
+  return v.length === i;
+};
+
+console.log(findInArray([], trueIfEven)); // -1
+console.log(findInArray([1, 3, 5, 6, 7], trueIfEven)); // 3
+console.log(findInArray([2, 4, 6, 8], trueIfEven)); // 0
+console.log(findInArray([2, 4, 6, 8], neverTrue)); // -1
+console.log(findInArray([13, 5, 3, 1, 4, 5], trueIfValueEqualsIndex)); // 4
+console.log(
+  findInArray(
+    ['one', 'two', 'three', 'four', 'five', 'six'],
+    trueIfLengthEqualsIndex
+  )
+); // 4
+console.log(findInArray(['bc', 'af', 'd', 'e'], trueIfLengthEqualsIndex)); // -1
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`getNames()`, function () {
+describe(`Find within array`, function () {
   const equal = chai.assert.equal;
   const notEqual = chai.assert.notEqual;
   const deepEqual = chai.assert.deepEqual;
@@ -14,13 +14,33 @@ describe(`getNames()`, function () {
   const lengthOf = chai.assert.lengthOf;
   const throws = chai.assert.throws;
 
-  it('should return the names of the people passed in', function () {
-    const data = [
-      { name: 'Joe', age: 20 },
-      { name: 'Bill', age: 30 },
-      { name: 'Kate', age: 23 },
-    ];
-    deepEqual(getNames(data), ['Joe', 'Bill', 'Kate']);
+  it('test', () => {
+    const trueIfEven = function (v, i) {
+      return v % 2 === 0;
+    };
+    const neverTrue = function (v, i) {
+      return false;
+    };
+    const trueIfValueEqualsIndex = function (v, i) {
+      return v === i;
+    };
+    const trueIfLengthEqualsIndex = function (v, i) {
+      return v.length === i;
+    };
+
+    equal(findInArray([], trueIfEven), -1);
+    equal(findInArray([1, 3, 5, 6, 7], trueIfEven), 3);
+    equal(findInArray([2, 4, 6, 8], trueIfEven), 0);
+    equal(findInArray([2, 4, 6, 8], neverTrue), -1);
+    equal(findInArray([13, 5, 3, 1, 4, 5], trueIfValueEqualsIndex), 4);
+    equal(
+      findInArray(
+        ['one', 'two', 'three', 'four', 'five', 'six'],
+        trueIfLengthEqualsIndex
+      ),
+      4
+    );
+    equal(findInArray(['bc', 'af', 'd', 'e'], trueIfLengthEqualsIndex), -1);
   });
 });
 

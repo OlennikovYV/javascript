5d86f4e43c329e37805acd3fd340d52efc4f7bd6
OlennikovYV <olennikovyv@mail.ru>
2023-10-22 17:45:27 +0500

    Combinator Flip

--- src/js/event.js
+++ src/js/event.js
@@ -1,37 +1,10 @@
-function maxProduct(a) {
-  let max1 = 0,
-    max2 = 0;
+const flip =
+  fn =>
+  (...arg) =>
+    fn(...arg.reverse());
 
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] > max1) {
-      if (max1 > max2) max2 = max1;
-      max1 = a[i];
-    } else {
-      if (a[i] > max2) max2 = a[i];
-    }
-  }
-
-  return max1 * max2;
+function print(a, b) {
+  return a + ' -> ' + b;
 }
 
-console.log(maxProduct([56, 335, 195, 443, 6, 494, 252])); // 218842
-console.log(maxProduct([154, 428, 455, 346])); // 194740
-console.log(
-  maxProduct([
-    39, 135, 47, 275, 37, 108, 265, 457, 2, 133, 316, 330, 153, 253, 321, 411,
-  ])
-); // 187827
-console.log(maxProduct([136, 376, 10, 146, 105, 63, 234])); // 87984
-console.log(
-  maxProduct([
-    354, 463, 165, 62, 472, 53, 347, 293, 252, 378, 420, 398, 255, 89,
-  ])
-); // 218536
-console.log(
-  maxProduct([346, 446, 26, 425, 432, 349, 123, 269, 285, 93, 75, 14])
-); // 192672
-console.log(maxProduct([134, 320, 266, 299])); // 95680
-console.log(
-  maxProduct([114, 424, 53, 272, 128, 215, 25, 329, 272, 313, 100, 24, 252])
-); // 139496
-console.log(maxProduct([375, 56, 337, 466, 203])); // 174750
+console.log(flip(print)(4, 5)); // '5 -> 4'

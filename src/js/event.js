26929815b9aa189eaae8927a0363f8ed512d0260
OlennikovYV <olennikovyv@mail.ru>
2023-10-28 18:46:27 +0500

    Thinkful - String Drills: Poem formatter

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,41 @@
-function area(d, l) {
-  let area;
-
-  if (d <= l) return 'Not a rectangle';
-
-  area = l * Math.sqrt(Math.pow(d, 2) - Math.pow(l, 2));
-
-  return Math.round(area * 100) / 100;
+function formatPoem(poem) {
+  const last = poem[poem.length - 1];
+  return (
+    poem
+      .split(/[.!?]/)
+      .slice(0, -1)
+      .map(string => string.trim())
+      .join('.\n') + last
+  );
 }
 
-console.log(area(5, 4)); // 12
-console.log(area(10, 6)); // 48
-console.log(area(13, 5)); // 60
-console.log(area(12, 5)); // 54.54
-console.log(area(12, 12)); // 'Not a rectangle'
+console.log(
+  formatPoem(
+    'Beautiful is better than ugly. Explicit is better than implicit. Simple is better than complex. Complex is better than complicated.'
+  )
+);
+// 'Beautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.'
+console.log(
+  formatPoem(
+    "Flat is better than nested. Sparse is better than dense. Readability counts. Special cases aren't special enough to break the rules."
+  )
+);
+// "Flat is better than nested.\nSparse is better than dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules."
+console.log(
+  formatPoem(
+    'Although practicality beats purity. Errors should never pass silently. Unless explicitly silenced. In the face of ambiguity, refuse the temptation to guess.'
+  )
+);
+// 'Although practicality beats purity.\nErrors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.'
+console.log(
+  formatPoem(
+    "There should be one-- and preferably only one --obvious way to do it. Although that way may not be obvious at first unless you're Dutch. Now is better than never. Although never is often better than *right* now."
+  )
+);
+// "There should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you're Dutch.\nNow is better than never.\nAlthough never is often better than *right* now."
+console.log(
+  formatPoem(
+    "If the implementation is hard to explain, it's a bad idea. If the implementation is easy to explain, it may be a good idea. Namespaces are one honking great idea -- let's do more of those!"
+  )
+);
+// "If the implementation is hard to explain, it's a bad idea.\nIf the implementation is easy to explain, it may be a good idea.\nNamespaces are one honking great idea -- let's do more of those!"

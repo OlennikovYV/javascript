b220e50916813ee526d30ea47ae15d32e67501d4
Yury Olennikov <OlennikovYV@mail.ru>
2023-11-08 08:45:22 +0500

    Between Extremes

--- src/js/event.js
+++ src/js/event.js
@@ -1,20 +1,6 @@
-function solve(n) {
-  let balance = n;
-  let countBanktots = 0;
-
-  [500, 200, 100, 50, 20, 10].forEach(nominal => {
-    countBanktots += Math.floor(balance / nominal);
-    balance = balance % nominal;
-  });
-
-  return balance ? -1 : countBanktots;
+function betweenExtremes(numbers) {
+  return Math.max(...numbers) - Math.min(...numbers);
 }
 
-console.log(solve(770)); // 4
-console.log(solve(550)); // 2
-console.log(solve(10)); // 1
-console.log(solve(1250)); // 4
-
-console.log(solve(125)); // -1
-console.log(solve(666)); // -1
-console.log(solve(42)); // -1
+console.log(betweenExtremes([21, 34, 54, 43, 26, 12])); // 42
+console.log(betweenExtremes([-1, -41, -77, -100])); // 99
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`ATM`, function () {
+describe(`Between Extremes`, function () {
   const equal = chai.assert.equal;
   const notEqual = chai.assert.notEqual;
   const deepEqual = chai.assert.deepEqual;
@@ -13,17 +13,9 @@ describe(`ATM`, function () {
   const isNotEmpty = chai.assert.isNotEmpty;
   const lengthOf = chai.assert.lengthOf;
 
-  it('should work when chosing notes is possible', function () {
-    equal(solve(770), 4, 'Wrong result for 770');
-    equal(solve(550), 2, 'Wrong result for 550');
-    equal(solve(10), 1, 'Wrong result for 10');
-    equal(solve(1250), 4, 'Wrong result for 1250');
-  });
-
-  it('should return -1 if chosing notes is not possible', function () {
-    equal(solve(125), -1, 'Wrong result for 125');
-    equal(solve(666), -1, 'Wrong result for 666');
-    equal(solve(42), -1, 'Wrong result for 42');
+  it('test', () => {
+    equal(betweenExtremes([21, 34, 54, 43, 26, 12]), 42);
+    equal(betweenExtremes([-1, -41, -77, -100]), 99);
   });
 });
 

8ee8f301657425fa35cac1492333a209c398711e
OlennikovYV <olennikovyv@mail.ru>
2023-11-12 17:26:53 +0500

    Alternate Square Sum

--- src/js/event.js
+++ src/js/event.js
@@ -1,33 +1,11 @@
-function countWins(winnerList, country) {
-  return winnerList.reduce(
-    (sum, winner) => (winner.country === country ? sum + 1 : sum),
-    0
-  );
+function alternateSqSum(arr) {
+  return arr.reduce((sum, digit, i) => {
+    const add = (i + 1) % 2 ? digit : digit ** 2;
+    return sum + add;
+  }, 0);
 }
 
-const winnerList1 = [
-  { season: '1996–97', team: 'Borussia Dortmund', country: 'Germany' },
-  { season: '1997–98', team: 'Real Madrid', country: 'Spain' },
-  { season: '1998–99', team: 'Manchester United', country: 'England' },
-  { season: '1999–00', team: 'Real Madrid', country: 'Spain' },
-  { season: '2000–01', team: 'Bayern Munich', country: 'Germany' },
-  { season: '2001–02', team: 'Real Madrid', country: 'Spain' },
-  { season: '2002–03', team: 'Milan', country: 'Italy' },
-  { season: '2003–04', team: 'Porto', country: 'Portugal' },
-  { season: '2004–05', team: 'Liverpool', country: 'England' },
-  { season: '2005–06', team: 'Barcelona', country: 'Spain' },
-  { season: '2006–07', team: 'Milan', country: 'Italy' },
-  { season: '2007–08', team: 'Manchester United', country: 'England' },
-  { season: '2008–09', team: 'Barcelona', country: 'Spain' },
-  { season: '2009–10', team: 'Internazionale', country: 'Italy' },
-  { season: '2010–11', team: 'Barcelona', country: 'Spain' },
-  { season: '2011–12', team: 'Chelsea', country: 'England' },
-  { season: '2012–13', team: 'Bayern', country: 'Germany' },
-  { season: '2013–14', team: 'Real Madrid', country: 'Spain' },
-  { season: '2014–15', team: 'Barcelona', country: 'Spain' },
-  { season: '2015–16', team: 'Real Madrid', country: 'Spain' },
-];
-
-console.log(countWins(winnerList1, 'Portugal')); // 1
-console.log(countWins(winnerList1, 'FootLand')); // 0
-console.log(countWins(winnerList1, 'Spain')); // 9
+console.log(alternateSqSum([])); // 0
+console.log(alternateSqSum([1, 2, 3, 4, 5])); // 29
+console.log(alternateSqSum([-1, 0, -3, 0, -5, 3])); // 0
+console.log(alternateSqSum([-1, 2, -3, 4, -5])); // 11
--- src/test/test.js
+++ src/test/test.js
@@ -1,50 +1,24 @@
 mocha.setup('bdd');
 
-describe(
-  `Find how many times did a team from a` +
-    ` given country win the Champions League?`,
-  function () {
-    const equal = chai.assert.equal;
-    const notEqual = chai.assert.notEqual;
-    const deepEqual = chai.assert.deepEqual;
-    const isDefined = chai.assert.isDefined;
-    const isNull = chai.assert.isNull;
-    const isTrue = chai.assert.isTrue;
-    const isFalse = chai.assert.isFalse;
-    const approximately = chai.assert.approximately;
-    const oneOf = chai.assert.oneOf;
-    const isNotEmpty = chai.assert.isNotEmpty;
-    const lengthOf = chai.assert.lengthOf;
+describe(`Alternate Square Sum`, function () {
+  const equal = chai.assert.equal;
+  const notEqual = chai.assert.notEqual;
+  const deepEqual = chai.assert.deepEqual;
+  const isDefined = chai.assert.isDefined;
+  const isNull = chai.assert.isNull;
+  const isTrue = chai.assert.isTrue;
+  const isFalse = chai.assert.isFalse;
+  const approximately = chai.assert.approximately;
+  const oneOf = chai.assert.oneOf;
+  const isNotEmpty = chai.assert.isNotEmpty;
+  const lengthOf = chai.assert.lengthOf;
 
-    it('test', () => {
-      const winnerList1 = [
-        { season: '1996–97', team: 'Borussia Dortmund', country: 'Germany' },
-        { season: '1997–98', team: 'Real Madrid', country: 'Spain' },
-        { season: '1998–99', team: 'Manchester United', country: 'England' },
-        { season: '1999–00', team: 'Real Madrid', country: 'Spain' },
-        { season: '2000–01', team: 'Bayern Munich', country: 'Germany' },
-        { season: '2001–02', team: 'Real Madrid', country: 'Spain' },
-        { season: '2002–03', team: 'Milan', country: 'Italy' },
-        { season: '2003–04', team: 'Porto', country: 'Portugal' },
-        { season: '2004–05', team: 'Liverpool', country: 'England' },
-        { season: '2005–06', team: 'Barcelona', country: 'Spain' },
-        { season: '2006–07', team: 'Milan', country: 'Italy' },
-        { season: '2007–08', team: 'Manchester United', country: 'England' },
-        { season: '2008–09', team: 'Barcelona', country: 'Spain' },
-        { season: '2009–10', team: 'Internazionale', country: 'Italy' },
-        { season: '2010–11', team: 'Barcelona', country: 'Spain' },
-        { season: '2011–12', team: 'Chelsea', country: 'England' },
-        { season: '2012–13', team: 'Bayern', country: 'Germany' },
-        { season: '2013–14', team: 'Real Madrid', country: 'Spain' },
-        { season: '2014–15', team: 'Barcelona', country: 'Spain' },
-        { season: '2015–16', team: 'Real Madrid', country: 'Spain' },
-      ];
-
-      equal(countWins(winnerList1, 'Portugal'), 1);
-      equal(countWins(winnerList1, 'FootLand'), 0);
-      equal(countWins(winnerList1, 'Spain'), 9);
-    });
-  }
-);
+  it('test', () => {
+    equal(alternateSqSum([]), 0);
+    equal(alternateSqSum([1, 2, 3, 4, 5]), 29);
+    equal(alternateSqSum([-1, 0, -3, 0, -5, 3]), 0);
+    equal(alternateSqSum([-1, 2, -3, 4, -5]), 11);
+  });
+});
 
 mocha.run();

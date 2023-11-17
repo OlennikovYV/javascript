bb6bc6937fb281223fd4ebdedbae5549459da35c
Yury Olennikov <OlennikovYV@mail.ru>
2023-11-17 08:43:03 +0500

    Lexical this

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,24 @@
-function add(...numbers) {
-  return Math.round(
-    numbers.reduce((sum, digit, index) => sum + digit / (index + 1), 0)
-  );
-}
+const Person = function () {
+  const person = {
+    _name: 'Leroy',
+    _friends: [],
+    fillFriends(f) {
+      this._friends = f;
+    },
+  };
 
-console.log(add()); // 0
-console.log(add(100, 200, 300)); // 300
-console.log(add(2)); // 2
-console.log(add(4, -3, -2)); // 2
-console.log(add(-1, -2, -3, -4)); // -4
-console.log(add(446, -275, 241, 76, -272, -231, 327, -493)); // 300
+  return person;
+};
+
+const people = [
+  ['bob', 'john'],
+  ['bob', 'john', 'dave'],
+  [],
+  ['bob', 'john', 'dave', 'matt', 'alex'],
+];
+
+for (let i = 0; i < 4; i++) {
+  const person = Person();
+  person.fillFriends(people[i]);
+  console.log(person._friends); // people[i]
+}
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Decreasing Inputs`, function () {
+describe(`Lexical this`, function () {
   const equal = chai.assert.equal;
   const notEqual = chai.assert.notEqual;
   const deepEqual = chai.assert.deepEqual;
@@ -13,28 +13,19 @@ describe(`Decreasing Inputs`, function () {
   const isNotEmpty = chai.assert.isNotEmpty;
   const lengthOf = chai.assert.lengthOf;
 
-  it('add() = 0', () => {
-    equal(add(), 0);
-  });
+  it('test', () => {
+    const people = [
+      ['bob', 'john'],
+      ['bob', 'john', 'dave'],
+      [],
+      ['bob', 'john', 'dave', 'matt', 'alex'],
+    ];
 
-  it('add(100, 200, 300) = 300', () => {
-    equal(add(100, 200, 300), 300);
-  });
-
-  it('add(2) = 2', () => {
-    equal(add(2), 2);
-  });
-
-  it('add(4, -3, -2) = 2', () => {
-    equal(add(4, -3, -2), 2);
-  });
-
-  it('add(-1, -2, -3, -4) = -4', () => {
-    equal(add(-1, -2, -3, -4), -4);
-  });
-
-  it('add(446, -275, 241, 76, -272, -231, 327, -493) = 300', () => {
-    console.log(add(446, -275, 241, 76, -272, -231, 327, -493)); // 300
+    for (let i = 0; i < 4; i++) {
+      const person = Person();
+      person.fillFriends(people[i]);
+      deepEqual(person._friends, people[i]);
+    }
   });
 });
 

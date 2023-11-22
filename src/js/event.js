b3e60eb9fe1a97a53bdfd12e087a2fe4cb0227a7
OlennikovYV <olennikovyv@mail.ru>
2023-11-22 19:02:20 +0500

    101 Dalmatians - squash the bugs, not the dogs! refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,21 +1,13 @@
 function howManyDalmatians(number) {
-  const dogs = [
-    'Hardly any',
-    'More than a handful!',
-    "Woah that's a lot of dogs!",
-    '101 DALMATIANS!!!',
-  ];
-
-  const respond =
-    number <= 10
-      ? dogs[0]
-      : number <= 50
-      ? dogs[1]
-      : number === 101
-      ? dogs[3]
-      : dogs[2];
-
-  return respond;
+  if (number <= 10) {
+    return 'Hardly any';
+  } else if (number <= 50) {
+    return 'More than a handful!';
+  } else if (number === 101) {
+    return '101 DALMATIANS!!!';
+  } else {
+    return "Woah that's a lot of dogs!";
+  }
 }
 
 console.log(howManyDalmatians(26)); // 'More than a handful!'

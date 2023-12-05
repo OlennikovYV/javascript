4bee51cce33dabfe5d568648a75b6f4d59727e2d
Yury Olennikov <OlennikovYV@mail.ru>
2023-12-05 11:33:46 +0500

    Organise duplicate numbers in list refactor 2

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,5 @@
 function group(arr) {
-  const countDuplicates = {};
-  const orderList = [...new Set(arr)];
-
-  arr.reduce((duplicates, digit) => {
-    duplicates[digit] = (duplicates[digit] || 0) + 1;
-    return duplicates;
-  }, countDuplicates);
-
-  return orderList.map(element =>
-    Array.from({ length: countDuplicates[element] }, _ => element)
-  );
+  return [...new Set(arr)].map(digit => arr.filter(el => el === digit));
 }
 
 console.log(group([3, 2, 6, 2, 1, 3]));

a209d3d961b353bc627d3a98957a0a91accdb9eb
Yury Olennikov <OlennikovYV@mail.ru>
2023-12-15 14:38:14 +0500

    Fibonacci's FizzBuzz

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,26 @@
-function tea42(input) {
-  return String(input).replace(/[2]/g, 't');
-}
+const fibsFizzBuzz = function (n) {
+  const arrayFibonacci = [1];
 
-console.log(tea42(2)); // 't'
-console.log(tea42('2')); // 't'
-console.log(tea42(102)); // '10t'
-console.log(tea42('m2')); // 'mt'
-console.log(tea42('pre2ty')); // 'pretty'
-console.log(tea42('t22222')); // 'tttttt'
+  for (let i = 1; i < n; i++) {
+    arrayFibonacci[i] = (arrayFibonacci[i - 2] || 0) + arrayFibonacci[i - 1];
+  }
+
+  return arrayFibonacci.map(numberFibonacci => {
+    let fizzBuzzString = '';
+
+    if (numberFibonacci % 3 === 0) fizzBuzzString += `Fizz`;
+    if (numberFibonacci % 5 === 0) fizzBuzzString += `Buzz`;
+
+    return fizzBuzzString ? fizzBuzzString : numberFibonacci;
+  });
+};
+
+console.log(fibsFizzBuzz(2));
+// [1, 1]
+
+console.log(fibsFizzBuzz(5));
+// [1, 1, 2, 'Fizz', 'Buzz']
+
+console.log(fibsFizzBuzz(20));
+// [1, 1, 2, 'Fizz', 'Buzz', 8, 13, 'Fizz', 34, 'Buzz',
+// 89, 'Fizz', 233, 377, 'Buzz', 'Fizz', 1597, 2584, 4181, 'FizzBuzz']
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Tea for two`, function () {
+describe(`Fibonacci's FizzBuzz`, function () {
   const equal = chai.assert.equal;
   const notEqual = chai.assert.notEqual;
   const deepEqual = chai.assert.deepEqual;
@@ -13,13 +13,37 @@ describe(`Tea for two`, function () {
   const isNotEmpty = chai.assert.isNotEmpty;
   const lengthOf = chai.assert.lengthOf;
 
-  it('test', () => {
-    equal(tea42(2), 't', 'Twos you lose!');
-    equal(tea42('2'), 't', 'Twos you lose!');
-    equal(tea42(102), '10t', 'Twos you lose!');
-    equal(tea42('m2'), 'mt', 'Twos you lose!');
-    equal(tea42('pre2ty'), 'pretty', 'Twos you lose!');
-    equal(tea42('t22222'), 'tttttt', 'Twos you lose!');
+  it('should work for a value of 2', function () {
+    deepEqual(fibsFizzBuzz(2), [1, 1]);
+  });
+
+  it('should work for a value of 5', function () {
+    deepEqual(fibsFizzBuzz(5), [1, 1, 2, 'Fizz', 'Buzz']);
+  });
+
+  it('should work for a value of 20', function () {
+    deepEqual(fibsFizzBuzz(20), [
+      1,
+      1,
+      2,
+      'Fizz',
+      'Buzz',
+      8,
+      13,
+      'Fizz',
+      34,
+      'Buzz',
+      89,
+      'Fizz',
+      233,
+      377,
+      'Buzz',
+      'Fizz',
+      1597,
+      2584,
+      4181,
+      'FizzBuzz',
+    ]);
   });
 });
 

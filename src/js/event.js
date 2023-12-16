cf732fffdf516cb80c001f1b08b9503a0da5ea7b
OlennikovYV <olennikovyv@mail.ru>
2023-12-16 20:21:36 +0500

    Character Counter

--- src/js/event.js
+++ src/js/event.js
@@ -1,30 +1,26 @@
-const fibsFizzBuzz = function (n) {
-  const arrayFibonacci = [];
-  let [prev, next] = [0, 1];
+function validateWord(s) {
+  let repeatChar;
+  const countChars = {};
 
-  for (let i = 1; i <= n; i++) {
-    [prev, next] = [next, prev + next];
+  s.toLowerCase()
+    .split('')
+    .forEach(char => (countChars[char] = (countChars[char] || 0) + 1));
 
-    arrayFibonacci.push(
-      prev % 15 == 0
-        ? 'FizzBuzz'
-        : prev % 3 == 0
-        ? 'Fizz'
-        : prev % 5 == 0
-        ? 'Buzz'
-        : prev
-    );
+  for (let key in countChars) {
+    if (!repeatChar) {
+      repeatChar = countChars[key];
+      continue;
+    }
+
+    if (countChars[key] !== repeatChar) return false;
   }
 
-  return arrayFibonacci;
-};
+  return true;
+}
 
-console.log(fibsFizzBuzz(2));
-// [1, 1]
-
-console.log(fibsFizzBuzz(5));
-// [1, 1, 2, 'Fizz', 'Buzz']
-
-console.log(fibsFizzBuzz(20));
-// [1, 1, 2, 'Fizz', 'Buzz', 8, 13, 'Fizz', 34, 'Buzz',
-// 89, 'Fizz', 233, 377, 'Buzz', 'Fizz', 1597, 2584, 4181, 'FizzBuzz']
+console.log(validateWord('abcabc')); // true
+console.log(validateWord('Abcabc')); // true
+console.log(validateWord('abc123')); // true
+console.log(validateWord('abcabcd')); // false
+console.log(validateWord('abc!abc!')); // true
+console.log(validateWord('abc:abc')); // false
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Fibonacci's FizzBuzz`, function () {
+describe(`Character Counter`, function () {
   const equal = chai.assert.equal;
   const notEqual = chai.assert.notEqual;
   const deepEqual = chai.assert.deepEqual;
@@ -13,37 +13,13 @@ describe(`Fibonacci's FizzBuzz`, function () {
   const isNotEmpty = chai.assert.isNotEmpty;
   const lengthOf = chai.assert.lengthOf;
 
-  it('should work for a value of 2', function () {
-    deepEqual(fibsFizzBuzz(2), [1, 1]);
-  });
-
-  it('should work for a value of 5', function () {
-    deepEqual(fibsFizzBuzz(5), [1, 1, 2, 'Fizz', 'Buzz']);
-  });
-
-  it('should work for a value of 20', function () {
-    deepEqual(fibsFizzBuzz(20), [
-      1,
-      1,
-      2,
-      'Fizz',
-      'Buzz',
-      8,
-      13,
-      'Fizz',
-      34,
-      'Buzz',
-      89,
-      'Fizz',
-      233,
-      377,
-      'Buzz',
-      'Fizz',
-      1597,
-      2584,
-      4181,
-      'FizzBuzz',
-    ]);
+  it('Tests', function () {
+    equal(validateWord('abcabc'), true, 'The word was: "abcabc" \n');
+    equal(validateWord('Abcabc'), true, 'The word was: "Abcabc" \n');
+    equal(validateWord('abc123'), true, 'The word was: "abc123" \n');
+    equal(validateWord('abcabcd'), false, 'The word was: "abcabcd" \n');
+    equal(validateWord('abc!abc!'), true, 'The word was: "abc!abc!" \n');
+    equal(validateWord('abc:abc'), false, 'The word was: "abc:abc" \n');
   });
 });
 

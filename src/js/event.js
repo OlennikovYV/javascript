5cac40f09e5e44a6b996e8a7b7930e4e8cdc4efb
OlennikovYV <olennikovyv@mail.ru>
2023-12-17 18:35:05 +0500

    The Ladies of ENIAC

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,33 @@
-function validateWord(s) {
-  return s.length % new Set(s.toLowerCase()).size === 0;
+function radLadies(name) {
+  return name.replace(/[^a-z !]/gi, '').toUpperCase();
 }
 
-console.log(validateWord('abcabc')); // true
-console.log(validateWord('Abcabc')); // true
-console.log(validateWord('abc123')); // true
-console.log(validateWord('abcabcd')); // false
-console.log(validateWord('abc!abc!')); // true
-console.log(validateWord('abc:abc')); // false
+console.log(
+  radLadies('k?%35a&&/y@@@£5599 m93753&$$$c$n///79u??@@%l?975$t?%5y%&$3$1!')
+);
+// 'KAY MCNULTY!'
+
+console.log(
+  radLadies(
+    '9?9?9?m335%$£@a791%&$r$$$l£@53$&y&n%$5@ $£5577w&7e931%s$£c$o%%%f351f??%!%%'
+  )
+);
+// 'MARLYN WESCOFF!'
+console.log(
+  radLadies(
+    '%&$557f953//1/$£@%r%935$$£a@£3111$@???%n???5 $%157b%///$i%55&31£@l?%&$$a%@£$s5757!$$%%%%53'
+  )
+);
+// 'FRAN BILAS!'
+console.log(
+  radLadies(
+    '///$%&£$553791£r357%??@$%u?$%@7993111£@$%t£$h3% 3$£l$311i3%@?&c3£h%&t&&?%11e%$?@11957r79%£&£m$$a55n1!111%%'
+  )
+);
+// 'RUTH LICHTERMAN!'
+console.log(
+  radLadies(
+    '??£@%&a5d15??e599713%l%%e%75913 1£$%&@g@£%o&$@13l5d11s$%&t15i9n&5%%@%e@£$!£%$£'
+  )
+);
+// 'ADELE GOLDSTINE!'
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Character Counter`, function () {
+describe(`The Ladies of ENIAC`, function () {
   const equal = chai.assert.equal;
   const notEqual = chai.assert.notEqual;
   const deepEqual = chai.assert.deepEqual;
@@ -14,12 +14,36 @@ describe(`Character Counter`, function () {
   const lengthOf = chai.assert.lengthOf;
 
   it('Tests', function () {
-    equal(validateWord('abcabc'), true, 'The word was: "abcabc" \n');
-    equal(validateWord('Abcabc'), true, 'The word was: "Abcabc" \n');
-    equal(validateWord('abc123'), true, 'The word was: "abc123" \n');
-    equal(validateWord('abcabcd'), false, 'The word was: "abcabcd" \n');
-    equal(validateWord('abc!abc!'), true, 'The word was: "abc!abc!" \n');
-    equal(validateWord('abc:abc'), false, 'The word was: "abc:abc" \n');
+    equal(
+      radLadies(
+        'k?%35a&&/y@@@£5599 m93753&$$$c$n///79u??@@%l?975$t?%5y%&$3$1!'
+      ),
+      'KAY MCNULTY!'
+    );
+    equal(
+      radLadies(
+        '9?9?9?m335%$£@a791%&$r$$$l£@53$&y&n%$5@ $£5577w&7e931%s$£c$o%%%f351f??%!%%'
+      ),
+      'MARLYN WESCOFF!'
+    );
+    equal(
+      radLadies(
+        '%&$557f953//1/$£@%r%935$$£a@£3111$@???%n???5 $%157b%///$i%55&31£@l?%&$$a%@£$s5757!$$%%%%53'
+      ),
+      'FRAN BILAS!'
+    );
+    equal(
+      radLadies(
+        '///$%&£$553791£r357%??@$%u?$%@7993111£@$%t£$h3% 3$£l$311i3%@?&c3£h%&t&&?%11e%$?@11957r79%£&£m$$a55n1!111%%'
+      ),
+      'RUTH LICHTERMAN!'
+    );
+    equal(
+      radLadies(
+        '??£@%&a5d15??e599713%l%%e%75913 1£$%&@g@£%o&$@13l5d11s$%&t15i9n&5%%@%e@£$!£%$£'
+      ),
+      'ADELE GOLDSTINE!'
+    );
   });
 });
 

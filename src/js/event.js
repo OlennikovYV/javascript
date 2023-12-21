300cf5da649617e486349211ee79ab1825ca6a1f
Yury Olennikov <OlennikovYV@mail.ru>
2023-12-21 08:47:20 +0500

    Convert Integer to Binary

--- src/js/event.js
+++ src/js/event.js
@@ -1,25 +1,9 @@
-function stockList(listOfArt, listOfCat) {
-  if (listOfArt.length === 0 || listOfCat.length === 0) return '';
-
-  return listOfCat
-    .map(categoryBook => {
-      return `(${categoryBook} : ${listOfArt
-        .filter(code => code[0] === categoryBook)
-        .reduce(
-          (sumBooks, countBooks) => sumBooks + Number(countBooks.split(' ')[1]),
-          0
-        )})`;
-    })
-    .join(' - ');
+function toBinary(n) {
+  return (n >>> 0).toString(2);
 }
 
-let b, c, res;
-b = ['BBAR 150', 'CDXE 515', 'BKWR 250', 'BTSQ 890', 'DRTY 600'];
-c = ['A', 'B', 'C', 'D'];
-console.log(stockList(b, c));
-// '(A : 0) - (B : 1290) - (C : 515) - (D : 600)'
-
-b = ['ABAR 200', 'CDXE 500', 'BKWR 250', 'BTSQ 890', 'DRTY 600'];
-c = ['A', 'B'];
-console.log(stockList(b, c));
-// '(A : 200) - (B : 1140)'
+console.log(toBinary(2)); // '10'
+console.log(toBinary(3)); // '11'
+console.log(toBinary(-3)); // '11111111111111111111111111111101'
+console.log(toBinary(4)); // '100'
+console.log(toBinary(5)); // '101'
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Help the bookseller !`, function () {
+describe(`Convert Integer to Binary`, function () {
   const equal = chai.assert.equal;
   const notEqual = chai.assert.notEqual;
   const deepEqual = chai.assert.deepEqual;
@@ -14,16 +14,11 @@ describe(`Help the bookseller !`, function () {
   const lengthOf = chai.assert.lengthOf;
 
   it('test', () => {
-    let b, c, res;
-    b = ['BBAR 150', 'CDXE 515', 'BKWR 250', 'BTSQ 890', 'DRTY 600'];
-    c = ['A', 'B', 'C', 'D'];
-    res = '(A : 0) - (B : 1290) - (C : 515) - (D : 600)';
-    equal(stockList(b, c), res);
-
-    b = ['ABAR 200', 'CDXE 500', 'BKWR 250', 'BTSQ 890', 'DRTY 600'];
-    c = ['A', 'B'];
-    res = '(A : 200) - (B : 1140)';
-    equal(stockList(b, c), res);
+    equal(toBinary(2), '10');
+    equal(toBinary(3), '11');
+    equal(toBinary(-3), '11111111111111111111111111111101');
+    equal(toBinary(4), '100');
+    equal(toBinary(5), '101');
   });
 });
 

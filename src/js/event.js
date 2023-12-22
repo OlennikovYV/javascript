1794ed3b10009f68637791172b275304c9d0fb06
Yury Olennikov <OlennikovYV@mail.ru>
2023-12-22 09:30:58 +0500

    Absent vowel refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,5 @@
 function absentVowel(x) {
-  const vowels = {
-    A: 0,
-    E: 1,
-    I: 2,
-    O: 3,
-    U: 4,
-  };
-
-  for (let key of Object.keys(vowels)) {
-    if (!x.includes(key.toLocaleLowerCase())) return vowels[key];
-  }
+  return [...'aeiou'].findIndex(letter => !x.includes(letter));
 }
 
 console.log(absentVowel('John Doe hs seven red pples under his bsket'));

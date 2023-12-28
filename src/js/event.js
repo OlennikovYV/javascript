e173ad5ea05cf9987994465320d92431827fdc0f
Yury Olennikov <OlennikovYV@mail.ru>
2023-12-28 15:41:24 +0500

    Basics 03: Strings, Numbers and Calculation

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,34 @@
-function strToHash(str) {
-  const hash = {};
+function calculateString(st) {
+  let result;
+  const expression = st.replaceAll(/[^0-9\.+\-\*\/]/gi, '');
+  const operator = [...expression]
+    .filter(char => ['+', '-', '*', '/'].includes(char))
+    .join('');
+  let [value1, value2] = expression.split(operator);
 
-  if (str.length) {
-    const items = str.split(', ');
-    items.forEach(el => {
-      const [key, value] = el.split('=');
+  value1 = Number(value1);
+  value2 = Number(value2);
 
-      hash[key] = Number(value);
-    });
+  switch (operator) {
+    case '+':
+      result = Math.round(value1 + value2);
+      break;
+    case '-':
+      result = Math.round(value1 - value2);
+      break;
+    case '*':
+      result = Math.round(value1 * value2);
+      break;
+    case '/':
+      result = Math.round(value1 / value2);
+      break;
   }
 
-  return hash;
+  return result.toString();
 }
 
-console.log(strToHash('a=1, b=2, c=3, d=4')); //
-// {a: 1, b: 2, c: 3, d: 4}
-console.log(strToHash(''));
-// {}
+console.log(calculateString(';$%§fsdfsd235??df/sdfgf5gh.000kk0000')); // '47'
+console.log(calculateString('sdfsd23454sdf*2342')); // '54929268'
+console.log(calculateString('fsdfsd235???34.4554s4234df-sdfgf2g3h4j442')); // '-210908'
+console.log(calculateString('fsdfsd234.4554s4234df+sf234442')); // '234676'
+console.log(calculateString('a1a2b3c.c0c/a1a0b.cc00c')); // '12'
--- src/test/test.js
+++ src/test/test.js
@@ -1,7 +1,8 @@
 mocha.setup('bdd');
 
-describe(`Turn String Input into Hash`, function () {
+describe(`Basics 03: Strings, Numbers and Calculation`, function () {
   const equal = chai.assert.equal;
+  const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
   const deepEqual = chai.assert.deepEqual;
   const isDefined = chai.assert.isDefined;
@@ -13,14 +14,15 @@ describe(`Turn String Input into Hash`, function () {
   const isNotEmpty = chai.assert.isNotEmpty;
   const lengthOf = chai.assert.lengthOf;
 
-  it('should pass sample tests', () => {
-    deepEqual(strToHash('a=1, b=2, c=3, d=4'), {
-      a: 1,
-      b: 2,
-      c: 3,
-      d: 4,
-    });
-    deepEqual(strToHash(''), {});
+  it('basic tests', function () {
+    strictEqual(calculateString(';$%§fsdfsd235??df/sdfgf5gh.000kk0000'), '47');
+    strictEqual(calculateString('sdfsd23454sdf*2342'), '54929268');
+    strictEqual(
+      calculateString('fsdfsd235???34.4554s4234df-sdfgf2g3h4j442'),
+      '-210908'
+    );
+    strictEqual(calculateString('fsdfsd234.4554s4234df+sf234442'), '234676');
+    strictEqual(calculateString('a1a2b3c.c0c/a1a0b.cc00c'), '12');
   });
 });
 

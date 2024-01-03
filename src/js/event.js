f1e35b695610daee218d9a32dd63a404c5292ed1
OlennikovYV <olennikovyv@mail.ru>
2024-01-03 13:23:54 +0500

    How sexy is your name?

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,63 @@
-String.prototype.toCents = function () {
-  return /^\$\d+\.\d\d$/.test(this) ? Number(this.replace(/[$.]/g, '')) : null;
+SCORES = {
+  A: 100,
+  B: 14,
+  C: 9,
+  D: 28,
+  E: 145,
+  F: 12,
+  G: 3,
+  H: 10,
+  I: 200,
+  J: 100,
+  K: 114,
+  L: 100,
+  M: 25,
+  N: 450,
+  O: 80,
+  P: 2,
+  Q: 12,
+  R: 400,
+  S: 113,
+  T: 405,
+  U: 11,
+  V: 10,
+  W: 10,
+  X: 3,
+  Y: 210,
+  Z: 23,
 };
 
-console.log(''.toCents()); // null
-console.log('1'.toCents()); // null
-console.log('1.23'.toCents()); // null
-console.log('$1'.toCents()); // null
-console.log('$1.23'.toCents()); // 123
-console.log('$99.99'.toCents()); // 9999
-console.log('$12345678.90'.toCents()); // 1234567890
-console.log('$9.69'.toCents()); // 969
-console.log('$9.70'.toCents()); // 970
-console.log('$9.71'.toCents()); // 971
-console.log('$1.23\n'.toCents()); // null
-console.log('\n$1.23'.toCents()); // null
-console.log('$0.69'.toCents()); // 69
-console.log('$9.69$4.3.7'.toCents()); // null
-console.log('$9.692'.toCents()); // null
+function sexyName(name) {
+  const sexyScores = name
+    .split('')
+    .reduce((scores, char) => scores + (SCORES[char.toUpperCase()] || 0), 0);
+
+  if (sexyScores <= 60) return 'NOT TOO SEXY';
+  if (sexyScores <= 300) return 'PRETTY SEXY';
+  if (sexyScores <= 599) return 'VERY SEXY';
+
+  return 'THE ULTIMATE SEXIEST';
+}
+
+console.log(sexyName('GUV')); // 'NOT TOO SEXY'
+console.log(sexyName('PHUG')); // 'NOT TOO SEXY'
+console.log(sexyName('FFFFF')); // 'NOT TOO SEXY'
+console.log(sexyName('')); // 'NOT TOO SEXY'
+console.log(sexyName('PHUG')); // 'NOT TOO SEXY'
+console.log(sexyName('BOB')); // 'PRETTY SEXY'
+console.log(sexyName('JLJ')); // 'PRETTY SEXY'
+console.log(sexyName('HHHHHU')); // 'PRETTY SEXY'
+console.log(sexyName('BOB')); // 'PRETTY SEXY'
+console.log(sexyName('WWWWWU')); // 'PRETTY SEXY'
+console.log(sexyName('YOU')); // 'VERY SEXY'
+console.log(sexyName('FABIO')); // 'VERY SEXY'
+console.log(sexyName('ARUUUUUUUUU')); // 'VERY SEXY'
+console.log(sexyName('ROBBY')); // 'THE ULTIMATE SEXIEST'
+console.log(sexyName('SAMANTHA')); // 'THE ULTIMATE SEXIEST'
+console.log(sexyName('DONALD TRUMP')); // 'THE ULTIMATE SEXIEST'
+console.log(sexyName('BILL GATES')); // 'THE ULTIMATE SEXIEST'
+console.log(sexyName('SCARLETT JOHANSSON')); // 'THE ULTIMATE SEXIEST'
+console.log(sexyName('CODEWARS')); // 'THE ULTIMATE SEXIEST'
+console.log(sexyName('PAMELA ANDERSON')); // 'THE ULTIMATE SEXIEST'
+console.log(sexyName('Lxu')); // 'PRETTY SEXY'
+console.log(sexyName('wK l u hWIiwbZ')); // 'THE ULTIMATE SEXIEST'
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Regexp basics - parsing prices`, function () {
+describe(`How sexy is your name?`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -14,22 +14,36 @@ describe(`Regexp basics - parsing prices`, function () {
   const isNotEmpty = chai.assert.isNotEmpty;
   const lengthOf = chai.assert.lengthOf;
 
-  it('Testing for fixed tests', () => {
-    strictEqual(''.toCents(), null);
-    strictEqual('1'.toCents(), null);
-    strictEqual('1.23'.toCents(), null);
-    strictEqual('$1'.toCents(), null);
-    strictEqual('$1.23'.toCents(), 123);
-    strictEqual('$99.99'.toCents(), 9999);
-    strictEqual('$12345678.90'.toCents(), 1234567890);
-    strictEqual('$9.69'.toCents(), 969);
-    strictEqual('$9.70'.toCents(), 970);
-    strictEqual('$9.71'.toCents(), 971);
-    strictEqual('$1.23\n'.toCents(), null);
-    strictEqual('\n$1.23'.toCents(), null);
-    strictEqual('$0.69'.toCents(), 69);
-    strictEqual('$9.69$4.3.7'.toCents(), null);
-    strictEqual('$9.692'.toCents(), null);
+  it('Not too sexy!', () => {
+    equal(sexyName('GUV'), 'NOT TOO SEXY');
+    equal(sexyName('PHUG'), 'NOT TOO SEXY');
+    equal(sexyName('FFFFF'), 'NOT TOO SEXY');
+    equal(sexyName(''), 'NOT TOO SEXY');
+    equal(sexyName('PHUG'), 'NOT TOO SEXY');
+  });
+  it('Pretty sexy!', () => {
+    equal(sexyName('BOB'), 'PRETTY SEXY');
+    equal(sexyName('JLJ'), 'PRETTY SEXY');
+    equal(sexyName('HHHHHU'), 'PRETTY SEXY');
+    equal(sexyName('BOB'), 'PRETTY SEXY');
+    equal(sexyName('WWWWWU'), 'PRETTY SEXY');
+  });
+  it('Very sexy!', () => {
+    equal(sexyName('YOU'), 'VERY SEXY');
+    equal(sexyName('FABIO'), 'VERY SEXY');
+    equal(sexyName('ARUUUUUUUUU'), 'VERY SEXY');
+  });
+  it('The utlimate sexiest!', () => {
+    equal(sexyName('ROBBY'), 'THE ULTIMATE SEXIEST');
+    equal(sexyName('SAMANTHA'), 'THE ULTIMATE SEXIEST');
+    equal(sexyName('DONALD TRUMP'), 'THE ULTIMATE SEXIEST');
+    equal(sexyName('BILL GATES'), 'THE ULTIMATE SEXIEST');
+    equal(sexyName('SCARLETT JOHANSSON'), 'THE ULTIMATE SEXIEST');
+    equal(sexyName('CODEWARS'), 'THE ULTIMATE SEXIEST');
+    equal(sexyName('PAMELA ANDERSON'), 'THE ULTIMATE SEXIEST');
+  });
+  it('Different case', () => {
+    equal(sexyName('Lxu'), 'PRETTY SEXY');
   });
 });
 

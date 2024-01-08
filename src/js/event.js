8f5277af273dfb420412ad90e730b333696f9f6b
OlennikovYV <olennikovyv@mail.ru>
2024-01-08 20:11:27 +0500

    Greatest common divisor

--- src/js/event.js
+++ src/js/event.js
@@ -1,22 +1,13 @@
-function isOpposite(s1, s2) {
-  const convertCase = string =>
-    string
-      .split('')
-      .map(el =>
-        el === el.toUpperCase() ? el.toLowerCase() : el.toUpperCase()
-      )
-      .join('');
-
-  return s1.length === 0 && s1.length === 0
-    ? false
-    : s1 === convertCase(s2)
-    ? true
-    : false;
+function mygcd(x, y) {
+  if (y > 0) {
+    let k = x % y;
+    return mygcd(y, k);
+  } else {
+    return Math.abs(x);
+  }
 }
 
-console.log(isOpposite('ab', 'AB')); // true
-console.log(isOpposite('aB', 'Ab')); // true
-console.log(isOpposite('aBcd', 'AbCD')); // true
-console.log(isOpposite('aBcde', 'AbCD')); // false
-console.log(isOpposite('AB', 'Ab')); // false
-console.log(isOpposite('', '')); // false
+console.log(mygcd(30, 12)); // 6
+console.log(mygcd(36, 12)); // 12
+console.log(mygcd(8, 9)); // 1
+console.log(mygcd(1, 1)); // 1
--- src/test/test.js
+++ src/test/test.js
@@ -1,32 +1,25 @@
 mocha.setup('bdd');
 
-describe(
-  `They say that only the name is long enough to attract attention.` +
-    ` They also said that only a simple Kata will have someone to solve it.` +
-    ` This is a sadly story #1: Are they opposite?`,
-  function () {
-    const equal = chai.assert.equal;
-    const strictEqual = chai.assert.strictEqual;
-    const notEqual = chai.assert.notEqual;
-    const deepEqual = chai.assert.deepEqual;
-    const isDefined = chai.assert.isDefined;
-    const isNull = chai.assert.isNull;
-    const isTrue = chai.assert.isTrue;
-    const isFalse = chai.assert.isFalse;
-    const approximately = chai.assert.approximately;
-    const oneOf = chai.assert.oneOf;
-    const isNotEmpty = chai.assert.isNotEmpty;
-    const lengthOf = chai.assert.lengthOf;
+describe(`Greatest common divisor`, function () {
+  const equal = chai.assert.equal;
+  const strictEqual = chai.assert.strictEqual;
+  const notEqual = chai.assert.notEqual;
+  const deepEqual = chai.assert.deepEqual;
+  const isDefined = chai.assert.isDefined;
+  const isNull = chai.assert.isNull;
+  const isTrue = chai.assert.isTrue;
+  const isFalse = chai.assert.isFalse;
+  const approximately = chai.assert.approximately;
+  const oneOf = chai.assert.oneOf;
+  const isNotEmpty = chai.assert.isNotEmpty;
+  const lengthOf = chai.assert.lengthOf;
 
-    it('test', () => {
-      equal(isOpposite('ab', 'AB'), true);
-      equal(isOpposite('aB', 'Ab'), true);
-      equal(isOpposite('aBcd', 'AbCD'), true);
-      equal(isOpposite('aBcde', 'AbCD'), false);
-      equal(isOpposite('AB', 'Ab'), false);
-      equal(isOpposite('', ''), false);
-    });
-  }
-);
+  it('Example tests', () => {
+    strictEqual(mygcd(30, 12), 6);
+    strictEqual(mygcd(36, 12), 12);
+    strictEqual(mygcd(8, 9), 1);
+    strictEqual(mygcd(1, 1), 1);
+  });
+});
 
 mocha.run();

527ba816d1e3fc5da99cc7eb526942af1d3aa70a
Yury Olennikov <OlennikovYV@mail.ru>
2024-01-15 09:48:30 +0500

    Round by 0.5 steps

--- src/js/event.js
+++ src/js/event.js
@@ -1,54 +1,8 @@
-function nameFile(fmt, nbr, start) {
-  const result = [];
-
-  if (
-    nbr <= 0 ||
-    Number.isInteger(nbr) === false ||
-    Number.isInteger(start) === false
-  )
-    return [];
-
-  for (let i = start; i < start + nbr; i++) {
-    result.push(fmt.replaceAll(/<index_no>/g, i));
-  }
-
-  return result;
+function solution(n) {
+  return Math.round(n * 2) / 2;
 }
 
-console.log(nameFile('IMG <index_no>', 4, 1));
-// [
-//   'IMG 1',
-//   'IMG 2',
-//   'IMG 3',
-//   'IMG 4',
-// ]
-console.log(nameFile('image #<index_no>.jpg', 3, 7));
-// [
-//   'image #7.jpg',
-//   'image #8.jpg',
-//   'image #9.jpg',
-// ]
-console.log(nameFile('#<index_no> #<index_no>', 3, -2));
-// [
-//   '#-2 #-2',
-//   '#-1 #-1',
-//   '#0 #0',
-// ]
-console.log(nameFile('<file> number <index_no>', 5, 0));
-// [
-//   '<file> number 0',
-//   '<file> number 1',
-//   '<file> number 2',
-//   '<file> number 3',
-//   '<file> number 4',
-// ]
-console.log(nameFile('<file_no> number <index_no>', 2, -1));
-// [
-//   '<file_no> number -1',
-//   '<file_no> number 0',
-// ]
-console.log(nameFile('file', 2, 3)); // ['file', 'file']
-console.log(nameFile('<file_no> number <index_no>', -1, 0)); // []
-console.log(nameFile('file <index_no>', 2, 0.1)); // []
-console.log(nameFile('file <index_no>', 0.2, 0)); // []
-console.log(nameFile('file <index_no>', 0, 0)); // []
+console.log(solution(4.2)); // 4
+console.log(solution(4.4)); // 4.5
+console.log(solution(4.6)); // 4.5
+console.log(solution(4.8)); // 5
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Naming Files`, function () {
+describe(`Round by 0.5 steps`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -15,41 +15,11 @@ describe(`Naming Files`, function () {
   const lengthOf = chai.assert.lengthOf;
   const error = chai.assert.throws;
 
-  it('Description cases', function () {
-    deepEqual(nameFile('IMG <index_no>', 4, 1), [
-      'IMG 1',
-      'IMG 2',
-      'IMG 3',
-      'IMG 4',
-    ]);
-    deepEqual(nameFile('image #<index_no>.jpg', 3, 7), [
-      'image #7.jpg',
-      'image #8.jpg',
-      'image #9.jpg',
-    ]);
-    deepEqual(nameFile('#<index_no> #<index_no>', 3, -2), [
-      '#-2 #-2',
-      '#-1 #-1',
-      '#0 #0',
-    ]);
-  });
-  it('More examples', function () {
-    deepEqual(nameFile('<file> number <index_no>', 5, 0), [
-      '<file> number 0',
-      '<file> number 1',
-      '<file> number 2',
-      '<file> number 3',
-      '<file> number 4',
-    ]);
-    deepEqual(nameFile('<file_no> number <index_no>', 2, -1), [
-      '<file_no> number -1',
-      '<file_no> number 0',
-    ]);
-    deepEqual(nameFile('file', 2, 3), ['file', 'file']);
-    deepEqual(nameFile('<file_no> number <index_no>', -1, 0), []);
-    deepEqual(nameFile('file <index_no>', 2, 0.1), []);
-    deepEqual(nameFile('file <index_no>', 0.2, 0), []);
-    deepEqual(nameFile('file <index_no>', 0, 0), []);
+  it('test', () => {
+    equal(solution(4.2), 4);
+    equal(solution(4.4), 4.5);
+    equal(solution(4.6), 4.5);
+    equal(solution(4.8), 5);
   });
 });
 

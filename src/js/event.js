64c9f6acf7e128e3588f1a5bf0cb58f9d5f04ff7
Yury Olennikov <OlennikovYV@mail.ru>
2024-01-16 11:02:17 +0500

    Tic-Tac-Toe Checker refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,36 +1,10 @@
-function checkWinner(arr) {
-  function Eq(a, b, c) {
-    return a != 0 && a == b && a == c;
-  }
-
-  for (let i = 0; i < 3; i++) {
-    if (Eq(arr[0][i], arr[1][i], arr[2][i])) {
-      return arr[0][i];
-    }
-    if (Eq(arr[i][0], arr[i][1], arr[i][2])) {
-      return arr[i][0];
-    }
-  }
-
-  if (Eq(arr[0][0], arr[1][1], arr[2][2])) {
-    return arr[0][0];
-  }
-  if (Eq(arr[0][2], arr[1][1], arr[2][0])) {
-    return arr[0][2];
-  }
-
-  return 0;
-}
-
 function isSolved(board) {
-  const winner = checkWinner(board);
+  board = board.join('-').replace(/,/g, '');
 
-  if (winner) {
-    if (winner === 1) return 1;
-    if (winner === 2) return 2;
-  }
+  if (/222|2...2...2|2....2....2|2..2..2/.test(board)) return 2;
+  if (/111|1...1...1|1....1....1|1..1..1/.test(board)) return 1;
 
-  if (board.toString().includes('0')) return -1;
+  if (/0/.test(board)) return -1;
 
   return 0;
 }

93e34b76ebe322af9db853cf040e9c8470810f1a
Yury Olennikov <OlennikovYV@mail.ru>
2024-01-25 09:01:00 +0500

    Grasshopper - If/else syntax debug

--- src/js/event.js
+++ src/js/event.js
@@ -1,20 +1,10 @@
-function arrayMash(array1, array2) {
-  return array1.reduce(
-    (resultArray, el, index) => resultArray.concat([el, array2[index]]),
-    []
-  );
+function checkAlive(health) {
+  if (health <= 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-console.log(arrayMash([1, 2, 3], ['a', 'b', 'c']));
-// [1, 'a', 2, 'b', 3, 'c']
-console.log(arrayMash([1, 2, 3, 4, 5], ['a', 'b', 'c', 'd', 'e']));
-// [1, 'a', 2, 'b', 3, 'c', 4, 'd', 5, 'e',]
-console.log(arrayMash([1, 1, 1, 1], [2, 2, 2, 2]));
-// [1, 2, 1, 2, 1, 2, 1, 2]
-console.log(arrayMash([1, 8, 'hello', 'dog'], ['fish', '2', 9, 10]));
-// [1, 'fish', 8, '2', 'hello', 9, 'dog', 10,]
-console.log(arrayMash([null, null, 4], [NaN, null, 'hello']));
-//  [null, NaN, null, null, 4, 'hello']
-console.log(arrayMash([1], [2])); // [1, 2]
-console.log(arrayMash(['h', 'l', 'o', 'o', 'l'], ['e', 'l', 'w', 'r', 'd']));
-//  ['h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd']
+console.log(checkAlive(5)); // true
+console.log(checkAlive(0)); // false
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Array Mash`, function () {
+describe(`Grasshopper - If/else syntax debug`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -15,52 +15,9 @@ describe(`Array Mash`, function () {
   const lengthOf = chai.assert.lengthOf;
   const error = chai.assert.throws;
 
-  it('Fixed tests', () => {
-    deepEqual(arrayMash([1, 2, 3], ['a', 'b', 'c']), [1, 'a', 2, 'b', 3, 'c']);
-    deepEqual(arrayMash([1, 2, 3, 4, 5], ['a', 'b', 'c', 'd', 'e']), [
-      1,
-      'a',
-      2,
-      'b',
-      3,
-      'c',
-      4,
-      'd',
-      5,
-      'e',
-    ]);
-    deepEqual(arrayMash([1, 1, 1, 1], [2, 2, 2, 2]), [1, 2, 1, 2, 1, 2, 1, 2]);
-    deepEqual(arrayMash([1, 8, 'hello', 'dog'], ['fish', '2', 9, 10]), [
-      1,
-      'fish',
-      8,
-      '2',
-      'hello',
-      9,
-      'dog',
-      10,
-    ]);
-    deepEqual(arrayMash([null, null, 4], [NaN, null, 'hello']), [
-      null,
-      NaN,
-      null,
-      null,
-      4,
-      'hello',
-    ]);
-    deepEqual(arrayMash([1], [2]), [1, 2]);
-    deepEqual(arrayMash(['h', 'l', 'o', 'o', 'l'], ['e', 'l', 'w', 'r', 'd']), [
-      'h',
-      'e',
-      'l',
-      'l',
-      'o',
-      'w',
-      'o',
-      'r',
-      'l',
-      'd',
-    ]);
+  it('test', () => {
+    equal(checkAlive(5), true);
+    equal(checkAlive(0), false);
   });
 });
 

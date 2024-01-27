ec0ca4af0f83f19c0e52cd628cdd2926edef978b
OlennikovYV <olennikovyv@mail.ru>
2024-01-27 17:21:35 +0500

    Sum of a Sequence [Hard-Core Version]

--- src/js/event.js
+++ src/js/event.js
@@ -1,10 +1,27 @@
-String.prototype.contains = function (string, caseInsensitive = false) {
-  return caseInsensitive
-    ? this.includes(string)
-    : this.toLowerCase().includes(string.toLowerCase());
-};
+function sequenceSum(begin, end, step) {
+  let sum = 0;
 
-console.log('Hello, World!'.contains('Hel')); // true
-console.log('Hello, World!'.contains('wor')); // true
-console.log('Hello, World!'.contains('wor', true)); // false
-console.log('Hello, World!'.contains('a')); // false
+  if (begin > end && step > 0) return 0;
+
+  if ((begin < 0 && end < 0 && step < 0) || (begin > end && step < 0)) {
+    for (let i = begin; i >= end; i += step) sum += i;
+  }
+
+  if (begin < end && step > 0) {
+    for (let i = begin; i <= end; i += step) sum += i;
+  }
+
+  return sum;
+}
+
+console.log(sequenceSum(2, 6, 2)); // 12
+console.log(sequenceSum(1, 5, 1)); // 15
+console.log(sequenceSum(1, 5, 3)); // 5
+console.log(sequenceSum(-1, -5, -3)); // -5
+console.log(sequenceSum(16, 15, 3)); // 0
+console.log(sequenceSum(-24, -2, 22)); // -26
+console.log(sequenceSum(-2, 4, 658)); // -2
+console.log(sequenceSum(780, 68515438, 5)); // 469436517521190
+console.log(sequenceSum(9383, 71418, 2)); // 1253127200
+console.log(sequenceSum(20, 67338879, 5)); // 453452442295970
+console.log(sequenceSum(27370145, -0, -6)); // 62427083461825
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Extending JavaScript Objects: Contains Method`, function () {
+describe(`Sum of a Sequence [Hard-Core Version]`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -16,10 +16,17 @@ describe(`Extending JavaScript Objects: Contains Method`, function () {
   const error = chai.assert.throws;
 
   it('test', () => {
-    equal('Hello, World!'.contains('Hel'), true);
-    equal('Hello, World!'.contains('wor'), true);
-    equal('Hello, World!'.contains('wor', true), false);
-    equal('Hello, World!'.contains('a'), false);
+    equal(sequenceSum(2, 6, 2), 12);
+    equal(sequenceSum(1, 5, 1), 15);
+    equal(sequenceSum(1, 5, 3), 5);
+    equal(sequenceSum(-1, -5, -3), -5);
+    equal(sequenceSum(16, 15, 3), 0);
+    equal(sequenceSum(-24, -2, 22), -26);
+    equal(sequenceSum(-2, 4, 658), -2);
+    equal(sequenceSum(780, 68515438, 5), 469436517521190);
+    equal(sequenceSum(9383, 71418, 2), 1253127200);
+    equal(sequenceSum(20, 67338879, 5), 453452442295970);
+    equal(sequenceSum(27370145, -0, -6), 62427083461825);
   });
 });
 

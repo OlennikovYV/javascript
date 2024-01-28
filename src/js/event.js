23a85d26a87022f5c33a8bdd0da8b4c68e714c40
OlennikovYV <olennikovyv@mail.ru>
2024-01-28 19:08:51 +0500

    All Star Code Challenge #20

--- src/js/event.js
+++ src/js/event.js
@@ -1,27 +1,14 @@
-function sequenceSum(begin, end, step) {
-  let sum = 0;
+function addArrays(array1, array2) {
+  if (array1.length !== array2.length) throw new Error('Error');
 
-  if (begin > end && step > 0) return 0;
-
-  if ((begin < 0 && end < 0 && step < 0) || (begin > end && step < 0)) {
-    for (let i = begin; i >= end; i += step) sum += i;
-  }
-
-  if (begin < end && step > 0) {
-    for (let i = begin; i <= end; i += step) sum += i;
-  }
-
-  return sum;
+  return array1.map((el, index) => el + array2[index]);
 }
 
-console.log(sequenceSum(2, 6, 2)); // 12
-console.log(sequenceSum(1, 5, 1)); // 15
-console.log(sequenceSum(1, 5, 3)); // 5
-console.log(sequenceSum(-1, -5, -3)); // -5
-console.log(sequenceSum(16, 15, 3)); // 0
-console.log(sequenceSum(-24, -2, 22)); // -26
-console.log(sequenceSum(-2, 4, 658)); // -2
-console.log(sequenceSum(780, 68515438, 5)); // 469436517521190
-console.log(sequenceSum(9383, 71418, 2)); // 1253127200
-console.log(sequenceSum(20, 67338879, 5)); // 453452442295970
-console.log(sequenceSum(27370145, -0, -6)); // 62427083461825
+console.log(addArrays([1, 2], [4, 5])); // [5, 7]
+console.log(addArrays(['a'], ['b'])); // ['ab']
+
+try {
+  addArrays([1, 2, 3], [4, 5]);
+} catch (error) {
+  console.log(error.message); // Error
+}
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Sum of a Sequence [Hard-Core Version]`, function () {
+describe(`All Star Code Challenge #20`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -16,17 +16,15 @@ describe(`Sum of a Sequence [Hard-Core Version]`, function () {
   const error = chai.assert.throws;
 
   it('test', () => {
-    equal(sequenceSum(2, 6, 2), 12);
-    equal(sequenceSum(1, 5, 1), 15);
-    equal(sequenceSum(1, 5, 3), 5);
-    equal(sequenceSum(-1, -5, -3), -5);
-    equal(sequenceSum(16, 15, 3), 0);
-    equal(sequenceSum(-24, -2, 22), -26);
-    equal(sequenceSum(-2, 4, 658), -2);
-    equal(sequenceSum(780, 68515438, 5), 469436517521190);
-    equal(sequenceSum(9383, 71418, 2), 1253127200);
-    equal(sequenceSum(20, 67338879, 5), 453452442295970);
-    equal(sequenceSum(27370145, -0, -6), 62427083461825);
+    deepEqual(addArrays([1, 2], [4, 5]), [5, 7]);
+    deepEqual(
+      addArrays(['a'], ['b']),
+      ['ab'],
+      'Should work with strings as well'
+    );
+    error(function () {
+      addArrays([1, 2, 3], [4, 5]);
+    }, 'Error');
   });
 });
 

719455fa52fe4088d3ccb1a42d93c627bc75c999
Yury Olennikov <OlennikovYV@mail.ru>
2024-01-31 11:00:04 +0500

    Difference of 2 refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,8 @@
 function twosDifference(input) {
-  const difference = [];
-  const inputData = [...new Set(input)].slice(0).sort((a, b) => a - b);
-
-  for (let i = 0; i < input.length - 1; i++) {
-    const temp = inputData.slice(i + 1);
-    const value = inputData[i];
-
-    if (temp.includes(value + 2)) difference.push([value, value + 2]);
-  }
-
-  return difference;
+  return input
+    .sort((a, b) => a - b)
+    .filter((digit, _, arr) => arr.indexOf(digit + 2) !== -1)
+    .map(digit => [digit, digit + 2]);
 }
 
 console.log(twosDifference([1, 2, 3, 4]));

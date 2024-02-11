79d2d138317094d957645fda813f247709c4863a
OlennikovYV <olennikovyv@mail.ru>
2024-02-11 19:44:15 +0500

    String reverse slicing 101

--- src/js/event.js
+++ src/js/event.js
@@ -1,22 +1,15 @@
-function convert(time) {
-  const addZero = (number, countZero) =>
-    String(number).padStart(countZero, '0');
+function reverseSlice(str) {
+  const reverseStr = [...str].reverse();
 
-  const hours = addZero(time.getHours(), 2);
-  const minutes = addZero(time.getMinutes(), 2);
-  const seconds = addZero(time.getSeconds(), 2);
-  const milliseconds = addZero(time.getMilliseconds(), 3);
-
-  return `${hours}:${minutes}:${seconds},${milliseconds}`;
+  return reverseStr.map((_, i, arr) => arr.slice(i).join(''));
 }
 
-console.log(convert(new Date(2016, 2, 8, 16, 42, 59))); // '16:42:59,000'
-console.log(convert(new Date(1951, 10, 31, 2, 2, 24, 399))); // '02:02:24,399'
-console.log(convert(new Date(1523, 5, 29, 23, 2, 2, 9))); // '23:02:02,009'
-console.log(convert(new Date(1, 1, 1, 1, 1, 1, 110))); // '01:01:01,110'
-console.log(convert(new Date(9999, 9, 9, 9, 9, 9, 999))); // '09:09:09,999'
-console.log(convert(new Date(2, 12, 30, 23, 59, 59, 875))); // '23:59:59,875'
-console.log(convert(new Date(1850, 12, 30, 13, 39, 19))); // '13:39:19,000'
-console.log(convert(new Date(1978, 3, 18, 12, 0, 0, 0))); // '12:00:00,000'
-console.log(convert(new Date(1850, 12, 30, 11, 11, 11, 123))); // '11:11:11,123'
-console.log(convert(new Date(1850, 12, 30, 0, 0, 0, 321))); // '00:00:00,321'
+console.log(reverseSlice('123')); // ['321', '21', '1']
+console.log(reverseSlice('abcde'));
+// [
+//   'edcba',
+//   'dcba',
+//   'cba',
+//   'ba',
+//   'a',
+// ]
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Time Converter: hours, minutes, seconds and milliseconds`, function () {
+describe(`String reverse slicing 101`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -15,17 +15,9 @@ describe(`Time Converter: hours, minutes, seconds and milliseconds`, function ()
   const lengthOf = chai.assert.lengthOf;
   const error = chai.assert.throws;
 
-  it('Sample tests', function () {
-    equal(convert(new Date(2016, 2, 8, 16, 42, 59)), '16:42:59,000');
-    equal(convert(new Date(1951, 10, 31, 2, 2, 24, 399)), '02:02:24,399');
-    equal(convert(new Date(1523, 5, 29, 23, 2, 2, 9)), '23:02:02,009');
-    equal(convert(new Date(1, 1, 1, 1, 1, 1, 110)), '01:01:01,110');
-    equal(convert(new Date(9999, 9, 9, 9, 9, 9, 999)), '09:09:09,999');
-    equal(convert(new Date(2, 12, 30, 23, 59, 59, 875)), '23:59:59,875');
-    equal(convert(new Date(1850, 12, 30, 13, 39, 19)), '13:39:19,000');
-    equal(convert(new Date(1978, 3, 18, 12, 0, 0, 0)), '12:00:00,000');
-    equal(convert(new Date(1850, 12, 30, 11, 11, 11, 123)), '11:11:11,123');
-    equal(convert(new Date(1850, 12, 30, 0, 0, 0, 321)), '00:00:00,321');
+  it('Basic tests', function () {
+    deepEqual(reverseSlice('123'), ['321', '21', '1']);
+    deepEqual(reverseSlice('abcde'), ['edcba', 'dcba', 'cba', 'ba', 'a']);
   });
 });
 

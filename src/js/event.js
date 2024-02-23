c7656e0503cf50e72f14d18d991b08ccdfbb47d2
OlennikovYV <olennikovyv@mail.ru>
2024-02-23 20:53:14 +0500

    Thinking & Testing : Incomplete string

--- src/js/event.js
+++ src/js/event.js
@@ -1,34 +1,28 @@
-let sorted = 0;
+function testit(s) {
+  let result = '';
 
-function sort(array) {
-  // Mark function as called through the global variable
-  sorted = sorted + 1;
-
-  // Transform the input data
-  for (let i = 0; i < array.length; i++) {
-    array[i] = 3 * array[i] + 2;
+  for (let i = 0; i < s.length; i += 2) {
+    const code1 = s.charCodeAt(i);
+    const code2 = s.charCodeAt(i + 1) || code1;
+    const number = (code1 + code2) / 2;
+    result += String.fromCharCode(Math.floor(number));
   }
 
-  // Remove middle value
-  let n = array.length,
-    mid = Math.floor(n / 2);
-  array = array.slice(0, mid).concat(array.slice(mid + 1));
-
-  // Sort the array
-  let isSorted = false;
-  for (let i = array.length - 1; i >= 0 && !isSorted; i--) {
-    isSorted = true;
-    for (let j = 0; j < i; j++) {
-      if (array[j] > array[j + 1]) {
-        swap(array, j, j + 1);
-        isSorted = false;
-      }
-    }
-  }
-
-  return array;
+  return result;
 }
 
-console.log(sorted); // 0
-sort([]);
-console.log(sorted); // 1
+//return s ?
+console.log(testit('')); // ""
+console.log(testit('a')); // "a"
+console.log(testit('b')); // "b"
+//return s.substr(0,1) ?
+console.log(testit('aa')); // "a"
+console.log(testit('ab')); // "a"
+console.log(testit('bc')); // "b"
+//return s.substr(0,s.length/2) ?
+console.log(testit('aaaa')); // "aa"
+console.log(testit('aaaaaa')); // "aaa"
+//click "Submit" try more testcase...
+console.log(testit('lwtflr')); // 'qmo'
+console.log(testit('hheelllloo')); // 'hello'
+console.log(testit('hheellllo')); // 'hello'
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Hoisting`, function () {
+describe(`Thinking & Testing : Incomplete string`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -16,12 +16,22 @@ describe(`Hoisting`, function () {
   const error = chai.assert.throws;
   const include = chai.assert.include;
 
-  it('should have the flag set to 0 before running sort', function () {
-    equal(sorted, 1);
-  });
-  it('should have the flag set to 1 after running sort', function () {
-    sort([]);
-    equal(sorted, 2);
+  it('Fixed tests', () => {
+    //return s ?
+    equal(testit(''), '', '');
+    equal(testit('a'), 'a', '');
+    equal(testit('b'), 'b', '');
+    //return s.substr(0,1) ?
+    equal(testit('aa'), 'a', '');
+    equal(testit('ab'), 'a', '');
+    equal(testit('bc'), 'b', '');
+    //return s.substr(0,s.length/2) ?
+    equal(testit('aaaa'), 'aa', '');
+    equal(testit('aaaaaa'), 'aaa', '');
+    //click "Submit" try more testcase...
+    equal(testit('lwtflr'), 'qmo', '');
+    equal(testit('hheelllloo'), 'hello', '');
+    equal(testit('hheellllo'), 'hello', '');
   });
 });
 

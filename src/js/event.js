127f29848d95f02ab8ccc7925674bb82be5802c6
OlennikovYV <olennikovyv@mail.ru>
2024-02-25 17:52:07 +0500

    Nice Array

--- src/js/event.js
+++ src/js/event.js
@@ -1,20 +1,10 @@
-function scoreThrows(radii) {
-  let isAllPointsLessFive = true;
-  const sumPoints = radii.reduce((sum, point) => {
-    if (point >= 5) isAllPointsLessFive = false;
-
-    if (point >= 5 && point <= 10) sum += 5;
-    if (point >= 0 && point < 5) sum += 10;
-
-    return sum;
-  }, 0);
-
-  return sumPoints + Number(isAllPointsLessFive && radii.length > 0) * 100;
+function isNice(arr) {
+  return (
+    Boolean(arr.length) &&
+    arr.every(el => arr.includes(el - 1) || arr.includes(el + 1))
+  );
 }
 
-console.log(scoreThrows([1, 5, 11])); // 15
-console.log(scoreThrows([15, 20, 30, 31, 32, 44, 100])); // 0
-console.log(scoreThrows([1, 2, 3, 4])); // 140
-console.log(scoreThrows([])); // 0, 'Empty list'
-console.log(scoreThrows([1, 2, 3, 4, 5, 6, 7, 8, 9])); // 65
-console.log(scoreThrows([0, 5, 10, 10.5, 4.5])); // 30
+console.log(isNice([2, 10, 9, 3])); // true
+console.log(isNice([3, 4, 5, 7])); // false
+console.log(isNice([])); // false
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Throwing Darts`, function () {
+describe(`Nice Array`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -16,13 +16,9 @@ describe(`Throwing Darts`, function () {
   const error = chai.assert.throws;
   const include = chai.assert.include;
 
-  it('test', () => {
-    equal(scoreThrows([1, 5, 11]), 15);
-    equal(scoreThrows([15, 20, 30, 31, 32, 44, 100]), 0);
-    equal(scoreThrows([1, 2, 3, 4]), 140);
-    equal(scoreThrows([]), 0, 'Empty list');
-    equal(scoreThrows([1, 2, 3, 4, 5, 6, 7, 8, 9]), 65);
-    equal(scoreThrows([0, 5, 10, 10.5, 4.5]), 30);
+  it('simple test', () => {
+    equal(isNice([2, 10, 9, 3]), true);
+    equal(isNice([3, 4, 5, 7]), false);
   });
 });
 

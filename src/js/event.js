5b3b2bce652f248a9146be0f3db0bea29ef67c66
Yury Olennikov <OlennikovYV@mail.ru>
2024-03-04 08:07:40 +0500

    Possibilities Array refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,7 +1,5 @@
 function isAllPossibilities(x) {
-  const arr = [...x].sort((a, b) => a - b);
-
-  return arr.every((el, i) => el === i);
+  return x.length > 0 ? x.every((_, i) => x.includes(i)) : false;
 }
 
 console.log(isAllPossibilities([0, 1, 2, 3])); // true

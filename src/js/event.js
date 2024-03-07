2e8575b5d48f832c32015cd58ee09fb533b70051
Yury Olennikov <OlennikovYV@mail.ru>
2024-03-07 08:27:54 +0500

    Array Array Array

--- src/js/event.js
+++ src/js/event.js
@@ -1,25 +1,42 @@
-const find = (a, e) => {
-  i = a.indexOf(e);
-  return i < 0 ? 'Not found' : a.indexOf(e);
-};
+function explode(x) {
+  const isIncludesNumber = x.some(el => typeof el == 'number');
+  const sumNumber = x.reduce(
+    (sum, el) => (typeof el == 'number' ? sum + el : sum),
+    0
+  );
 
-let array = [2, 3, 5, 7, 11];
+  return isIncludesNumber ? Array(sumNumber).fill(x) : 'Void!';
+}
 
-console.log(find(array, 5)); // 2
-console.log(find(array, 11)); // 4
-console.log(find(array, 3)); // 1
-console.log(find(array, 2)); // 0
-console.log(find(array, 7)); // 3
-console.log(find(array, 1)); // 'Not found'
-console.log(find(array, 8)); // 'Not found'
-
-array = [true, 'Hello World', false, 'Lorem Ipsum', 6, Math.PI];
-
-console.log(find(array, 'Hello World')); // 1
-console.log(find(array, 'lorem ipsum')); // 'Not found'
-console.log(find(array, 'Lorem Ipsum')); // 3
-console.log(find(array, false)); // 2
-console.log(find(array, true)); // 0
-console.log(find(array, Math.PI)); // 5
-console.log(find(array, 3.14)); // 'Not found'
-console.log(find(array, 6)); // 4
+console.log(explode([9, 3]));
+// [
+//   [9, 3],
+//   [9, 3],
+//   [9, 3],
+//   [9, 3],
+//   [9, 3],
+//   [9, 3],
+//   [9, 3],
+//   [9, 3],
+//   [9, 3],
+//   [9, 3],
+//   [9, 3],
+//   [9, 3],
+// ]
+console.log(explode(['a', 3]));
+// [
+//   ['a', 3],
+//   ['a', 3],
+//   ['a', 3],
+// ]
+console.log(explode([6, 'c']));
+// [
+//   [6, 'c'],
+//   [6, 'c'],
+//   [6, 'c'],
+//   [6, 'c'],
+//   [6, 'c'],
+//   [6, 'c'],
+// ]
+console.log(explode(['a', 'b'])); // 'Void!'
+console.log(explode(['a', 0])); // []
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Be Concise IV - Index of an element in an array`, function () {
+describe(`Array Array Array`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -16,27 +16,36 @@ describe(`Be Concise IV - Index of an element in an array`, function () {
   const error = chai.assert.throws;
   const include = chai.assert.include;
 
-  it('should behave as expected', () => {
-    let array = [2, 3, 5, 7, 11];
-
-    equal(find(array, 5), 2);
-    equal(find(array, 11), 4);
-    equal(find(array, 3), 1);
-    equal(find(array, 2), 0);
-    equal(find(array, 7), 3);
-    equal(find(array, 1), 'Not found');
-    equal(find(array, 8), 'Not found');
-
-    array = [true, 'Hello World', false, 'Lorem Ipsum', 6, Math.PI];
-
-    equal(find(array, 'Hello World'), 1);
-    equal(find(array, 'lorem ipsum'), 'Not found');
-    equal(find(array, 'Lorem Ipsum'), 3);
-    equal(find(array, false), 2);
-    equal(find(array, true), 0);
-    equal(find(array, Math.PI), 5);
-    equal(find(array, 3.14), 'Not found');
-    equal(find(array, 6), 4);
+  it('Testing for fixed tests', () => {
+    deepEqual(explode([9, 3]), [
+      [9, 3],
+      [9, 3],
+      [9, 3],
+      [9, 3],
+      [9, 3],
+      [9, 3],
+      [9, 3],
+      [9, 3],
+      [9, 3],
+      [9, 3],
+      [9, 3],
+      [9, 3],
+    ]);
+    deepEqual(explode(['a', 3]), [
+      ['a', 3],
+      ['a', 3],
+      ['a', 3],
+    ]);
+    deepEqual(explode([6, 'c']), [
+      [6, 'c'],
+      [6, 'c'],
+      [6, 'c'],
+      [6, 'c'],
+      [6, 'c'],
+      [6, 'c'],
+    ]);
+    deepEqual(explode(['a', 'b']), 'Void!');
+    deepEqual(explode(['a', 0]), []);
   });
 });
 

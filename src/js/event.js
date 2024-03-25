9583edb70f9cfb5f16bcabc021c953d89b29402e
Yury Olennikov <OlennikovYV@mail.ru>
2024-03-25 09:13:13 +0500

    The Lamp: Revisited

--- src/js/event.js
+++ src/js/event.js
@@ -1,30 +1,33 @@
-function ipToNum(ip) {
-  return ip.split('.').reduce(function (int, value) {
-    return int * 256 + +value;
-  });
+class Lamp {
+  constructor(color) {
+    this.colorValue = color;
+    this.on = false;
+  }
+
+  toggleSwitch() {
+    this.on = !this.on;
+  }
+
+  state() {
+    return this.on ? 'The lamp is on.' : 'The lamp is off.';
+  }
+
+  set color(color) {
+    this.colorValue = color;
+  }
+
+  get color() {
+    return this.colorValue;
+  }
 }
 
-function numToIp(num) {
-  const part1 = num & 255;
-  const part2 = (num >> 8) & 255;
-  const part3 = (num >> 16) & 255;
-  const part4 = (num >> 24) & 255;
+const myLamp = new Lamp('Blue');
 
-  return part4 + '.' + part3 + '.' + part2 + '.' + part1;
-}
-
-// 'ipToNum'
-console.log(ipToNum('192.168.1.1')); // 3232235777
-console.log(ipToNum('10.0.0.0')); // 167772160
-console.log(ipToNum('176.16.0.1')); // 2953838593
-// 'numToIp'
-console.log(numToIp(3232235777)); // '192.168.1.1'
-console.log(numToIp(167772160)); // '10.0.0.0'
-console.log(numToIp(2953838593)); // '176.16.0.1'
-// 'Combined'
-console.log(numToIp(ipToNum('192.168.1.1'))); // '192.168.1.1'
-console.log(numToIp(ipToNum('10.0.0.0'))); // '10.0.0.0'
-console.log(numToIp(ipToNum('176.16.0.1'))); // '176.16.0.1'
-console.log(ipToNum(numToIp(3232235777))); // 3232235777
-console.log(ipToNum(numToIp(167772160))); // 167772160
-console.log(ipToNum(numToIp(2953838593))); // 2953838593
+console.log(myLamp.color); // 'Blue'
+console.log(myLamp.on); // false
+console.log(myLamp.state()); // 'The lamp is off.'
+// now switch it on
+myLamp.toggleSwitch();
+console.log(myLamp.state()); // 'The lamp is on.'
+myLamp.color = 'White';
+console.log(myLamp.color);
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`IP Address to Number`, function () {
+describe(`The Lamp: Revisited`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -16,23 +16,15 @@ describe(`IP Address to Number`, function () {
   const error = chai.assert.throws;
   const include = chai.assert.include;
 
-  it('ipToNum', () => {
-    strictEqual(ipToNum('192.168.1.1'), 3232235777);
-    strictEqual(ipToNum('10.0.0.0'), 167772160);
-    strictEqual(ipToNum('176.16.0.1'), 2953838593);
-  });
-  it('numToIp', () => {
-    strictEqual(numToIp(3232235777), '192.168.1.1');
-    strictEqual(numToIp(167772160), '10.0.0.0');
-    strictEqual(numToIp(2953838593), '176.16.0.1');
-  });
-  it('Combined', () => {
-    strictEqual(numToIp(ipToNum('192.168.1.1')), '192.168.1.1');
-    strictEqual(numToIp(ipToNum('10.0.0.0')), '10.0.0.0');
-    strictEqual(numToIp(ipToNum('176.16.0.1')), '176.16.0.1');
-    strictEqual(ipToNum(numToIp(3232235777)), 3232235777);
-    strictEqual(ipToNum(numToIp(167772160)), 167772160);
-    strictEqual(ipToNum(numToIp(2953838593)), 2953838593);
+  it('Lamp', function () {
+    const myLamp = new Lamp('Blue');
+
+    equal(myLamp.color, 'Blue');
+    equal(myLamp.on, false);
+    equal(myLamp.state(), 'The lamp is off.');
+    // now switch it on
+    myLamp.toggleSwitch();
+    equal(myLamp.state(), 'The lamp is on.');
   });
 });
 

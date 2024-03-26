3b7986360345f144cb7799b8dfdf93d00989ad46
Yury Olennikov <OlennikovYV@mail.ru>
2024-03-26 08:31:27 +0500

    EAN Validation refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,11 @@
 function validateEAN(eanCode) {
-  const checkDigit = Number(eanCode.slice(-1));
-  const code = eanCode.slice(0, -1);
-  const sum = code.split('').reduce((sum, digit, index) => {
-    sum += Number(digit) * (index % 2 === 0 ? 1 : 3);
-    return sum;
-  }, 0);
-  let checkSum = 10 - (sum % 10);
-  checkSum = checkSum === 10 ? 0 : checkSum;
+  let sum = 0;
 
-  return checkDigit === checkSum;
+  for (let i = 0; i < 12; i++) {
+    sum += eanCode[i] * (i & 1 ? 3 : 1);
+  }
+
+  return (10 - (sum % 10)) % 10 === Number(eanCode[12]);
 }
 
 console.log(validateEAN('4003301018398')); // true

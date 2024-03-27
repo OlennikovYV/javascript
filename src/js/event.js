377f72f3a3511bc897f08f45a90e85782105790d
Yury Olennikov <OlennikovYV@mail.ru>
2024-03-27 08:18:39 +0500

    Is it a letter? refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,7 @@
 function isItLetter(character) {
-  return /[a-z]/i.test(character);
+  if (character.toLowerCase() === character.toUpperCase()) return false;
+
+  return true;
 }
 
 console.log(isItLetter('Z')); // true

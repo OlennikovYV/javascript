ba4c069be8681bfce721e962c3309a74e022165f
Yury Olennikov <OlennikovYV@mail.ru>
2024-03-27 08:16:57 +0500

    Is it a letter?

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,7 @@
-function validateEAN(eanCode) {
-  let sum = 0;
-
-  for (let i = 0; i < 12; i++) {
-    sum += eanCode[i] * (i & 1 ? 3 : 1);
-  }
-
-  return (10 - (sum % 10)) % 10 === Number(eanCode[12]);
+function isItLetter(character) {
+  return /[a-z]/i.test(character);
 }
 
-console.log(validateEAN('4003301018398')); // true
-console.log(validateEAN('9783815820865')); // true
-console.log(validateEAN('9783815820864')); // false
-console.log(validateEAN('9783827317100')); // true
+console.log(isItLetter('Z')); // true
+console.log(isItLetter('a')); // true
+console.log(isItLetter('1')); // false
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`EAN Validation`, function () {
+describe(`Is it a letter?`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -16,33 +16,10 @@ describe(`EAN Validation`, function () {
   const error = chai.assert.throws;
   const include = chai.assert.include;
 
-  it('Test 4003301018398', function () {
-    isTrue(
-      validateEAN('4003301018398'),
-      true,
-      'Check EAN-Code "4003301018398"'
-    );
-  });
-  it('Test 9783815820865', function () {
-    isTrue(
-      validateEAN('9783815820865'),
-      true,
-      'Check EAN-Code "9783815820865"'
-    );
-  });
-  it('Test 9783815820864', function () {
-    isFalse(
-      validateEAN('9783815820864'),
-      false,
-      'Check EAN-Code "9783815820864"'
-    );
-  });
-  it('Test 9783827317100', function () {
-    isTrue(
-      validateEAN('9783827317100'),
-      true,
-      'Check EAN-Code "9783827317100"'
-    );
+  it('example tests', function () {
+    equal(isItLetter('Z'), true, `'Z' is a letter`);
+    equal(isItLetter('a'), true, `'a' is a letter`);
+    equal(isItLetter('1'), false, `'1' is not a letter`);
   });
 });
 

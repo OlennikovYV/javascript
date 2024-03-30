113b4165d188769649d183e57a2594d38044e198
OlennikovYV <olennikovyv@mail.ru>
2024-03-30 21:34:27 +0500

    Maximum Triplet Sum (Array Series #7)

--- src/js/event.js
+++ src/js/event.js
@@ -1,15 +1,17 @@
-function isValid(idn) {
-  return /^[a-z$_]{1}[\w]*$/gi.test(idn);
+function maxTriSum(numbers) {
+  return [...new Set(numbers)]
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((sum, el) => sum + el, 0);
 }
 
-console.log(isValid('okay_ok1')); // true
-console.log(isValid('$ok')); // true
-console.log(isValid('___')); // true
-console.log(isValid('str_STR')); // true
-console.log(isValid('myIdentf')); // true
-
-console.log(isValid('1ok0okay')); // false
-console.log(isValid('!Ok')); // false
-console.log(isValid('')); // false
-console.log(isValid('str-str')); // false
-console.log(isValid('no no')); // false
+console.log(maxTriSum([3, 2, 6, 8, 2, 3])); // 17
+console.log(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5])); // 32
+console.log(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4])); // 18
+console.log(maxTriSum([-3, -27, -4, -2, -27, -2])); // -9
+console.log(maxTriSum([-14, -12, -7, -42, -809, -14, -12])); // -33
+console.log(maxTriSum([-13, -50, 57, 13, 67, -13, 57, 108, 67])); // 232
+console.log(maxTriSum([-7, 12, -7, 29, -5, 0, -7, 0, 0, 29])); // 41
+console.log(maxTriSum([-2, 0, 2])); // 0
+console.log(maxTriSum([-2, -4, 0, -9, 2])); // 0
+console.log(maxTriSum([-5, -1, -9, 0, 2])); // 1
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Is valid identifier?`, function () {
+describe(`Maximum Triplet Sum (Array Series #7)`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -16,20 +16,17 @@ describe(`Is valid identifier?`, function () {
   const error = chai.assert.throws;
   const include = chai.assert.include;
 
-  it('should return true for valid identifiers', function () {
-    equal(isValid('okay_ok1'), true, "Wrong result for 'okay_ok1'");
-    equal(isValid('$ok'), true, "Wrong result for '$ok'");
-    equal(isValid('___'), true, "Wrong result for '___'");
-    equal(isValid('str_STR'), true, "Wrong result for 'str_STR'");
-    equal(isValid('myIdentf'), true, "Wrong result for 'myIdentf'");
-  });
-
-  it('should return false for invalid identifiers', function () {
-    equal(isValid('1ok0okay'), false, "Wrong result for '1ok0okay'");
-    equal(isValid('!Ok'), false, "Wrong result for '!Ok'");
-    equal(isValid(''), false, 'Wrong result for an empty string');
-    equal(isValid('str-str'), false, "Wrong result for 'str-str'");
-    equal(isValid('no no'), false, "Wrong result for 'no no'");
+  it('Testing for fixed tests', () => {
+    equal(maxTriSum([3, 2, 6, 8, 2, 3]), 17);
+    equal(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5]), 32);
+    equal(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4]), 18);
+    equal(maxTriSum([-3, -27, -4, -2, -27, -2]), -9);
+    equal(maxTriSum([-14, -12, -7, -42, -809, -14, -12]), -33);
+    equal(maxTriSum([-13, -50, 57, 13, 67, -13, 57, 108, 67]), 232);
+    equal(maxTriSum([-7, 12, -7, 29, -5, 0, -7, 0, 0, 29]), 41);
+    equal(maxTriSum([-2, 0, 2]), 0);
+    equal(maxTriSum([-2, -4, 0, -9, 2]), 0);
+    equal(maxTriSum([-5, -1, -9, 0, 2]), 1);
   });
 });
 

bcb0a7e3a0f7c322d53c11a81cc4893d7afa7477
OlennikovYV <olennikovyv@mail.ru>
2024-03-31 15:49:35 +0500

    Zebulan's Nightmare

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,12 @@
-function maxTriSum(numbers) {
-  const [a, b, c] = [...new Set(numbers)].sort((a, b) => b - a);
-
-  return a + b + c;
+function zebulansNightmare(functionName) {
+  return functionName.replace(/_[a-z]/g, el => el[1].toUpperCase());
 }
 
-console.log(maxTriSum([3, 2, 6, 8, 2, 3])); // 17
-console.log(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5])); // 32
-console.log(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4])); // 18
-console.log(maxTriSum([-3, -27, -4, -2, -27, -2])); // -9
-console.log(maxTriSum([-14, -12, -7, -42, -809, -14, -12])); // -33
-console.log(maxTriSum([-13, -50, 57, 13, 67, -13, 57, 108, 67])); // 232
-console.log(maxTriSum([-7, 12, -7, 29, -5, 0, -7, 0, 0, 29])); // 41
-console.log(maxTriSum([-2, 0, 2])); // 0
-console.log(maxTriSum([-2, -4, 0, -9, 2])); // 0
-console.log(maxTriSum([-5, -1, -9, 0, 2])); // 1
+console.log(zebulansNightmare('camel_case')); // 'camelCase'
+console.log(zebulansNightmare('mark_as_issue')); // 'markAsIssue'
+console.log(zebulansNightmare('copy_paste_pep8')); // 'copyPastePep8'
+console.log(zebulansNightmare('goto_next_kata')); // 'gotoNextKata'
+console.log(zebulansNightmare('repeat')); // 'repeat'
+console.log(zebulansNightmare('trolling_is_fun')); // 'trollingIsFun'
+console.log(zebulansNightmare('why')); // 'why'
+console.log(zebulansNightmare('123_abc_def')); // '123AbcDef'
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Maximum Triplet Sum (Array Series #7)`, function () {
+describe(`Zebulan's Nightmare`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -17,16 +17,14 @@ describe(`Maximum Triplet Sum (Array Series #7)`, function () {
   const include = chai.assert.include;
 
   it('Testing for fixed tests', () => {
-    equal(maxTriSum([3, 2, 6, 8, 2, 3]), 17);
-    equal(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5]), 32);
-    equal(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4]), 18);
-    equal(maxTriSum([-3, -27, -4, -2, -27, -2]), -9);
-    equal(maxTriSum([-14, -12, -7, -42, -809, -14, -12]), -33);
-    equal(maxTriSum([-13, -50, 57, 13, 67, -13, 57, 108, 67]), 232);
-    equal(maxTriSum([-7, 12, -7, 29, -5, 0, -7, 0, 0, 29]), 41);
-    equal(maxTriSum([-2, 0, 2]), 0);
-    equal(maxTriSum([-2, -4, 0, -9, 2]), 0);
-    equal(maxTriSum([-5, -1, -9, 0, 2]), 1);
+    equal(zebulansNightmare('camel_case'), 'camelCase');
+    equal(zebulansNightmare('mark_as_issue'), 'markAsIssue');
+    equal(zebulansNightmare('copy_paste_pep8'), 'copyPastePep8');
+    equal(zebulansNightmare('goto_next_kata'), 'gotoNextKata');
+    equal(zebulansNightmare('repeat'), 'repeat');
+    equal(zebulansNightmare('trolling_is_fun'), 'trollingIsFun');
+    equal(zebulansNightmare('why'), 'why');
+    equal(zebulansNightmare('123_abc_def'), '123AbcDef');
   });
 });
 

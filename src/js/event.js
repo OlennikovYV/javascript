7f7cc3eebff16db0bf88cdca417347a5aa92e274
Yury Olennikov <OlennikovYV@mail.ru>
2024-04-04 08:24:55 +0500

    The Pony Express

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,22 @@
-function stringExpansion(s) {
-  return s.replace(/\d\D*/g, m =>
-    m.slice(1).replace(/./g, n => n.repeat(m[0]))
-  );
+function riders(stations) {
+  let countRider = 1;
+
+  stations.reduce((countMiles, miles) => {
+    countMiles += miles;
+    if (countMiles > 100) {
+      countRider++;
+      return miles;
+    }
+
+    return countMiles;
+  }, 0);
+
+  return countRider;
 }
 
-console.log(stringExpansion('1111')); // ''
-console.log(stringExpansion('')); // ''
-console.log(stringExpansion('3abc')); // 'aaabbbccc'
-console.log(stringExpansion('3D2a5d2f')); // 'DDDaadddddff'
-console.log(stringExpansion('0d0a0v0t0y')); // ''
-console.log(stringExpansion('3d332f2a')); // 'dddffaa'
-console.log(stringExpansion('abcde')); // 'abcde'
-console.log(stringExpansion('a2bcde')); // 'abbccddee'
+console.log(riders([18, 15])); // 1
+console.log(riders([50, 50])); // 1
+console.log(riders([43, 23, 40, 13])); // 2
+console.log(riders([33, 8, 16, 47, 30, 30, 46])); // 3
+console.log(riders([6, 24, 6, 8, 28, 8, 23, 47, 17, 29, 37, 18, 40, 49])); // 4
+console.log(riders([17, 16, 12, 7, 11, 8, 18, 47, 36, 17, 6, 23, 50, 42])); // 4
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Simple Simple Simple String Expansion`, function () {
+describe(`The Pony Express`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -16,15 +16,13 @@ describe(`Simple Simple Simple String Expansion`, function () {
   const error = chai.assert.throws;
   const include = chai.assert.include;
 
-  it('test', () => {
-    strictEqual(stringExpansion('1111'), '');
-    strictEqual(stringExpansion(''), '');
-    strictEqual(stringExpansion('3abc'), 'aaabbbccc');
-    strictEqual(stringExpansion('3D2a5d2f'), 'DDDaadddddff');
-    strictEqual(stringExpansion('0d0a0v0t0y'), '');
-    strictEqual(stringExpansion('3d332f2a'), 'dddffaa');
-    strictEqual(stringExpansion('abcde'), 'abcde');
-    strictEqual(stringExpansion('a2bcde'), 'abbccddee');
+  it('ex', function () {
+    equal(riders([18, 15]), 1);
+    equal(riders([50, 50]), 1);
+    equal(riders([43, 23, 40, 13]), 2);
+    equal(riders([33, 8, 16, 47, 30, 30, 46]), 3);
+    equal(riders([6, 24, 6, 8, 28, 8, 23, 47, 17, 29, 37, 18, 40, 49]), 4);
+    equal(riders([17, 16, 12, 7, 11, 8, 18, 47, 36, 17, 6, 23, 50, 42]), 4);
   });
 });
 

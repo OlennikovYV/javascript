51a05baf09211ac80f518410d5baf373b6b9fa9f
Yury Olennikov <OlennikovYV@mail.ru>
2024-04-08 08:19:52 +0500

    Sort deck of cards refactor 2

--- src/js/event.js
+++ src/js/event.js
@@ -1,21 +1,7 @@
 function sortCards(array) {
-  const weightCards = {
-    A: 0,
-    2: 1,
-    3: 2,
-    4: 3,
-    5: 4,
-    6: 5,
-    7: 6,
-    8: 7,
-    9: 8,
-    T: 9,
-    J: 10,
-    Q: 11,
-    K: 12,
-  };
+  const weightCards = 'A23456789TJQK';
 
-  return array.sort((a, b) => weightCards[a] - weightCards[b]);
+  return array.sort((a, b) => weightCards.indexOf(a) - weightCards.indexOf(b));
 }
 
 console.log(sortCards([3, 9, 'A', 5, 'T', 8, 2, 4, 'Q', 7, 'J', 6, 'K']));

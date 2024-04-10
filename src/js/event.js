e26bc49648038696e9b0a9a2296eff7f9b65c9fe
Yury Olennikov <OlennikovYV@mail.ru>
2024-04-10 08:33:05 +0500

    String basics

--- src/js/event.js
+++ src/js/event.js
@@ -1,60 +1,40 @@
-function isSameLanguage(list) {
-  return list.every(el => el.language === list[0].language);
+function getUsersIds(str) {
+  return str.split(', ').map(word => {
+    let result = word;
+
+    result = result.trim();
+
+    result = result.replace(/#/g, '');
+    if (result.startsWith('uid')) result = result.substring(3).toLowerCase();
+
+    result = result.trim();
+
+    return result;
+  });
 }
 
-const list1 = [
-  {
-    firstName: 'Daniel',
-    lastName: 'J.',
-    country: 'Aruba',
-    continent: 'Americas',
-    age: 42,
-    language: 'JavaScript',
-  },
-  {
-    firstName: 'Kseniya',
-    lastName: 'T.',
-    country: 'Belarus',
-    continent: 'Europe',
-    age: 22,
-    language: 'JavaScript',
-  },
-  {
-    firstName: 'Hanna',
-    lastName: 'L.',
-    country: 'Hungary',
-    continent: 'Europe',
-    age: 65,
-    language: 'JavaScript',
-  },
-];
+console.log(getUsersIds('uid12345')); // ['12345']
+console.log(getUsersIds('   uidabc  ')); // ['abc']
+console.log(getUsersIds('#uidswagger')); // ['swagger']
+console.log(getUsersIds('uidone, uidtwo')); // ['one', 'two']
+console.log(getUsersIds('uidCAPSLOCK')); // ['capslock']
 
-const list2 = [
-  {
-    firstName: 'Mariami',
-    lastName: 'G.',
-    country: 'Georgia',
-    continent: 'Europe',
-    age: 29,
-    language: 'Python',
-  },
-  {
-    firstName: 'Mia',
-    lastName: 'H.',
-    country: 'Germany',
-    continent: 'Europe',
-    age: 39,
-    language: 'Ruby',
-  },
-  {
-    firstName: 'Maria',
-    lastName: 'I.',
-    country: 'Greece',
-    continent: 'Europe',
-    age: 32,
-    language: 'C',
-  },
-];
-
-console.log(isSameLanguage(list1)); // true
-console.log(isSameLanguage(list2)); // false
+console.log(getUsersIds('uid##doublehashtag')); // ['doublehashtag']
+console.log(getUsersIds('  uidin name whitespace'));
+// ['in name whitespace',]
+console.log(getUsersIds('uidMultipleuid')); // ['multipleuid']
+console.log(getUsersIds('uid12 ab, uid#, uidMiXeDcHaRs'));
+// ['12 ab', '', 'mixedchars']
+console.log(getUsersIds(' uidT#e#S#t# ')); // ['test']
+console.log(
+  getUsersIds(
+    'uid123456, uidHAx0r, uidhAx#0R, uidHax0r  , uid 1234567890, uid12 34, uidhaX0r, uid12345  67, uid12345 67890, uid123456, uidm#y_iD'
+  )
+);
+// ['123456', 'hax0r', 'hax0r', 'hax0r', '1234567890', '12 34', 'hax0r', '12345  67', '12345 67890', '123456', 'my_id']
+console.log(
+  getUsersIds(
+    'uidhaX0#r, uid  1234567, uid12345  , uidUser, uid1234567 89#, uid123'
+  )
+);
+// ['hax0r', '1234567', '12345', 'user', '1234567 89', '123']
--- src/test/test.js
+++ src/test/test.js
@@ -1,84 +1,63 @@
 mocha.setup('bdd');
 
-describe(
-  `Coding Meetup #6 - Higher-Order Functions ` +
-    `Series - Can they code in the same language?`,
-  function () {
-    const equal = chai.assert.equal;
-    const strictEqual = chai.assert.strictEqual;
-    const notEqual = chai.assert.notEqual;
-    const deepEqual = chai.assert.deepEqual;
-    const isDefined = chai.assert.isDefined;
-    const isUndefined = chai.assert.isUndefined;
-    const isNull = chai.assert.isNull;
-    const isTrue = chai.assert.isTrue;
-    const isFalse = chai.assert.isFalse;
-    const approximately = chai.assert.approximately;
-    const oneOf = chai.assert.oneOf;
-    const isNotEmpty = chai.assert.isNotEmpty;
-    const lengthOf = chai.assert.lengthOf;
-    const error = chai.assert.throws;
-    const include = chai.assert.include;
+describe(`String basics`, function () {
+  const equal = chai.assert.equal;
+  const strictEqual = chai.assert.strictEqual;
+  const notEqual = chai.assert.notEqual;
+  const deepEqual = chai.assert.deepEqual;
+  const isDefined = chai.assert.isDefined;
+  const isUndefined = chai.assert.isUndefined;
+  const isNull = chai.assert.isNull;
+  const isTrue = chai.assert.isTrue;
+  const isFalse = chai.assert.isFalse;
+  const approximately = chai.assert.approximately;
+  const oneOf = chai.assert.oneOf;
+  const isNotEmpty = chai.assert.isNotEmpty;
+  const lengthOf = chai.assert.lengthOf;
+  const error = chai.assert.throws;
+  const include = chai.assert.include;
 
-    it('test', () => {
-      const list1 = [
-        {
-          firstName: 'Daniel',
-          lastName: 'J.',
-          country: 'Aruba',
-          continent: 'Americas',
-          age: 42,
-          language: 'JavaScript',
-        },
-        {
-          firstName: 'Kseniya',
-          lastName: 'T.',
-          country: 'Belarus',
-          continent: 'Europe',
-          age: 22,
-          language: 'JavaScript',
-        },
-        {
-          firstName: 'Hanna',
-          lastName: 'L.',
-          country: 'Hungary',
-          continent: 'Europe',
-          age: 65,
-          language: 'JavaScript',
-        },
-      ];
+  it('test', () => {
+    deepEqual(getUsersIds('uid12345'), ['12345']);
+    deepEqual(getUsersIds('   uidabc  '), ['abc']);
+    deepEqual(getUsersIds('#uidswagger'), ['swagger']);
+    deepEqual(getUsersIds('uidone, uidtwo'), ['one', 'two']);
+    deepEqual(getUsersIds('uidCAPSLOCK'), ['capslock']);
 
-      const list2 = [
-        {
-          firstName: 'Mariami',
-          lastName: 'G.',
-          country: 'Georgia',
-          continent: 'Europe',
-          age: 29,
-          language: 'Python',
-        },
-        {
-          firstName: 'Mia',
-          lastName: 'H.',
-          country: 'Germany',
-          continent: 'Europe',
-          age: 39,
-          language: 'Ruby',
-        },
-        {
-          firstName: 'Maria',
-          lastName: 'I.',
-          country: 'Greece',
-          continent: 'Europe',
-          age: 32,
-          language: 'C',
-        },
-      ];
-
-      isTrue(isSameLanguage(list1), true);
-      isFalse(isSameLanguage(list2), false);
-    });
-  }
-);
+    deepEqual(getUsersIds('uid##doublehashtag'), ['doublehashtag']);
+    deepEqual(getUsersIds('  uidin name whitespace'), ['in name whitespace']);
+    deepEqual(getUsersIds('uidMultipleuid'), ['multipleuid']);
+    deepEqual(getUsersIds('uid12 ab, uid#, uidMiXeDcHaRs'), [
+      '12 ab',
+      '',
+      'mixedchars',
+    ]);
+    deepEqual(getUsersIds(' uidT#e#S#t# '), ['test']);
+    deepEqual(
+      getUsersIds(
+        'uid123456, uidHAx0r, uidhAx#0R, uidHax0r  , uid 1234567890, uid12 34, uidhaX0r, uid12345  67, uid12345 67890, uid123456, uidm#y_iD'
+      ),
+      [
+        '123456',
+        'hax0r',
+        'hax0r',
+        'hax0r',
+        '1234567890',
+        '12 34',
+        'hax0r',
+        '12345  67',
+        '12345 67890',
+        '123456',
+        'my_id',
+      ]
+    );
+    deepEqual(
+      getUsersIds(
+        'uidhaX0#r, uid  1234567, uid12345  , uidUser, uid1234567 89#, uid123'
+      ),
+      ['hax0r', '1234567', '12345', 'user', '1234567 89', '123']
+    );
+  });
+});
 
 mocha.run();

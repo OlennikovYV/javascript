73ecefcfba79747592cc08832ad77c28bd435a94
Yury Olennikov <OlennikovYV@mail.ru>
2024-04-11 08:31:59 +0500

    Replace every nth

--- src/js/event.js
+++ src/js/event.js
@@ -1,33 +1,24 @@
-function getUsersIds(str) {
-  return str
-    .split(',')
-    .map(word =>
-      word.replace(/#/g, '').replace(/uid/, '').trim().toLowerCase()
-    );
+function replaceNth(text, n, oldValue, newValue) {
+  let count = 0;
+
+  return n < 1
+    ? text
+    : text.replace(new RegExp(oldValue, 'g'), char =>
+        ++count % n ? char : newValue
+      );
 }
 
-console.log(getUsersIds('uid12345')); // ['12345']
-console.log(getUsersIds('   uidabc  ')); // ['abc']
-console.log(getUsersIds('#uidswagger')); // ['swagger']
-console.log(getUsersIds('uidone, uidtwo')); // ['one', 'two']
-console.log(getUsersIds('uidCAPSLOCK')); // ['capslock']
-
-console.log(getUsersIds('uid##doublehashtag')); // ['doublehashtag']
-console.log(getUsersIds('  uidin name whitespace'));
-// ['in name whitespace',]
-console.log(getUsersIds('uidMultipleuid')); // ['multipleuid']
-console.log(getUsersIds('uid12 ab, uid#, uidMiXeDcHaRs'));
-// ['12 ab', '', 'mixedchars']
-console.log(getUsersIds(' uidT#e#S#t# ')); // ['test']
-console.log(
-  getUsersIds(
-    'uid123456, uidHAx0r, uidhAx#0R, uidHax0r  , uid 1234567890, uid12 34, uidhaX0r, uid12345  67, uid12345 67890, uid123456, uidm#y_iD'
-  )
-);
-// ['123456', 'hax0r', 'hax0r', 'hax0r', '1234567890', '12 34', 'hax0r', '12345  67', '12345 67890', '123456', 'my_id']
-console.log(
-  getUsersIds(
-    'uidhaX0#r, uid  1234567, uid12345  , uidUser, uid1234567 89#, uid123'
-  )
-);
-// ['hax0r', '1234567', '12345', 'user', '1234567 89', '123']
+console.log(replaceNth('Vader said: No, I am your father!', 2, 'a', 'o'));
+// 'Vader soid: No, I am your fother!'
+console.log(replaceNth('Vader said: No, I am your father!', 4, 'a', 'o'));
+// 'Vader said: No, I am your fother!'
+console.log(replaceNth('Vader said: No, I am your father!', 6, 'a', 'o'));
+// 'Vader said: No, I am your father!'
+console.log(replaceNth('Vader said: No, I am your father!', 0, 'a', 'o'));
+// 'Vader said: No, I am your father!'
+console.log(replaceNth('Vader said: No, I am your father!', -2, 'a', 'o'));
+// 'Vader said: No, I am your father!'
+console.log(replaceNth('Vader said: No, I am your father!', 1, 'i', 'y'));
+// 'Vader sayd: No, I am your father!'
+console.log(replaceNth('Luke cries: Noooooooooooooooo!', 6, 'o', 'i'));
+// 'Luke cries: Noooooioooooioooo!'
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`String basics`, function () {
+describe(`Replace every nth`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -18,44 +18,34 @@ describe(`String basics`, function () {
   const include = chai.assert.include;
 
   it('test', () => {
-    deepEqual(getUsersIds('uid12345'), ['12345']);
-    deepEqual(getUsersIds('   uidabc  '), ['abc']);
-    deepEqual(getUsersIds('#uidswagger'), ['swagger']);
-    deepEqual(getUsersIds('uidone, uidtwo'), ['one', 'two']);
-    deepEqual(getUsersIds('uidCAPSLOCK'), ['capslock']);
-
-    deepEqual(getUsersIds('uid##doublehashtag'), ['doublehashtag']);
-    deepEqual(getUsersIds('  uidin name whitespace'), ['in name whitespace']);
-    deepEqual(getUsersIds('uidMultipleuid'), ['multipleuid']);
-    deepEqual(getUsersIds('uid12 ab, uid#, uidMiXeDcHaRs'), [
-      '12 ab',
-      '',
-      'mixedchars',
-    ]);
-    deepEqual(getUsersIds(' uidT#e#S#t# '), ['test']);
-    deepEqual(
-      getUsersIds(
-        'uid123456, uidHAx0r, uidhAx#0R, uidHax0r  , uid 1234567890, uid12 34, uidhaX0r, uid12345  67, uid12345 67890, uid123456, uidm#y_iD'
-      ),
-      [
-        '123456',
-        'hax0r',
-        'hax0r',
-        'hax0r',
-        '1234567890',
-        '12 34',
-        'hax0r',
-        '12345  67',
-        '12345 67890',
-        '123456',
-        'my_id',
-      ]
+    equal(
+      replaceNth('Vader said: No, I am your father!', 2, 'a', 'o'),
+      'Vader soid: No, I am your fother!'
     );
-    deepEqual(
-      getUsersIds(
-        'uidhaX0#r, uid  1234567, uid12345  , uidUser, uid1234567 89#, uid123'
-      ),
-      ['hax0r', '1234567', '12345', 'user', '1234567 89', '123']
+    equal(
+      replaceNth('Vader said: No, I am your father!', 4, 'a', 'o'),
+      'Vader said: No, I am your fother!'
+    );
+    equal(
+      replaceNth('Vader said: No, I am your father!', 6, 'a', 'o'),
+      'Vader said: No, I am your father!'
+    );
+    equal(
+      replaceNth('Vader said: No, I am your father!', 0, 'a', 'o'),
+      'Vader said: No, I am your father!'
+    );
+    equal(
+      replaceNth('Vader said: No, I am your father!', -2, 'a', 'o'),
+      'Vader said: No, I am your father!'
+    );
+    equal(
+      replaceNth('Vader said: No, I am your father!', 1, 'i', 'y'),
+      'Vader sayd: No, I am your father!'
+    );
+
+    equal(
+      replaceNth('Luke cries: Noooooooooooooooo!', 6, 'o', 'i'),
+      'Luke cries: Noooooioooooioooo!'
     );
   });
 });

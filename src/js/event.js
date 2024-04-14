0b33923a1e7d3e443dadafdafd0914904eb571b0
OlennikovYV <olennikovyv@mail.ru>
2024-04-14 16:49:23 +0500

    Array2Binary addition refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,8 @@
 function arr2bin(arr) {
-  const onlyInt = arr.filter(el => Number.isInteger(el));
-
-  if (onlyInt.length < arr.length) return false;
-
-  return onlyInt.reduce((sum, number) => sum + number, 0).toString(2);
+  return (
+    arr.every(Number.isInteger) &&
+    arr.reduce((sum, number) => sum + number, 0).toString(2)
+  );
 }
 
 console.log(arr2bin([1, 2])); // '11'

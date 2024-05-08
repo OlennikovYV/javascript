2d15666d12b9768f8451b6be162ec209251f02aa
Yury Olennikov <OlennikovYV@mail.ru>
2024-05-08 08:38:43 +0500

    Nth power rules them all! refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,5 @@
 function modifiedSum(a, n) {
-  const sumOfPower = a.reduce((sum, number) => sum + Math.pow(number, n), 0);
-  const sum = a.reduce((sum, number) => sum + number, 0);
-
-  return sumOfPower - sum;
+  return a.reduce((sum, number) => sum + Math.pow(number, n) - number, 0);
 }
 
 console.log(modifiedSum([1, 2, 3], 3)); // 30

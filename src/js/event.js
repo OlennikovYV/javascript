1ee5eecfca10a9a54abc29e2d454272b91bfd4f7
Yury Olennikov <OlennikovYV@mail.ru>
2024-05-14 08:35:49 +0500

    Largest Square Inside A Circle

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,5 @@
-function validPass(password) {
-  return /^(?=.*[a-z])(?=.*\d)[a-z\d]{4,19}$/gi.test(password)
-    ? 'VALID'
-    : 'INVALID';
-}
+const areaLargestSquare = r => 2 * r ** 2;
 
-console.log(validPass('Username123')); // 'VALID'
-console.log(validPass('Username')); // 'INVALID'
-console.log(validPass('1Username')); // 'VALID'
-console.log(validPass('123')); // 'INVALID'
-console.log(validPass('a12')); // 'INVALID'
-console.log(validPass('1234')); // 'INVALID'
-console.log(validPass('a123')); // 'VALID'
-console.log(validPass('Username123!')); // 'INVALID'
-console.log(validPass('passw0rdIsntTooLong')); // 'VALID'
-console.log(validPass('passw0rd1CharTooLong')); // 'INVALID'
+console.log(areaLargestSquare(5)); // 50
+console.log(areaLargestSquare(7)); // 98
+console.log(areaLargestSquare(15)); // 450
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-describe(`Validate my Password`, function () {
+describe(`Largest Square Inside A Circle`, function () {
   const equal = chai.assert.equal;
   const strictEqual = chai.assert.strictEqual;
   const notEqual = chai.assert.notEqual;
@@ -18,17 +18,10 @@ describe(`Validate my Password`, function () {
   const noError = chai.assert.doesNotThrow;
   const include = chai.assert.include;
 
-  it('test', () => {
-    equal(validPass('Username123'), 'VALID');
-    equal(validPass('Username'), 'INVALID');
-    equal(validPass('1Username'), 'VALID');
-    equal(validPass('123'), 'INVALID');
-    equal(validPass('a12'), 'INVALID');
-    equal(validPass('1234'), 'INVALID');
-    equal(validPass('a123'), 'VALID');
-    equal(validPass('Username123!'), 'INVALID');
-    equal(validPass('passw0rdIsntTooLong'), 'VALID');
-    equal(validPass('passw0rd1CharTooLong'), 'INVALID');
+  it('Basic tests', () => {
+    equal(areaLargestSquare(5), 50);
+    equal(areaLargestSquare(7), 98);
+    equal(areaLargestSquare(15), 450);
   });
 });
 

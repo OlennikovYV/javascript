d0efa70e9ec6d71da3336da32da7f86f8dadd66f
Yury Olennikov <OlennikovYV@mail.ru>
2024-05-27 08:59:30 +0500

    Find Added refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,26 +1,9 @@
 function findAdded(st1, st2) {
-  const calculateCountNumber = list =>
-    list.reduce((counts, number) => {
-      counts[number] = (counts[number] || 0) + 1;
-      return counts;
-    }, {});
-  let listNumbers = '';
-  const countNumbers1 = calculateCountNumber([...st1]);
-  const countNumbers2 = calculateCountNumber([...st2]);
-
-  for (let key in countNumbers2) {
-    if (countNumbers1[key] == undefined) {
-      listNumbers += key.repeat(countNumbers2[key]);
-    }
+  for (let i = 0; i < st1.length; i++) {
+    st2 = st2.replace(st1[i], '');
   }
 
-  for (let key in countNumbers1) {
-    let countNumbersFind = countNumbers2[key] - countNumbers1[key];
-
-    if (countNumbersFind > 0) listNumbers += key.repeat(countNumbersFind);
-  }
-
-  return [...listNumbers].sort().join('');
+  return st2.split('').sort().join('');
 }
 
 console.log(findAdded('44554466', '447554466')); // '7'

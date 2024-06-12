dde0003143d18ecf3197d2d912b66650aeaa4df0
OlennikovYV <olennikovyv@mail.ru>
2024-06-12 17:27:02 +0500

    Count the number of cubes with paint on

--- src/js/event.js
+++ src/js/event.js
@@ -1,38 +1,9 @@
-const sumMoney = student =>
-  student.fives * 5 + student.tens * 10 + student.twenties * 20;
+function countSquares(cuts) {
+  const pieces = cuts + 1;
 
-function mostMoney(students) {
-  students.sort((a, b) => sumMoney(b) - sumMoney(a));
-  if (students.length > 1 && sumMoney(students[0]) == sumMoney(students[1]))
-    return 'all';
-  return students[0].name;
+  return cuts == 0 ? 1 : 2 * (pieces ** 2 + (pieces - 2) * (2 * pieces - 2));
 }
 
-class Student {
-  constructor(name, fives, tens, twenties) {
-    this.name = name;
-    this.fives = fives;
-    this.tens = tens;
-    this.twenties = twenties;
-  }
-}
-
-const andy = new Student('Andy', 0, 0, 2);
-const stephen = new Student('Stephen', 0, 4, 0);
-const eric = new Student('Eric', 8, 1, 0);
-const david = new Student('David', 2, 0, 1);
-const phil = new Student('Phil', 0, 2, 1);
-const cam = new Student('Cameron', 2, 2, 0);
-const geoff = new Student('Geoff', 0, 3, 0);
-
-// "What happens if one student has the most money?"
-console.log(mostMoney([andy, stephen, eric, david, phil])); // 'Eric'
-console.log(mostMoney([cam, geoff, andy, stephen, eric, david, phil])); // 'Eric'
-
-// "What happens if there is only one student?"
-console.log(mostMoney([andy])); // 'Andy'
-console.log(mostMoney([stephen])); // 'Stephen'
-
-// "What happens if all students have the same amount of money?"
-console.log(mostMoney([cam, geoff])); // 'all'
-console.log(mostMoney([david, cam, geoff])); // 'all'
+console.log(countSquares(5)); // 152
+console.log(countSquares(16)); // 1538
+console.log(countSquares(23)); // 3176
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-const nameTask = `Who has the most money?`;
+const nameTask = `Count the number of cubes with paint on`;
 
 describe(nameTask, function () {
   const equal = chai.assert.equal;
@@ -25,29 +25,10 @@ describe(nameTask, function () {
     Например: [{b:1},{a:2}] === [{b:1},{a:2}] */
   const includeDeepOrderedMembers = chai.assert.includeDeepOrderedMembers;
 
-  it('Basic tests', () => {
-    const andy = new Student('Andy', 0, 0, 2);
-    const stephen = new Student('Stephen', 0, 4, 0);
-    const eric = new Student('Eric', 8, 1, 0);
-    const david = new Student('David', 2, 0, 1);
-    const phil = new Student('Phil', 0, 2, 1);
-    const cam = new Student('Cameron', 2, 2, 0);
-    const geoff = new Student('Geoff', 0, 3, 0);
-
-    // "What happens if one student has the most money?"
-    strictEqual(mostMoney([andy, stephen, eric, david, phil]), 'Eric');
-    strictEqual(
-      mostMoney([cam, geoff, andy, stephen, eric, david, phil]),
-      'Eric'
-    );
-
-    // "What happens if there is only one student?"
-    strictEqual(mostMoney([andy]), 'Andy');
-    strictEqual(mostMoney([stephen]), 'Stephen');
-
-    // "What happens if all students have the same amount of money?"
-    strictEqual(mostMoney([cam, geoff]), 'all');
-    strictEqual(mostMoney([david, cam, geoff]), 'all');
+  it('Fixed tests', () => {
+    strictEqual(countSquares(5), 152);
+    strictEqual(countSquares(16), 1538);
+    strictEqual(countSquares(23), 3176);
   });
 });
 

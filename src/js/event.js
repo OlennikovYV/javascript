d24fa528785b62f8d0b2ce5f4da7763cad4fe2c7
Yury Olennikov <OlennikovYV@mail.ru>
2024-06-17 08:44:07 +0500

    Vampire Numbers

--- src/js/event.js
+++ src/js/event.js
@@ -1,24 +1,21 @@
-function vectorLength(vector) {
-  const [[x1, y1], [x2, y2]] = vector;
+function vampireTest(a, b) {
+  const convertToSortString = numbers =>
+    numbers
+      .map(number => String(Math.abs(number)))
+      .reduce((str, number) => str + number, '')
+      .split('')
+      .sort((a, b) => a - b)
+      .join('');
+  const srcNumber = convertToSortString([a, b]);
+  const destNumber = convertToSortString([a * b]);
+  const isSignDouble = Math.sign(a) == -1 && Math.sign(b) == -1;
 
-  return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+  return srcNumber == destNumber && !isSignDouble;
 }
 
-console.log(
-  vectorLength([
-    [0, 1],
-    [0, 0],
-  ])
-); // 1
-console.log(
-  vectorLength([
-    [0, 3],
-    [4, 0],
-  ])
-); // 5
-console.log(
-  vectorLength([
-    [1, -1],
-    [1, -1],
-  ])
-); // 0
+console.log(vampireTest(21, 6)); // true
+console.log(vampireTest(204, 615)); // true
+console.log(vampireTest(30, -51)); // true
+console.log(vampireTest(-246, -510)); // false
+console.log(vampireTest(2947050, 8469153)); // true
+console.log(vampireTest(2947051, 8469153)); // false
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-const nameTask = `[Geometry A-2]: Length of a vector`;
+const nameTask = `Vampire Numbers`;
 
 describe(nameTask, function () {
   const equal = chai.assert.equal;
@@ -31,30 +31,36 @@ describe(nameTask, function () {
     Например: [{b:1},{a:2}] === [{b:1},{a:2}] */
   const includeDeepOrderedMembers = chai.assert.includeDeepOrderedMembers;
 
-  it('test', () => {
-    closeTo(
-      vectorLength([
-        [0, 1],
-        [0, 0],
-      ]),
-      1,
-      1e-7
+  it('Fixed Tests', () => {
+    strictEqual(
+      vampireTest(21, 6),
+      true,
+      'Basic: 21 * 6 = 126 should return true'
     );
-    closeTo(
-      vectorLength([
-        [0, 3],
-        [4, 0],
-      ]),
-      5,
-      1e-7
+    strictEqual(
+      vampireTest(204, 615),
+      true,
+      'Basic: 204 * 615 = 125460 should return true'
     );
-    closeTo(
-      vectorLength([
-        [1, -1],
-        [1, -1],
-      ]),
-      0,
-      1e-7
+    strictEqual(
+      vampireTest(30, -51),
+      true,
+      'One Negative: 30 * -51 = -1530 should return true'
+    );
+    strictEqual(
+      vampireTest(-246, -510),
+      false,
+      "Double Negatives: -246 * -510 = 125460 should return false (The negative signs aren't present on the product)"
+    );
+    strictEqual(
+      vampireTest(2947050, 8469153),
+      true,
+      'Large: 2947050 * 8469153 = 24959017348650 should return true'
+    );
+    strictEqual(
+      vampireTest(2947051, 8469153),
+      false,
+      'Large: 2947051 * 8469153 = 24959025817803 should return false'
     );
   });
 });

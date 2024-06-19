ec0516457bbd73a7765ac335e86509d29b11ee47
Yury Olennikov <OlennikovYV@mail.ru>
2024-06-19 10:46:12 +0500

    Easy Balance Checking

--- src/js/event.js
+++ src/js/event.js
@@ -1,19 +1,102 @@
-const beeramid = function (bonus, price) {
-  let beers = Math.floor(bonus / price);
-  let row = 0;
+function balance(book) {
+  const roundTwo = num => Number(num).toFixed(2);
+  const splitN = '\r\n';
+  let totalExpense = 0;
+  let textTotalExpense = '';
+  let textAverageExpense = '';
+  let countPurchase = 0;
+  let listPurchasePrint = '';
+  let [total, ...listBook] = book.split('\n');
+  let caption = '';
+  const shoppingList = listBook
+    .filter(el => el != '')
+    .map(el =>
+      el
+        .split('')
+        .filter(el => /[ a-z0-9\.]/gi.test(el))
+        .join('')
+    )
+    .map(el => {
+      const numberCheck = el.match(/^[\d]+/);
+      const purchase = el.match(/[a-z]+/i);
+      const price = el.match(/[\d]+\.[\d]+$/);
 
-  while (beers >= ++row * row) beers -= row * row;
+      return {
+        numberCheck: numberCheck[0],
+        purchase: purchase[0],
+        price: price[0],
+      };
+    });
 
-  return --row;
-};
+  total = parseFloat(total);
+  caption = `Original Balance: ${roundTwo(total)}${splitN}`;
+  countPurchase = shoppingList.length;
+  listPurchasePrint =
+    shoppingList
+      .map(el => {
+        totalExpense += +el.price;
+        total -= el.price;
 
-console.log(beeramid(9, 2)); // 1
-console.log(beeramid(10, 2)); // 2
-console.log(beeramid(11, 2)); // 2
-console.log(beeramid(21, 1.5)); // 3
-console.log(beeramid(454, 5)); // 5
-console.log(beeramid(455, 5)); // 6
-console.log(beeramid(4, 4)); // 1
-console.log(beeramid(3, 4)); // 0
-console.log(beeramid(0, 4)); // 0
-console.log(beeramid(-1, 4)); // 0
+        return `${el.numberCheck} ${el.purchase} ${roundTwo(
+          el.price
+        )} Balance ${roundTwo(total)}`;
+      })
+      .join(splitN) + `${splitN}`;
+  textTotalExpense = `Total expense  ${roundTwo(totalExpense)}${splitN}`;
+  textAverageExpense = `Average expense  ${roundTwo(
+    totalExpense / countPurchase
+  )}`;
+
+  return caption + listPurchasePrint + textTotalExpense + textAverageExpense;
+}
+
+const b1 = `1000.00!=
+
+125 Market !=:125.45
+126 Hardware =34.95
+127 Video! 7.45
+128 Book :14.32
+129 Gasoline ::16.10
+`;
+
+const b1sol = `Original Balance: 1000.00\r
+125 Market 125.45 Balance 874.55\r
+126 Hardware 34.95 Balance 839.60\r
+127 Video 7.45 Balance 832.15\r
+128 Book 14.32 Balance 817.83\r
+129 Gasoline 16.10 Balance 801.73\r
+Total expense  198.27\r
+Average expense  39.65`;
+
+const b2 = `1233.00
+125 Hardware;! 24.8?;
+123 Flowers 93.5
+127 Meat 120.90
+120 Picture 34.00
+124 Gasoline 11.00
+123 Photos;! 71.4?;
+122 Picture 93.5
+132 Tyres;! 19.00,?;
+129 Stamps 13.6
+129 Fruits{} 17.6
+129 Market;! 128.00?;
+121 Gasoline;! 13.6?;`;
+
+var b2sol = `Original Balance: 1233.00\r
+125 Hardware 24.80 Balance 1208.20\r
+123 Flowers 93.50 Balance 1114.70\r
+127 Meat 120.90 Balance 993.80\r
+120 Picture 34.00 Balance 959.80\r
+124 Gasoline 11.00 Balance 948.80\r
+123 Photos 71.40 Balance 877.40\r
+122 Picture 93.50 Balance 783.90\r
+132 Tyres 19.00 Balance 764.90\r
+129 Stamps 13.60 Balance 751.30\r
+129 Fruits 17.60 Balance 733.70\r
+129 Market 128.00 Balance 605.70\r
+121 Gasoline 13.60 Balance 592.10\r
+Total expense  640.90\r
+Average expense  53.41`;
+
+console.log(balance(b1)); // b1sol
+console.log(balance(b2)); // b2sol
--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,6 @@
 mocha.setup('bdd');
 
-const nameTask = `Beeramid`;
+const nameTask = `Easy Balance Checking`;
 
 describe(nameTask, function () {
   const equal = chai.assert.equal;
@@ -31,17 +31,9 @@ describe(nameTask, function () {
     Например: [{b:1},{a:2}] === [{b:1},{a:2}] */
   const includeDeepOrderedMembers = chai.assert.includeDeepOrderedMembers;
 
-  it('Fixed Tests', () => {
-    equal(beeramid(9, 2), 1);
-    equal(beeramid(10, 2), 2);
-    equal(beeramid(11, 2), 2);
-    equal(beeramid(21, 1.5), 3);
-    equal(beeramid(454, 5), 5);
-    equal(beeramid(455, 5), 6);
-    equal(beeramid(4, 4), 1);
-    equal(beeramid(3, 4), 0);
-    equal(beeramid(0, 4), 0);
-    equal(beeramid(-1, 4), 0);
+  it('Basic tests', () => {
+    equal(balance(b1), b1sol);
+    equal(balance(b2), b2sol);
   });
 });
 

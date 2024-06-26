34d123e9a5ab00b6bb798bb94e56c1144b8b13d8
Yury Olennikov <OlennikovYV@mail.ru>
2024-06-26 08:38:31 +0500

    Thinkful - Object Drills: Quarks refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,10 @@
 class Quark {
   constructor(color, flavor) {
-    this.color = color;
-    this.flavor = flavor;
-    this.baryon_number = 1 / 3;
+    Object.assign(this, { color, flavor, baryon_number: 1 / 3 });
   }
 
   interact(quark) {
-    const thisColor = this.color;
-    this.color = quark.color;
-    quark.color = thisColor;
+    [this.color, quark.color] = [quark.color, this.color];
   }
 }
 

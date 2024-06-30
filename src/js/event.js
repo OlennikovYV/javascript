cd7cdb8ed083087d2fe99145074a34059973937e
OlennikovYV <olennikovyv@mail.ru>
2024-06-30 18:48:34 +0500

    reverseIt refactor

--- src/js/event.js
+++ src/js/event.js
@@ -1,12 +1,12 @@
 function reverseIt(data) {
-  if (typeof data == 'number') {
-    return +String(data).split('').reverse().join('');
+  switch (typeof data) {
+    case 'string':
+      return data.split('').reverse().join('');
+    case 'number':
+      return +String(data).split('').reverse().join('');
+    default:
+      return data;
   }
-  if (typeof data == 'string') {
-    return data.split('').reverse().join('');
-  }
-
-  return data;
 }
 
 console.log(reverseIt('Hello')); // 'olleH'
